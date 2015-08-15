ca2a3ba0e87f6f4a07888a5606e6da71bdb14b5f
Dan Brown <ssddanbrown@googlemail.com>
2015-08-16 00:50:27 +0100

    Made sidebar hierachy clearer

--- resources/views/pages/sidebar-tree-list.blade.php
+++ resources/views/pages/sidebar-tree-list.blade.php
@@ -2,8 +2,8 @@
 <ul class="sidebar-page-list menu">
     <li class="book-header"><a href="{{$book->getUrl()}}" class="book"><i class="zmdi zmdi-book"></i>{{$book->name}}</a></li>
     @foreach($book->children() as $bookChild)
-        <li>
-            <a href="{{$bookChild->getUrl()}}" class="@if(is_a($bookChild, 'Oxbow\Chapter')) chapter @else page @endif">
+        <li class="list-item-{{is_a($bookChild, 'Oxbow\Chapter') ? 'chapter' : 'page' }}">
+            <a href="{{$bookChild->getUrl()}}" class="{{is_a($bookChild, 'Oxbow\Chapter') ? 'chapter' : 'page' }}">
                 @if(is_a($bookChild, 'Oxbow\Chapter'))
                     <i class="zmdi zmdi-collection-bookmark chapter-toggle"></i>
                 @else
@@ -15,14 +15,9 @@
             @if(is_a($bookChild, 'Oxbow\Chapter') && count($bookChild->pages) > 0)
                 <ul class="menu">
                     @foreach($bookChild->pages as $page)
-                        <li>
-                            <a href="{{$page->getUrl()}}" class="@if(is_a($page, 'Oxbow\Chapter')) chapter @else page @endif">
-                                @if(is_a($page, 'Oxbow\Chapter'))
-                                    <i class="zmdi zmdi-collection-bookmark chapter-toggle"></i>
-                                @else
-                                    <i class="zmdi zmdi-file-text"></i>
-                                @endif
-                                {{ $page->name }}
+                        <li class="list-item-page">
+                            <a href="{{$page->getUrl()}}" class="page">
+                                <i class="zmdi zmdi-file-text"></i> {{ $page->name }}
                             </a>
                         </li>
                     @endforeach
