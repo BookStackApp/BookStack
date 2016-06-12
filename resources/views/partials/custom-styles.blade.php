d582e76fed59ecb1e5ea438ff529e90c6b4d361c
Dan Brown <ssddanbrown@googlemail.com>
2016-06-12 13:37:15 +0100

    Fixed theme color elements not showing on new instance
    Also cleaned notification session access

--- resources/views/partials/custom-styles.blade.php
+++ resources/views/partials/custom-styles.blade.php
@@ -1,22 +1,20 @@
-@if(Setting::get('app-color'))
-    <style>
-        header, #back-to-top, .primary-background {
-            background-color: {{ Setting::get('app-color') }} !important;
-        }
-        .faded-small, .primary-background-light {
-            background-color: {{ Setting::get('app-color-light') }};
-        }
-        .button-base, .button, input[type="button"], input[type="submit"] {
-            background-color: {{ Setting::get('app-color') }};
-        }
-        .button-base:hover, .button:hover, input[type="button"]:hover, input[type="submit"]:hover, .button:focus {
-            background-color: {{ Setting::get('app-color') }};
-        }
-        .nav-tabs a.selected, .nav-tabs .tab-item.selected {
-            border-bottom-color: {{ Setting::get('app-color') }};
-        }
-        p.primary:hover, p .primary:hover, span.primary:hover, .text-primary:hover, a, a:hover, a:focus, .text-button, .text-button:hover, .text-button:focus {
-            color: {{ Setting::get('app-color') }};
-        }
-    </style>
-@endif
+<style>
+    header, #back-to-top, .primary-background {
+        background-color: {{ Setting::get('app-color') }} !important;
+    }
+    .faded-small, .primary-background-light {
+        background-color: {{ Setting::get('app-color-light') }};
+    }
+    .button-base, .button, input[type="button"], input[type="submit"] {
+        background-color: {{ Setting::get('app-color') }};
+    }
+    .button-base:hover, .button:hover, input[type="button"]:hover, input[type="submit"]:hover, .button:focus {
+        background-color: {{ Setting::get('app-color') }};
+    }
+    .nav-tabs a.selected, .nav-tabs .tab-item.selected {
+        border-bottom-color: {{ Setting::get('app-color') }};
+    }
+    p.primary:hover, p .primary:hover, span.primary:hover, .text-primary:hover, a, a:hover, a:focus, .text-button, .text-button:hover, .text-button:focus {
+        color: {{ Setting::get('app-color') }};
+    }
+</style>
