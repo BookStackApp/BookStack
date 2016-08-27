4244c23eb4497946d1062c258191f25afaf52ff5
Dan Brown <ssddanbrown@googlemail.com>
2016-08-27 11:27:23 +0100

    Updated travis testing to work with new helper configuration (#175)
    * Updated travis to call phpunit globally rather then booting application first

--- app/helpers.php
+++ app/helpers.php
@@ -2,33 +2,31 @@
 
 use BookStack\Ownable;
 
-if (!function_exists('versioned_asset')) {
-    /**
-     * Get the path to a versioned file.
-     *
-     * @param  string $file
-     * @return string
-     *
-     * @throws \InvalidArgumentException
-     */
-    function versioned_asset($file)
-    {
-        static $manifest = null;
+/**
+ * Get the path to a versioned file.
+ *
+ * @param  string $file
+ * @return string
+ *
+ * @throws \InvalidArgumentException
+ */
+function versioned_asset($file)
+{
+    static $manifest = null;
 
-        if (is_null($manifest)) {
-            $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-        }
-
-        if (isset($manifest[$file])) {
-            return baseUrl($manifest[$file]);
-        }
-
-        if (file_exists(public_path($file))) {
-            return baseUrl($file);
-        }
-
-        throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
+    if (is_null($manifest)) {
+        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
     }
+
+    if (isset($manifest[$file])) {
+        return baseUrl($manifest[$file]);
+    }
+
+    if (file_exists(public_path($file))) {
+        return baseUrl($file);
+    }
+
+    throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
 }
 
 /**
