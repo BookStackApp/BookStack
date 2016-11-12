e691b401c8369e60b15ed599666bcdeec91312ad
Robert Landes <robert.landes+github@gmail.com>
2016-11-12 14:16:07 +0100

    Added initial translation into German (formal)
    This translation is based on the current english language files. It is a
    transaltion into German (formal) language.  A language selector on the
    Settings page would be a nice addition.

---
+++ resources/lang/de/activities.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /**
+     * Activity text strings.
+     * Is used for all the text within activity logs & notifications.
+     */
+
+    // Pages
+    'page_create'                 => 'Seite erstellt',
+    'page_create_notification'    => 'Seite erfolgreich erstellt',
+    'page_update'                 => 'Seite aktualisiert',
+    'page_update_notification'    => 'Seite erfolgreich aktualisiert',
+    'page_delete'                 => 'Seite gel&ouml;scht',
+    'page_delete_notification'    => 'Seite erfolgreich gel&ouml;scht',
+    'page_restore'                => 'Seite wiederhergstellt',
+    'page_restore_notification'   => 'Seite erfolgreich wiederhergstellt',
+    'page_move'                   => 'Seite verschoben',
+
+    // Chapters
+    'chapter_create'              => 'Kapitel erstellt',
+    'chapter_create_notification' => 'Kapitel erfolgreich erstellt',
+    'chapter_update'              => 'Kapitel aktualisiert',
+    'chapter_update_notification' => 'Kapitel erfolgreich aktualisiert',
+    'chapter_delete'              => 'Kapitel gel&ouml;scht',
+    'chapter_delete_notification' => 'Kapitel erfolgreich gel&ouml;scht',
+    'chapter_move'                => 'Kapitel verschoben',
+
+    // Books
+    'book_create'                 => 'Buch erstellt',
+    'book_create_notification'    => 'Buch erfolgreich erstellt',
+    'book_update'                 => 'Buch aktualisiert',
+    'book_update_notification'    => 'Buch erfolgreich aktualisiert',
+    'book_delete'                 => 'Buch gel&ouml;scht',
+    'book_delete_notification'    => 'Buch erfolgreich gel&ouml;scht',
+    'book_sort'                   => 'Buch sortiert',
+    'book_sort_notification'      => 'Buch erfolgreich neu sortiert',
+
+];
