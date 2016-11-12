38e1cca6da7b72de68ca9a2a2275b3cafef29196
Robert Landes <robert.landes+github@gmail.com>
2016-11-12 13:45:46 +0100

    Added initial transaltion into German (formal) language
    This transaltion is based on the current english language files. It is a
    transaltion into German (formal) language.  A language selector on the
    Settings page would be a nice addition.

---
+++ resources/lang/de/activities.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /**
+     * Activity text strings.
+     * Is used for all the text within activity logs & notifications.
+     */
+
+    // Seites
+    'Seite_create'                 => 'Seite erstellt',
+    'Seite_create_notification'    => 'Seite erfolgreich erstellt',
+    'Seite_update'                 => 'Seite aktualisiert',
+    'Seite_update_notification'    => 'Seite erfolgreich aktualisiert',
+    'Seite_delete'                 => 'Seite gel&ouml;scht',
+    'Seite_delete_notification'    => 'Seite erfolgreich gel&ouml;scht',
+    'Seite_restore'                => 'Seite wiederhergstellt',
+    'Seite_restore_notification'   => 'Seite erfolgreich wiederhergstellt',
+    'Seite_move'                   => 'Seite verschoben',
+
+    // Kapitels
+    'Kapitel_create'              => 'Kapitel erstellt',
+    'Kapitel_create_notification' => 'Kapitel erfolgreich erstellt',
+    'Kapitel_update'              => 'Kapitel aktualisiert',
+    'Kapitel_update_notification' => 'Kapitel erfolgreich aktualisiert',
+    'Kapitel_delete'              => 'Kapitel gel&ouml;scht',
+    'Kapitel_delete_notification' => 'Kapitel erfolgreich gel&ouml;scht',
+    'Kapitel_move'                => 'Kapitel verschoben',
+
+    // Buchs
+    'Buch_create'                 => 'Buch erstellt',
+    'Buch_create_notification'    => 'Buch erfolgreich erstellt',
+    'Buch_update'                 => 'Buch aktualisiert',
+    'Buch_update_notification'    => 'Buch erfolgreich aktualisiert',
+    'Buch_delete'                 => 'Buch gel&ouml;scht',
+    'Buch_delete_notification'    => 'Buch erfolgreich gel&ouml;scht',
+    'Buch_sort'                   => 'Buch sortiert',
+    'Buch_sort_notification'      => 'Buch erfolgreich neu sortiert',
+
+];
---
+++ resources/lang/de/auth.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed' => 'Dies sind keine g&uuml;ltigen Anmeldedaten.',
+    'throttle' => 'Zu viele Anmeldeversuche. Bitte versuchen sie es in :seconds Sekunden erneut.',
+
+    /**
+     * Email Confirmation Text
+     */
+    'email_confirm_subject' => 'Best&auml;tigen sie ihre E-Mail Adresse bei :appName',
+    'email_confirm_greeting' => 'Danke, dass sie :appName beigetreten sind!',
+    'email_confirm_text' => 'Bitte best&auml;tigen sie ihre E-Mail Adresse, indem sie auf den Button klicken:',
+    'email_confirm_action' => 'E-Mail Adresse best&auml;tigen',
+    'email_confirm_send_error' => 'Best&auml;tigungs-E-Mail ben&ouml;tigt, aber das System konnte die E-Mail nicht versenden. Kontaktieren sie den Administrator, um sicherzustellen, dass das Sytsem korrekt eingerichtet ist.',
+    'email_confirm_success' => 'Ihre E-Mail Adresse wurde best&auml;tigt!',
+    'email_confirm_resent' => 'Best&auml;tigungs-E-Mail wurde erneut versendet, bitte &uuml;berpr&uuml;fen sie ihren Posteingang.',
+];
