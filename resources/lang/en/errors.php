3d562e50bd6f117932f4d1228b96a2bb879b88bd
Dan Brown <ssddanbrown@googlemail.com>
2016-12-03 13:31:54 +0000

    Moved text for errors and form views.
    Updated 404 page with additional links

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -6,7 +6,15 @@ return [
      * Error text strings.
      */
 
-    // Pages
+    // Permissions
     'permission' => 'You do not have permission to access the requested page.',
-    'permissionJson' => 'You do not have permission to perform the requested action.'
+    'permissionJson' => 'You do not have permission to perform the requested action.',
+
+    // Error pages
+    'page_not_found' => 'Page Not Found',
+    'sorry_page_not_found' => 'Sorry, The page you were looking for could not be found.',
+    'return_home' => 'Return to home',
+    'error_occurred' => 'An Error Occurred',
+    'app_down' => ':appName is down right now',
+    'back_soon' => 'It will be back up soon.',
 ];
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,9 +4,28 @@
 
 
 <div class="container">
-    <h1 class="text-muted">{{ $message or 'Page Not Found' }}</h1>
-    <p>Sorry, The page you were looking for could not be found.</p>
-    <a href="{{ baseUrl('/') }}" class="button">Return To Home</a>
+
+
+    <h1>{{ $message or trans('errors.page_not_found') }}</h1>
+    <p>{{ trans('errors.sorry_page_not_found') }}</p>
+    <p><a href="{{ baseUrl('/') }}" class="button">{{ trans('errors.return_home') }}</a></p>
+
+    <hr>
+
+    <div class="row">
+        <div class="col-md-4">
+            <h3 class="text-muted">{{ trans('entities.pages_popular') }}</h3>
+            @include('partials.entity-list', ['entities' => Views::getPopular(10, 0, [\BookStack\Page::class]), 'style' => 'compact'])
+        </div>
+        <div class="col-md-4">
+            <h3 class="text-muted">{{ trans('entities.books_popular') }}</h3>
+            @include('partials.entity-list', ['entities' => Views::getPopular(10, 0, [\BookStack\Book::class]), 'style' => 'compact'])
+        </div>
+        <div class="col-md-4">
+            <h3 class="text-muted">{{ trans('entities.chapters_popular') }}</h3>
+            @include('partials.entity-list', ['entities' => Views::getPopular(10, 0, [\BookStack\Chapter::class]), 'style' => 'compact'])
+        </div>
+    </div>
 </div>
 
 @stop
