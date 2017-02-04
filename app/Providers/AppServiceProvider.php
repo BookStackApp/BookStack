46c86fa7687db420c8d18e018fb90e203d74d62f
Dan Brown <ssddanbrown@googlemail.com>
2017-02-04 11:01:49 +0000

    Added facebook, slack & twitter sign in options.
    Also added icon svg blade helper.
    Closes #125. Starts #213.
    Requires documentation.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,10 @@ class AppServiceProvider extends ServiceProvider
             $imageMimes = ['image/png', 'image/bmp', 'image/gif', 'image/jpeg', 'image/jpg', 'image/tiff', 'image/webp'];
             return in_array($value->getMimeType(), $imageMimes);
         });
+
+        \Blade::directive('icon', function($expression) {
+            return "<?php echo icon($expression); ?>";
+        });
     }
 
     /**
