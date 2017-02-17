7c436a9bca9e29325f76d0cb817a5b5ccddddf77
Arie Timmerman <arietimmerman@Aries-MacBook-Pro.local>
2017-02-17 22:36:23 +0100

    Nescio support + NewUser command

--- routes/web.php
+++ routes/web.php
@@ -177,3 +177,7 @@ Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail
 // Password reset routes...
 Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
 Route::post('/password/reset', 'Auth\ResetPasswordController@reset');
+
+// Route::get('/settings/users/{id}', function(){
+// 	die("asdgasd");
+// });
