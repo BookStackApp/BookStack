f7645824d99d34e12814057e478e026c4c88ba54
Diego Jose Sosa Diaz <diego.venezuela@gmail.com>
2017-03-03 00:21:33 +0100

    First spanish translation effort

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'El Password debe ser como mínimo de seis caracteres y coincidir con la confirmación.',
+    'user' => "No podemos encontrar un usuario con esta dirección de e-mail.",
+    'token' => 'El token de reset del password es inválido.',
+    'sent' => 'Hemos enviado a su cuenta de e-mail un link para restaurar su password!',
+    'reset' => 'Su password ha sido restaurado!',
+
+];
