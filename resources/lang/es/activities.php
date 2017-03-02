b7daa2baa52e2fd9ae6d2beeb937be1269796f21
Diego Jose Sosa Diaz <diego.venezuela@gmail.com>
2017-03-03 00:21:33 +0100

    First spanish translation effort

---
+++ resources/lang/es/activities.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /**
+     * Activity text strings.
+     * Is used for all the text within activity logs & notifications.
+     */
+
+    // Pages
+    'page_create'                 => 'página creada',
+    'page_create_notification'    => 'Página creada exitosamente',
+    'page_update'                 => 'página actualizada',
+    'page_update_notification'    => 'Página actualizada exitosamente',
+    'page_delete'                 => 'página borrada',
+    'page_delete_notification'    => 'Página borrada exitosamente,
+    'page_restore'                => 'página restaurada',
+    'page_restore_notification'   => 'Página restaurada exitosamente',
+    'page_move'                   => 'página movida',
+
+    // Chapters
+    'chapter_create'              => 'capítulo creado',
+    'chapter_create_notification' => 'Capítulo creado exitosamente',
+    'chapter_update'              => 'capítulo actualizado',
+    'chapter_update_notification' => 'Capítulo actualizado exitosamente',
+    'chapter_delete'              => 'capítulo borrado',
+    'chapter_delete_notification' => 'Capítulo borrado exitosamente',
+    'chapter_move'                => 'capítulo movido',
+
+    // Books
+    'book_create'                 => 'libro creado',
+    'book_create_notification'    => 'Libro creado exitosamente',
+    'book_update'                 => 'libro actualizado',
+    'book_update_notification'    => 'Libro actualizado exitosamente',
+    'book_delete'                 => 'libro borrado',
+    'book_delete_notification'    => 'Libro borrado exitosamente',
+    'book_sort'                   => 'libro ordenado',
+    'book_sort_notification'      => 'Libro re-ordenado exitosamente',
+
+];
