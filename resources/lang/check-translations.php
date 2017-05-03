101495a88d8c39d04f673db54eb76fb262f08526
Diego Jose Sosa Diaz <diego.venezuela@gmail.com>
2017-05-03 12:25:48 +0200

    Detecting left Over translations

--- resources/lang/check-translations.php
+++ resources/lang/check-translations.php
@@ -5,6 +5,7 @@ if( isset( $argv[ 1 ] ) && is_dir( $argv[ 1 ] ) ){
     $dirHandler = opendir( __dir__ . DIRECTORY_SEPARATOR .$argv[1] );
     $missingKeysTotal = array();
     $missingTranslationsTotal = array();
+    $leftOverTranslationsTotal = array();
 
     while ( $filename = readdir( $dirHandler ) ){
         if( strpos( $filename, '.php') ){
@@ -17,20 +18,25 @@ if( isset( $argv[ 1 ] ) && is_dir( $argv[ 1 ] ) ){
 
             $missingKeysTotal = array_merge( $missingKeysTotal, getMissingKeys( $originalArrayFlatten, $translationArrayFlatten ));
             $missingTranslationsTotal = array_merge( $missingTranslationsTotal, getMissingTranslations( $originalArrayFlatten, $translationArrayFlatten ) );
+            $leftOverTranslationsTotal = array_merge( $leftOverTranslationsTotal, getLeftOverTranslations( $translationArrayFlatten, $originalArrayFlatten ) );
         }  
     }
     
     if( ( $countMissingKeysTotal = count( $missingKeysTotal) ) > 0 )
     {
-        echo PHP_EOL."$countMissingKeysTotal translations pending to define".PHP_EOL.PHP_EOL;
+        echo PHP_EOL."$countMissingKeysTotal translations pending to define".PHP_EOL;
         printArrayKeys( $missingKeysTotal );
     }
     if( ( $countMissingTranslationsTotal = count( $missingTranslationsTotal) ) > 0 )
     {
-        echo PHP_EOL."$countMissingTranslationsTotal items defined but pending for a translation".PHP_EOL.PHP_EOL;
+        echo PHP_EOL."$countMissingTranslationsTotal items defined but pending for a translation".PHP_EOL;
         printArrayKeys( $missingTranslationsTotal );
     }
- 
+    if( ( $countLeftOverTranslationsTotal = count( $leftOverTranslationsTotal) ) > 0 )
+    {
+        echo PHP_EOL."$countLeftOverTranslationsTotal items not longer needed".PHP_EOL;
+        printArrayKeys( $leftOverTranslationsTotal );
+    } 
 
 }else{
     echo "Not translation available $argv[1]";
@@ -66,6 +72,11 @@ function getMissingKeys( array $originalArray, array $translations  )
 function getMissingTranslations( array $originalArray, array $translations  )
 {
     return array_intersect_assoc( $originalArray, $translations );
+}
+
+function getLeftOverTranslations( array $translations, $originalArray )
+{
+    return array_intersect_assoc( $translations, $originalArray );
 }
 
 function printArrayKeys( array $array )
