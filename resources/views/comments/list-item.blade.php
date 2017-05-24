7cf82752a2e9e91b26581aa82757dcf7b63fc471
Abijeet <abijeetpatro@gmail.com>
2017-05-24 07:02:11 +0530

    #47 Hides the reply button based if comments are 2 levels deep.

--- resources/views/comments/list-item.blade.php
+++ resources/views/comments/list-item.blade.php
@@ -11,17 +11,17 @@
         </div>
         <div class="comment-actions">
             <ul>
-                <li><a href="#" comment-reply-link no-comment-reply-dupe="true" comment="comment" is-reply="true">Reply</a></li>
+                <li ng-if="level < 3"><a href="#" comment-reply-link no-comment-reply-dupe="true" comment="comment" is-reply="true">Reply</a></li>
                 <li><a href="#" comment-reply-link no-comment-reply-dupe="true" comment="comment">Edit</a></li>
-                <li><a href="#">@{{::comment.created_at}}</a></li>            
-            </ul>                
+                <li><a href="#">@{{::comment.created_at}}</a></li>
+            </ul>
         </div>
         <a href="#" ng-click="vm.loadSubComments($event, comment, $index)" class="load-more-comments" ng-if="comment.cnt_sub_comments > 0 && !comment.is_loaded">
             Load @{{::comment.cnt_sub_comments}} more comment(s)
-        </a>        
-        <div class="comment-box" ng-repeat="comment in comments = comment.comments track by comment.id">        
+        </a>
+        <div class="comment-box" ng-repeat="comment in comments = comment.comments track by comment.id" ng-init="level = level + 1">
             <div ng-include src="'comment-list-item.html'">
             </div>
-        </div>        
-    </div>    
+        </div>
+    </div>
 </div>
