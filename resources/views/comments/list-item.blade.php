9a97995f181c13c225f62b1b582f6bf802dd6578
Abijeet <abijeetpatro@gmail.com>
2017-05-25 08:04:19 +0530

    #47 Displays the time for comments and border bottom for sub comments.

--- resources/views/comments/list-item.blade.php
+++ resources/views/comments/list-item.blade.php
@@ -1,4 +1,4 @@
-<div class='page-comment'>
+<div class='page-comment' id="comment-@{{::pageId}}-@{{::comment.id}}">
     <div class="user-image">
         <img ng-src="@{{::defaultAvatar}}" alt="user avatar">
     </div>
@@ -9,16 +9,19 @@
         <div ng-bind-html="comment.html" class="comment-body">
 
         </div>
-        <div class="comment-actions">
+        <div class="comment-actions" ng-class="{'has-border': comment.cnt_sub_comments === 0 || comment.is_loaded}">
             <ul>
                 <li ng-if="level < 3"><a href="#" comment-reply-link no-comment-reply-dupe="true" comment="comment" is-reply="true">Reply</a></li>
                 <li><a href="#" comment-reply-link no-comment-reply-dupe="true" comment="comment">Edit</a></li>
-                <li><a href="#">@{{::comment.created_at}}</a></li>
+                <li>Created <a title="@{{::comment.created.day_time_str}}" href="#comment-@{{::comment.id}}-@{{::pageId}}">@{{::comment.created.diff}}</a></li>
+                <li ng-if="comment.updated"><span title="@{{::comment.updated.day_time_str}}">Updated @{{::comment.updated.diff}}</span></li>
             </ul>
         </div>
-        <a href="#" ng-click="vm.loadSubComments($event, comment, $index)" class="load-more-comments" ng-if="comment.cnt_sub_comments > 0 && !comment.is_loaded">
-            Load @{{::comment.cnt_sub_comments}} more comment(s)
-        </a>
+        <div class="load-more-comments" ng-if="comment.cnt_sub_comments > 0 && !comment.is_loaded">
+            <a href="#" ng-click="vm.loadSubComments($event, comment, $index)">
+                Load @{{::comment.cnt_sub_comments}} more comment(s)
+            </a>
+        </div>
         <div class="comment-box" ng-repeat="comment in comments = comment.comments track by comment.id" ng-init="level = level + 1">
             <div ng-include src="'comment-list-item.html'">
             </div>
