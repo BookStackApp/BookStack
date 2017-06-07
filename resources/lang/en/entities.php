80208e1f54c4e0888e909692601f31b258bc54ad
Abijeet <abijeetpatro@gmail.com>
2017-06-08 01:30:43 +0530

    #47 - Fetching values from language files.

--- resources/lang/en/entities.php
+++ resources/lang/en/entities.php
@@ -253,5 +253,6 @@ return [
     'comment_created' => 'Comment added',
     'comment_updated' => 'Comment updated',
     'comment_deleted' => 'Comment deleted',
-    'comment_updated_text' => 'Updated :updateDiff by'
+    'comment_updated_text' => 'Updated :updateDiff by',
+    'comment_delete_confirm' => 'This will remove the contents of the comment. Are you sure you want to delete this comment?'
 ];
