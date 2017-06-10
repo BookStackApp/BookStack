552943c03349100c8655059554b9fadfc3291e75
Abijeet <abijeetpatro@gmail.com>
2017-06-10 19:46:00 +0530

    #47 - Undos changes in config files.

--- config/database.php
+++ config/database.php
@@ -71,9 +71,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'bookstack'),
-            'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'Change123'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
