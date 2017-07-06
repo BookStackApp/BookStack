1c86ba6df35a9c1027374631c5db53da71d6ad4e
Nilesh Deepak <deepak.n@osmosys.in>
2017-07-06 10:05:11 +0530

    Fixed pagination on change of display type.

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -22,16 +22,19 @@
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-9">
                 <h1>{{ trans('entities.books') }}</h1>
-                {!! $books->render() !!}
                 @if(count($books) > 0)
                     @if($books_display=='grid')
                         @foreach($books as $book)
                             @include('books/grid-item', ['book' => $book])
                         @endforeach
+                        <div class="col-xs-12">
+                            {!! $books->render() !!}
+                        </div>
                     @else
-                    @foreach($books as $book)
+                        @foreach($books as $book)
                             @include('books/list-item', ['book' => $book])
                         @endforeach
+                        {!! $books->render() !!}
                     @endif
                 @else
                     <p class="text-muted">{{ trans('entities.books_empty') }}</p>
@@ -58,7 +61,6 @@
                     @endif
                 </div>
             </div>
-            {!! $books->render() !!}
         </div>
     </div>
 
