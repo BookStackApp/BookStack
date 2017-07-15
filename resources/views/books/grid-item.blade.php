6d20f700648a80c2a28a6b8849cc3715d91f6582
Nilesh Deepak <deepak.n@osmosys.in>
2017-07-15 16:15:45 +0530

    Resolves book heading issues in grid view.

--- resources/views/books/grid-item.blade.php
+++ resources/views/books/grid-item.blade.php
@@ -1,7 +1,7 @@
 <div class="col-xs-6 col-sm-4 col-md-4 col-lg-3"  data-entity-type="book" data-entity-id="{{$book->id}}">
     <div class="gallery-item">
     <h4>
-        <a class="text-book entity-list-item-link" href="{{$book->getUrl()}}"><i class="zmdi zmdi-book"></i><span class="entity-list-item-name">{{$book->name}}</span>
+        <a class="text-book entity-list-item-link" href="{{$book->getUrl()}}" title="{{$book->name}}"><i class="zmdi zmdi-book"></i><span class="entity-list-item-name">{{$book->getHeadingExcerpt()}}</span>
         <br>
         </a>
     </h4>
