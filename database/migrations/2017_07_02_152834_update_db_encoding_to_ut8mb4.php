42acc1050c1da1097484afbb0dec0781bc76b651
Dan Brown <ssddanbrown@googlemail.com>
2017-07-22 16:19:17 +0100

    Updated utfmb4 upgrade command

--- database/migrations/2017_07_02_152834_update_db_encoding_to_ut8mb4.php
+++ database/migrations/2017_07_02_152834_update_db_encoding_to_ut8mb4.php
@@ -12,7 +12,7 @@ class UpdateDbEncodingToUt8mb4 extends Migration
     public function up()
     {
         // Migration removed due to issues during live migration.
-        // Instead you can run the command `artisan bookstack:db-utf8mb4-syntax`
+        // Instead you can run the command `artisan bookstack:db-utf8mb4`
         // which will generate out the SQL request to upgrade your DB to utf8mb4.
     }
 
