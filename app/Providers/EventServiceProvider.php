9ba29770e116808d82c4f36923973c3a14db41a3
Dan Brown <ssddanbrown@googlemail.com>
2017-10-01 13:19:17 +0100

    Added azureAD social auth option
    Closes #509

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,6 +16,7 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         SocialiteWasCalled::class => [
             'SocialiteProviders\Slack\SlackExtendSocialite@handle',
+            'SocialiteProviders\Azure\AzureExtendSocialite@handle',
         ],
     ];
 
