6bb7b5465fd5d62693723f75151b4dcc1914bad7
Abijeet <abijeetpatro@gmail.com>
2017-11-16 00:05:24 +0530

    Added code in the settings to disable comments. Based on that hiding the comments section on the page display.

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -161,13 +161,19 @@ class PageController extends Controller
         $page->html = $this->entityRepo->renderPage($page);
         $sidebarTree = $this->entityRepo->getBookChildren($page->book);
         $pageNav = $this->entityRepo->getPageNav($page->html);
-        $page->load(['comments.createdBy']);
+
+        // check if the comment's are enabled
+        $areCommentsEnabled = !setting('app-disable-comments');
+        if ($areCommentsEnabled) {
+          $page->load(['comments.createdBy']);
+        }
 
         Views::add($page);
         $this->setPageTitle($page->getShortName());
         return view('pages/show', [
             'page' => $page,'book' => $page->book,
             'current' => $page, 'sidebarTree' => $sidebarTree,
+            'commentsEnabled' => $areCommentsEnabled,
             'pageNav' => $pageNav]);
     }
 
