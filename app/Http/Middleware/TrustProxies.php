56d5af1336d863412f715d462696b006457ee324
Dan Brown <ssddanbrown@googlemail.com>
2017-12-07 19:46:25 +0000

    Made it possible to configure proxies via env
    In reference to #146

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,6 +2,7 @@
 
 namespace BookStack\Http\Middleware;
 
+use Closure;
 use Illuminate\Http\Request;
 use Fideloper\Proxy\TrustProxies as Middleware;
 
@@ -26,4 +27,21 @@ class TrustProxies extends Middleware
         Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
         Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
     ];
+
+    /**
+     * Handle the request, Set the correct user-configured proxy information.
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $setProxies = config('app.proxies');
+        if ($setProxies !== '**' && $setProxies !== '*' && $setProxies !== '') {
+            $setProxies = explode(',', $setProxies);
+        }
+        $this->proxies = $setProxies;
+
+        return parent::handle($request, $next);
+    }
 }
