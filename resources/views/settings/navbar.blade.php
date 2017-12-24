12cd5f586f977478d787adccb219cf28bd54772a
Yoginth <yoginth@aol.com>
2017-12-24 10:42:55 +0530

    Update navbar.blade.php

--- resources/views/settings/navbar.blade.php
+++ resources/views/settings/navbar.blade.php
@@ -1,4 +1,4 @@
-
+@if($currentUser->can('user-manage') || $currentUser->can('settings-manage') || $currentUser->can('user-roles-manage'))
 <div class="col-md-12 setting-nav nav-tabs">
     @if($currentUser->can('settings-manage'))
         <a href="{{ baseUrl('/settings') }}" @if($selected == 'settings') class="selected text-button" @endif><i class="zmdi zmdi-settings"></i>{{ trans('settings.settings') }}</a>
@@ -10,3 +10,4 @@
         <a href="{{ baseUrl('/settings/roles') }}" @if($selected == 'roles') class="selected text-button" @endif><i class="zmdi zmdi-lock-open"></i>{{ trans('settings.roles') }}</a>
     @endif
 </div>
+@endif
