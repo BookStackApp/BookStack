1d79f7c949a86e02871414c42ec1fdaf723fea2a
BlackSheep <blsheepmail@gmail.com>
2018-01-29 09:28:56 +0100

    Add twitch socialite auth provider

--- app/Services/SocialAuthService.php
+++ app/Services/SocialAuthService.php
@@ -16,7 +16,7 @@ class SocialAuthService
     protected $socialite;
     protected $socialAccount;
 
-    protected $validSocialDrivers = ['google', 'github', 'facebook', 'slack', 'twitter', 'azure', 'okta'];
+    protected $validSocialDrivers = ['google', 'github', 'facebook', 'slack', 'twitter', 'azure', 'okta', 'twitch'];
 
     /**
      * SocialAuthService constructor.
