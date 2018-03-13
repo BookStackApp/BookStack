f42d355fd74fc32ea62fd85e17de304e146ca9c3
Abijeet <abijeetpatro@gmail.com>
2018-03-13 09:10:23 +0530

    Fixes issue with the validation message not being translated.
    Signed-off-by: Abijeet <abijeetpatro@gmail.com>

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -37,7 +37,7 @@ return [
     'email'                => 'The :attribute must be a valid email address.',
     'filled'               => 'The :attribute field is required.',
     'exists'               => 'The selected :attribute is invalid.',
-    'image'                => 'The :attribute must be an image.',
+    'image'              => 'The :attribute must be an image.',
     'in'                   => 'The selected :attribute is invalid.',
     'integer'              => 'The :attribute must be an integer.',
     'ip'                   => 'The :attribute must be a valid IP address.',
