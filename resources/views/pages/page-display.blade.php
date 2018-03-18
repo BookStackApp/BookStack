8b27ce32962fc70a2b46e8424c88595a9c5f1bae
Dan Brown <ssddanbrown@googlemail.com>
2018-03-18 12:23:48 +0000

    Fixed large content previews and improved mobile styles
    Listing content previews no longer pre-wrap and instead simply break
    correctly.
    Updated titles to ensure they break on mobile devices.
    Reduced spacing and font sizes on mobile to better adjust content to
    screen size.
    
    Fixes #739

--- resources/views/pages/page-display.blade.php
+++ resources/views/pages/page-display.blade.php
@@ -1,6 +1,6 @@
 <div ng-non-bindable>
 
-    <h1 id="bkmrk-page-title">{{$page->name}}</h1>
+    <h1 class="break-text" v-pre id="bkmrk-page-title">{{$page->name}}</h1>
 
     <div style="clear:left;"></div>
 
