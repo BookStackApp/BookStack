3df7d828eb1134009d2d4bd65edc4ddffa750844
Dan Brown <ssddanbrown@googlemail.com>
2018-03-24 15:25:13 +0000

    Fixed failing tests
    Fixed syntax error in french translations.
    Removed 'required' on image validation which was breaking tests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // Custom validation methods
-        Validator::extend('image', function ($attribute, $value, $parameters, $validator) {
+        Validator::extend('is_image', function ($attribute, $value, $parameters, $validator) {
             $imageMimes = ['image/png', 'image/bmp', 'image/gif', 'image/jpeg', 'image/jpg', 'image/tiff', 'image/webp'];
             return in_array($value->getMimeType(), $imageMimes);
         });
