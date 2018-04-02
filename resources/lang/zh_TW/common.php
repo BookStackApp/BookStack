106432ee4ec139736406eb90a5f62460b92d9970
Jason Cheng <sanyu3u@gmail.com>
2018-04-02 15:03:07 +0800

    Added Language zh_TW
    Added Language zh_TW

---
+++ resources/lang/zh_TW/common.php
@@ -0,0 +1,64 @@
+<?php
+return [
+
+    /**
+     * Buttons
+     */
+    'cancel' => '取消',
+    'confirm' => '確認',
+    'back' => '返回',
+    'save' => '儲存',
+    'continue' => '繼續',
+    'select' => '選擇',
+    'more' => '更多',
+
+    /**
+     * Form Labels
+     */
+    'name' => '名稱',
+    'description' => '摘要',
+    'role' => '角色',
+    'cover_image' => '封面圖片',
+    'cover_image_description' => '所使用圖片大小必須是440x250px。',
+    
+    /**
+     * Actions
+     */
+    'actions' => '動作',
+    'view' => '檢視',
+    'create' => '建立',
+    'update' => '更新',
+    'edit' => '編輯',
+    'sort' => '排序',
+    'move' => '移動',
+    'reply' => '回複',
+    'delete' => '刪除',
+    'search' => '搜尋',
+    'search_clear' => '清除搜尋',
+    'reset' => '重置',
+    'remove' => '刪除',
+    'add' => '新增',
+
+    /**
+     * Misc
+     */
+    'deleted_user' => '刪除使用者',
+    'no_activity' => '無活動',
+    'no_items' => '無項目',
+    'back_to_top' => '回到頂端',
+    'toggle_details' => '顯示/隱藏詳細資訊',
+    'toggle_thumbnails' => '顯示/隱藏縮圖',
+    'details' => '詳細資訊',
+
+    /**
+     * Header
+     */
+    'view_profile' => '檢視資料',
+    'edit_profile' => '編輯資料',
+
+    /**
+     * Email Content
+     */
+    'email_action_help' => '如果您無法點選“:actionText”按鈕，請將下面的網址複製到您的瀏覽器中打開：',
+    'email_rights' => 'All rights reserved',
+];
---
+++ resources/lang/zh_TW/errors.php
@@ -0,0 +1,79 @@
+<?php
+
+return [
+
+    /**
+     * Error text strings.
+     */
+
+    // Permissions
+    'permission' => '您沒有權限進入所請求的頁面。',
+    'permissionJson' => '您沒有權限執行所請求的操作。',
+
+    // Auth
+    'error_user_exists_different_creds' => 'Email為 :email 的使用者已經存在，但具有不同的憑據。',
+    'email_already_confirmed' => 'Email已被確認，請嘗試登錄。',
+    'email_confirmation_invalid' => '此確認 Session 無效或已被使用，請重新註冊。',
+    'email_confirmation_expired' => '確認 Session 已過期，已發送新的確認電子郵件。',
+    'ldap_fail_anonymous' => '使用匿名綁定的LDAP進入失敗。',
+    'ldap_fail_authed' => '帶有標識名稱和密碼的LDAP進入失敗。',
+    'ldap_extension_not_installed' => '未安裝LDAP PHP外掛程式',
+    'ldap_cannot_connect' => '無法連接到ldap伺服器，第一次連接失敗',
+    'social_no_action_defined' => '沒有定義行為',
+    'social_login_bad_response' => "在 :socialAccount 登錄時遇到錯誤：\n:error",
+    'social_account_in_use' => ':socialAccount 帳號已被使用，請嘗試透過 :socialAccount 選項登錄。',
+    'social_account_email_in_use' => 'Email :email 已經被使用。如果您已有帳號，則可以在個人資料設定中綁定您的 :socialAccount。',
+    'social_account_existing' => ':socialAccount已經被綁定到您的帳號。',
+    'social_account_already_used_existing' => ':socialAccount帳號已經被其他使用者使用。',
+    'social_account_not_used' => ':socialAccount帳號沒有綁定到任何使用者，請在您的個人資料設定中綁定。',
+    'social_account_register_instructions' => '如果您還沒有帳號，您可以使用 :socialAccount 選項註冊帳號。',
+    'social_driver_not_found' => '未找到社交驅動程式',
+    'social_driver_not_configured' => '您的:socialAccount社交設定不正確。',
+
+    // System
+    'path_not_writable' => '無法上傳到檔案路徑“:filePath”，請確保它可寫入伺服器。',
+    'cannot_get_image_from_url' => '無法從 :url 中獲取圖片',
+    'cannot_create_thumbs' => '伺服器無法建立縮圖，請檢查您是否安裝了GD PHP外掛。',
+    'server_upload_limit' => '上傳圖片時發生錯誤。',
+    'image_upload_error' => '上傳圖片時發生錯誤',
+
+    // Attachments
+    'attachment_page_mismatch' => '附件更新期間的頁面不符合',
+
+    // Pages
+    'page_draft_autosave_fail' => '無法儲存草稿，確保您在儲存頁面之前已經連接到互聯網',
+    'page_custom_home_deletion' => '無法刪除一個被設定為首頁的頁面',
+
+    // Entities
+    'entity_not_found' => '未找到實體',
+    'book_not_found' => '未找到圖書',
+    'page_not_found' => '未找到頁面',
+    'chapter_not_found' => '未找到章節',
+    'selected_book_not_found' => '選中的書未找到',
+    'selected_book_chapter_not_found' => '未找到所選的圖書或章節',
+    'guests_cannot_save_drafts' => '訪客不能儲存草稿',
+
+    // Users
+    'users_cannot_delete_only_admin' => '您不能刪除唯一的管理員帳號',
+    'users_cannot_delete_guest' => '您不能刪除訪客使用者',
+
+    // Roles
+    'role_cannot_be_edited' => '無法編輯這個角色',
+    'role_system_cannot_be_deleted' => '無法刪除系統角色',
+    'role_registration_default_cannot_delete' => '無法刪除設定為預設註冊的角色',
+
+    // Comments
+    'comment_list' => '讀取評論時發生錯誤。',
+    'cannot_add_comment_to_draft' => '您不能為草稿加入評論。',
+    'comment_add' => '加入/更新評論時發生錯誤。',
+    'comment_delete' => '刪除評論時發生錯誤。',
+    'empty_comment' => '不能加入空的評論。',
+
+    // Error pages
+    '404_page_not_found' => '無法找到頁面',
+    'sorry_page_not_found' => '對不起，無法找到您想進入的頁面。',
+    'return_home' => '返回首頁',
+    'error_occurred' => '發生錯誤',
+    'app_down' => ':appName現在正在關閉',
+    'back_soon' => '請耐心等待網站的恢複。',
+];
