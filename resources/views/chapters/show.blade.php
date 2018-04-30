58a0a59d7ee545c1b8052a9767184b23b2d44e46
Dan Brown <ssddanbrown@googlemail.com>
2018-04-30 13:53:04 +0100

    Cleaned details sidebar box and merged with permissions

--- resources/views/chapters/show.blade.php
+++ resources/views/chapters/show.blade.php
@@ -57,34 +57,6 @@
         </div>
     </div>
 
-    @if($book->restricted || $chapter->restricted)
-        <div class="card">
-            <h3>@icon('permission') {{ trans('entities.permissions') }}</h3>
-            <div class="body">
-                @if($book->restricted)
-                    <p class="text-muted">
-                        @if(userCan('restrictions-manage', $book))
-                            <a href="{{ $book->getUrl('/permissions') }}">@icon('lock'){{ trans('entities.books_permissions_active') }}</a>
-                        @else
-                            @icon('lock'){{ trans('entities.books_permissions_active') }}
-                        @endif
-                    </p>
-                @endif
-
-                @if($chapter->restricted)
-                    <p class="text-muted">
-                        @if(userCan('restrictions-manage', $chapter))
-                            <a href="{{ $chapter->getUrl('/permissions') }}">@icon('lock'){{ trans('entities.chapters_permissions_active') }}</a>
-                        @else
-                            @icon('lock'){{ trans('entities.chapters_permissions_active') }}
-                        @endif
-                    </p>
-                @endif
-            </div>
-        </div>
-    @endif
-
-
     @if($chapter->tags->count() > 0)
         <div class="card tag-display">
             <h3>@icon('tag') {{ trans('entities.chapter_tags') }}</h3>
@@ -94,10 +66,30 @@
         </div>
     @endif
 
-    <div class="card">
+    <div class="card entity-details">
         <h3>@icon('info') {{ trans('common.details') }}</h3>
-        <div class="body">
+        <div class="body blended-links text-small text-muted">
             @include('partials.entity-meta', ['entity' => $chapter])
+
+            @if($book->restricted)
+                <div class="active-restriction">
+                    @if(userCan('restrictions-manage', $book))
+                        <a href="{{ $book->getUrl('/permissions') }}">@icon('lock'){{ trans('entities.books_permissions_active') }}</a>
+                    @else
+                        @icon('lock'){{ trans('entities.books_permissions_active') }}
+                    @endif
+                </div>
+            @endif
+
+            @if($chapter->restricted)
+                <div class="active-restriction">
+                    @if(userCan('restrictions-manage', $chapter))
+                        <a href="{{ $chapter->getUrl('/permissions') }}">@icon('lock'){{ trans('entities.chapters_permissions_active') }}</a>
+                    @else
+                        @icon('lock'){{ trans('entities.chapters_permissions_active') }}
+                    @endif
+                </div>
+            @endif
         </div>
     </div>
 
--- resources/views/partials/entity-meta.blade.php
+++ resources/views/partials/entity-meta.blade.php
@@ -1,25 +1,34 @@
-<p class="text-muted small">
+<div class="entity-meta">
     @if($entity->isA('revision'))
         {{ trans('entities.pages_revision') }}
         {{ trans('entities.pages_revisions_number') }}{{ $entity->revision_number == 0 ? '' : $entity->revision_number }}
         <br>
     @endif
-    @if ($entity->isA('page')) {{ trans('entities.meta_revision', ['revisionCount' => $entity->revision_count]) }} <br> @endif
+
+    @if ($entity->isA('page'))
+        @if (userCan('page-update', $entity)) <a href="{{ $entity->getUrl('/revisions') }}"> @endif
+            @icon('history'){{ trans('entities.meta_revision', ['revisionCount' => $entity->revision_count]) }} <br>
+            @if (userCan('page-update', $entity))</a>@endif
+    @endif
+
+
     @if ($entity->createdBy)
-        {!! trans('entities.meta_created_name', [
+        @icon('star'){!! trans('entities.meta_created_name', [
             'timeLength' => '<span title="'.$entity->created_at->toDayDateTimeString().'">'.$entity->created_at->diffForHumans() . '</span>',
             'user' => "<a href='{$entity->createdBy->getProfileUrl()}'>".htmlentities($entity->createdBy->name). "</a>"
             ]) !!}
     @else
-        <span title="{{$entity->created_at->toDayDateTimeString()}}">{{ trans('entities.meta_created', ['timeLength' => $entity->created_at->diffForHumans()]) }}</span>
+        @icon('star')<span title="{{$entity->created_at->toDayDateTimeString()}}">{{ trans('entities.meta_created', ['timeLength' => $entity->created_at->diffForHumans()]) }}</span>
     @endif
+
     <br>
+
     @if ($entity->updatedBy)
-        {!! trans('entities.meta_updated_name', [
+        @icon('edit'){!! trans('entities.meta_updated_name', [
                 'timeLength' => '<span title="' . $entity->updated_at->toDayDateTimeString() .'">' . $entity->updated_at->diffForHumans() .'</span>',
                 'user' => "<a href='{$entity->updatedBy->getProfileUrl()}'>".htmlentities($entity->updatedBy->name). "</a>"
             ]) !!}
     @elseif (!$entity->isA('revision'))
-        <span title="{{ $entity->updated_at->toDayDateTimeString() }}">{{ trans('entities.meta_updated', ['timeLength' => $entity->updated_at->diffForHumans()]) }}</span>
+        @icon('edit')<span title="{{ $entity->updated_at->toDayDateTimeString() }}">{{ trans('entities.meta_updated', ['timeLength' => $entity->updated_at->diffForHumans()]) }}</span>
     @endif
-</p>
+</div>
