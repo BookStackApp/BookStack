86b2ddbd28b49169463a1d6be6bc4e4c24490bf7
Abijeet <abijeetpatro@gmail.com>
2018-05-10 09:05:18 +0530

    Implemented displaying of the books list on home page.

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -52,34 +52,5 @@
 @stop
 
 @section('body')
-    @if($booksViewType === 'list')
-        <div class="container small" ng-non-bindable>
-    @else
-        <div class="container" ng-non-bindable>
-    @endif
-        <h1>{{ trans('entities.books') }}</h1>
-        @if(count($books) > 0)
-            @if($booksViewType === 'list')
-                @foreach($books as $book)
-                    @include('books/list-item', ['book' => $book])
-                    <hr>
-                @endforeach
-                {!! $books->render() !!}
-            @else
-                 <div class="grid third">
-                    @foreach($books as $key => $book)
-                            @include('books/grid-item', ['book' => $book])
-                    @endforeach
-                 </div>
-                <div>
-                    {!! $books->render() !!}
-                </div>
-            @endif
-        @else
-            <p class="text-muted">{{ trans('entities.books_empty') }}</p>
-            @if(userCan('books-create-all'))
-                <a href="{{ baseUrl("/create-book") }}" class="text-pos">@icon('edit'){{ trans('entities.create_one_now') }}</a>
-            @endif
-        @endif
-    </div>
+    @include('books/list', ['books' => $books, 'bookViewType' => $booksViewType])
 @stop
