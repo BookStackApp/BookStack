26a5d26bd7e8c5e54d1dc29c6c0c8097c0e02f45
Abijeet <abijeetpatro@gmail.com>
2018-05-12 14:16:05 +0530

    Added the book view toggle option on the homepage.
    Signed-off-by: Abijeet <abijeetpatro@gmail.com>

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,16 +3,7 @@
 @section('toolbar')
     <div class="col-xs-6">
         <div class="action-buttons text-left">
-            <form action="{{ baseUrl("/settings/users/{$currentUser->id}/switch-book-view") }}" method="POST" class="inline">
-                {!! csrf_field() !!}
-                {!! method_field('PATCH') !!}
-                <input type="hidden" value="{{ $booksViewType === 'list'? 'grid' : 'list' }}" name="book_view_type">
-                @if ($booksViewType === 'list')
-                    <button type="submit" class="text-pos text-button">@icon('grid'){{ trans('common.grid_view') }}</button>
-                @else
-                    <button type="submit" class="text-pos text-button">@icon('list'){{ trans('common.list_view') }}</button>
-                @endif
-            </form>
+            @include('partials/book-view-toggle', ['booksViewType' => $booksViewType])
         </div>
     </div>
     <div class="col-xs-6 faded">
