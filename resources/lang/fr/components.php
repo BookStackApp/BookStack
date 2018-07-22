fe88d3bb8aba45317c47dc9eae5e35c672ae4dee
nicobubulle <nicobubulle@gmail.com>
2018-07-22 18:20:09 +0200

    French translation update

--- resources/lang/fr/components.php
+++ resources/lang/fr/components.php
@@ -4,7 +4,7 @@ return [
     /**
      * Image Manager
      */
-    'image_select' => 'Selectionner une image',
+    'image_select' => 'Sélectionner une image',
     'image_all' => 'Toutes',
     'image_all_title' => 'Voir toutes les images',
     'image_book_title' => 'Voir les images ajoutées à ce livre',
@@ -15,19 +15,20 @@ return [
     'image_image_name' => 'Nom de l\'image',
     'image_delete_used' => 'Cette image est utilisée dans les pages ci-dessous.',
     'image_delete_confirm' => 'Confirmez que vous souhaitez bien supprimer cette image.',
-    'image_select_image' => 'Selectionner l\'image',
+    'image_select_image' => 'Sélectionner l\'image',
     'image_dropzone' => 'Glissez les images ici ou cliquez pour les ajouter',
     'images_deleted' => 'Images supprimées',
     'image_preview' => 'Prévisualiser l\'image',
     'image_upload_success' => 'Image ajoutée avec succès',
     'image_update_success' => 'Détails de l\'image mis à jour',
     'image_delete_success' => 'Image supprimée avec succès',
+    'image_upload_remove' => 'Supprimer',
 
     /**
      * Code editor
      */
     'code_editor' => 'Editer le code',
-    'code_language' => 'Language du code',
+    'code_language' => 'Langage du code',
     'code_content' => 'Contenu du code',
     'code_save' => 'Enregistrer le code',
 ];
