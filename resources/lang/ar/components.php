b9227580b2130053c8f842182aad75481b971e5e
Khalid <kmoj86@gmail.com>
2018-07-23 13:08:07 +0300

    partial file translation

---
+++ resources/lang/ar/components.php
@@ -0,0 +1,34 @@
+<?php
+return [
+
+    /**
+     * Image Manager / قم بإسقاط الصورة أو اضغط هنا للرفع
+     */
+    'image_select' => 'تحديد صورة',
+    'image_all' => 'الكل',
+    'image_all_title' => 'عرض جميع الصور',
+    'image_book_title' => 'عرض الصور المرفوعة لهذا الكتاب',
+    'image_page_title' => 'عرض الصور المرفوعة لهذه الصفحة',
+    'image_search_hint' => 'البحث باستخدام اسم الصورة',
+    'image_uploaded' => 'وقت الرفع :uploadedDate',
+    'image_load_more' => 'المزيد',
+    'image_image_name' => 'اسم الصورة',
+    'image_delete_used' => 'هذه الصورة مستخدمة بالصفحات أدناه.',
+    'image_delete_confirm' => 'اضغط زر الحذف مرة أخرى لتأكيد حذف هذه الصورة.',
+    'image_select_image' => 'تحديد الصورة',
+    'image_dropzone' => 'قم بإسقاط الصورة أو اضغط هنا للرفع',
+    'images_deleted' => 'تم حذف الصور',
+    'image_preview' => 'معاينة الصور',
+    'image_upload_success' => 'تم رفع الصورة بنجاح',
+    'image_update_success' => 'تم تحديث تفاصيل الصورة بنجاح',
+    'image_delete_success' => 'تم حذف الصورة بنجاح',
+    'image_upload_remove' => 'إزالة',
+
+    /**
+     * Code editor
+     */
+    'code_editor' => 'تعديل الشفرة',
+    'code_language' => 'لغة الشفرة',
+    'code_content' => 'محتويات الشفرة',
+    'code_save' => 'حفظ الشفرة',
+];
