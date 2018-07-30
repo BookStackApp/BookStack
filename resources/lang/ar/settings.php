9768c8eba89262b912ae5a3cd0b24a5b4a2293ba
Khalid <kmoj86@gmail.com>
2018-07-30 12:54:32 +0300

    translated

--- resources/lang/ar/settings.php
+++ resources/lang/ar/settings.php
@@ -21,8 +21,8 @@ return [
     'app_name_desc' => 'سيتم عرض هذا الاسم في الترويسة وفي أي رسالة بريد إلكتروني.',
     'app_name_header' => 'عرض اسم التطبيق في الترويسة؟',
     'app_public_viewing' => 'السماح بالعرض على العامة؟',
-    'app_secure_images' => 'Enable higher security image uploads?', // جار البحث عن الترجمة الأنسب
-    'app_secure_images_desc' => 'For performance reasons, all images are public. This option adds a random, hard-to-guess string in front of image urls. Ensure directory indexes are not enabled to prevent easy access.', // جار البحث عن الترجمة الأنسب
+    'app_secure_images' => 'تفعيل حماية أكبر لرفع الصور؟',
+    'app_secure_images_desc' => 'لتحسين أداء النظام, ستكون جميع الصور متاحة للعامة. هذا الخيار يضيف سلسلة من الحروف والأرقام العشوائية صعبة التخمين إلى رابط الصورة. الرجاء التأكد من تعطيل فهرسة المسارات لمنع الوصول السهل.',
     'app_editor' => 'محرر الصفحة',
     'app_editor_desc' => 'الرجاء اختيار محرر النص الذي سيستخدم من قبل جميع المستخدمين لتحرير الصفحات.',
     'app_custom_html' => 'Custom HTML head content', // جار البحث عن الترجمة الأنسب
@@ -32,11 +32,11 @@ return [
     'app_primary_color' => 'اللون الأساسي للتطبيق',
     'app_primary_color_desc' => 'يجب أن تكون القيمة من نوع hex. <br>اترك الخانة فارغة للرجوع للون الافتراضي.',
     'app_homepage' => 'الصفحة الرئيسية للتطبيق',
-    'app_homepage_desc' => 'Select a page to show on the homepage instead of the default view. Page permissions are ignored for selected pages.', // جار البحث عن الترجمة الأنسب
+    'app_homepage_desc' => 'الرجاء اختيار صفحة لتصبح الصفحة الرئيسية بدل من الافتراضية. سيتم تجاهل جميع الأذونات الخاصة بالصفحة المختارة.',
     'app_homepage_default' => 'شكل الصفحة الافتراضية المختارة',
-    'app_homepage_books' => 'Or select the books page as your homepage. This will override any page selected as your homepage.', // جار البحث عن الترجمة الأنسب
+    'app_homepage_books' => 'أو من الممكن اختيار صفحة الكتب كصفحة رئيسية. سيتم استبدالها بأي صفحة سابقة تم اختيارها كصفحة رئيسية.',
     'app_disable_comments' => 'تعطيل التعليقات',
-    'app_disable_comments_desc' => 'Disable comments across all pages in the application. Existing comments are not shown.', // جار البحث عن الترجمة الأنسب
+    'app_disable_comments_desc' => 'تعطيل التعليقات على جميع الصفحات داخل التطبيق. التعليقات الموجودة من الأصل لن تكون ظاهرة.',
 
     /**
      * Registration settings
@@ -46,7 +46,7 @@ return [
     'reg_allow' => 'السماح بالتسجيل؟',
     'reg_default_role' => 'دور المستخدم الأساسي بعد التسجيل',
     'reg_confirm_email' => 'فرض التأكيد عن طريق البريد الإلكتروني؟',
-    'reg_confirm_email_desc' => 'If domain restriction is used then email confirmation will be required and the below value will be ignored.', // جار البحث عن الترجمة الأنسب
+    'reg_confirm_email_desc' => 'إذا تم استخدام قيود للمجال سيصبح التأكيد عن طريق البريد الإلكتروني إلزامي وسيتم تجاهل القيمة أسفله.',
     'reg_confirm_restrict_domain' => 'تقييد التسجيل على مجال محدد',
     'reg_confirm_restrict_domain_desc' => 'Enter a comma separated list of email domains you would like to restrict registration to. Users will be sent an email to confirm their address before being allowed to interact with the application. <br> Note that users will be able to change their email addresses after successful registration.', // جار البحث عن الترجمة الأنسب
     'reg_confirm_restrict_domain_placeholder' => 'لم يتم اختيار أي قيود',
@@ -60,9 +60,9 @@ return [
     'maint_image_cleanup_desc' => "Scans page & revision content to check which images and drawings are currently in use and which images are redundant. Ensure you create a full database and image backup before running this.", // جار البحث عن الترجمة الأنسب
     'maint_image_cleanup_ignore_revisions' => 'تجاهل الصور في المراجعات',
     'maint_image_cleanup_run' => 'بدء التنظيف',
-    'maint_image_cleanup_warning' => ':count potentially unused images were found. Are you sure you want to delete these images?', // جار البحث عن الترجمة الأنسب
-    'maint_image_cleanup_success' => ':count potentially unused images found and deleted!', // جار البحث عن الترجمة الأنسب
-    'maint_image_cleanup_nothing_found' => 'No unused images found, Nothing deleted!', // جار البحث عن الترجمة الأنسب
+    'maint_image_cleanup_warning' => 'يوجد عدد :count من الصور المحتمل عدم استخدامها. تأكيد حذف الصور؟',
+    'maint_image_cleanup_success' => 'تم إيجاد وحذف عدد :count من الصور المحتمل عدم استخدامها!',
+    'maint_image_cleanup_nothing_found' => 'لم يتم حذف أي شيء لعدم وجود أي صور غير مسمتخدمة',
 
     /**
      * Role settings
@@ -75,12 +75,12 @@ return [
     'role_delete' => 'حذف الدور',
     'role_delete_confirm' => 'سيتم حذف الدور المسمى \':roleName\'.',
     'role_delete_users_assigned' => 'This role has :userCount users assigned to it. If you would like to migrate the users from this role select a new role below.',
-    'role_delete_no_migration' => "Don't migrate users",
-    'role_delete_sure' => 'Are you sure you want to delete this role?',
-    'role_delete_success' => 'Role successfully deleted',
-    'role_edit' => 'Edit Role',
-    'role_details' => 'Role Details',
-    'role_name' => 'Role Name',
+    'role_delete_no_migration' => "لا تقم بترجيل المستخدمين",
+    'role_delete_sure' => 'تأكيد حذف الدور؟',
+    'role_delete_success' => 'تم حذف الدور بنجاح',
+    'role_edit' => 'تعديل الدور',
+    'role_details' => 'تفاصيل الدور',
+    'role_name' => 'اسم الدور',
     'role_desc' => 'Short Description of Role',
     'role_external_auth_id' => 'External Authentication IDs',
     'role_system' => 'System Permissions',
