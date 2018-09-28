cbebe7c8dee12a2532dc859860f802a8cc525835
André Luiz da Silva <andreluiz.97@outlook.com>
2018-09-28 16:19:06 -0300

    Adds missing "errors" pt_BR translations

--- resources/lang/pt_BR/errors.php
+++ resources/lang/pt_BR/errors.php
@@ -49,6 +49,7 @@ return [
 
     // Entities
     'entity_not_found' => 'Entidade não encontrada',
+    'bookshelf_not_found' => 'Prateleira de Livros não encontrada',
     'book_not_found' => 'Livro não encontrado',
     'page_not_found' => 'Página não encontrada',
     'chapter_not_found' => 'Capítulo não encontrado',
