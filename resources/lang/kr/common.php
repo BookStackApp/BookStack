fca7689e1ae8194ddcc51126e1115b26d0f80f87
codev-kuk-mac <kuk@codev.kr>
2018-10-05 10:24:37 +0900

    translate to kr 20/100

---
+++ resources/lang/kr/common.php
@@ -0,0 +1,68 @@
+<?php
+return [
+
+    /**
+     * Buttons
+     */
+    'cancel' => '취소',
+    'confirm' => '확인',
+    'back' => '뒤로',
+    'save' => '저장',
+    'continue' => '계속하기',
+    'select' => '선택',
+    'more' => '더보기',
+
+    /**
+     * Form Labels
+     */
+    'name' => '이름',
+    'description' => '설명',
+    'role' => 'Role',
+    'cover_image' => '대표 이미지',
+    'cover_image_description' => 'This image should be approx 440x250px.',
+    
+    /**
+     * Actions
+     */
+    'actions' => 'Actions',
+    'view' => 'View',
+    'create' => '생성',
+    'update' => '업데이트',
+    'edit' => '수정',
+    'sort' => '정렬',
+    'move' => '이동',
+    'copy' => '복사',
+    'reply' => 'Reply',
+    'delete' => '삭제',
+    'search' => '검색',
+    'search_clear' => '검색기록 삭제',
+    'reset' => 'Reset',
+    'remove' => '제거',
+    'add' => '추가',
+
+    /**
+     * Misc
+     */
+    'deleted_user' => 'Deleted User',
+    'no_activity' => 'No activity to show',
+    'no_items' => 'No items available',
+    'back_to_top' => 'Back to top',
+    'toggle_details' => 'Toggle Details',
+    'toggle_thumbnails' => 'Toggle Thumbnails',
+    'details' => 'Details',
+    'grid_view' => 'Grid View',
+    'list_view' => 'List View',
+    'default' => 'Default',
+
+    /**
+     * Header
+     */
+    'view_profile' => '프로파일 보기',
+    'edit_profile' => '프로파일 수정하기',
+
+    /**
+     * Email Content
+     */
+    'email_action_help' => 'If you’re having trouble clicking the ":actionText" button, copy and paste the URL below into your web browser:',
+    'email_rights' => 'All rights reserved',
+];
