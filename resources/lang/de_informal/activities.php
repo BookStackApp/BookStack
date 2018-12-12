f943f0d401e7e4e9e3325a2f9c4e090aca70a307
ezzra <ezzra@posteo.de>
2018-12-12 10:37:24 +0100

    de_informal - remove comments from unused lines

--- resources/lang/de_informal/activities.php
+++ resources/lang/de_informal/activities.php
@@ -2,21 +2,6 @@
 $de_formal = (include resource_path() . '/lang/de/' . basename(__FILE__));
 
 $de_informal = [
-
-    /**
-     * Activity text strings.
-     * Is used for all the text within activity logs & notifications.
-     */
-
-    // Pages
- 
-    // Chapters
-   
-    // Books
-    
-    // Bookshelves
-    
-    // Other
     
 ];
 
