a112c11df80d388f22a0b4619b22801e13688bea
Dan Brown <ssddanbrown@googlemail.com>
2019-02-16 14:17:35 +0000

    Re-ordered and updated main settings page

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -1,14 +1,11 @@
 @extends('simple-layout')
 
-{{--TODO - Reorder--}}
-
 @section('body')
     <div class="container small">
 
-
         <div class="grid left-focus v-center">
             <div class="py-m">
-                @include('settings/navbar', ['selected' => 'settings'])
+                @include('settings.navbar', ['selected' => 'settings'])
             </div>
             <div class="text-right mb-l px-m">
                 <br>
@@ -16,8 +13,72 @@
             </div>
         </div>
 
-        <div class="card content-wrap">
-            <h2 class="list-heading">{{ trans('settings.app_settings') }}</h2>
+        <div class="card content-wrap auto-height">
+            <h2 class="list-heading">{{ trans('settings.app_features_security') }}</h2>
+            <form action="{{ baseUrl("/settings") }}" method="POST">
+                {!! csrf_field() !!}
+
+                <div class="setting-list">
+
+
+                    <div class="grid half large-gap">
+                        <div>
+                            <label for="setting-app-public" class="setting-list-label">{{ trans('settings.app_public_access') }}</label>
+                            <p class="small">{!! trans('settings.app_public_access_desc') !!}</p>
+                            @if(userCan('users-manage'))
+                                <p class="small mb-none">
+                                    <a href="{{ baseUrl($guestUser->getEditUrl()) }}">{!! trans('settings.app_public_access_desc_guest') !!}</a>
+                                </p>
+                            @endif
+                        </div>
+                        <div>
+                            @include('components.toggle-switch', [
+                                'name' => 'setting-app-public',
+                                'value' => setting('app-public'),
+                                'label' => trans('settings.app_public_access_toggle'),
+                            ])
+                        </div>
+                    </div>
+
+                    <div class="grid half large-gap">
+                        <div>
+                            <label class="setting-list-label">{{ trans('settings.app_secure_images') }}</label>
+                            <p class="small">{{ trans('settings.app_secure_images_desc') }}</p>
+                        </div>
+                        <div>
+                            @include('components.toggle-switch', [
+                                'name' => 'setting-app-secure-images',
+                                'value' => setting('app-secure-images'),
+                                'label' => trans('settings.app_secure_images_toggle'),
+                            ])
+                        </div>
+                    </div>
+
+                    <div class="grid half large-gap">
+                        <div>
+                            <label class="setting-list-label">{{ trans('settings.app_disable_comments') }}</label>
+                            <p class="small">{!! trans('settings.app_disable_comments_desc') !!}</p>
+                        </div>
+                        <div>
+                            @include('components.toggle-switch', [
+                                'name' => 'setting-app-disable-comments',
+                                'value' => setting('app-disable-comments'),
+                                'label' => trans('settings.app_disable_comments'),
+                            ])
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div class="form-group text-right">
+                    <button type="submit" class="button primary">{{ trans('settings.settings_save') }}</button>
+                </div>
+            </form>
+        </div>
+
+        <div class="card content-wrap auto-height">
+            <h2 class="list-heading">{{ trans('settings.app_customization') }}</h2>
             <form action="{{ baseUrl("/settings") }}" method="POST">
                 {!! csrf_field() !!}
 
@@ -40,51 +101,6 @@
 
                     <div class="grid half large-gap">
                         <div>
-                            <label for="setting-app-public" class="setting-list-label">{{ trans('settings.app_public_viewing') }}</label>
-                            {{--<p class="small">{{ trans('settings.app_name_desc') }}</p>--}}
-                        </div>
-                        <div>
-                            @include('components.toggle-switch', [
-                                'name' => 'setting-app-public',
-                                'value' => setting('app-public'),
-                                'label' => trans('settings.app_public_viewing'),
-                            ])
-                            {{--TODO - Text --}}
-                        </div>
-                    </div>
-
-                    <div class="grid half large-gap">
-                        <div>
-                            <label class="setting-list-label">{{ trans('settings.app_secure_images') }}</label>
-                            <p class="small">{{ trans('settings.app_secure_images_desc') }}</p>
-                        </div>
-                        <div>
-                            @include('components.toggle-switch', [
-                                'name' => 'setting-app-secure-images',
-                                'value' => setting('app-secure-images'),
-                                'label' => trans('settings.app_secure_images'),
-                            ])
-                            {{--TODO - Text --}}
-                        </div>
-                    </div>
-
-                    <div class="grid half large-gap">
-                        <div>
-                            <label class="setting-list-label">{{ trans('settings.app_disable_comments') }}</label>
-                            <p class="small">{{ trans('settings.app_disable_comments_desc') }}</p>
-                        </div>
-                        <div>
-                            @include('components.toggle-switch', [
-                                'name' => 'setting-app-disable-comments',
-                                'value' => setting('app-disable-comments'),
-                                'label' => trans('settings.app_disable_comments'),
-                            ])
-                            {{--TODO - Text --}}
-                        </div>
-                    </div>
-
-                    <div class="grid half large-gap">
-                        <div>
                             <label class="setting-list-label">{{ trans('settings.app_editor') }}</label>
                             <p class="small">{{ trans('settings.app_editor_desc') }}</p>
                         </div>
@@ -93,14 +109,13 @@
                                 <option @if(setting('app-editor') === 'wysiwyg') selected @endif value="wysiwyg">WYSIWYG</option>
                                 <option @if(setting('app-editor') === 'markdown') selected @endif value="markdown">Markdown</option>
                             </select>
-                            {{--TODO - Text --}}
                         </div>
                     </div>
 
                     <div class="grid half large-gap">
                         <div>
                             <label class="setting-list-label">{{ trans('settings.app_logo') }}</label>
-                            <p class="small">{{ trans('settings.app_logo_desc') }}</p>
+                            <p class="small">{!! trans('settings.app_logo_desc') !!}</p>
                         </div>
                         <div>
                             @include('components.image-picker', [
@@ -162,8 +177,7 @@
             </form>
         </div>
 
-
-        <div class="card content-wrap">
+        <div class="card content-wrap auto-height">
             <h2 class="list-heading">{{ trans('settings.reg_settings') }}</h2>
             <form action="{{ baseUrl("/settings") }}" method="POST">
                 {!! csrf_field() !!}
@@ -171,25 +185,17 @@
                 <div class="setting-list">
                     <div class="grid half large-gap">
                         <div>
-                            <label class="setting-list-label">{{ trans('settings.reg_allow') }}</label>
-                            {{--<p class="small">{!! trans('settings.app_primary_color_desc') !!}</p>--}}{{-- TODO--}}
+                            <label class="setting-list-label">{{ trans('settings.reg_enable') }}</label>
+                            <p class="small">{!! trans('settings.reg_enable_desc') !!}</p>
                         </div>
                         <div>
                             @include('components.toggle-switch', [
                                 'name' => 'setting-registration-enabled',
                                 'value' => setting('registration-enabled'),
-                                'label' => trans('settings.reg_allow')
+                                'label' => trans('settings.reg_enable')
                             ])
-                            {{--TODO - label --}}
-                        </div>
-                    </div>
 
-                    <div class="grid half large-gap">
-                        <div>
-                            <label for="setting-registration-role" class="setting-list-label">{{ trans('settings.reg_default_role') }}</label>
-                            {{--<p class="small">{!! trans('settings.app_primary_color_desc') !!}</p>--}}{{-- TODO--}}
-                        </div>
-                        <div>
+                            <label for="setting-registration-role">{{ trans('settings.reg_default_role') }}</label>
                             <select id="setting-registration-role" name="setting-registration-role" @if($errors->has('setting-registration-role')) class="neg" @endif>
                                 @foreach(\BookStack\Auth\Role::all() as $role)
                                     <option value="{{$role->id}}" data-role-name="{{ $role->name }}"
@@ -204,21 +210,6 @@
 
                     <div class="grid half large-gap">
                         <div>
-                            <label class="setting-list-label">{{ trans('settings.reg_confirm_email') }}</label>
-                            <p class="small">{{ trans('settings.reg_confirm_email_desc') }}</p>
-                        </div>
-                        <div>
-                            @include('components.toggle-switch', [
-                                'name' => 'setting-registration-confirmation',
-                                'value' => setting('registration-confirmation'),
-                                'label' => trans('settings.reg_confirm_email')
-                            ])
-                            {{--TODO - label --}}
-                        </div>
-                    </div>
-
-                    <div class="grid half large-gap">
-                        <div>
                             <label for="setting-registration-restrict" class="setting-list-label">{{ trans('settings.reg_confirm_restrict_domain') }}</label>
                             <p class="small">{!! trans('settings.reg_confirm_restrict_domain_desc') !!}</p>
                         </div>
@@ -226,6 +217,21 @@
                             <input type="text" id="setting-registration-restrict" name="setting-registration-restrict" placeholder="{{ trans('settings.reg_confirm_restrict_domain_placeholder') }}" value="{{ setting('registration-restrict', '') }}">
                         </div>
                     </div>
+
+                    <div class="grid half large-gap">
+                        <div>
+                            <label class="setting-list-label">{{ trans('settings.reg_email_confirmation') }}</label>
+                            <p class="small">{{ trans('settings.reg_confirm_email_desc') }}</p>
+                        </div>
+                        <div>
+                            @include('components.toggle-switch', [
+                                'name' => 'setting-registration-confirmation',
+                                'value' => setting('registration-confirmation'),
+                                'label' => trans('settings.reg_email_confirmation_toggle')
+                            ])
+                        </div>
+                    </div>
+
                 </div>
 
                 <div class="form-group text-right">
@@ -241,24 +247,23 @@
 @stop
 
 @section('scripts')
-    {{--TODO - Merge into JS--}}
     <script src="{{ baseUrl("/libs/jq-color-picker/tiny-color-picker.min.js?version=1.0.0") }}"></script>
     <script type="text/javascript">
         $('#setting-app-color').colorPicker({
             opacity: false,
             renderCallback: function($elm, toggled) {
-                var hexVal = '#' + this.color.colors.HEX;
-                var rgb = this.color.colors.RND.rgb;
-                var rgbLightVal = 'rgba('+ [rgb.r, rgb.g, rgb.b, '0.15'].join(',') +')';
+                const hexVal = '#' + this.color.colors.HEX;
+                const rgb = this.color.colors.RND.rgb;
+                const rgbLightVal = 'rgba('+ [rgb.r, rgb.g, rgb.b, '0.15'].join(',') +')';
 
                 // Set textbox color to hex color code.
-                var isEmpty = $.trim($elm.val()).length === 0;
+                const isEmpty = $.trim($elm.val()).length === 0;
                 if (!isEmpty) $elm.val(hexVal);
                 $('#setting-app-color-light').val(isEmpty ? '' : rgbLightVal);
 
-                var customStyles = document.getElementById('custom-styles');
-                var oldColor = customStyles.getAttribute('data-color');
-                var oldColorLight = customStyles.getAttribute('data-color-light');
+                const customStyles = document.getElementById('custom-styles');
+                const oldColor = customStyles.getAttribute('data-color');
+                const oldColorLight = customStyles.getAttribute('data-color-light');
 
                 customStyles.innerHTML = customStyles.innerHTML.split(oldColor).join(hexVal);
                 customStyles.innerHTML = customStyles.innerHTML.split(oldColorLight).join(rgbLightVal);
