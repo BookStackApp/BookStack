042a6f9760337d28138d4c1e4c7ab570bce8804c
Dan Brown <ssddanbrown@googlemail.com>
2019-03-09 21:15:45 +0000

    Updated shelf menu item to show on custom permission
    - Extended new 'userCanOnAny' helper to take a entity class for
    filtering.
    
    Closes #1201

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -48,7 +48,7 @@
                             </form>
                         </div>
                         <div class="links text-center">
-                            @if(userCan('bookshelf-view-all') || userCan('bookshelf-view-own'))
+                            @if(userCanOnAny('view', \BookStack\Entities\Bookshelf::class) || userCan('bookshelf-view-own'))
                                 <a href="{{ baseUrl('/shelves') }}">@icon('bookshelf'){{ trans('entities.shelves') }}</a>
                             @endif
                             <a href="{{ baseUrl('/books') }}">@icon('book'){{ trans('entities.books') }}</a>
