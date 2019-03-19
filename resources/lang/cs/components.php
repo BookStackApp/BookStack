b3a4d8af2a0421adaa3115a6125e4df457cdd380
Martin Šimek <cima.m@seznam.cz>
2019-03-19 22:45:14 +0100

    Czech translation
    + Czech language (cs)
    + settings.php in english populated by new option
    Note: validation php taken from  lavarel official translation (https://github.com/caouecs/Laravel-lang/blob/master/src/cs/validation.php)

---
+++ resources/lang/cs/components.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Text used in custom JavaScript driven components.
+ */
+return [
+
+    // Image Manager
+    'image_select' => 'Volba obrázku',
+    'image_all' => 'Vše',
+    'image_all_title' => 'Zobrazit všechny obrázky',
+    'image_book_title' => 'Zobrazit obrázky nahrané k této knize',
+    'image_page_title' => 'Zobrazit obrázky nahrané k této stránce',
+    'image_search_hint' => 'Hledat podle názvu obrázku',
+    'image_uploaded' => 'Nahráno :uploadedDate',
+    'image_load_more' => 'Načíst další',
+    'image_image_name' => 'Název obrázku',
+    'image_delete_used' => 'Tento obrázek je použit v následujících stránkách.',
+    'image_delete_confirm' => 'Stisknětě smazat ještě jednou pro potvrzení smazání tohoto obrázku.',
+    'image_select_image' => 'Zvolte obrázek',
+    'image_dropzone' => 'Přetáhněte sem obrázky myší nebo sem kliknětě pro vybrání souboru.',
+    'images_deleted' => 'Obrázky smazány',
+    'image_preview' => 'Náhled obrázku',
+    'image_upload_success' => 'Obrázek byl úspěšně nahrán',
+    'image_update_success' => 'Podrobnosti o obrázku byly úspěšně aktualizovány',
+    'image_delete_success' => 'Obrázek byl úspěšně smazán',
+    'image_upload_remove' => 'Odstranit',
+
+    // Code Editor
+    'code_editor' => 'Upravit kód',
+    'code_language' => 'Jazyk kódu',
+    'code_content' => 'Obsah kódu',
+    'code_save' => 'Uložit kód',
+];
