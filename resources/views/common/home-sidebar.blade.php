745a0bb98d25ab5c5a4d49d522ccafa784c7f112
Dan Brown <ssddanbrown@googlemail.com>
2019-04-06 17:31:59 +0100

    Updated custom homepage views

--- resources/views/common/home-sidebar.blade.php
+++ resources/views/common/home-sidebar.blade.php
@@ -1,23 +1,23 @@
 @if(count($draftPages) > 0)
-    <div id="recent-drafts" class="card">
-        <h3>@icon('edit') {{ trans('entities.my_recent_drafts') }}</h3>
-        @include('partials/entity-list', ['entities' => $draftPages, 'style' => 'compact'])
+    <div id="recent-drafts" class="mb-xl">
+        <h5>{{ trans('entities.my_recent_drafts') }}</h5>
+        @include('partials.entity-list', ['entities' => $draftPages, 'style' => 'compact'])
     </div>
 @endif
 
-<div class="card">
-    <h3>@icon($signedIn ? 'view' : 'star-circle') {{ trans('entities.' . ($signedIn ? 'my_recently_viewed' : 'books_recent')) }}</h3>
-    @include('partials/entity-list', [
+<div class="mb-xl">
+    <h5>{{ trans('entities.' . ($signedIn ? 'my_recently_viewed' : 'books_recent')) }}</h5>
+    @include('partials.entity-list', [
         'entities' => $recents,
         'style' => 'compact',
         'emptyText' => $signedIn ? trans('entities.no_pages_viewed') : trans('entities.books_empty')
         ])
 </div>
 
-<div class="card">
-    <h3>@icon('file') <a class="no-color" href="{{ baseUrl("/pages/recently-updated") }}">{{ trans('entities.recently_updated_pages') }}</a></h3>
+<div class="mb-xl">
+    <h5><a class="no-color" href="{{ baseUrl("/pages/recently-updated") }}">{{ trans('entities.recently_updated_pages') }}</a></h5>
     <div id="recently-updated-pages">
-        @include('partials/entity-list', [
+        @include('partials.entity-list', [
         'entities' => $recentlyUpdatedPages,
         'style' => 'compact',
         'emptyText' => trans('entities.no_pages_recently_updated')
@@ -25,7 +25,7 @@
     </div>
 </div>
 
-<div id="recent-activity" class="card">
-    <h3>@icon('time') {{ trans('entities.recent_activity') }}</h3>
-    @include('partials/activity-list', ['activity' => $activity])
+<div id="recent-activity" class="mb-xl">
+    <h5>{{ trans('entities.recent_activity') }}</h5>
+    @include('partials.activity-list', ['activity' => $activity])
 </div>
