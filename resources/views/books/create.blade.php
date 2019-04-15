84419005e73f9b3bc8a6f4d074c27a11df1439dc
Christopher Wilkinson <cwilkinson000@gmail.com>
2019-04-15 10:56:21 +0100

    Update create new book button on shelves to 2019 design

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -3,16 +3,26 @@
 @section('body')
     <div class="container small">
         <div class="my-s">
-            @include('partials.breadcrumbs', ['crumbs' => [
-                '/books' => [
-                    'text' => trans('entities.books'),
-                    'icon' => 'book'
-                ],
-                '/create-book' => [
-                    'text' => trans('entities.books_create'),
-                    'icon' => 'add'
-                ]
-            ]])
+            @if (isset($bookshelf))
+                @include('partials.breadcrumbs', ['crumbs' => [
+                    $bookshelf,
+                    $bookshelf->getUrl('/create-book') => [
+                        'text' => trans('entities.books_create'),
+                        'icon' => 'add'
+                    ]
+                ]])
+            @else
+                @include('partials.breadcrumbs', ['crumbs' => [
+                    '/books' => [
+                        'text' => trans('entities.books'),
+                        'icon' => 'book'
+                    ],
+                    '/create-book' => [
+                        'text' => trans('entities.books_create'),
+                        'icon' => 'add'
+                    ]
+                ]])
+            @endif
         </div>
 
         <div class="content-wrap card">
