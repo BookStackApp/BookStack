e00c170d85760ffc8ee52188bf634074641fced9
Nooto <50425610+NootoNooto@users.noreply.github.com>
2019-05-08 23:24:22 +0200

    Update common.php

--- resources/lang/nl/common.php
+++ resources/lang/nl/common.php
@@ -1,31 +1,28 @@
 <?php
 return [
 
-    /**
-     * Buttons
-     */
+    
+    // Buttons
     'cancel' => 'Annuleren',
     'confirm' => 'Bevestigen',
     'back' => 'Terug',
     'save' => 'Opslaan',
     'continue' => 'Doorgaan',
     'select' => 'Kies',
+    'toggle_all' => 'Toggle Alles',
     'more' => 'Meer',
-
-    /**
-     * Form Labels
-     */
+    
+    // Form Labels
     'name' => 'Naam',
     'description' => 'Beschrijving',
     'role' => 'Rol',
     'cover_image' => 'Omslagfoto',
     'cover_image_description' => 'Deze afbeelding moet ongeveer 300x170px zijn.',
-    /**
-     * Actions
-     */
+    
+    // Actions
     'actions' => 'Acties',
     'view' => 'Bekijk',
-    'view_all' => 'Bekijk alle',
+    'view_all' => 'Bekijk Alle',
     'create' => 'Aanmaken',
     'update' => 'Update',
     'edit' => 'Bewerk',
@@ -40,20 +37,22 @@ return [
     'remove' => 'Verwijderen',
     'add' => 'Toevoegen',
 
-     // Sort Options
+    // Sort Options
     'sort_name' => 'Naam',
     'sort_created_at' => 'Aanmaakdatum',
     'sort_updated_at' => 'Gewijzigd op',
     
-    /**
-     * Misc
-     */
+    // Misc
     'deleted_user' => 'Verwijderde gebruiker',
     'no_activity' => 'Geen activiteiten',
     'no_items' => 'Geen items beschikbaar',
     'back_to_top' => 'Terug naar boven',
     'toggle_details' => 'Details Weergeven',
     'toggle_thumbnails' => 'Thumbnails Weergeven',
+    'details' => 'Details',
+    'grid_view' => 'Grid weergave',
+    'list_view' => 'Lijst weergave',
+    'default' => 'Standaard',
 
     /**
      * Header
@@ -61,6 +60,10 @@ return [
     'view_profile' => 'Profiel Weergeven',
     'edit_profile' => 'Profiel Bewerken',
 
+    // Layout tabs
+    'tab_info' => 'Info',
+    'tab_content' => 'Inhoud',
+    
     /**
      * Email Content
      */
