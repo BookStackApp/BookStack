c6b1f36412435dc969183913e01de13b3e8fafca
Timo Schwarzer <me@timoschwarzer.com>
2019-08-12 22:19:58 +0200

    Alter docker paths

--- dev/docker/entrypoint.app.sh
+++ dev/docker/entrypoint.app.sh
@@ -2,11 +2,13 @@
 
 set -e
 
-if [[ "$1" == "composer" ]]; then
+env
+
+if [[ -n "$1" ]]; then
     exec "$@"
 else
     wait-for-it db:3306 -t 45
-    php artisan migrate --database=mysql_docker_dev
+    php artisan migrate --database=mysql
     chown -R www-data:www-data storage
     exec apache2-foreground
 fi
