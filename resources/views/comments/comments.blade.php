c128c9539cf9ffe3038c1a33bd5ec706e483bb4e
Dan Brown <ssddanbrown@googlemail.com>
2019-08-25 11:02:58 +0100

    Set comment add box to show with correct permissions
    - Also fixed const assignment issue in translations.js

--- resources/views/comments/comments.blade.php
+++ resources/views/comments/comments.blade.php
@@ -9,7 +9,7 @@
 
     <div comment-count-bar class="grid half left-focus v-center no-row-gap">
         <h5 comments-title>{{ trans_choice('entities.comment_count', count($page->comments), ['count' => count($page->comments)]) }}</h5>
-        @if (count($page->comments) === 0)
+        @if (count($page->comments) === 0 && userCan('comment-create-all'))
             <div class="text-m-right" comment-add-button-container>
                 <button type="button" action="addComment"
                         class="button outline">{{ trans('entities.comment_add') }}</button>
@@ -25,13 +25,13 @@
 
     @if(userCan('comment-create-all'))
         @include('comments.create')
-    @endif
 
-    @if (count($page->comments) > 0)
-        <div class="text-right" comment-add-button-container>
-            <button type="button" action="addComment"
-                    class="button outline">{{ trans('entities.comment_add') }}</button>
-        </div>
+        @if (count($page->comments) > 0)
+            <div class="text-right" comment-add-button-container>
+                <button type="button" action="addComment"
+                        class="button outline">{{ trans('entities.comment_add') }}</button>
+            </div>
+        @endif
     @endif
 
 </div>
