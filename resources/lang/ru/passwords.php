bfb4e24542ea3bbcc061ad2521f7f279f12cb060
kostefun <43705266+kostefun@users.noreply.github.com>
2019-09-05 09:52:24 +0700

    Update passwords.php
    Corresponds to the English version

--- resources/lang/ru/passwords.php
+++ resources/lang/ru/passwords.php
@@ -1,22 +1,15 @@
 <?php
-
+/**
+ * Password Reminder Language Lines
+ * The following language lines are the default lines which match reasons
+ * that are given by the password broker for a password update attempt has failed.
+ */
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reminder Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
-
-    'password' => 'Пароль должен содержать не менее шести символов для применения.',
-    'user' => 'Невозможно найти пользователя с указанным email адресом.',
-    'token' => 'Этот токен для сброса пароля недействителен.',
-    'sent' => 'Ссылка для сброса пароля была отправлена на электронную почту!',
+    'password' => 'Пароль должен содержать не менее шести символов и совпадать с подтверждением.',
+    'user' => 'Пользователь с указаным email отсутствует.',
+    'token' => 'Токен сброса пароля недействителен.',
+    'sent' => 'Ссылка для сброса пароля отправлена на email!',
     'reset' => 'Ваш пароль был сброшен!',
 
 ];
