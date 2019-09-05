6f000d9b5326b0709ec8250077746f3043236ead
kostefun <43705266+kostefun@users.noreply.github.com>
2019-09-05 09:46:45 +0700

    Update pagination.php
    Corresponds to the English version

--- resources/lang/ru/pagination.php
+++ resources/lang/ru/pagination.php
@@ -1,17 +1,10 @@
 <?php
-
+/**
+ * Pagination Language Lines
+ * The following language lines are used by the paginator library to build
+ * the simple pagination links.
+ */
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Pagination Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used by the paginator library to build
-    | the simple pagination links. You are free to change them to anything
-    | you want to customize your views to better match your application.
-    |
-    */
 
     'previous' => '&laquo; Предыдущая',
     'next'     => 'Следующая &raquo;',
