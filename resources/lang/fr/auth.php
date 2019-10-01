9d704b7d2c76d0cd2c82df3e94ea15b8a41bca12
Quentin Ligier <quentin.ligier@bluewin.ch>
2019-10-01 20:34:54 +0200

    French translation update

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -28,19 +28,19 @@ return [
     'password' => 'Mot de passe',
     'password_confirm' => 'Confirmez le mot de passe',
     'password_hint' => 'Doit faire plus de 7 caractères',
-    'forgot_password' => 'Mot de passe oublié ?',
+    'forgot_password' => 'Mot de passe oublié ?',
     'remember_me' => 'Se souvenir de moi',
-    'ldap_email_hint' => "Merci d'entrer une adresse e-mail pour ce compte",
+    'ldap_email_hint' => "Merci d'entrer une adresse e-mail pour ce compte.",
     'create_account' => 'Créer un compte',
-    'already_have_account' => 'Vous avez déjà un compte?',
-    'dont_have_account' => 'Vous n\'avez pas de compte?',
-    'social_login' => 'Social Login',
-    'social_registration' => 'Enregistrement Social',
-    'social_registration_text' => "S'inscrire et se connecter avec un réseau social",
+    'already_have_account' => 'Vous avez déjà un compte ?',
+    'dont_have_account' => 'Vous n\'avez pas de compte ?',
+    'social_login' => 'Connexion avec un réseau social',
+    'social_registration' => 'Inscription avec un réseau social',
+    'social_registration_text' => "S'inscrire et se connecter avec un réseau social.",
 
-    'register_thanks' => 'Merci pour votre enregistrement',
+    'register_thanks' => 'Merci pour votre inscription !',
     'register_confirm' => 'Vérifiez vos e-mails et cliquez sur le lien de confirmation pour rejoindre :appName.',
-    'registrations_disabled' => "L'inscription est désactivée pour le moment",
+    'registrations_disabled' => "Les inscriptions sont désactivées pour le moment",
     'registration_email_domain_invalid' => 'Cette adresse e-mail ne peut pas accéder à l\'application',
     'register_success' => 'Merci pour votre inscription. Vous êtes maintenant inscrit(e) et connecté(e)',
 
@@ -48,14 +48,14 @@ return [
     /**
      * Password Reset
      */
-    'reset_password' => 'Reset Password',
-    'reset_password_send_instructions' => 'Entrez votre adresse e-mail ci-dessous et un e-mail avec un lien de réinitialisation de mot de passe vous sera envoyé',
+    'reset_password' => 'Réinitialiser le mot de passe',
+    'reset_password_send_instructions' => 'Entrez votre adresse e-mail ci-dessous et un e-mail avec un lien de réinitialisation de mot de passe vous sera envoyé.',
     'reset_password_send_button' => 'Envoyer un lien de réinitialisation',
     'reset_password_sent_success' => 'Un lien de réinitialisation a été envoyé à :email.',
     'reset_password_success' => 'Votre mot de passe a été réinitialisé avec succès.',
 
     'email_reset_subject' => 'Réinitialisez votre mot de passe pour :appName',
-    'email_reset_text' => 'Vous recevez cet e-mail parce que nous avons reçu une demande de réinitialisation pour votre compte',
+    'email_reset_text' => 'Vous recevez cet e-mail parce que nous avons reçu une demande de réinitialisation pour votre compte.',
     'email_reset_not_requested' => 'Si vous n\'avez pas effectué cette demande, vous pouvez ignorer cet e-mail.',
 
 
@@ -63,11 +63,11 @@ return [
      * Email Confirmation
      */
     'email_confirm_subject' => 'Confirmez votre adresse e-mail pour :appName',
-    'email_confirm_greeting' => 'Merci d\'avoir rejoint :appName !',
-    'email_confirm_text' => 'Merci de confirmer en cliquant sur le lien ci-dessous :',
+    'email_confirm_greeting' => 'Merci d\'avoir rejoint :appName !',
+    'email_confirm_text' => 'Merci de confirmer en cliquant sur le lien ci-dessous :',
     'email_confirm_action' => 'Confirmez votre adresse e-mail',
     'email_confirm_send_error' => 'La confirmation par e-mail est requise mais le système n\'a pas pu envoyer l\'e-mail. Contactez l\'administrateur système.',
-    'email_confirm_success' => 'Votre adresse e-mail a été confirmée !',
+    'email_confirm_success' => 'Votre adresse e-mail a été confirmée !',
     'email_confirm_resent' => 'L\'e-mail de confirmation a été ré-envoyé. Vérifiez votre boîte de récéption.',
 
     'email_not_confirmed' => 'Adresse e-mail non confirmée',
@@ -75,4 +75,14 @@ return [
     'email_not_confirmed_click_link' => 'Merci de cliquer sur le lien dans l\'e-mail qui vous a été envoyé après l\'enregistrement.',
     'email_not_confirmed_resend' => 'Si vous ne retrouvez plus l\'e-mail, vous pouvez renvoyer un e-mail de confirmation en utilisant le formulaire ci-dessous.',
     'email_not_confirmed_resend_button' => 'Renvoyez l\'e-mail de confirmation',
+
+    // User Invite
+    'user_invite_email_subject' => 'Vous avez été invité(e) à rejoindre :appName !',
+    'user_invite_email_greeting' => 'Un compte vous a été créé sur :appName.',
+    'user_invite_email_text' => 'Cliquez sur le bouton ci-dessous pour renseigner le mot de passe et récupérer l\'accès :',
+    'user_invite_email_action' => 'Renseignez le mot de passe de votre compte',
+    'user_invite_page_welcome' => 'Bienvenue dans :appName !',
+    'user_invite_page_text' => 'Pour finaliser votre compte et recevoir l\'accès, vous devez renseigner le mot de passe qui sera utilisé pour la connexion à :appName les prochaines fois.',
+    'user_invite_page_confirm_button' => 'Confirmez le mot de passe',
+    'user_invite_success' => 'Mot de passe renseigné, vous avez maintenant accès à :appName !'
 ];
--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -16,7 +16,7 @@ return [
     'password' => 'Les mots de passe doivent faire au moins 6 caractères et correspondre à la confirmation.',
     'user' => "Nous n'avons pas trouvé d'utilisateur avec cette adresse.",
     'token' => 'Le jeton de réinitialisation est invalide.',
-    'sent' => 'Nous vous avons envoyé un lien de réinitialisation de mot de passe !',
-    'reset' => 'Votre mot de passe a été réinitialisé !',
+    'sent' => 'Nous vous avons envoyé un lien de réinitialisation de mot de passe !',
+    'reset' => 'Votre mot de passe a été réinitialisé !',
 
 ];
