64c541ef34abed62528818f33fb5c2856062fbd1
Dan Brown <ssddanbrown@googlemail.com>
2019-10-05 12:27:21 +0100

    Fixed issued caused by viewservice change

--- app/Entities/Entity.php
+++ app/Entities/Entity.php
@@ -12,7 +12,6 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 /**
  * Class Entity
