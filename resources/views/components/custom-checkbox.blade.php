e48d7d59cc9a5f9f18021fcdeb14f1e7505de387
Dan Brown <ssddanbrown@googlemail.com>
2019-10-17 14:19:35 +0100

    Removed tabindexes where found to be not required

--- resources/views/components/custom-checkbox.blade.php
+++ resources/views/components/custom-checkbox.blade.php
@@ -3,12 +3,10 @@ $name
 $value
 $checked
 $label
-$tabindex
 --}}
 <label custom-checkbox class="toggle-switch @if($errors->has($name)) text-neg @endif">
     <input type="checkbox" name="{{$name}}" value="{{ $value }}" @if($checked) checked="checked" @endif>
-    <span tabindex="{{ $tabindex ?? '0' }}"
-          role="checkbox"
+    <span role="checkbox"
           aria-checked="{{ $checked ? 'true' : 'false' }}"
           class="custom-checkbox text-primary">@icon('check')</span>
     <span class="label">{{$label}}</span>
