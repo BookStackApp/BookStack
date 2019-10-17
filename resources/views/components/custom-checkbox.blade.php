3959841dbca4449e32a3af4101008526486d220d
Dan Brown <ssddanbrown@googlemail.com>
2019-10-17 14:21:13 +0100

    Added back in some tabindex that shouldn't have been removed

--- resources/views/components/custom-checkbox.blade.php
+++ resources/views/components/custom-checkbox.blade.php
@@ -6,7 +6,7 @@ $label
 --}}
 <label custom-checkbox class="toggle-switch @if($errors->has($name)) text-neg @endif">
     <input type="checkbox" name="{{$name}}" value="{{ $value }}" @if($checked) checked="checked" @endif>
-    <span role="checkbox"
+    <span tabindex="0" role="checkbox"
           aria-checked="{{ $checked ? 'true' : 'false' }}"
           class="custom-checkbox text-primary">@icon('check')</span>
     <span class="label">{{$label}}</span>
