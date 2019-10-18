8964cd10bd30531ab07b1fab79cdb3246c3489f9
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 14:17:57 +0100

    New translations activities.php (German Informal)

--- resources/lang/de_informal/activities.php
+++ resources/lang/de_informal/activities.php
@@ -1,6 +1,48 @@
 <?php
-
-// Extends 'de'
+/**
+ * Activity text strings.
+ * Is used for all the text within activity logs & notifications.
+ */
 return [
-    //
+
+    // Pages
+    'page_create'                 => '',
+    'page_create_notification'    => '',
+    'page_update'                 => '',
+    'page_update_notification'    => '',
+    'page_delete'                 => '',
+    'page_delete_notification'    => '',
+    'page_restore'                => '',
+    'page_restore_notification'   => '',
+    'page_move'                   => '',
+
+    // Chapters
+    'chapter_create'              => '',
+    'chapter_create_notification' => '',
+    'chapter_update'              => '',
+    'chapter_update_notification' => '',
+    'chapter_delete'              => '',
+    'chapter_delete_notification' => '',
+    'chapter_move'                => '',
+
+    // Books
+    'book_create'                 => '',
+    'book_create_notification'    => '',
+    'book_update'                 => '',
+    'book_update_notification'    => '',
+    'book_delete'                 => '',
+    'book_delete_notification'    => '',
+    'book_sort'                   => '',
+    'book_sort_notification'      => '',
+
+    // Bookshelves
+    'bookshelf_create'            => '',
+    'bookshelf_create_notification'    => '',
+    'bookshelf_update'                 => '',
+    'bookshelf_update_notification'    => '',
+    'bookshelf_delete'                 => '',
+    'bookshelf_delete_notification'    => '',
+
+    // Other
+    'commented_on'                => '',
 ];
