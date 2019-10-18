f9d77433d39f9f841df5f2974d1873b7044c26d4
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:03:33 +0100

    New translations components.php (Swedish)

---
+++ sv/components.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Text used in custom JavaScript driven components.
+ */
+return [
+
+    // Image Manager
+    'image_select' => '',
+    'image_all' => '',
+    'image_all_title' => '',
+    'image_book_title' => '',
+    'image_page_title' => '',
+    'image_search_hint' => '',
+    'image_uploaded' => '',
+    'image_load_more' => '',
+    'image_image_name' => '',
+    'image_delete_used' => '',
+    'image_delete_confirm' => '',
+    'image_select_image' => '',
+    'image_dropzone' => '',
+    'images_deleted' => '',
+    'image_preview' => '',
+    'image_upload_success' => '',
+    'image_update_success' => '',
+    'image_delete_success' => '',
+    'image_upload_remove' => '',
+
+    // Code Editor
+    'code_editor' => '',
+    'code_language' => '',
+    'code_content' => '',
+    'code_save' => '',
+];
