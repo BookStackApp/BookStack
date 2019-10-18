7a099ea56ec57206c23686cc05b3a2bed65136e4
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 14:16:30 +0100

    New translations auth.php (French)

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -1,67 +1,56 @@
 <?php
+/**
+ * Authentication Language Lines
+ * The following language lines are used during authentication for various
+ * messages that we need to display to the user.
+ */
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-    'failed' => 'Ces informations ne correspondent à aucun compte.',
-    'throttle' => "Trop d'essais, veuillez réessayer dans :seconds secondes.",
 
-    /**
-     * Login & Register
-     */
-    'sign_up' => "S'inscrire",
+    'failed' => 'Ces informations ne correspondent à aucun compte.',
+    'throttle' => 'Trop d\'essais, veuillez réessayer dans :seconds secondes.',
+
+    // Login & Register
+    'sign_up' => 'S\'inscrire',
     'log_in' => 'Se connecter',
     'log_in_with' => 'Se connecter avec :socialDriver',
     'sign_up_with' => 'S\'inscrire avec :socialDriver',
     'logout' => 'Se déconnecter',
 
     'name' => 'Nom',
-    'username' => "Nom d'utilisateur",
+    'username' => 'Nom d\'utilisateur',
     'email' => 'E-mail',
     'password' => 'Mot de passe',
     'password_confirm' => 'Confirmez le mot de passe',
     'password_hint' => 'Doit faire plus de 7 caractères',
     'forgot_password' => 'Mot de passe oublié ?',
     'remember_me' => 'Se souvenir de moi',
-    'ldap_email_hint' => "Merci d'entrer une adresse e-mail pour ce compte.",
+    'ldap_email_hint' => 'Merci d\'entrer une adresse e-mail pour ce compte.',
     'create_account' => 'Créer un compte',
     'already_have_account' => 'Vous avez déjà un compte ?',
     'dont_have_account' => 'Vous n\'avez pas de compte ?',
     'social_login' => 'Connexion avec un réseau social',
     'social_registration' => 'Inscription avec un réseau social',
-    'social_registration_text' => "S'inscrire et se connecter avec un réseau social.",
+    'social_registration_text' => 'S\'inscrire et se connecter avec un réseau social.',
 
     'register_thanks' => 'Merci pour votre inscription !',
     'register_confirm' => 'Vérifiez vos e-mails et cliquez sur le lien de confirmation pour rejoindre :appName.',
-    'registrations_disabled' => "Les inscriptions sont désactivées pour le moment",
+    'registrations_disabled' => 'Les inscriptions sont désactivées pour le moment',
     'registration_email_domain_invalid' => 'Cette adresse e-mail ne peut pas accéder à l\'application',
     'register_success' => 'Merci pour votre inscription. Vous êtes maintenant inscrit(e) et connecté(e)',
 
 
-    /**
-     * Password Reset
-     */
+    // Password Reset
     'reset_password' => 'Réinitialiser le mot de passe',
     'reset_password_send_instructions' => 'Entrez votre adresse e-mail ci-dessous et un e-mail avec un lien de réinitialisation de mot de passe vous sera envoyé.',
     'reset_password_send_button' => 'Envoyer un lien de réinitialisation',
     'reset_password_sent_success' => 'Un lien de réinitialisation a été envoyé à :email.',
     'reset_password_success' => 'Votre mot de passe a été réinitialisé avec succès.',
-
     'email_reset_subject' => 'Réinitialisez votre mot de passe pour :appName',
     'email_reset_text' => 'Vous recevez cet e-mail parce que nous avons reçu une demande de réinitialisation pour votre compte.',
     'email_reset_not_requested' => 'Si vous n\'avez pas effectué cette demande, vous pouvez ignorer cet e-mail.',
 
 
-    /**
-     * Email Confirmation
-     */
+    // Email Confirmation
     'email_confirm_subject' => 'Confirmez votre adresse e-mail pour :appName',
     'email_confirm_greeting' => 'Merci d\'avoir rejoint :appName !',
     'email_confirm_text' => 'Merci de confirmer en cliquant sur le lien ci-dessous :',
