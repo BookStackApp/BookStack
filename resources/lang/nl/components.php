71ff3f60d2bb3776c8595259e027f96ba1728b86
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 14:20:16 +0100

    New translations components.php (Dutch)

--- resources/lang/nl/components.php
+++ resources/lang/nl/components.php
@@ -1,16 +1,17 @@
 <?php
+/**
+ * Text used in custom JavaScript driven components.
+ */
 return [
 
-    /**
-     * Image Manager
-     */
+    // Image Manager
     'image_select' => 'Selecteer Afbeelding',
     'image_all' => 'Alles',
     'image_all_title' => 'Alle afbeeldingen weergeven',
     'image_book_title' => 'Afbeeldingen van dit boek weergeven',
     'image_page_title' => 'Afbeeldingen van deze pagina weergeven',
     'image_search_hint' => 'Zoek op afbeeldingsnaam',
-    'image_uploaded' => 'Uploaded :uploadedDate',
+    'image_uploaded' => '',
     'image_load_more' => 'Meer Laden',
     'image_image_name' => 'Afbeeldingsnaam',
     'image_delete_used' => 'Deze afbeeldingen is op onderstaande pagina\'s in gebruik.',
@@ -22,9 +23,9 @@ return [
     'image_upload_success' => 'Afbeelding succesvol geüpload',
     'image_update_success' => 'Afbeeldingsdetails succesvol verwijderd',
     'image_delete_success' => 'Afbeelding succesvol verwijderd',
-    /**
-     * Code editor
-     */
+    'image_upload_remove' => '',
+
+    // Code Editor
     'code_editor' => 'Code invoegen',
     'code_language' => 'Code taal',
     'code_content' => 'Code',
