c07a18fd227c410961505b0a9adf9a603fe7710b
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:03:13 +0100

    New translations passwords.php (Japanese)

---
+++ ja/passwords.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Password Reminder Language Lines
+ * The following language lines are the default lines which match reasons
+ * that are given by the password broker for a password update attempt has failed.
+ */
+return [
+
+    'password' => '',
+    'user' => "",
+    'token' => '',
+    'sent' => '',
+    'reset' => '',
+
+];
