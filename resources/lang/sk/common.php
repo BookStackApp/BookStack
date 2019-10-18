91069aa9b9f0b3a87cb2036918e419ef0535ab84
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 14:18:26 +0100

    New translations common.php (Slovak)

--- resources/lang/sk/common.php
+++ resources/lang/sk/common.php
@@ -1,60 +1,76 @@
 <?php
+/**
+ * Common elements found throughout many areas of BookStack.
+ */
 return [
 
-    /**
-     * Buttons
-     */
+    // Buttons
     'cancel' => 'Zrušiť',
     'confirm' => 'Potvrdiť',
     'back' => 'Späť',
     'save' => 'Uložiť',
     'continue' => 'Pokračovať',
     'select' => 'Vybrať',
+    'toggle_all' => '',
+    'more' => '',
 
-    /**
-     * Form Labels
-     */
+    // Form Labels
     'name' => 'Meno',
     'description' => 'Popis',
     'role' => 'Rola',
     'cover_image' => 'Obal knihy',
     'cover_image_description' => 'Tento obrázok by mal byť približne 300 x 170 pixelov.',
-    /**
-     * Actions
-     */
+    
+    // Actions
     'actions' => 'Akcie',
     'view' => 'Zobraziť',
+    'view_all' => '',
     'create' => 'Vytvoriť',
     'update' => 'Aktualizovať',
     'edit' => 'Editovať',
     'sort' => 'Zoradiť',
     'move' => 'Presunúť',
+    'copy' => '',
+    'reply' => '',
     'delete' => 'Zmazať',
     'search' => 'Hľadť',
     'search_clear' => 'Vyčistiť hľadanie',
-    'reset' => 'Reset',
+    'reset' => '',
     'remove' => 'Odstrániť',
+    'add' => '',
 
+    // Sort Options
+    'sort_options' => '',
+    'sort_direction_toggle' => '',
+    'sort_ascending' => '',
+    'sort_descending' => '',
+    'sort_name' => '',
+    'sort_created_at' => '',
+    'sort_updated_at' => '',
 
-    /**
-     * Misc
-     */
+    // Misc
     'deleted_user' => 'Odstránený používateľ',
     'no_activity' => 'Žiadna aktivita na zobrazenie',
     'no_items' => 'Žiadne položky nie sú dostupné',
     'back_to_top' => 'Späť nahor',
     'toggle_details' => 'Prepnúť detaily',
     'toggle_thumbnails' => 'Prepnúť náhľady',
+    'details' => '',
+    'grid_view' => '',
+    'list_view' => '',
+    'default' => '',
+    'breadcrumb' => '',
 
-    /**
-     * Header
-     */
+    // Header
+    'profile_menu' => '',
     'view_profile' => 'Zobraziť profil',
     'edit_profile' => 'Upraviť profil',
 
-    /**
-     * Email Content
-     */
+    // Layout tabs
+    'tab_info' => '',
+    'tab_content' => '',
+
+    // Email Content
     'email_action_help' => 'Ak máte problém klinkúť na tlačidlo ":actionText", skopírujte a vložte URL uvedenú nižšie do Vášho prehliadača:',
     'email_rights' => 'Všetky práva vyhradené',
 ];
