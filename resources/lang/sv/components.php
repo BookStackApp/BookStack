dd62c554df7c1b53110da2c09dfb2d515ce251f5
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:10:25 +0100

    New translations components.php (Swedish)

--- resources/lang/sv/components.php
+++ resources/lang/sv/components.php
@@ -1,34 +1,33 @@
 <?php
+/**
+ * Text used in custom JavaScript driven components.
+ */
 return [
 
-    /**
-     * Image Manager
-     */
-    'image_select' => 'Val av bild',
-    'image_all' => 'Alla',
-    'image_all_title' => 'Visa alla bilder',
-    'image_book_title' => 'Visa bilder som laddats upp till den aktuella boken',
-    'image_page_title' => 'Visa bilder som laddats upp till den aktuella sidan',
-    'image_search_hint' => 'Sök efter bildens namn',
-    'image_uploaded' => 'Laddades upp :uploadedDate',
-    'image_load_more' => 'Ladda fler',
-    'image_image_name' => 'Bildnamn',
-    'image_delete_used' => 'Den här bilden används på nedanstående sidor.',
-    'image_delete_confirm' => 'Klicka på "ta bort" en gång till för att bekräfta att du vill ta bort bilden.',
-    'image_select_image' => 'Välj bild',
-    'image_dropzone' => 'Släpp bilder här eller klicka för att ladda upp',
-    'images_deleted' => 'Bilder borttagna',
-    'image_preview' => 'Förhandsgranskning',
-    'image_upload_success' => 'Bilden har laddats upp',
-    'image_update_success' => 'Bildens uppgifter har ändrats',
-    'image_delete_success' => 'Bilden har tagits bort',
-    'image_upload_remove' => 'Radera',
+    // Image Manager
+    'image_select' => '',
+    'image_all' => '',
+    'image_all_title' => '',
+    'image_book_title' => '',
+    'image_page_title' => '',
+    'image_search_hint' => '',
+    'image_uploaded' => '',
+    'image_load_more' => '',
+    'image_image_name' => '',
+    'image_delete_used' => '',
+    'image_delete_confirm' => '',
+    'image_select_image' => '',
+    'image_dropzone' => '',
+    'images_deleted' => '',
+    'image_preview' => '',
+    'image_upload_success' => '',
+    'image_update_success' => '',
+    'image_delete_success' => '',
+    'image_upload_remove' => '',
 
-    /**
-     * Code editor
-     */
-    'code_editor' => 'Redigera kod',
-    'code_language' => 'Språk',
-    'code_content' => 'Kod',
-    'code_save' => 'Spara',
+    // Code Editor
+    'code_editor' => '',
+    'code_language' => '',
+    'code_content' => '',
+    'code_save' => '',
 ];
