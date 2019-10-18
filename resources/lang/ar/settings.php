e322d8d562f01ee0c36d0391cc320c9ef35b6194
Dan Brown <ssddanbrown@googlemail.com>
2019-10-19 00:06:35 +0100

    New translations settings.php (Arabic)

--- resources/lang/ar/settings.php
+++ resources/lang/ar/settings.php
@@ -135,9 +135,8 @@ return [
     'users_social_connected' => 'تم ربط حساب :socialAccount بملفك بنجاح.',
     'users_social_disconnected' => 'تم فصل حساب :socialAccount من ملفك بنجاح.',
 
-    //! Since these labels are already localized this array does not need to be
-    //! translated in the language-specific files.
-    //! DELETE BELOW IF COPIED FROM EN
+    //! If editing translations files directly please ignore this in all
+    //! languages apart from en. Content will be auto-copied from en.
     //!////////////////////////////////
     'language_select' => [
         'en' => 'English',
