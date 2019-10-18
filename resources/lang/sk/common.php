81f86d2b3e6cebadb72bbb14efe3fb45fe0d67b5
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:10:46 +0100

    New translations common.php (Slovak)

--- resources/lang/sk/common.php
+++ resources/lang/sk/common.php
@@ -1,60 +1,76 @@
 <?php
+/**
+ * Common elements found throughout many areas of BookStack.
+ */
 return [
 
-    /**
-     * Buttons
-     */
-    'cancel' => 'Zrušiť',
-    'confirm' => 'Potvrdiť',
-    'back' => 'Späť',
-    'save' => 'Uložiť',
-    'continue' => 'Pokračovať',
-    'select' => 'Vybrať',
+    // Buttons
+    'cancel' => '',
+    'confirm' => '',
+    'back' => '',
+    'save' => '',
+    'continue' => '',
+    'select' => '',
+    'toggle_all' => '',
+    'more' => '',
 
-    /**
-     * Form Labels
-     */
-    'name' => 'Meno',
-    'description' => 'Popis',
-    'role' => 'Rola',
-    'cover_image' => 'Obal knihy',
-    'cover_image_description' => 'Tento obrázok by mal byť približne 300 x 170 pixelov.',
-    /**
-     * Actions
-     */
-    'actions' => 'Akcie',
-    'view' => 'Zobraziť',
-    'create' => 'Vytvoriť',
-    'update' => 'Aktualizovať',
-    'edit' => 'Editovať',
-    'sort' => 'Zoradiť',
-    'move' => 'Presunúť',
-    'delete' => 'Zmazať',
-    'search' => 'Hľadť',
-    'search_clear' => 'Vyčistiť hľadanie',
-    'reset' => 'Reset',
-    'remove' => 'Odstrániť',
+    // Form Labels
+    'name' => '',
+    'description' => '',
+    'role' => '',
+    'cover_image' => '',
+    'cover_image_description' => '',
+    
+    // Actions
+    'actions' => '',
+    'view' => '',
+    'view_all' => '',
+    'create' => '',
+    'update' => '',
+    'edit' => '',
+    'sort' => '',
+    'move' => '',
+    'copy' => '',
+    'reply' => '',
+    'delete' => '',
+    'search' => '',
+    'search_clear' => '',
+    'reset' => '',
+    'remove' => '',
+    'add' => '',
 
+    // Sort Options
+    'sort_options' => '',
+    'sort_direction_toggle' => '',
+    'sort_ascending' => '',
+    'sort_descending' => '',
+    'sort_name' => '',
+    'sort_created_at' => '',
+    'sort_updated_at' => '',
 
-    /**
-     * Misc
-     */
-    'deleted_user' => 'Odstránený používateľ',
-    'no_activity' => 'Žiadna aktivita na zobrazenie',
-    'no_items' => 'Žiadne položky nie sú dostupné',
-    'back_to_top' => 'Späť nahor',
-    'toggle_details' => 'Prepnúť detaily',
-    'toggle_thumbnails' => 'Prepnúť náhľady',
+    // Misc
+    'deleted_user' => '',
+    'no_activity' => '',
+    'no_items' => '',
+    'back_to_top' => '',
+    'toggle_details' => '',
+    'toggle_thumbnails' => '',
+    'details' => '',
+    'grid_view' => '',
+    'list_view' => '',
+    'default' => '',
+    'breadcrumb' => '',
 
-    /**
-     * Header
-     */
-    'view_profile' => 'Zobraziť profil',
-    'edit_profile' => 'Upraviť profil',
+    // Header
+    'profile_menu' => '',
+    'view_profile' => '',
+    'edit_profile' => '',
 
-    /**
-     * Email Content
-     */
-    'email_action_help' => 'Ak máte problém klinkúť na tlačidlo ":actionText", skopírujte a vložte URL uvedenú nižšie do Vášho prehliadača:',
-    'email_rights' => 'Všetky práva vyhradené',
+    // Layout tabs
+    'tab_info' => '',
+    'tab_content' => '',
+
+    // Email Content
+    'email_action_help' => '',
+    'email_rights' => '',
 ];
