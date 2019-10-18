340a46f9035574c51fd17888188f98a67b01dec0
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:11:06 +0100

    New translations activities.php (Spanish, Argentina)

--- resources/lang/es_AR/activities.php
+++ resources/lang/es_AR/activities.php
@@ -2,50 +2,47 @@
 /**
  * Activity text strings.
  * Is used for all the text within activity logs & notifications.
- * 
- * Cadenas de texto de Actividades
- * Se usa para todos los textos dentro de los registros de actividad y notificaciones.
  */
 return [
 
     // Pages
-    'page_create'                 => 'página creada',
-    'page_create_notification'    => 'Página creada exitosamente',
-    'page_update'                 => 'página actualizada',
-    'page_update_notification'    => 'Página actualizada exitosamente',
-    'page_delete'                 => 'página borrada',
-    'page_delete_notification'    => 'Página borrada exitosamente',
-    'page_restore'                => 'página restaurada',
-    'page_restore_notification'   => 'Página restaurada exitosamente',
-    'page_move'                   => 'página movida',
+    'page_create'                 => '',
+    'page_create_notification'    => '',
+    'page_update'                 => '',
+    'page_update_notification'    => '',
+    'page_delete'                 => '',
+    'page_delete_notification'    => '',
+    'page_restore'                => '',
+    'page_restore_notification'   => '',
+    'page_move'                   => '',
 
     // Chapters
-    'chapter_create'              => 'capítulo creado',
-    'chapter_create_notification' => 'Capítulo creado exitosamente',
-    'chapter_update'              => 'capítulo actualizado',
-    'chapter_update_notification' => 'Capítulo actualizado exitosamente',
-    'chapter_delete'              => 'capítulo borrado',
-    'chapter_delete_notification' => 'Capítulo borrado exitosamente',
-    'chapter_move'                => 'capítulo movido',
+    'chapter_create'              => '',
+    'chapter_create_notification' => '',
+    'chapter_update'              => '',
+    'chapter_update_notification' => '',
+    'chapter_delete'              => '',
+    'chapter_delete_notification' => '',
+    'chapter_move'                => '',
 
     // Books
-    'book_create'                 => 'libro creado',
-    'book_create_notification'    => 'Libro creado exitosamente',
-    'book_update'                 => 'libro actualizado',
-    'book_update_notification'    => 'Libro actualizado exitosamente',
-    'book_delete'                 => 'libro borrado',
-    'book_delete_notification'    => 'Libro borrado exitosamente',
-    'book_sort'                   => 'libro ordenado',
-    'book_sort_notification'      => 'Libro reordenado exitosamente',
+    'book_create'                 => '',
+    'book_create_notification'    => '',
+    'book_update'                 => '',
+    'book_update_notification'    => '',
+    'book_delete'                 => '',
+    'book_delete_notification'    => '',
+    'book_sort'                   => '',
+    'book_sort_notification'      => '',
 
     // Bookshelves
-    'bookshelf_create'            => 'Estante creado',
-    'bookshelf_create_notification'    => 'Estante creado exitosamente',
-    'bookshelf_update'                 => 'Estante actualizado',
-    'bookshelf_update_notification'    => 'Estante actualizado exitosamente',
-    'bookshelf_delete'                 => 'Estante borrado',
-    'bookshelf_delete_notification'    => 'Estante borrado exitosamente',
+    'bookshelf_create'            => '',
+    'bookshelf_create_notification'    => '',
+    'bookshelf_update'                 => '',
+    'bookshelf_update_notification'    => '',
+    'bookshelf_delete'                 => '',
+    'bookshelf_delete_notification'    => '',
 
     // Other
-    'commented_on'                => 'comentado',
+    'commented_on'                => '',
 ];
