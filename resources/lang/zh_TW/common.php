73cf05eca5e50060f1d5ab4afe8feca608787595
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:11:51 +0100

    New translations common.php (Chinese Traditional)

--- resources/lang/zh_TW/common.php
+++ resources/lang/zh_TW/common.php
@@ -1,68 +1,76 @@
 <?php
+/**
+ * Common elements found throughout many areas of BookStack.
+ */
 return [
 
-    /**
-     * Buttons
-     */
-    'cancel' => '取消',
-    'confirm' => '確認',
-    'back' => '返回',
-    'save' => '儲存',
-    'continue' => '繼續',
-    'select' => '選擇',
-    'more' => '更多',
+    // Buttons
+    'cancel' => '',
+    'confirm' => '',
+    'back' => '',
+    'save' => '',
+    'continue' => '',
+    'select' => '',
+    'toggle_all' => '',
+    'more' => '',
 
-    /**
-     * Form Labels
-     */
-    'name' => '名稱',
-    'description' => '摘要',
-    'role' => '角色',
-    'cover_image' => '封面圖片',
-    'cover_image_description' => '所使用圖片大小必須是440x250px。',
+    // Form Labels
+    'name' => '',
+    'description' => '',
+    'role' => '',
+    'cover_image' => '',
+    'cover_image_description' => '',
+    
+    // Actions
+    'actions' => '',
+    'view' => '',
+    'view_all' => '',
+    'create' => '',
+    'update' => '',
+    'edit' => '',
+    'sort' => '',
+    'move' => '',
+    'copy' => '',
+    'reply' => '',
+    'delete' => '',
+    'search' => '',
+    'search_clear' => '',
+    'reset' => '',
+    'remove' => '',
+    'add' => '',
 
-    /**
-     * Actions
-     */
-    'actions' => '動作',
-    'view' => '檢視',
-    'create' => '建立',
-    'update' => '更新',
-    'edit' => '編輯',
-    'sort' => '排序',
-    'move' => '移動',
-    'copy' => '複製',
-    'reply' => '回覆',
-    'delete' => '刪除',
-    'search' => '搜尋',
-    'search_clear' => '清除搜尋',
-    'reset' => '重置',
-    'remove' => '刪除',
-    'add' => '新增',
+    // Sort Options
+    'sort_options' => '',
+    'sort_direction_toggle' => '',
+    'sort_ascending' => '',
+    'sort_descending' => '',
+    'sort_name' => '',
+    'sort_created_at' => '',
+    'sort_updated_at' => '',
 
-    /**
-     * Misc
-     */
-    'deleted_user' => '刪除使用者',
-    'no_activity' => '無活動',
-    'no_items' => '無項目',
-    'back_to_top' => '回到頂端',
-    'toggle_details' => '顯示/隱藏詳細資訊',
-    'toggle_thumbnails' => '顯示/隱藏縮圖',
-    'details' => '詳細資訊',
-    'grid_view' => '縮圖檢視',
-    'list_view' => '清單撿視',
-    'default' => '預設',
+    // Misc
+    'deleted_user' => '',
+    'no_activity' => '',
+    'no_items' => '',
+    'back_to_top' => '',
+    'toggle_details' => '',
+    'toggle_thumbnails' => '',
+    'details' => '',
+    'grid_view' => '',
+    'list_view' => '',
+    'default' => '',
+    'breadcrumb' => '',
 
-    /**
-     * Header
-     */
-    'view_profile' => '檢視資料',
-    'edit_profile' => '編輯資料',
+    // Header
+    'profile_menu' => '',
+    'view_profile' => '',
+    'edit_profile' => '',
 
-    /**
-     * Email Content
-     */
-    'email_action_help' => '如果您無法點選“:actionText”按鈕，請將下面的網址複製到您的瀏覽器中打開：',
-    'email_rights' => 'All rights reserved',
+    // Layout tabs
+    'tab_info' => '',
+    'tab_content' => '',
+
+    // Email Content
+    'email_action_help' => '',
+    'email_rights' => '',
 ];
