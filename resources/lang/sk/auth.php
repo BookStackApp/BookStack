53aff229059787aff1ea7ce5f2b6ca4b9d8a6b1a
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:10:45 +0100

    New translations auth.php (Slovak)

--- resources/lang/sk/auth.php
+++ resources/lang/sk/auth.php
@@ -1,76 +1,77 @@
 <?php
+/**
+ * Authentication Language Lines
+ * The following language lines are used during authentication for various
+ * messages that we need to display to the user.
+ */
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-    'failed' => 'Tieto údaje nesedia s našimi záznamami.',
-    'throttle' => 'Priveľa pokusov o prihlásenie. Skúste znova o :seconds sekúnd.',
 
-    /**
-     * Login & Register
-     */
-    'sign_up' => 'Registrácia',
-    'log_in' => 'Prihlásenie',
-    'log_in_with' => 'Prihlásiť sa cez :socialDriver',
-    'sign_up_with' => 'Registrovať sa cez :socialDriver',
-    'logout' => 'Odhlásenie',
+    'failed' => '',
+    'throttle' => '',
 
-    'name' => 'Meno',
-    'username' => 'Používateľské meno',
-    'email' => 'Email',
-    'password' => 'Heslo',
-    'password_confirm' => 'Potvrdiť heslo',
-    'password_hint' => 'Musí mať viac ako 7 znakov',
-    'forgot_password' => 'Zabudli ste heslo?',
-    'remember_me' => 'Zapamätať si ma',
-    'ldap_email_hint' => 'Zadajte prosím email, ktorý sa má použiť pre tento účet.',
-    'create_account' => 'Vytvoriť účet',
-    'social_login' => 'Sociálne prihlásenie',
-    'social_registration' => 'Sociálna registrácia',
-    'social_registration_text' => 'Registrovať sa a prihlásiť sa použitím inej služby.',
+    // Login & Register
+    'sign_up' => '',
+    'log_in' => '',
+    'log_in_with' => '',
+    'sign_up_with' => '',
+    'logout' => '',
 
-    'register_thanks' => 'Ďakujeme zaregistráciu!',
-    'register_confirm' => 'Skontrolujte prosím svoj email a kliknite na potvrdzujúce tlačidlo pre prístup k :appName.',
-    'registrations_disabled' => 'Registrácie sú momentálne zablokované',
-    'registration_email_domain_invalid' => 'Táto emailová doména nemá prístup k tejto aplikácii',
-    'register_success' => 'Ďakujeme za registráciu! Teraz ste registrovaný a prihlásený.',
+    'name' => '',
+    'username' => '',
+    'email' => '',
+    'password' => '',
+    'password_confirm' => '',
+    'password_hint' => '',
+    'forgot_password' => '',
+    'remember_me' => '',
+    'ldap_email_hint' => '',
+    'create_account' => '',
+    'already_have_account' => '',
+    'dont_have_account' => '',
+    'social_login' => '',
+    'social_registration' => '',
+    'social_registration_text' => '',
+
+    'register_thanks' => '',
+    'register_confirm' => '',
+    'registrations_disabled' => '',
+    'registration_email_domain_invalid' => '',
+    'register_success' => '',
 
 
-    /**
-     * Password Reset
-     */
-    'reset_password' => 'Reset hesla',
-    'reset_password_send_instructions' => 'Zadajte svoj email nižšie a bude Vám odoslaný email s odkazom pre reset hesla.',
-    'reset_password_send_button' => 'Poslať odkaz na reset hesla',
-    'reset_password_sent_success' => 'Odkaz na reset hesla bol poslaný na :email.',
-    'reset_password_success' => 'Vaše heslo bolo úspešne resetované.',
-
-    'email_reset_subject' => 'Reset Vášho :appName hesla',
-    'email_reset_text' => 'Tento email Ste dostali pretože sme dostali požiadavku na reset hesla pre Váš účet.',
-    'email_reset_not_requested' => 'Ak ste nepožiadali o reset hesla, nemusíte nič robiť.',
+    // Password Reset
+    'reset_password' => '',
+    'reset_password_send_instructions' => '',
+    'reset_password_send_button' => '',
+    'reset_password_sent_success' => '',
+    'reset_password_success' => '',
+    'email_reset_subject' => '',
+    'email_reset_text' => '',
+    'email_reset_not_requested' => '',
 
 
-    /**
-     * Email Confirmation
-     */
-    'email_confirm_subject' => 'Potvrdiť email na :appName',
-    'email_confirm_greeting' => 'Ďakujeme za pridanie sa k :appName!',
-    'email_confirm_text' => 'Prosím potvrďte Vašu emailovú adresu kliknutím na tlačidlo nižšie:',
-    'email_confirm_action' => 'Potvrdiť email',
-    'email_confirm_send_error' => 'Je požadované overenie emailu, ale systém nemohol odoslať email. Kontaktujte administrátora by ste sa uistili, že email je nastavený správne.',
-    'email_confirm_success' => 'Váš email bol overený!',
-    'email_confirm_resent' => 'Potvrdzujúci email bol poslaný znovu, skontrolujte prosím svoju emailovú schránku.',
+    // Email Confirmation
+    'email_confirm_subject' => '',
+    'email_confirm_greeting' => '',
+    'email_confirm_text' => '',
+    'email_confirm_action' => '',
+    'email_confirm_send_error' => '',
+    'email_confirm_success' => '',
+    'email_confirm_resent' => '',
 
-    'email_not_confirmed' => 'Emailová adresa nebola overená',
-    'email_not_confirmed_text' => 'Vaša emailová adresa nebola zatiaľ overená.',
-    'email_not_confirmed_click_link' => 'Prosím, kliknite na odkaz v emaili, ktorý bol poslaný krátko po Vašej registrácii.',
-    'email_not_confirmed_resend' => 'Ak nemôžete násť email, môžete znova odoslať overovací email odoslaním doleuvedeného formulára.',
-    'email_not_confirmed_resend_button' => 'Znova odoslať overovací email',
+    'email_not_confirmed' => '',
+    'email_not_confirmed_text' => '',
+    'email_not_confirmed_click_link' => '',
+    'email_not_confirmed_resend' => '',
+    'email_not_confirmed_resend_button' => '',
+
+    // User Invite
+    'user_invite_email_subject' => '',
+    'user_invite_email_greeting' => '',
+    'user_invite_email_text' => '',
+    'user_invite_email_action' => '',
+    'user_invite_page_welcome' => '',
+    'user_invite_page_text' => '',
+    'user_invite_page_confirm_button' => '',
+    'user_invite_success' => ''
 ];
