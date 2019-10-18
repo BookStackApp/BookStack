00aee909f278ddbd66b83844208bb6234d3760a7
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:11:01 +0100

    New translations passwords.php (Portuguese, Brazilian)

--- resources/lang/pt_BR/passwords.php
+++ resources/lang/pt_BR/passwords.php
@@ -6,10 +6,10 @@
  */
 return [
 
-    'password' => 'Senhas devem ter ao menos 6 caraceres e combinar com os atributos mínimos para a senha.',
-    'user' => "Não pudemos encontrar um usuário com o e-mail fornecido.",
-    'token' => 'O token de reset de senha é inválido.',
-    'sent' => 'Enviamos para seu e-mail o link de reset de senha!',
-    'reset' => 'Sua senha foi resetada com sucesso!',
+    'password' => '',
+    'user' => "",
+    'token' => '',
+    'sent' => '',
+    'reset' => '',
 
 ];
