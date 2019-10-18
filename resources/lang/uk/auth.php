3a3ea696c76aac81e3da202425049f01ff5cc8a4
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:10:18 +0100

    New translations auth.php (Ukrainian)

--- resources/lang/uk/auth.php
+++ resources/lang/uk/auth.php
@@ -1,67 +1,77 @@
 <?php
 /**
  * Authentication Language Lines
- * The following language lines are used during authentication for various / Під час автентифікації використовуються наступні лінії мов для різних повідомлень
- * messages that we need to display to the user. / які нам потрібно показати користувачеві.
+ * The following language lines are used during authentication for various
+ * messages that we need to display to the user.
  */
 return [
 
-    'failed' => 'Цей обліковий запис не знайдено.',
-    'throttle' => 'Забагато спроб входу в систему. Будь ласка, спробуйте ще раз через :seconds секунд.',
+    'failed' => '',
+    'throttle' => '',
 
     // Login & Register
-    'sign_up' => 'Реєстрація',
-    'log_in' => 'Увійти',
-    'log_in_with' => 'Увійти з :socialDriver',
-    'sign_up_with' => 'Зареєструватись з :socialDriver',
-    'logout' => 'Вихід',
+    'sign_up' => '',
+    'log_in' => '',
+    'log_in_with' => '',
+    'sign_up_with' => '',
+    'logout' => '',
 
-    'name' => 'Ім\'я',
-    'username' => 'Логін',
-    'email' => 'Email',
-    'password' => 'Пароль',
-    'password_confirm' => 'Підтвердження пароля',
-    'password_hint' => 'Має бути більше 7 символів',
-    'forgot_password' => 'Забули пароль?',
-    'remember_me' => 'Запам’ятати мене',
-    'ldap_email_hint' => 'Введіть email для цього облікового запису.',
-    'create_account' => 'Створити обліковий запис',
-    'already_have_account' => 'Вже є обліковий запис?',
-    'dont_have_account' => 'Немає облікового запису?',
-    'social_login' => 'Вхід через соціальну мережу',
-    'social_registration' => 'Реєстрація через соціальну мережу',
-    'social_registration_text' => 'Реєстрація і вхід через інший сервіс',
+    'name' => '',
+    'username' => '',
+    'email' => '',
+    'password' => '',
+    'password_confirm' => '',
+    'password_hint' => '',
+    'forgot_password' => '',
+    'remember_me' => '',
+    'ldap_email_hint' => '',
+    'create_account' => '',
+    'already_have_account' => '',
+    'dont_have_account' => '',
+    'social_login' => '',
+    'social_registration' => '',
+    'social_registration_text' => '',
 
-    'register_thanks' => 'Дякуємо за реєстрацію!',
-    'register_confirm' => 'Будь ласка, перевірте свою електронну пошту та натисніть кнопку підтвердження, щоб отримати доступ до :appName.',
-    'registrations_disabled' => 'Реєстрацію вимкнено',
-    'registration_email_domain_invalid' => 'Цей домен електронної пошти заборонений для реєстрації',
-    'register_success' => 'Дякуємо за реєстрацію! Ви зареєстровані та ввійшли в систему.',
+    'register_thanks' => '',
+    'register_confirm' => '',
+    'registrations_disabled' => '',
+    'registration_email_domain_invalid' => '',
+    'register_success' => '',
 
 
     // Password Reset
-    'reset_password' => 'Скинути пароль',
-    'reset_password_send_instructions' => 'Введіть адресу електронної пошти нижче, і вам буде надіслано електронне повідомлення з посиланням на зміну пароля.',
-    'reset_password_send_button' => 'Надіслати посилання для скидання',
-    'reset_password_sent_success' => 'Посилання для скидання пароля було надіслано на :email.',
-    'reset_password_success' => 'Ваш пароль успішно скинуто.',
-    'email_reset_subject' => 'Скинути ваш пароль :appName',
-    'email_reset_text' => 'Ви отримали цей електронний лист, оскільки до нас надійшов запит на скидання пароля для вашого облікового запису.',
-    'email_reset_not_requested' => 'Якщо ви не надсилали запит на скидання пароля, подальші дії не потрібні.',
+    'reset_password' => '',
+    'reset_password_send_instructions' => '',
+    'reset_password_send_button' => '',
+    'reset_password_sent_success' => '',
+    'reset_password_success' => '',
+    'email_reset_subject' => '',
+    'email_reset_text' => '',
+    'email_reset_not_requested' => '',
 
 
     // Email Confirmation
-    'email_confirm_subject' => 'Підтвердьте свою електронну пошту на :appName',
-    'email_confirm_greeting' => 'Дякуємо, що приєдналися до :appName!',
-    'email_confirm_text' => 'Будь ласка, підтвердьте свою адресу електронної пошти, натиснувши кнопку нижче:',
-    'email_confirm_action' => 'Підтвердити Email',
-    'email_confirm_send_error' => 'Необхідно підтвердження електронною поштою, але система не змогла надіслати електронний лист. Зверніться до адміністратора, щоб правильно налаштувати електронну пошту.',
-    'email_confirm_success' => 'Ваш електронну адресу підтверджено!',
-    'email_confirm_resent' => 'Лист з підтвердженням надіслано, перевірте свою пошту.',
+    'email_confirm_subject' => '',
+    'email_confirm_greeting' => '',
+    'email_confirm_text' => '',
+    'email_confirm_action' => '',
+    'email_confirm_send_error' => '',
+    'email_confirm_success' => '',
+    'email_confirm_resent' => '',
 
-    'email_not_confirmed' => 'Адреса Email не підтверджена',
-    'email_not_confirmed_text' => 'Ваша електронна адреса ще не підтверджена.',
-    'email_not_confirmed_click_link' => 'Будь-ласка, натисніть на посилання в електронному листі, яке було надіслано після реєстрації.',
-    'email_not_confirmed_resend' => 'Якщо ви не можете знайти електронний лист, ви можете повторно надіслати підтвердження електронною поштою, на формі нижче.',
-    'email_not_confirmed_resend_button' => 'Повторне підтвердження електронної пошти',
+    'email_not_confirmed' => '',
+    'email_not_confirmed_text' => '',
+    'email_not_confirmed_click_link' => '',
+    'email_not_confirmed_resend' => '',
+    'email_not_confirmed_resend_button' => '',
+
+    // User Invite
+    'user_invite_email_subject' => '',
+    'user_invite_email_greeting' => '',
+    'user_invite_email_text' => '',
+    'user_invite_email_action' => '',
+    'user_invite_page_welcome' => '',
+    'user_invite_page_text' => '',
+    'user_invite_page_confirm_button' => '',
+    'user_invite_success' => ''
 ];
