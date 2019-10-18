608b02045e52b0bff4a5b5bd3eb0f3052436e545
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:10:27 +0100

    New translations passwords.php (Ukrainian)

--- resources/lang/uk/passwords.php
+++ resources/lang/uk/passwords.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Password Reminder Language Lines / Нагадування про пароль
+ * Password Reminder Language Lines
  * The following language lines are the default lines which match reasons
  * that are given by the password broker for a password update attempt has failed.
  */
 return [
 
-    'password' => 'Паролі повинні містити принаймні шість символів і відповідати підтвердженню.',
-    'user' => "Ми не можемо знайти користувача з цією адресою електронної пошти.",
-    'token' => 'Цей токен для скидання пароля недійсний.',
-    'sent' => 'Ми надіслали вам електронний лист із посиланням на скидання пароля!',
-    'reset' => 'Ваш пароль був скинутий!',
+    'password' => '',
+    'user' => "",
+    'token' => '',
+    'sent' => '',
+    'reset' => '',
 
 ];
