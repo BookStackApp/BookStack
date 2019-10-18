5589eef219365ebf4b2f2a2b39dfa53abd4f0e5c
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:11:39 +0100

    New translations common.php (Chinese Simplified)

--- resources/lang/zh_CN/common.php
+++ resources/lang/zh_CN/common.php
@@ -1,68 +1,76 @@
 <?php
+/**
+ * Common elements found throughout many areas of BookStack.
+ */
 return [
 
-    /**
-     * Buttons
-     */
-    'cancel' => '取消',
-    'confirm' => '确认',
-    'back' => '返回',
-    'save' => '保存',
-    'continue' => '继续',
-    'select' => '选择',
-    'more' => '更多',
+    // Buttons
+    'cancel' => '',
+    'confirm' => '',
+    'back' => '',
+    'save' => '',
+    'continue' => '',
+    'select' => '',
+    'toggle_all' => '',
+    'more' => '',
 
-    /**
-     * Form Labels
-     */
-    'name' => '名称',
-    'description' => '概要',
-    'role' => '角色',
-    'cover_image' => '封面图片',
-    'cover_image_description' => '该图像大小需要为440x250px。',
+    // Form Labels
+    'name' => '',
+    'description' => '',
+    'role' => '',
+    'cover_image' => '',
+    'cover_image_description' => '',
     
-    /**
-     * Actions
-     */
-    'actions' => '操作',
-    'view' => '浏览',
-    'create' => '创建',
-    'update' => '更新',
-    'edit' => '编辑',
-    'sort' => '排序',
-    'move' => '移动',
-   'copy' => '复制',
-    'reply' => '回复',
-    'delete' => '删除',
-    'search' => '搜索',
-    'search_clear' => '清除搜索',
-    'reset' => '重置',
-    'remove' => '删除',
-    'add' => '添加',
+    // Actions
+    'actions' => '',
+    'view' => '',
+    'view_all' => '',
+    'create' => '',
+    'update' => '',
+    'edit' => '',
+    'sort' => '',
+    'move' => '',
+    'copy' => '',
+    'reply' => '',
+    'delete' => '',
+    'search' => '',
+    'search_clear' => '',
+    'reset' => '',
+    'remove' => '',
+    'add' => '',
 
-    /**
-     * Misc
-     */
-    'deleted_user' => '删除用户',
-    'no_activity' => '没有活动要显示',
-    'no_items' => '没有可用的项目',
-    'back_to_top' => '回到顶部',
-    'toggle_details' => '显示/隐藏详细信息',
-    'toggle_thumbnails' => '显示/隐藏缩略图',
-    'details' => '详细信息',
-    'grid_view' => '网格视图',
-    'list_view' => '列表视图',
-    'default' => '默认',
-	
-    /**
-     * Header
-     */
-    'view_profile' => '查看资料',
-    'edit_profile' => '编辑资料',
+    // Sort Options
+    'sort_options' => '',
+    'sort_direction_toggle' => '',
+    'sort_ascending' => '',
+    'sort_descending' => '',
+    'sort_name' => '',
+    'sort_created_at' => '',
+    'sort_updated_at' => '',
 
-    /**
-     * Email Content
-     */
-    'email_action_help' => '如果您无法点击“:actionText”按钮，请将下面的网址复制到您的浏览器中打开：',
-    'email_rights' => 'All rights reserved',
+    // Misc
+    'deleted_user' => '',
+    'no_activity' => '',
+    'no_items' => '',
+    'back_to_top' => '',
+    'toggle_details' => '',
+    'toggle_thumbnails' => '',
+    'details' => '',
+    'grid_view' => '',
+    'list_view' => '',
+    'default' => '',
+    'breadcrumb' => '',
+
+    // Header
+    'profile_menu' => '',
+    'view_profile' => '',
+    'edit_profile' => '',
+
+    // Layout tabs
+    'tab_info' => '',
+    'tab_content' => '',
+
+    // Email Content
+    'email_action_help' => '',
+    'email_rights' => '',
 ];
