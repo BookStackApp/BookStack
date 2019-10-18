26765688067cbaaba63a067be2e889927c9ef94e
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:12:03 +0100

    New translations passwords.php (Polish)

--- resources/lang/pl/passwords.php
+++ resources/lang/pl/passwords.php
@@ -1,22 +1,15 @@
 <?php
-
+/**
+ * Password Reminder Language Lines
+ * The following language lines are the default lines which match reasons
+ * that are given by the password broker for a password update attempt has failed.
+ */
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reminder Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
-
-    'password' => 'Hasło musi zawierać co najmniej 6 znaków i być zgodne z powtórzeniem.',
-    'user' => "Nie znaleziono użytkownika o takim adresie e-mail.",
-    'token' => 'Ten token resetowania hasła jest nieprawidłowy.',
-    'sent' => 'Wysłaliśmy Ci link do resetowania hasła!',
-    'reset' => 'Twoje hasło zostało zresetowane!',
+    'password' => '',
+    'user' => "",
+    'token' => '',
+    'sent' => '',
+    'reset' => '',
 
 ];
