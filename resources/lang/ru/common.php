492091cc189335d9db3c20106cfa01d56eea61c7
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:10:35 +0100

    New translations common.php (Russian)

--- resources/lang/ru/common.php
+++ resources/lang/ru/common.php
@@ -4,73 +4,73 @@
  */
 return [
 
-    //Buttons
-    'cancel' => 'Отмена',
-    'confirm' => 'Применить',
-    'back' => 'Назад',
-    'save' => 'Сохранить',
-    'continue' => 'Продолжить',
-    'select' => 'Выбрать',
-    'toggle_all' => 'Переключить все',
-    'more' => 'Еще',
+    // Buttons
+    'cancel' => '',
+    'confirm' => '',
+    'back' => '',
+    'save' => '',
+    'continue' => '',
+    'select' => '',
+    'toggle_all' => '',
+    'more' => '',
 
-    //Form Labels
-    'name' => 'Имя',
-    'description' => 'Описание',
-    'role' => 'Роль',
-    'cover_image' => 'Обложка',
-    'cover_image_description' => 'Изображение должно быть размером около 440x250px.',
-
-    //Actions
-    'actions' => 'Действия',
-    'view' => 'Просмотр',
-    'view_all' => 'Показать все',
-    'create' => 'Создание',
-    'update' => 'Обновление',
-    'edit' => 'Редактировать',
-    'sort' => 'Сортировать',
-    'move' => 'Переместить',
-    'copy' => 'Скопировать',
-    'reply' => 'Ответить',
-    'delete' => 'Удалить',
-    'search' => 'Поиск',
-    'search_clear' => 'Очистить поиск',
-    'reset' => 'Сбросить',
-    'remove' => 'Удалить',
-    'add' => 'Добавить',
+    // Form Labels
+    'name' => '',
+    'description' => '',
+    'role' => '',
+    'cover_image' => '',
+    'cover_image_description' => '',
     
+    // Actions
+    'actions' => '',
+    'view' => '',
+    'view_all' => '',
+    'create' => '',
+    'update' => '',
+    'edit' => '',
+    'sort' => '',
+    'move' => '',
+    'copy' => '',
+    'reply' => '',
+    'delete' => '',
+    'search' => '',
+    'search_clear' => '',
+    'reset' => '',
+    'remove' => '',
+    'add' => '',
+
     // Sort Options
-    'sort_options' => 'Параметры сортировки',
-    'sort_direction_toggle' => 'Переключить направления сортировки',
-    'sort_ascending' => 'По возрастанию',
-    'sort_descending' => 'По убыванию',
-    'sort_name' => 'По имени',
-    'sort_created_at' => 'По дате создания',
-    'sort_updated_at' => 'По дате обновления',
+    'sort_options' => '',
+    'sort_direction_toggle' => '',
+    'sort_ascending' => '',
+    'sort_descending' => '',
+    'sort_name' => '',
+    'sort_created_at' => '',
+    'sort_updated_at' => '',
 
-    //Misc
-    'deleted_user' => 'Удаленный пользователь',
-    'no_activity' => 'Нет действий для просмотра',
-    'no_items' => 'Нет доступных элементов',
-    'back_to_top' => 'Наверх',
-    'toggle_details' => 'Подробности',
-    'toggle_thumbnails' => 'Миниатюры',
-    'details' => 'Детали',
-    'grid_view' => 'Вид сеткой',
-    'list_view' => 'Вид списком',
-    'default' => 'По умолчанию',
-    'breadcrumb' => 'Навигация',
+    // Misc
+    'deleted_user' => '',
+    'no_activity' => '',
+    'no_items' => '',
+    'back_to_top' => '',
+    'toggle_details' => '',
+    'toggle_thumbnails' => '',
+    'details' => '',
+    'grid_view' => '',
+    'list_view' => '',
+    'default' => '',
+    'breadcrumb' => '',
 
-    //Header
-    'profile_menu' => 'Меню профиля',
-    'view_profile' => 'Просмотреть профиль',
-    'edit_profile' => 'Редактировать профиль',
-    
+    // Header
+    'profile_menu' => '',
+    'view_profile' => '',
+    'edit_profile' => '',
+
     // Layout tabs
-    'tab_info' => 'Информация',
-    'tab_content' => 'Содержание',
+    'tab_info' => '',
+    'tab_content' => '',
 
-    //Email Content
-    'email_action_help' => 'Если у вас возникли проблемы с нажатием кнопки \':actionText\', то скопируйте и вставьте указанный URL-адрес в свой веб-браузер:',
-    'email_rights' => 'Все права зарезервированы',
+    // Email Content
+    'email_action_help' => '',
+    'email_rights' => '',
 ];
