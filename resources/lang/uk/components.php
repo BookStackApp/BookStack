5bcfffe5c214fc3eb39314572ac62182d59a7e61
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:10:21 +0100

    New translations components.php (Ukrainian)

--- resources/lang/uk/components.php
+++ resources/lang/uk/components.php
@@ -1,33 +1,33 @@
 <?php
 /**
- * Text used in custom JavaScript driven components. / Текст використовується в індивідуальних компонентах, керованих JavaScript.
+ * Text used in custom JavaScript driven components.
  */
 return [
 
     // Image Manager
-    'image_select' => 'Вибрати зображення',
-    'image_all' => 'Всі',
-    'image_all_title' => 'Переглянути всі зображення',
-    'image_book_title' => 'Переглянути зображення, завантажені в цю книгу',
-    'image_page_title' => 'Переглянути зображення, завантажені на цю сторінку',
-    'image_search_hint' => 'Пошук по імені зображення',
-    'image_uploaded' => 'Завантажено :uploadedDate',
-    'image_load_more' => 'Завантажити ще',
-    'image_image_name' => 'Назва зображення',
-    'image_delete_used' => 'Це зображення використовується на наступних сторінках.',
-    'image_delete_confirm' => 'Натисніть кнопку Видалити ще раз, щоб підтвердити, що хочете видалити це зображення.',
-    'image_select_image' => 'Вибрати зображення',
-    'image_dropzone' => 'Перетягніть зображення, або натисніть тут для завантаження',
-    'images_deleted' => 'Зображень видалено',
-    'image_preview' => 'Попередній перегляд зображення',
-    'image_upload_success' => 'Зображення завантажено успішно',
-    'image_update_success' => 'Деталі зображення успішно оновлені',
-    'image_delete_success' => 'Зображення успішно видалено',
-    'image_upload_remove' => 'Видалити',
+    'image_select' => '',
+    'image_all' => '',
+    'image_all_title' => '',
+    'image_book_title' => '',
+    'image_page_title' => '',
+    'image_search_hint' => '',
+    'image_uploaded' => '',
+    'image_load_more' => '',
+    'image_image_name' => '',
+    'image_delete_used' => '',
+    'image_delete_confirm' => '',
+    'image_select_image' => '',
+    'image_dropzone' => '',
+    'images_deleted' => '',
+    'image_preview' => '',
+    'image_upload_success' => '',
+    'image_update_success' => '',
+    'image_delete_success' => '',
+    'image_upload_remove' => '',
 
     // Code Editor
-    'code_editor' => 'Редагувати код',
-    'code_language' => 'Мова коду',
-    'code_content' => 'Вміст коду',
-    'code_save' => 'Зберегти Код',
+    'code_editor' => '',
+    'code_language' => '',
+    'code_content' => '',
+    'code_save' => '',
 ];
