893b66eb22ddbd4f0f0eb99dbb212cfbc9085347
Dan Brown <ssddanbrown@googlemail.com>
2019-10-19 00:06:13 +0100

    New translations settings.php (Chinese Traditional)

--- resources/lang/zh_TW/settings.php
+++ resources/lang/zh_TW/settings.php
@@ -135,9 +135,8 @@ return [
     'users_social_connected' => ':socialAccount 帳號已經成功連結到您的資料。',
     'users_social_disconnected' => ':socialAccount 帳號已經成功解除連結。',
 
-    //! Since these labels are already localized this array does not need to be
-    //! translated in the language-specific files.
-    //! DELETE BELOW IF COPIED FROM EN
+    //! If editing translations files directly please ignore this in all
+    //! languages apart from en. Content will be auto-copied from en.
     //!////////////////////////////////
     'language_select' => [
         'en' => 'English',
