30fd66cd9a6542bc99858bb7523ffcb1c9564723
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:10:34 +0100

    New translations auth.php (Russian)

--- resources/lang/ru/auth.php
+++ resources/lang/ru/auth.php
@@ -5,73 +5,73 @@
  * messages that we need to display to the user.
  */
 return [
-    
-    'failed' => 'Учетная запись не найдена.',
-    'throttle' => 'Слишком много попыток входа. Пожалуйста, попробуйте позже через :seconds секунд.',
+
+    'failed' => '',
+    'throttle' => '',
 
     // Login & Register
-    'sign_up' => 'Регистрация',
-    'log_in' => 'Вход',
-    'log_in_with' => 'Вход с :socialDriver',
-    'sign_up_with' => 'Регистрация с :socialDriver',
-    'logout' => 'Выход',
+    'sign_up' => '',
+    'log_in' => '',
+    'log_in_with' => '',
+    'sign_up_with' => '',
+    'logout' => '',
 
-    'name' => 'Имя',
-    'username' => 'Логин',
-    'email' => 'Email',
-    'password' => 'Пароль',
-    'password_confirm' => 'Подтверждение пароля',
-    'password_hint' => 'Должен быть больше 7 символов',
-    'forgot_password' => 'Забыли пароль?',
-    'remember_me' => 'Запомнить меня',
-    'ldap_email_hint' => 'Введите email адрес для данной учетной записи.',
-    'create_account' => 'Создать аккаунт',
-    'already_have_account' => 'Уже есть аккаунт?',
-    'dont_have_account' => 'У вас нет аккаунта?',
-    'social_login' => 'Вход через Соцсеть',
-    'social_registration' => 'Регистрация через Соцсеть',
-    'social_registration_text' => 'Регистрация и вход через другой сервис.',
+    'name' => '',
+    'username' => '',
+    'email' => '',
+    'password' => '',
+    'password_confirm' => '',
+    'password_hint' => '',
+    'forgot_password' => '',
+    'remember_me' => '',
+    'ldap_email_hint' => '',
+    'create_account' => '',
+    'already_have_account' => '',
+    'dont_have_account' => '',
+    'social_login' => '',
+    'social_registration' => '',
+    'social_registration_text' => '',
 
-    'register_thanks' => 'Благодарим за регистрацию!',
-    'register_confirm' => 'Проверьте свою электронную почту и нажмите кнопку подтверждения для доступа к :appName.',
-    'registrations_disabled' => 'Регистрация отключена',
-    'registration_email_domain_invalid' => 'Данный домен электронной почты недоступен для регистрации',
-    'register_success' => 'Спасибо за регистрацию! Регистрация и вход в систему выполнены.',
-
-
-    //Password Reset
-    'reset_password' => 'Сброс пароля',
-    'reset_password_send_instructions' => 'Введите свой email ниже, и вам будет отправлено письмо со ссылкой для сброса пароля.',
-    'reset_password_send_button' => 'Отправить ссылку для сброса',
-    'reset_password_sent_success' => 'Ссылка для сброса была отправлена на :email.',
-    'reset_password_success' => 'Ваш пароль был успешно сброшен.',
-    'email_reset_subject' => 'Сбросить ваш :appName пароль',
-    'email_reset_text' => 'Вы получили это письмо, потому что запросили сброс пароля для вашей учетной записи.',
-    'email_reset_not_requested' => 'Если вы не запрашивали сброса пароля, то никаких дополнительных действий не требуется.',
+    'register_thanks' => '',
+    'register_confirm' => '',
+    'registrations_disabled' => '',
+    'registration_email_domain_invalid' => '',
+    'register_success' => '',
 
 
-    //Email Confirmation
-    'email_confirm_subject' => 'Подтвердите ваш почтовый адрес на :appName',
-    'email_confirm_greeting' => 'Благодарим за участие :appName!',
-    'email_confirm_text' => 'Пожалуйста, подтвердите ваш email адрес кликнув на кнопку ниже:',
-    'email_confirm_action' => 'Подтвердить email',
-    'email_confirm_send_error' => 'Требуется подтверждение электронной почты, но система не может отправить письмо. Свяжитесь с администратором, чтобы убедиться, что адрес электронной почты настроен правильно.',
-    'email_confirm_success' => 'Ваш email был подтвержден!',
-    'email_confirm_resent' => 'Письмо с подтверждение выслано снова. Пожалуйста, проверьте ваш почтовый ящик.',
+    // Password Reset
+    'reset_password' => '',
+    'reset_password_send_instructions' => '',
+    'reset_password_send_button' => '',
+    'reset_password_sent_success' => '',
+    'reset_password_success' => '',
+    'email_reset_subject' => '',
+    'email_reset_text' => '',
+    'email_reset_not_requested' => '',
 
-    'email_not_confirmed' => 'email не подтвержден',
-    'email_not_confirmed_text' => 'Ваш email адрес все еще не подтвержден.',
-    'email_not_confirmed_click_link' => 'Пожалуйста, нажмите на ссылку в письме, которое было отправлено при регистрации.',
-    'email_not_confirmed_resend' => 'Если вы не можете найти электронное письмо, вы можете снова отправить письмо с подтверждением по форме ниже.',
-    'email_not_confirmed_resend_button' => 'Переотправить письмо с подтверждением',
-    
+
+    // Email Confirmation
+    'email_confirm_subject' => '',
+    'email_confirm_greeting' => '',
+    'email_confirm_text' => '',
+    'email_confirm_action' => '',
+    'email_confirm_send_error' => '',
+    'email_confirm_success' => '',
+    'email_confirm_resent' => '',
+
+    'email_not_confirmed' => '',
+    'email_not_confirmed_text' => '',
+    'email_not_confirmed_click_link' => '',
+    'email_not_confirmed_resend' => '',
+    'email_not_confirmed_resend_button' => '',
+
     // User Invite
-    'user_invite_email_subject' => 'Вас приглашают присоединиться к :appName!',
-    'user_invite_email_greeting' => 'Для вас создан аккаунт в :appName.',
-    'user_invite_email_text' => 'Нажмите кнопку ниже, чтобы задать пароль и получить доступ:',
-    'user_invite_email_action' => 'Установить пароль аккаунту.',
-    'user_invite_page_welcome' => 'Добро пожаловать в :appName!',
-    'user_invite_page_text' => 'Завершите настройку аккаунта, установите пароль для дальнейшего входа в :appName.',
-    'user_invite_page_confirm_button' => 'Подтвердите пароль',
-    'user_invite_success' => 'Пароль установлен, теперь у вас есть доступ к :appName!'
+    'user_invite_email_subject' => '',
+    'user_invite_email_greeting' => '',
+    'user_invite_email_text' => '',
+    'user_invite_email_action' => '',
+    'user_invite_page_welcome' => '',
+    'user_invite_page_text' => '',
+    'user_invite_page_confirm_button' => '',
+    'user_invite_success' => ''
 ];
