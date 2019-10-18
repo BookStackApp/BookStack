7040a336a44a4cea5fb90cfe99819bb4bce172ce
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 14:16:49 +0100

    New translations passwords.php (French)

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -1,17 +1,10 @@
 <?php
-
+/**
+ * Password Reminder Language Lines
+ * The following language lines are the default lines which match reasons
+ * that are given by the password broker for a password update attempt has failed.
+ */
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reminder Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
 
     'password' => 'Les mots de passe doivent faire au moins 6 caractères et correspondre à la confirmation.',
     'user' => "Nous n'avons pas trouvé d'utilisateur avec cette adresse.",
