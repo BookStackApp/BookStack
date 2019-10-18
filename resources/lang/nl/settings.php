00d9f69caea396cce9a2e10205654f67f9d49712
Dan Brown <ssddanbrown@googlemail.com>
2019-10-19 00:06:25 +0100

    New translations settings.php (Dutch)

--- resources/lang/nl/settings.php
+++ resources/lang/nl/settings.php
@@ -135,9 +135,8 @@ return [
     'users_social_connected' => ':socialAccount account is succesvol aan je profiel gekoppeld.',
     'users_social_disconnected' => ':socialAccount account is succesvol ontkoppeld van je profiel.',
 
-    //! Since these labels are already localized this array does not need to be
-    //! translated in the language-specific files.
-    //! DELETE BELOW IF COPIED FROM EN
+    //! If editing translations files directly please ignore this in all
+    //! languages apart from en. Content will be auto-copied from en.
     //!////////////////////////////////
     'language_select' => [
         'en' => 'English',
