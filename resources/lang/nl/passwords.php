96b91cd63e7e254fdfc2b0348ce15cfc98503f61
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 14:20:10 +0100

    New translations passwords.php (Dutch)

--- resources/lang/nl/passwords.php
+++ resources/lang/nl/passwords.php
@@ -1,17 +1,10 @@
 <?php
-
+/**
+ * Password Reminder Language Lines
+ * The following language lines are the default lines which match reasons
+ * that are given by the password broker for a password update attempt has failed.
+ */
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reminder Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
 
     'password' => 'Wachtwoorden moeten overeenkomen en minimaal zes tekens lang zijn.',
     'user' => "We kunnen niemand vinden met dat e-mailadres.",
