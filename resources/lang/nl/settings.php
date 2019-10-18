6758a5c9d6bc3fcaa839b2cb67b07305ef89fd7c
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 14:19:54 +0100

    New translations settings.php (Dutch)

--- resources/lang/nl/settings.php
+++ resources/lang/nl/settings.php
@@ -1,56 +1,70 @@
 <?php
-
+/**
+ * Settings text strings
+ * Contains all text strings used in the general settings sections of BookStack
+ * including users and roles.
+ */
 return [
 
-    /**
-     * Settings text strings
-     * Contains all text strings used in the general settings sections of BookStack
-     * including users and roles.
-     */
-
+    // Common Messages
     'settings' => 'Instellingen',
     'settings_save' => 'Instellingen Opslaan',
     'settings_save_success' => 'Instellingen Opgeslagen',
 
-    /**
-     * App settings
-     */
-
-    'app_settings' => 'App Instellingen',
+    // App Settings
+    'app_customization' => '',
+    'app_features_security' => '',
     'app_name' => 'Applicatienaam',
     'app_name_desc' => 'De applicatienaam wordt in e-mails in in de header weergegeven.',
     'app_name_header' => 'Applicatienaam in de header weergeven?',
+    'app_public_access' => '',
+    'app_public_access_desc' => '',
+    'app_public_access_desc_guest' => '',
+    'app_public_access_toggle' => '',
     'app_public_viewing' => 'Publieke bewerkingen toestaan?',
     'app_secure_images' => 'Beter beveiligide afbeeldingen gebruiken?',
+    'app_secure_images_toggle' => '',
     'app_secure_images_desc' => 'Omwille van de performance zijn alle afbeeldingen publiek toegankelijk. Zorg ervoor dat je de \'directory index\' niet hebt ingeschakeld.',
     'app_editor' => 'Pagina Bewerken',
     'app_editor_desc' => 'Selecteer welke tekstverwerker je wilt gebruiken.',
     'app_custom_html' => 'Speciale HTML toevoegen',
     'app_custom_html_desc' => 'Alles wat je hier toevoegd wordt in de <head> sectie van elke pagina meengenomen. Dit kun je bijvoorbeeld voor analytics gebruiken.',
+    'app_custom_html_disabled_notice' => '',
     'app_logo' => 'Applicatielogo',
     'app_logo_desc' => 'De afbeelding moet 43px hoog zijn. <br>Grotere afbeeldingen worden geschaald.',
     'app_primary_color' => 'Applicatie hoofdkleur',
     'app_primary_color_desc' => 'Geef een hexadecimale waarde. <br>Als je niks invult wordt de standaardkleur gebruikt.',
+    'app_homepage' => '',
+    'app_homepage_desc' => '',
+    'app_homepage_select' => '',
     'app_disable_comments' => 'Reacties uitschakelen',
+    'app_disable_comments_toggle' => '',
     'app_disable_comments_desc' => 'Schakel opmerkingen uit op alle pagina\'s in de applicatie. Bestaande opmerkingen worden niet getoond.',
 
-    /**
-     * Registration settings
-     */
-
+    // Registration Settings
     'reg_settings' => 'Registratieinstellingen',
-    'reg_allow' => 'Registratie toestaan?',
+    'reg_enable' => '',
+    'reg_enable_toggle' => '',
+    'reg_enable_desc' => '',
     'reg_default_role' => 'Standaard rol na registratie',
-    'reg_confirm_email' => 'E-mailbevesting vereist?',
+    'reg_email_confirmation' => '',
+    'reg_email_confirmation_toggle' => '',
     'reg_confirm_email_desc' => 'Als domeinrestricties aan staan dan is altijd e-maibevestiging nodig. Onderstaande instelling wordt dan genegeerd.',
     'reg_confirm_restrict_domain' => 'Beperk registratie tot een maildomein',
     'reg_confirm_restrict_domain_desc' => 'Geen een komma-gescheiden lijst van domeinnamen die gebruikt mogen worden bij registratie. <br> Let op: na registratie kunnen gebruikers hun e-mailadres nog steeds wijzigen.',
     'reg_confirm_restrict_domain_placeholder' => 'Geen beperkingen ingesteld',
 
-    /**
-     * Role settings
-     */
+    // Maintenance settings
+    'maint' => '',
+    'maint_image_cleanup' => '',
+    'maint_image_cleanup_desc' => "",
+    'maint_image_cleanup_ignore_revisions' => '',
+    'maint_image_cleanup_run' => '',
+    'maint_image_cleanup_warning' => '',
+    'maint_image_cleanup_success' => '',
+    'maint_image_cleanup_nothing_found' => '',
 
+    // Role Settings
     'roles' => 'Rollen',
     'role_user_roles' => 'Gebruikrollen',
     'role_create' => 'Nieuwe Rol Maken',
@@ -65,14 +79,17 @@ return [
     'role_details' => 'Rol Details',
     'role_name' => 'Rolnaam',
     'role_desc' => 'Korte beschrijving van de rol',
+    'role_external_auth_id' => '',
     'role_system' => 'Systeem Permissies',
     'role_manage_users' => 'Gebruikers beheren',
     'role_manage_roles' => 'Rollen en rechten beheren',
     'role_manage_entity_permissions' => 'Beheer alle boeken-, hoofdstukken- en paginaresitrcties',
     'role_manage_own_entity_permissions' => 'Beheer restricties van je eigen boeken, hoofdstukken en pagina\'s',
+    'role_manage_page_templates' => '',
     'role_manage_settings' => 'Beheer app instellingen',
     'role_asset' => 'Asset Permissies',
     'role_asset_desc' => 'Deze permissies bepalen de standaardtoegangsrechten. Permissies op boeken, hoofdstukken en pagina\'s overschrijven deze instelling.',
+    'role_asset_admins' => '',
     'role_all' => 'Alles',
     'role_own' => 'Eigen',
     'role_controlled_by_asset' => 'Gecontroleerd door de asset waar deze is geüpload',
@@ -81,19 +98,24 @@ return [
     'role_users' => 'Gebruikers in deze rol',
     'role_users_none' => 'Geen enkele gebruiker heeft deze rol',
 
-    /**
-     * Users
-     */
-
+    // Users
     'users' => 'Gebruikers',
     'user_profile' => 'Gebruikersprofiel',
     'users_add_new' => 'Gebruiker toevoegen',
     'users_search' => 'Gebruiker zoeken',
+    'users_details' => '',
+    'users_details_desc' => '',
+    'users_details_desc_no_email' => '',
     'users_role' => 'Gebruikersrollen',
-    'users_external_auth_id' => 'External Authentication ID',
+    'users_role_desc' => '',
+    'users_password' => '',
+    'users_password_desc' => '',
+    'users_send_invite_text' => '',
+    'users_send_invite_option' => '',
+    'users_external_auth_id' => '',
+    'users_external_auth_id_desc' => '',
     'users_password_warning' => 'Vul onderstaande formulier alleen in als je het wachtwoord wilt aanpassen:',
     'users_system_public' => 'De eigenschappen van deze gebruiker worden voor elke gastbezoeker gebruikt. Er kan niet mee ingelogd worden en wordt automatisch toegewezen.',
-    'users_books_view_type' => 'Voorkeursuitleg voor het weergeven van boeken',
     'users_delete' => 'Verwijder gebruiker',
     'users_delete_named' => 'Verwijder gebruiker :userName',
     'users_delete_warning' => 'Dit zal de gebruiker \':userName\' volledig uit het systeem verwijderen.',
@@ -105,10 +127,41 @@ return [
     'users_avatar' => 'Avatar',
     'users_avatar_desc' => 'De afbeelding moet vierkant zijn en ongeveer 256px breed.',
     'users_preferred_language' => 'Voorkeurstaal',
-    'users_social_accounts' => 'Social Accounts',
+    'users_preferred_language_desc' => '',
+    'users_social_accounts' => '',
     'users_social_accounts_info' => 'Hier kun je accounts verbinden om makkelijker in te loggen. Via je profiel kun je ook weer rechten intrekken die bij deze social accountsh horen.',
     'users_social_connect' => 'Account Verbinden',
     'users_social_disconnect' => 'Account Ontkoppelen',
     'users_social_connected' => ':socialAccount account is succesvol aan je profiel gekoppeld.',
     'users_social_disconnected' => ':socialAccount account is succesvol ontkoppeld van je profiel.',
+
+    //! Since these labels are already localized this array does not need to be
+    //! translated in the language-specific files.
+    //! DELETE BELOW IF COPIED FROM EN
+    //!////////////////////////////////
+    'language_select' => [
+        'en' => '',
+        'ar' => '',
+        'de' => '',
+        'de_informal' => '',
+        'es' => '',
+        'es_AR' => '',
+        'fr' => '',
+        'nl' => '',
+        'pt_BR' => '',
+        'sk' => '',
+        'cs' => '',
+        'sv' => '',
+        'kr' => '',
+        'ja' => '',
+        'pl' => '',
+        'it' => '',
+        'ru' => '',
+        'uk' => '',
+        'zh_CN' => '',
+        'zh_TW' => '',
+        'hu' => '',
+        'tr' => '',
+    ]
+    //!////////////////////////////////
 ];
