6ef717c0a83a5812a64cfee3f7d15c2707d3a30c
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 16:54:00 +0100

    New translations activities.php (German Informal)

--- resources/lang/de_informal/activities.php
+++ resources/lang/de_informal/activities.php
@@ -1,6 +1,48 @@
 <?php
-
-// Extends 'de'
+/**
+ * Activity text strings.
+ * Is used for all the text within activity logs & notifications.
+ */
 return [
-    //
+
+    // Pages
+    'page_create'                 => 'erstellt Seite',
+    'page_create_notification'    => 'Die Seite wurde erfolgreich erstellt.',
+    'page_update'                 => 'aktualisiert Seite',
+    'page_update_notification'    => 'Die Seite wurde erfolgreich aktualisiert.',
+    'page_delete'                 => 'löscht Seite',
+    'page_delete_notification'    => 'Die Seite wurde erfolgreich gelöscht.',
+    'page_restore'                => 'stellt Seite wieder her',
+    'page_restore_notification'   => 'Die Seite wurde erfolgreich wiederhergestellt.',
+    'page_move'                   => 'verschiebt Seite',
+
+    // Chapters
+    'chapter_create'              => 'erstellt Kapitel',
+    'chapter_create_notification' => 'Das Kapitel wurde erfolgreich erstellt.',
+    'chapter_update'              => 'aktualisiert Kapitel',
+    'chapter_update_notification' => 'Das Kapitel wurde erfolgreich aktualisiert.',
+    'chapter_delete'              => 'löscht Kapitel',
+    'chapter_delete_notification' => 'Das Kapitel wurde erfolgreich gelöscht.',
+    'chapter_move'                => 'verschiebt Kapitel',
+
+    // Books
+    'book_create'                 => 'erstellt Buch',
+    'book_create_notification'    => 'Das Buch wurde erfolgreich erstellt.',
+    'book_update'                 => 'aktualisiert Buch',
+    'book_update_notification'    => 'Das Buch wurde erfolgreich aktualisiert.',
+    'book_delete'                 => 'löscht Buch',
+    'book_delete_notification'    => 'Das Buch wurde erfolgreich gelöscht.',
+    'book_sort'                   => 'sortiert Buch',
+    'book_sort_notification'      => 'Das Buch wurde erfolgreich umsortiert.',
+
+    // Bookshelves
+    'bookshelf_create'            => 'erstellt Bücherregal',
+    'bookshelf_create_notification'    => 'Das Bücherregal wurde erfolgreich erstellt',
+    'bookshelf_update'                 => 'aktualisiert Bücherregal',
+    'bookshelf_update_notification'    => 'Das Bücherregal wurde erfolgreich aktualisiert',
+    'bookshelf_delete'                 => 'löscht Bücherregal',
+    'bookshelf_delete_notification'    => 'Das Bücherregal wurde erfolgreich gelöscht',
+
+    // Other
+    'commented_on'                => 'kommentiert',
 ];
