975b083f222d64f560f58edc411bcc24c1a42174
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:11:49 +0100

    New translations auth.php (Chinese Traditional)

--- resources/lang/zh_TW/auth.php
+++ resources/lang/zh_TW/auth.php
@@ -1,76 +1,77 @@
 <?php
+/**
+ * Authentication Language Lines
+ * The following language lines are used during authentication for various
+ * messages that we need to display to the user.
+ */
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-    'failed' => '使用者名稱或密碼錯誤。',
-    'throttle' => '您的登入次數過多，請在:seconds秒後重試。',
 
-    /**
-     * Login & Register
-     */
-    'sign_up' => '註冊',
-    'log_in' => '登入',
-    'log_in_with' => '以:socialDriver登入',
-    'sign_up_with' => '註冊:socialDriver',
-    'logout' => '登出',
+    'failed' => '',
+    'throttle' => '',
 
-    'name' => '名稱',
-    'username' => '使用者名稱',
-    'email' => 'Email位址',
-    'password' => '密碼',
-    'password_confirm' => '確認密碼',
-    'password_hint' => '必須超過7個字元',
-    'forgot_password' => '忘記密碼?',
-    'remember_me' => '記住我',
-    'ldap_email_hint' => '請輸入用於此帳號的電子郵件。',
-    'create_account' => '建立帳號',
-    'social_login' => 'SNS登入',
-    'social_registration' => 'SNS註冊',
-    'social_registration_text' => '其他服務註冊/登入.',
+    // Login & Register
+    'sign_up' => '',
+    'log_in' => '',
+    'log_in_with' => '',
+    'sign_up_with' => '',
+    'logout' => '',
 
-    'register_thanks' => '註冊完成！',
-    'register_confirm' => '請點選查收您的Email，並點選確認。',
-    'registrations_disabled' => '註冊目前被禁用',
-    'registration_email_domain_invalid' => '此Email域名沒有權限進入本系統',
-    'register_success' => '感謝您註冊:appName，您現在已經登入。',
+    'name' => '',
+    'username' => '',
+    'email' => '',
+    'password' => '',
+    'password_confirm' => '',
+    'password_hint' => '',
+    'forgot_password' => '',
+    'remember_me' => '',
+    'ldap_email_hint' => '',
+    'create_account' => '',
+    'already_have_account' => '',
+    'dont_have_account' => '',
+    'social_login' => '',
+    'social_registration' => '',
+    'social_registration_text' => '',
+
+    'register_thanks' => '',
+    'register_confirm' => '',
+    'registrations_disabled' => '',
+    'registration_email_domain_invalid' => '',
+    'register_success' => '',
 
 
-    /**
-     * Password Reset
-     */
-    'reset_password' => '重置密碼',
-    'reset_password_send_instructions' => '在下方輸入您的Email位址，您將收到一封帶有密碼重置連結的郵件。',
-    'reset_password_send_button' => '發送重置連結',
-    'reset_password_sent_success' => '密碼重置連結已發送到:email。',
-    'reset_password_success' => '您的密碼已成功重置。',
-
-    'email_reset_subject' => '重置您的:appName密碼',
-    'email_reset_text' => '您收到此電子郵件是因為我們收到了您的帳號的密碼重置請求。',
-    'email_reset_not_requested' => '如果您沒有要求重置密碼，則不需要採取進一步的操作。',
+    // Password Reset
+    'reset_password' => '',
+    'reset_password_send_instructions' => '',
+    'reset_password_send_button' => '',
+    'reset_password_sent_success' => '',
+    'reset_password_success' => '',
+    'email_reset_subject' => '',
+    'email_reset_text' => '',
+    'email_reset_not_requested' => '',
 
 
-    /**
-     * Email Confirmation
-     */
-    'email_confirm_subject' => '確認您在:appName的Email位址',
-    'email_confirm_greeting' => '感謝您加入:appName！',
-    'email_confirm_text' => '請點選下面的按鈕確認您的Email位址：',
-    'email_confirm_action' => '確認Email',
-    'email_confirm_send_error' => '需要Email驗證，但系統無法發送電子郵件，請聯繫網站管理員。',
-    'email_confirm_success' => '您的Email位址已成功驗證！',
-    'email_confirm_resent' => '驗證郵件已重新發送，請檢查收件箱。',
+    // Email Confirmation
+    'email_confirm_subject' => '',
+    'email_confirm_greeting' => '',
+    'email_confirm_text' => '',
+    'email_confirm_action' => '',
+    'email_confirm_send_error' => '',
+    'email_confirm_success' => '',
+    'email_confirm_resent' => '',
 
-    'email_not_confirmed' => 'Email位址未驗證',
-    'email_not_confirmed_text' => '您的電子郵件位址尚未確認。',
-    'email_not_confirmed_click_link' => '請檢查註冊時收到的電子郵件，然後點選確認連結。',
-    'email_not_confirmed_resend' => '如果找不到電子郵件，請透過下面的表單重新發送確認Email。',
-    'email_not_confirmed_resend_button' => '重新發送確認Email',
+    'email_not_confirmed' => '',
+    'email_not_confirmed_text' => '',
+    'email_not_confirmed_click_link' => '',
+    'email_not_confirmed_resend' => '',
+    'email_not_confirmed_resend_button' => '',
+
+    // User Invite
+    'user_invite_email_subject' => '',
+    'user_invite_email_greeting' => '',
+    'user_invite_email_text' => '',
+    'user_invite_email_action' => '',
+    'user_invite_page_welcome' => '',
+    'user_invite_page_text' => '',
+    'user_invite_page_confirm_button' => '',
+    'user_invite_success' => ''
 ];
