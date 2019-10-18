825c34986cc9f7e1e85a451e0140f00d2b086052
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:09:58 +0100

    New translations errors.php (Swedish)

--- resources/lang/sv/errors.php
+++ resources/lang/sv/errors.php
@@ -1,85 +1,85 @@
 <?php
-
+/**
+ * Text shown in error messaging.
+ */
 return [
 
-    /**
-     * Error text strings.
-     */
-
     // Permissions
-    'permission' => 'Du har inte tillgång till den här sidan.',
-    'permissionJson' => 'Du har inte rätt att utföra den här åtgärden.',
+    'permission' => '',
+    'permissionJson' => '',
 
     // Auth
-    'error_user_exists_different_creds' => 'En användare med adressen :email finns redan.',
-    'email_already_confirmed' => 'E-posten har redan bekräftats, prova att logga in.',
-    'email_confirmation_invalid' => 'Denna bekräftelsekod är inte giltig eller har redan använts. Vänligen prova att registera dig på nytt',
-    'email_confirmation_expired' => 'Denna bekräftelsekod har gått ut. Vi har skickat dig en ny.',
-    'ldap_fail_anonymous' => 'LDAP-inloggning misslyckades med anonym bindning',
-    'ldap_fail_authed' => 'LDAP-inloggning misslyckades med angivna dn- och lösenordsuppgifter',
-    'ldap_extension_not_installed' => 'LDAP PHP-tillägg inte installerat',
-    'ldap_cannot_connect' => 'Kan inte ansluta till ldap-servern. Anslutningen misslyckades',
-    'social_no_action_defined' => 'Ingen åtgärd definierad',
-    'social_login_bad_response' => "Ett fel inträffade vid inloggning genom :socialAccount: \n:error",
-    'social_account_in_use' => 'Detta konto från :socialAccount används redan. Testa att logga in med :socialAccount istället.',
-    'social_account_email_in_use' => 'E-posten :email används redan. Om du redan har ett konto kan du ansluta ditt konto från :socialAccount via dina profilinställningar.',
-    'social_account_existing' => 'Detta konto från :socialAccount är redan länkat till din profil.',
-    'social_account_already_used_existing' => 'Detta konto från :socialAccount används redan av en annan användare.',
-    'social_account_not_used' => 'Detta konto från :socialAccount är inte länkat till någon användare. Vänligen anslut via dina profilinställningar. ',
-    'social_account_register_instructions' => 'Om du inte har något konto ännu kan du registerar dig genom att välja :socialAccount.',
-    'social_driver_not_found' => 'Drivrutinen för den här tjänsten hittades inte',
-    'social_driver_not_configured' => 'Dina inställningar för :socialAccount är inte korrekta.',
+    'error_user_exists_different_creds' => '',
+    'email_already_confirmed' => '',
+    'email_confirmation_invalid' => '',
+    'email_confirmation_expired' => '',
+    'ldap_fail_anonymous' => '',
+    'ldap_fail_authed' => '',
+    'ldap_extension_not_installed' => '',
+    'ldap_cannot_connect' => '',
+    'social_no_action_defined' => '',
+    'social_login_bad_response' => "",
+    'social_account_in_use' => '',
+    'social_account_email_in_use' => '',
+    'social_account_existing' => '',
+    'social_account_already_used_existing' => '',
+    'social_account_not_used' => '',
+    'social_account_register_instructions' => '',
+    'social_driver_not_found' => '',
+    'social_driver_not_configured' => '',
+    'invite_token_expired' => '',
 
     // System
-    'path_not_writable' => 'Kunde inte ladda upp till sökvägen :filePath. Kontrollera att webbservern har skrivåtkomst.',
-    'cannot_get_image_from_url' => 'Kan inte hämta bild från :url',
-    'cannot_create_thumbs' => 'Servern kan inte skapa miniatyrer. Kontrollera att du har PHPs GD-tillägg aktiverat.',
-    'server_upload_limit' => 'Servern tillåter inte så här stora filer. Prova en mindre fil.',
-    'uploaded'  => 'Servern tillåter inte så här stora filer. Prova en mindre fil.',
-    'image_upload_error' => 'Ett fel inträffade vid uppladdningen',
-    'image_upload_type_error' => 'Filtypen du försöker ladda upp är ogiltig',
-    'file_upload_timeout' => 'Filuppladdningen har tagits ut.',
+    'path_not_writable' => '',
+    'cannot_get_image_from_url' => '',
+    'cannot_create_thumbs' => '',
+    'server_upload_limit' => '',
+    'uploaded'  => '',
+    'image_upload_error' => '',
+    'image_upload_type_error' => '',
+    'file_upload_timeout' => '',
 
     // Attachments
-    'attachment_page_mismatch' => 'Fel i sidmatchning vid uppdatering av bilaga',
-    'attachment_not_found' => 'Bilagan hittades ej',
+    'attachment_page_mismatch' => '',
+    'attachment_not_found' => '',
 
     // Pages
-    'page_draft_autosave_fail' => 'Kunde inte spara utkastet. Kontrollera att du är ansluten till internet.',
-    'page_custom_home_deletion' => 'Det går inte att ta bort sidan medan den används som startsida',
+    'page_draft_autosave_fail' => '',
+    'page_custom_home_deletion' => '',
 
     // Entities
-    'entity_not_found' => 'Innehållet hittades inte',
-    'bookshelf_not_found' => 'Hyllan hittades inte',
-    'book_not_found' => 'Boken hittades inte',
-    'page_not_found' => 'Sidan hittades inte',
-    'chapter_not_found' => 'Kapitlet hittades inte',
-    'selected_book_not_found' => 'Den valda boken hittades inte',
-    'selected_book_chapter_not_found' => 'Den valda boken eller kapitlet hittades inte',
-    'guests_cannot_save_drafts' => 'Gäster kan inte spara utkast',
+    'entity_not_found' => '',
+    'bookshelf_not_found' => '',
+    'book_not_found' => '',
+    'page_not_found' => '',
+    'chapter_not_found' => '',
+    'selected_book_not_found' => '',
+    'selected_book_chapter_not_found' => '',
+    'guests_cannot_save_drafts' => '',
 
     // Users
-    'users_cannot_delete_only_admin' => 'Du kan inte ta bort den enda admin-användaren',
-    'users_cannot_delete_guest' => 'Du kan inte ta bort gästanvändaren',
+    'users_cannot_delete_only_admin' => '',
+    'users_cannot_delete_guest' => '',
 
     // Roles
-    'role_cannot_be_edited' => 'Den här rollen kan inte redigeras',
-    'role_system_cannot_be_deleted' => 'Det här är en systemroll och kan därför inte tas bort',
-    'role_registration_default_cannot_delete' => 'Det går inte att ta bort rollen medan den används som standardroll.',
-    'role_cannot_remove_only_admin' => 'Detta är den enda användaren med administratörsroll. Gör någon annan användare till administratör innan du tar bort denna.',
+    'role_cannot_be_edited' => '',
+    'role_system_cannot_be_deleted' => '',
+    'role_registration_default_cannot_delete' => '',
+    'role_cannot_remove_only_admin' => '',
 
     // Comments
-    'comment_list' => 'Ett fel inträffade då kommentarer skulle hämtas.',
-    'cannot_add_comment_to_draft' => 'Du kan inte kommentera ett utkast.',
-    'comment_add' => 'Ett fel inträffade då kommentaren skulle sparas.',
-    'comment_delete' => 'Ett fel inträffade då kommentaren skulle tas bort.',
-    'empty_comment' => 'Kan inte lägga till en tom kommentar.',
+    'comment_list' => '',
+    'cannot_add_comment_to_draft' => '',
+    'comment_add' => '',
+    'comment_delete' => '',
+    'empty_comment' => '',
 
     // Error pages
-    '404_page_not_found' => 'Sidan hittades inte',
-    'sorry_page_not_found' => 'Tyvärr gick det inte att hitta sidan du söker.',
-    'return_home' => 'Återvänd till startsidan',
-    'error_occurred' => 'Ett fel inträffade',
-    'app_down' => ':appName är nere just nu',
-    'back_soon' => 'Vi är snart tillbaka.',
+    '404_page_not_found' => '',
+    'sorry_page_not_found' => '',
+    'return_home' => '',
+    'error_occurred' => '',
+    'app_down' => '',
+    'back_soon' => '',
+
 ];
