4c25e8f7e6570e6beb3ee51213e2f32473eca84f
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:11:10 +0100

    New translations components.php (Spanish, Argentina)

--- resources/lang/es_AR/components.php
+++ resources/lang/es_AR/components.php
@@ -1,34 +1,33 @@
 <?php
+/**
+ * Text used in custom JavaScript driven components.
+ */
 return [
 
-    /**
-     * Image Manager
-     */
-    'image_select' => 'Seleccionar Imagen',
-    'image_all' => 'Todo',
-    'image_all_title' => 'Ver todas las imágenes',
-    'image_book_title' => 'Ver las imágenes subidas a este libro',
-    'image_page_title' => 'Ver las imágenes subidas a esta página',
-    'image_search_hint' => 'Buscar por nombre de imagen',
-    'image_uploaded' => 'Subido el :uploadedDate',
-    'image_load_more' => 'Cargar más',
-    'image_image_name' => 'Nombre de imagen',
-    'image_delete_used' => 'Esta imagen esta siendo utilizada en las páginas a continuación.',
-    'image_delete_confirm' => 'Haga click de nuevo para confirmar que quiere borrar esta imagen.',
-    'image_select_image' => 'Seleccionar Imagen',
-    'image_dropzone' => 'Arrastre las imágenes o hacer click aquí para Subir',
-    'images_deleted' => 'Imágenes borradas',
-    'image_preview' => 'Preview de la imagen',
-    'image_upload_success' => 'Imagen subida éxitosamente',
-    'image_update_success' => 'Detalles de la imagen actualizados exitosamente',
-    'image_delete_success' => 'Imagen borrada exitosamente',
-    'image_upload_remove' => 'Quitar',
+    // Image Manager
+    'image_select' => '',
+    'image_all' => '',
+    'image_all_title' => '',
+    'image_book_title' => '',
+    'image_page_title' => '',
+    'image_search_hint' => '',
+    'image_uploaded' => '',
+    'image_load_more' => '',
+    'image_image_name' => '',
+    'image_delete_used' => '',
+    'image_delete_confirm' => '',
+    'image_select_image' => '',
+    'image_dropzone' => '',
+    'images_deleted' => '',
+    'image_preview' => '',
+    'image_upload_success' => '',
+    'image_update_success' => '',
+    'image_delete_success' => '',
+    'image_upload_remove' => '',
 
-    /**
-     * Code editor
-     */
-    'code_editor' => 'Editar Código',
-    'code_language' => 'Lenguaje del Código',
-    'code_content' => 'Contenido del Código',
-    'code_save' => 'Guardar Código',
+    // Code Editor
+    'code_editor' => '',
+    'code_language' => '',
+    'code_content' => '',
+    'code_save' => '',
 ];
