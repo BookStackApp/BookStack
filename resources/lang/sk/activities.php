5023542aaa1ab0a52d46cd94c14f2cec7b646eea
Dan Brown <ssddanbrown@googlemail.com>
2019-10-18 02:10:44 +0100

    New translations activities.php (Slovak)

--- resources/lang/sk/activities.php
+++ resources/lang/sk/activities.php
@@ -1,40 +1,48 @@
 <?php
-
+/**
+ * Activity text strings.
+ * Is used for all the text within activity logs & notifications.
+ */
 return [
 
-    /**
-     * Activity text strings.
-     * Is used for all the text within activity logs & notifications.
-     */
-
     // Pages
-    'page_create'                 => 'vytvoril stránku',
-    'page_create_notification'    => 'Stránka úspešne vytvorená',
-    'page_update'                 => 'aktualizoval stránku',
-    'page_update_notification'    => 'Stránka úspešne aktualizovaná',
-    'page_delete'                 => 'odstránil stránku',
-    'page_delete_notification'    => 'Stránka úspešne odstránená',
-    'page_restore'                => 'obnovil stránku',
-    'page_restore_notification'   => 'Stránka úspešne obnovená',
-    'page_move'                   => 'presunul stránku',
+    'page_create'                 => '',
+    'page_create_notification'    => '',
+    'page_update'                 => '',
+    'page_update_notification'    => '',
+    'page_delete'                 => '',
+    'page_delete_notification'    => '',
+    'page_restore'                => '',
+    'page_restore_notification'   => '',
+    'page_move'                   => '',
 
     // Chapters
-    'chapter_create'              => 'vytvoril kapitolu',
-    'chapter_create_notification' => 'Kapitola úspešne vytvorená',
-    'chapter_update'              => 'aktualizoval kapitolu',
-    'chapter_update_notification' => 'Kapitola úspešne aktualizovaná',
-    'chapter_delete'              => 'odstránil kapitolu',
-    'chapter_delete_notification' => 'Kapitola úspešne odstránená',
-    'chapter_move'                => 'presunul kapitolu',
+    'chapter_create'              => '',
+    'chapter_create_notification' => '',
+    'chapter_update'              => '',
+    'chapter_update_notification' => '',
+    'chapter_delete'              => '',
+    'chapter_delete_notification' => '',
+    'chapter_move'                => '',
 
     // Books
-    'book_create'                 => 'vytvoril knihu',
-    'book_create_notification'    => 'Kniha úspešne vytvorená',
-    'book_update'                 => 'aktualizoval knihu',
-    'book_update_notification'    => 'Kniha úspešne aktualizovaná',
-    'book_delete'                 => 'odstránil knihu',
-    'book_delete_notification'    => 'Kniha úspešne odstránená',
-    'book_sort'                   => 'zoradil knihu',
-    'book_sort_notification'      => 'Kniha úspešne znovu zoradená',
+    'book_create'                 => '',
+    'book_create_notification'    => '',
+    'book_update'                 => '',
+    'book_update_notification'    => '',
+    'book_delete'                 => '',
+    'book_delete_notification'    => '',
+    'book_sort'                   => '',
+    'book_sort_notification'      => '',
 
+    // Bookshelves
+    'bookshelf_create'            => '',
+    'bookshelf_create_notification'    => '',
+    'bookshelf_update'                 => '',
+    'bookshelf_update_notification'    => '',
+    'bookshelf_delete'                 => '',
+    'bookshelf_delete_notification'    => '',
+
+    // Other
+    'commented_on'                => '',
 ];
