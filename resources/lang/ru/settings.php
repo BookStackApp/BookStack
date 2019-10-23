8bc7901af2f3b35755350bfa985650ac40286514
Dan Brown <ssddanbrown@googlemail.com>
2019-10-23 20:29:46 +0100

    New translations settings.php (Russian)

--- resources/lang/ru/settings.php
+++ resources/lang/ru/settings.php
@@ -63,6 +63,13 @@ return [
     'maint_image_cleanup_warning' => 'Найдено :count возможно бесполезных изображений. Вы уверены, что хотите удалить эти изображения?',
     'maint_image_cleanup_success' => ':count возможно бесполезных изображений было найдено и удалено!',
     'maint_image_cleanup_nothing_found' => 'Не найдено ни одного бесполезного изображения!',
+    'maint_send_test_email' => 'Send a Test Email',
+    'maint_send_test_email_desc' => 'This sends a test email to your email address specified in your profile.',
+    'maint_send_test_email_run' => 'Send test email',
+    'maint_send_test_email_success' => 'Email sent to :address',
+    'maint_send_test_email_mail_subject' => 'Test Email',
+    'maint_send_test_email_mail_greeting' => 'Email delivery seems to work!',
+    'maint_send_test_email_mail_text' => 'Congratulations! As you received this email notification, your email settings seem to be configured properly.',
 
     // Role Settings
     'roles' => 'Роли',
