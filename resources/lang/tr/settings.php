1000a7e708ed3f58deb8b17fd21e88eda234bab0
Dan Brown <ssddanbrown@googlemail.com>
2019-10-23 20:29:35 +0100

    New translations settings.php (Turkish)

--- resources/lang/tr/settings.php
+++ resources/lang/tr/settings.php
@@ -63,6 +63,13 @@ return [
     'maint_image_cleanup_warning' => ':count potansiyel kullanılmayan görsel bulundu. Bu görselleri silmek istediğinizden emin misiniz?',
     'maint_image_cleanup_success' => ':count potanisyel kullanılmayan görsel bulundu ve silindi!',
     'maint_image_cleanup_nothing_found' => 'Kullanılmayan görsel bulunamadı ve birşey silinmedi!',
+    'maint_send_test_email' => 'Send a Test Email',
+    'maint_send_test_email_desc' => 'This sends a test email to your email address specified in your profile.',
+    'maint_send_test_email_run' => 'Send test email',
+    'maint_send_test_email_success' => 'Email sent to :address',
+    'maint_send_test_email_mail_subject' => 'Test Email',
+    'maint_send_test_email_mail_greeting' => 'Email delivery seems to work!',
+    'maint_send_test_email_mail_text' => 'Congratulations! As you received this email notification, your email settings seem to be configured properly.',
 
     // Role Settings
     'roles' => 'Roller',
