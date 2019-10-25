fa918f49d6cdf647a43d4b2716af048b8ad4fffc
jakob <jakob@mpib-berlin.mpg.de>
2019-10-25 10:39:21 +0200

    Takes APP_URL on redirect after login into account. Fixes issue #1377

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -106,7 +106,8 @@ class LoginController extends Controller
             $this->ldapService->syncGroups($user, $request->get($this->username()));
         }
 
-        return redirect()->intended('/');
+        $app_url = parse_url(env('APP_URL'));
+        return redirect(preg_replace("/".addcslashes($app_url['path'], '/')."/", '', redirect()->intended('/')->getTargetUrl(), 1));
     }
 
     /**
