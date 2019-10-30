a06058f9d026a22864fbead0fca354e414f3492d
jakob <jakob@mpib-berlin.mpg.de>
2019-10-30 11:42:37 +0100

    Add the "Create Shelf" resp. "Create Book" to the home view

--- resources/views/common/home-shelves.blade.php
+++ resources/views/common/home-shelves.blade.php
@@ -12,6 +12,12 @@
     <div class="actions mb-xl">
         <h5>{{ trans('common.actions') }}</h5>
         <div class="icon-list text-primary">
+            @if($currentUser->can('bookshelf-create-all'))
+                <a href="{{ url("/create-shelf") }}" class="icon-list-item">
+                    <span>@icon('add')</span>
+                    <span>{{ trans('entities.shelves_new_action') }}</span>
+                </a>
+            @endif
             @include('partials.view-toggle', ['view' => $view, 'type' => 'shelf'])
             @include('components.expand-toggle', ['target' => '.entity-list.compact .entity-item-snippet', 'key' => 'home-details'])
         </div>
