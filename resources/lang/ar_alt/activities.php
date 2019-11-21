19cc7feb21edc74f65ab91184c43c0e6b11d7e21
abublihi <abublihi@gmail.com>
2019-11-21 12:41:48 +0300

    add arabic alternative translation, modifies the some vocabularies.

---
+++ resources/lang/ar_alt/activities.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Activity text strings.
+ * Is used for all the text within activity logs & notifications.
+ */
+return [
+
+    // Pages
+    'page_create'                 => 'تم إنشاء صفحة',
+    'page_create_notification'    => 'تم إنشاء الصفحة بنجاح',
+    'page_update'                 => 'تم تحديث الصفحة',
+    'page_update_notification'    => 'تم تحديث الصفحة بنجاح',
+    'page_delete'                 => 'تم حذف الصفحة',
+    'page_delete_notification'    => 'تم حذف الصفحة بنجاح',
+    'page_restore'                => 'تمت استعادة الصفحة',
+    'page_restore_notification'   => 'تمت استعادة الصفحة بنجاح',
+    'page_move'                   => 'تم نقل الصفحة',
+
+    // Chapters
+    'chapter_create'              => 'تم إنشاء فصل',
+    'chapter_create_notification' => 'تم إنشاء فصل بنجاح',
+    'chapter_update'              => 'تم تحديث الفصل',
+    'chapter_update_notification' => 'تم تحديث الفصل بنجاح',
+    'chapter_delete'              => 'تم حذف الفصل',
+    'chapter_delete_notification' => 'تم حذف الفصل بنجاح',
+    'chapter_move'                => 'تم نقل الفصل',
+
+    // Books
+    'book_create'                 => 'تم إنشاء الدليل',
+    'book_create_notification'    => 'تم إنشاء الدليل بنجاح',
+    'book_update'                 => 'تم تحديث الدليل',
+    'book_update_notification'    => 'تم تحديث الدليل بنجاح',
+    'book_delete'                 => 'تم حذف الدليل',
+    'book_delete_notification'    => 'تم حذف الدليل بنجاح',
+    'book_sort'                   => 'تم سرد الدليل',
+    'book_sort_notification'      => 'تمت إعادة سرد الدليل بنجاح',
+
+    // Bookshelves
+    'bookshelf_create'            => 'created Bookshelf',
+    'bookshelf_create_notification'    => 'Bookshelf Successfully Created',
+    'bookshelf_update'                 => 'updated bookshelf',
+    'bookshelf_update_notification'    => 'Bookshelf Successfully Updated',
+    'bookshelf_delete'                 => 'deleted bookshelf',
+    'bookshelf_delete_notification'    => 'Bookshelf Successfully Deleted',
+
+    // Other
+    'commented_on'                => 'تم التعليق',
+];
