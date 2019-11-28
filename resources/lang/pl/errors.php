8bd6931ed98bd2aff4e8364c6e6699abe4498908
Artur Skoczylas <artur.skoczylas@eastsoft.pl>
2019-11-28 15:39:54 +0100

    Update polish language

--- resources/lang/pl/errors.php
+++ resources/lang/pl/errors.php
@@ -17,6 +17,12 @@ return [
     'ldap_fail_authed' => 'Dostęp LDAP przy użyciu tego DN i hasła nie powiódł się',
     'ldap_extension_not_installed' => 'Rozszerzenie LDAP PHP nie zostało zainstalowane',
     'ldap_cannot_connect' => 'Nie można połączyć z serwerem LDAP, połączenie nie zostało ustanowione',
+    'saml_already_logged_in' => 'Już zalogowany',
+    'saml_user_not_registered' => 'Użytkownik :name nie jest zarejestrowany i automatyczna rejestracja jest wyłączona',
+    'saml_no_email_address' => 'Nie można odnaleźć adresu email dla tego użytkownika w danych dostarczonych przez zewnętrzny system uwierzytelniania',
+    'saml_invalid_response_id' => 'The request from the external authentication system is not recognised by a process started by this application. Navigating back after a login could cause this issue.',
+    'saml_fail_authed' => 'Login using :system failed, system did not provide successful authorization',
+    'saml_email_exists' => 'Registration unsuccessful since a user already exists with email address ":email"',
     'social_no_action_defined' => 'Brak zdefiniowanej akcji',
     'social_login_bad_response' => "Podczas próby logowania :socialAccount wystąpił błąd: \n:error",
     'social_account_in_use' => 'To konto :socialAccount jest już w użyciu. Spróbuj zalogować się za pomocą opcji :socialAccount.',
@@ -27,7 +33,7 @@ return [
     'social_account_register_instructions' => 'Jeśli nie masz jeszcze konta, możesz zarejestrować je używając opcji :socialAccount.',
     'social_driver_not_found' => 'Funkcja społecznościowa nie została odnaleziona',
     'social_driver_not_configured' => 'Ustawienia konta :socialAccount nie są poprawne.',
-    'invite_token_expired' => 'This invitation link has expired. You can instead try to reset your account password.',
+    'invite_token_expired' => 'Zaproszenie wygasło. Możesz spróować zresetować swoje hasło.',
 
     // System
     'path_not_writable' => 'Zapis do ścieżki :filePath jest niemożliwy. Upewnij się że aplikacja ma prawa do zapisu plików na serwerze.',
@@ -50,11 +56,11 @@ return [
     // Entities
     'entity_not_found' => 'Nie znaleziono obiektu',
     'bookshelf_not_found' => 'Nie znaleziono półki',
-    'book_not_found' => 'Nie znaleziono podręcznika',
+    'book_not_found' => 'Nie znaleziono książki',
     'page_not_found' => 'Nie znaleziono strony',
     'chapter_not_found' => 'Nie znaleziono rozdziału',
-    'selected_book_not_found' => 'Wybrany podręcznik nie został znaleziony',
-    'selected_book_chapter_not_found' => 'Wybrany podręcznik lub rozdział nie został znaleziony',
+    'selected_book_not_found' => 'Wybrana książka nie została znaleziona',
+    'selected_book_chapter_not_found' => 'Wybrana książka lub rozdział nie został znaleziony',
     'guests_cannot_save_drafts' => 'Goście nie mogą zapisywać wersji roboczych',
 
     // Users
@@ -65,7 +71,7 @@ return [
     'role_cannot_be_edited' => 'Ta rola nie może być edytowana',
     'role_system_cannot_be_deleted' => 'Ta rola jest rolą systemową i nie może zostać usunięta',
     'role_registration_default_cannot_delete' => 'Ta rola nie może zostać usunięta, dopóki jest ustawiona jako domyślna rola użytkownika',
-    'role_cannot_remove_only_admin' => 'This user is the only user assigned to the administrator role. Assign the administrator role to another user before attempting to remove it here.',
+    'role_cannot_remove_only_admin' => 'Ten użytkownik jest jedynym użytkownikiem przypisanym do roli administratora. Przypisz rolę administratora innemu użytkownikowi przed próbą usunięcia.',
 
     // Comments
     'comment_list' => 'Wystąpił błąd podczas pobierania komentarzy.',
