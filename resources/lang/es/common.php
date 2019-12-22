a6ca7185a388d378bfc063ebf0f6c3c8b3ad0a78
Dan Brown <ssddanbrown@googlemail.com>
2019-12-22 14:23:37 +0000

    New translations common.php (Spanish)

--- resources/lang/es/common.php
+++ resources/lang/es/common.php
@@ -38,6 +38,7 @@ return [
     'reset' => 'Resetear',
     'remove' => 'Remover',
     'add' => 'Añadir',
+    'fullscreen' => 'Fullscreen',
 
     // Sort Options
     'sort_options' => 'Opciones de ordenación',
