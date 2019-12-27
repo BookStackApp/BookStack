cf743370a8ddf99ff6d03721197a2a8e1746432f
Dan Brown <ssddanbrown@googlemail.com>
2019-12-27 17:14:34 +0000

    Updated code block lang order and added extra pascal option
    - Fixed modal window sizing/positioning to be properly center and
    responsive.
    
    Related to #1730

--- resources/js/services/code.js
+++ resources/js/services/code.js
@@ -25,7 +25,7 @@ import 'codemirror/mode/sql/sql';
 import 'codemirror/mode/toml/toml';
 import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/yaml/yaml';
-import 'codemirror/mode/pascal/pascal'
+import 'codemirror/mode/pascal/pascal';
 
 // Addons
 import 'codemirror/addon/scroll/scrollpastend';
@@ -62,6 +62,8 @@ const modeMap = {
     powershell: 'powershell',
     properties: 'properties',
     ocaml: 'mllike',
+    pascal: 'text/x-pascal',
+    pas: 'text/x-pascal',
     php: (content) => {
         return content.includes('<?php') ? 'php' : 'text/x-php';
     },
@@ -79,7 +81,6 @@ const modeMap = {
     xml: 'xml',
     yaml: 'yaml',
     yml: 'yaml',
-    pascal: 'text/x-pascal',
 };
 
 /**
