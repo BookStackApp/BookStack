9fc8668c56bf3e2982266e3110b0b82c462fe82e
Dan Brown <ssddanbrown@googlemail.com>
2019-12-30 15:46:12 +0000

    Change email confirmation from own middle to trait
    Email confirmation middleware caused more mess than good, As caused
    priority issues and it depended on auth actions. Instead its now a trai
    used on auth middlewares.
    
    Also used 'EncryptCookies' middleware on API instead of custom
    decryption in custom middleware since we'd need to do replicate all the
    same actions anyway. Shouldn't have too much effect since it only
    actions over cookies that exist, of which none should be there for most
    API requests.
    
    Also split out some large guard functions to be a little more readable
    and appease codeclimate.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -13,26 +13,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \BookStack\Http\Middleware\TrimStrings::class,
         \BookStack\Http\Middleware\TrustProxies::class,
-
-    ];
-
-    /**
-     * The priority ordering of middleware.
-     */
-    protected $middlewarePriority = [
-        \BookStack\Http\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \BookStack\Http\Middleware\StartSessionIfCookieExists::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        \BookStack\Http\Middleware\VerifyCsrfToken::class,
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        \BookStack\Http\Middleware\Localization::class,
-        \BookStack\Http\Middleware\GlobalViewData::class,
-        \BookStack\Http\Middleware\Authenticate::class,
-        \BookStack\Http\Middleware\ApiAuthenticate::class,
-        \BookStack\Http\Middleware\ConfirmEmails::class,
     ];
 
     /**
@@ -50,13 +30,12 @@ class Kernel extends HttpKernel
             \BookStack\Http\Middleware\VerifyCsrfToken::class,
             \BookStack\Http\Middleware\Localization::class,
             \BookStack\Http\Middleware\GlobalViewData::class,
-            \BookStack\Http\Middleware\ConfirmEmails::class,
         ],
         'api' => [
             'throttle:60,1',
+            \BookStack\Http\Middleware\EncryptCookies::class,
             \BookStack\Http\Middleware\StartSessionIfCookieExists::class,
             \BookStack\Http\Middleware\ApiAuthenticate::class,
-            \BookStack\Http\Middleware\ConfirmEmails::class,
         ],
     ];
 
