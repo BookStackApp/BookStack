114c21e28643cacd6237da53eaad9c3736336721
Dan Brown <ssddanbrown@googlemail.com>
2019-12-30 20:48:23 +0000

    Added tests to cover API config and listing code

--- tests/TestsApi.php
+++ tests/TestsApi.php
@@ -8,12 +8,27 @@ trait TestsApi
     protected $apiTokenId = 'apitoken';
     protected $apiTokenSecret = 'password';
 
-    protected function errorResponse(string $messge, int $code)
+    /**
+     * Set the API editor role as the current user via the API driver.
+     */
+    protected function actingAsApiEditor()
     {
-        return ["error" => ["code" => $code, "message" => $messge]];
+        $this->actingAs($this->getEditor(), 'api');
+        return $this;
     }
 
-    protected function apiAuthHeader()
+    /**
+     * Format the given items into a standardised error format.
+     */
+    protected function errorResponse(string $message, int $code): array
+    {
+        return ["error" => ["code" => $code, "message" => $message]];
+    }
+
+    /**
+     * Get an approved API auth header.
+     */
+    protected function apiAuthHeader(): array
     {
         return [
             "Authorization" => "Token {$this->apiTokenId}:{$this->apiTokenSecret}"
