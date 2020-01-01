398bfe29399c04e6e48c9a4ad1da827073911441
Dan Brown <ssddanbrown@googlemail.com>
2020-01-01 21:50:00 +0000

    New translations activities.php (Portuguese, Brazilian)

--- resources/lang/pt_BR/activities.php
+++ resources/lang/pt_BR/activities.php
@@ -6,7 +6,7 @@
 return [
 
     // Pages
-    'page_create'                 => 'criou página',
+    'page_create'                 => 'criou a página',
     'page_create_notification'    => 'Página criada com sucesso',
     'page_update'                 => 'atualizou página',
     'page_update_notification'    => 'Página atualizada com sucesso',
