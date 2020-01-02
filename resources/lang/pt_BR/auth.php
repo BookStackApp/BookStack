8c118b2a17de59c267c2412180ab09d16e606d4d
Dan Brown <ssddanbrown@googlemail.com>
2020-01-02 21:27:07 +0000

    New translations auth.php (Portuguese, Brazilian)

--- resources/lang/pt_BR/auth.php
+++ resources/lang/pt_BR/auth.php
@@ -10,7 +10,7 @@ return [
     'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
 
     // Login & Register
-    'sign_up' => 'Cadastre-se',
+    'sign_up' => 'Criar Conta',
     'log_in' => 'Entrar',
     'log_in_with' => 'Entrar com :socialDriver',
     'sign_up_with' => 'Cadastre-se com :socialDriver',
@@ -40,37 +40,37 @@ return [
 
 
     // Password Reset
-    'reset_password' => 'Redefinir senha',
-    'reset_password_send_instructions' => 'Digite seu e-mail abaixo e uma mensagem com o link de redefinição de senha lhe será enviada.',
-    'reset_password_send_button' => 'Enviar o link de redefinição de senha',
+    'reset_password' => 'Redefinir Senha',
+    'reset_password_send_instructions' => 'Insira seu e-mail abaixo e uma mensagem com o link de redefinição de senha lhe será enviada.',
+    'reset_password_send_button' => 'Enviar o Link de Redefinição',
     'reset_password_sent_success' => 'Um link de redefinição de senha foi enviado para :email.',
     'reset_password_success' => 'Sua senha foi redefinida com sucesso.',
     'email_reset_subject' => 'Redefina a senha de :appName',
-    'email_reset_text' => 'Você recebeu esse e-mail pois recebemos uma solicitação de redefinição de senha para sua conta.',
+    'email_reset_text' => 'Você recebeu esse e-mail pois recebemos uma solicitação de redefinição de senha para a sua conta.',
     'email_reset_not_requested' => 'Caso não tenha sido você a solicitar a redefinição de senha, ignore esse e-mail.',
 
 
     // Email Confirmation
     'email_confirm_subject' => 'Confirme seu e-mail para :appName',
-    'email_confirm_greeting' => 'Obrigado por se registrar em :appName!',
+    'email_confirm_greeting' => 'Obrigado por se cadastrar em :appName!',
     'email_confirm_text' => 'Por favor, confirme seu endereço de e-mail clicando no botão abaixo:',
     'email_confirm_action' => 'Confirmar E-mail',
-    'email_confirm_send_error' => 'E-mail de confirmação é requerido, mas o sistema não pôde enviar a mensagem. Por favor, entre em contato com o administrador para se certificar que o serviço de envio de e-mails está corretamente configurado.',
+    'email_confirm_send_error' => 'A confirmação de e-mail é requerida, mas o sistema não pôde enviar a mensagem. Por favor, entre em contato com o administrador para se certificar que o serviço de envio de e-mails está corretamente configurado.',
     'email_confirm_success' => 'Seu e-mail foi confirmado!',
-    'email_confirm_resent' => 'E-mail de confirmação reenviado. Por favor, cheque sua caixa postal.',
+    'email_confirm_resent' => 'E-mail de confirmação reenviado. Por favor, verifique sua caixa de entrada.',
 
-    'email_not_confirmed' => 'Endereço de e-mail não confirmado',
+    'email_not_confirmed' => 'Endereço de E-mail Não Confirmado',
     'email_not_confirmed_text' => 'Seu endereço de e-mail ainda não foi confirmado.',
-    'email_not_confirmed_click_link' => 'Por favor, clique no link no e-mail que foi enviado após o registro.',
+    'email_not_confirmed_click_link' => 'Por favor, clique no link no e-mail que foi enviado após o cadastro.',
     'email_not_confirmed_resend' => 'Caso não encontre o e-mail você poderá reenviar a confirmação usando o formulário abaixo.',
-    'email_not_confirmed_resend_button' => 'Reenviar o e-mail de confirmação',
+    'email_not_confirmed_resend_button' => 'Reenviar o E-mail de Confirmação',
 
     // User Invite
-    'user_invite_email_subject' => 'Você recebeu um convite para ingressar :appName!',
+    'user_invite_email_subject' => 'Você recebeu um convite para :appName!',
     'user_invite_email_greeting' => 'Uma conta foi criada para você em :appName.',
     'user_invite_email_text' => 'Clique no botão abaixo para definir uma senha de conta e obter acesso:',
     'user_invite_email_action' => 'Defina a Senha da Conta',
-    'user_invite_page_welcome' => 'Bem vindo ao :appName!',
+    'user_invite_page_welcome' => 'Bem-vindo(a) a :appName!',
     'user_invite_page_text' => 'Para finalizar sua conta e obter acesso, você precisa definir uma senha que será usada para efetuar login em :appName em futuras visitas.',
     'user_invite_page_confirm_button' => 'Confirmar Senha',
     'user_invite_success' => 'Senha definida, você agora tem acesso a :appName!'
