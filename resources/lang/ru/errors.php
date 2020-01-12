f4c4e9a8be23321f2ce514541bf25292cdb31c33
Statium <statium@inbox.lv>
2020-01-12 18:51:49 +0500

    Update errors.php
    More accurate translation, error correction.

--- resources/lang/ru/errors.php
+++ resources/lang/ru/errors.php
@@ -9,23 +9,23 @@ return [
     'permissionJson' => 'У вас нет разрешения для запрашиваемого действия.',
 
     // Auth
-    'error_user_exists_different_creds' => 'Пользователь с электронной почтой: :email уже существует, но с другими учетными данными.',
+    'error_user_exists_different_creds' => 'Пользователь с электронной почтой :email уже существует, но с другими учетными данными.',
     'email_already_confirmed' => 'Электронная почта уже подтверждена, попробуйте войти в систему.',
     'email_confirmation_invalid' => 'Этот токен подтверждения недействителен или уже используется. Повторите попытку регистрации.',
     'email_confirmation_expired' => 'Истек срок действия токена. Отправлено новое письмо с подтверждением.',
     'ldap_fail_anonymous' => 'Недопустимый доступ LDAP с использованием анонимной привязки',
     'ldap_fail_authed' => 'Не удалось получить доступ к LDAP, используя данные dn & password',
-    'ldap_extension_not_installed' => 'LDAP расширения для PHP не установлено',
+    'ldap_extension_not_installed' => 'LDAP расширение для PHP не установлено',
     'ldap_cannot_connect' => 'Не удается подключиться к серверу ldap, не удалось выполнить начальное соединение',
-    'saml_already_logged_in' => 'Already logged in',
-    'saml_user_not_registered' => 'The user :name is not registered and automatic registration is disabled',
-    'saml_no_email_address' => 'Could not find an email address, for this user, in the data provided by the external authentication system',
-    'saml_invalid_response_id' => 'The request from the external authentication system is not recognised by a process started by this application. Navigating back after a login could cause this issue.',
-    'saml_fail_authed' => 'Login using :system failed, system did not provide successful authorization',
-    'saml_email_exists' => 'Registration unsuccessful since a user already exists with email address ":email"',
+    'saml_already_logged_in' => 'Вход в систему произведен ранее',
+    'saml_user_not_registered' => 'Пользователь :name не зарегистрирован и автоматическая регистрация отключена.',
+    'saml_no_email_address' => 'Не удалось найти адрес электронной почты этого пользователя в данных, предоставленных внешней системой аутентификации',
+    'saml_invalid_response_id' => 'Запрос от внешней системы аутентификации не распознается процессом, запущенным этим приложением. Переход назад после входа в систему может вызвать эту проблему.',
+    'saml_fail_authed' => 'Вход используя :system завершился неудачей, так как система не предоставила успешную авторизацию.',
+    'saml_email_exists' => 'Регистрация не удалась, так как уже есть пользователь с адресом электронной почты \':email\'',
     'social_no_action_defined' => 'Действие не определено',
-    'social_login_bad_response' => "При попытке входа с :socialAccount произошла ошибка: \\n:error",
-    'social_account_in_use' => 'Этот :socialAccount аккаунт уже исопльзуется, попробуйте войти с параметрами :socialAccount.',
+    'social_login_bad_response' => "При попытке входа с :socialAccount произошла ошибка: \n:error",
+    'social_account_in_use' => 'Этот :socialAccount аккаунт уже используется, попробуйте войти с параметрами :socialAccount.',
     'social_account_email_in_use' => 'Электронный ящик :email уже используется. Если у вас уже есть учетная запись, вы можете подключить свою учетную запись :socialAccount из настроек своего профиля.',
     'social_account_existing' => 'Этот :socialAccount уже привязан к вашему профилю.',
     'social_account_already_used_existing' => 'Этот :socialAccount уже используется другим пользователем.',
@@ -36,14 +36,14 @@ return [
     'invite_token_expired' => 'Срок действия приглашения истек. Вместо этого вы можете попытаться сбросить пароль своей учетной записи.',
 
     // System
-    'path_not_writable' => 'Невозможно загрузить файл по пути :filePath . Убедитесь что сервер доступен для записи.',
+    'path_not_writable' => 'Невозможно загрузить файл по пути \':filePath\'. Убедитесь что папка доступна для записи.',
     'cannot_get_image_from_url' => 'Не удается получить изображение из :url',
     'cannot_create_thumbs' => 'Сервер не может создавать эскизы. Убедитесь, что у вас установлено расширение GD PHP.',
-    'server_upload_limit' => 'Сервер не разрешает загрузку такого размера. Попробуйте уменьшить размер файла.',
-    'uploaded'  => 'Сервер не позволяет загружать файлы такого размера. Пожалуйста, попробуйте файл меньше.',
+    'server_upload_limit' => 'Сервер не позволяет загружать файлы такого размера. Необходмо уменьшить размер загружаемого файла.',
+    'uploaded'  => 'Сервер не позволяет загружать файлы такого размера. Необходимо уменьшить размер загружаемого файла.',
     'image_upload_error' => 'Произошла ошибка при загрузке изображения.',
     'image_upload_type_error' => 'Неправильный тип загружаемого изображения',
-    'file_upload_timeout' => 'Выгрузка файла закончилась.',
+    'file_upload_timeout' => 'Время загрузки файла истекло.',
 
     // Attachments
     'attachment_page_mismatch' => 'Несоответствие страницы во время обновления вложения',
@@ -61,7 +61,7 @@ return [
     'chapter_not_found' => 'Глава не найдена',
     'selected_book_not_found' => 'Выбранная книга не найдена',
     'selected_book_chapter_not_found' => 'Выбранная книга или глава не найдена',
-    'guests_cannot_save_drafts' => 'Гости не могут сохранить черновики',
+    'guests_cannot_save_drafts' => 'Гости не могут сохранять черновики',
 
     // Users
     'users_cannot_delete_only_admin' => 'Вы не можете удалить единственного администратора',
@@ -70,8 +70,8 @@ return [
     // Roles
     'role_cannot_be_edited' => 'Невозможно отредактировать данную роль',
     'role_system_cannot_be_deleted' => 'Эта роль является системной и не может быть удалена',
-    'role_registration_default_cannot_delete' => 'Эта роль не может быть удалена, так как она устанолена в качестве роли по умолчанию',
-    'role_cannot_remove_only_admin' => 'Этот пользователь единственный с правами администратора. Назначьте роль администратора другому пользователю, прежде чем удалить этого.',
+    'role_registration_default_cannot_delete' => 'Эта роль не может быть удалена, так как она установлена в качестве роли по умолчанию',
+    'role_cannot_remove_only_admin' => 'Это единственный пользователь с правами администратора. Назначьте роль администратора другому пользователю, прежде чем удалить этого.',
 
     // Comments
     'comment_list' => 'При получении комментариев произошла ошибка.',
