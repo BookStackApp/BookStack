c643272b69711f6c2369637f2a56ecc77805ac04
SoarinFerret <thecodye@kanto.cloud>
2020-01-14 13:50:29 -0600

    Add close icon to notifications

--- resources/views/partials/notifications.blade.php
+++ resources/views/partials/notifications.blade.php
@@ -1,11 +1,11 @@
 <div notification="success" style="display: none;" data-autohide class="pos" role="alert" @if(session()->has('success')) data-show @endif>
-    @icon('check-circle') <span>{!! nl2br(htmlentities(session()->get('success'))) !!}</span>
+    @icon('check-circle') <span>{!! nl2br(htmlentities(session()->get('success'))) !!}</span><div class="dismiss">@icon('close')</div>
 </div>
 
 <div notification="warning" style="display: none;" class="warning" role="alert" @if(session()->has('warning')) data-show @endif>
-    @icon('info') <span>{!! nl2br(htmlentities(session()->get('warning'))) !!}</span>
+    @icon('info') <span>{!! nl2br(htmlentities(session()->get('warning'))) !!}</span><div class="dismiss">@icon('close')</div>
 </div>
 
 <div notification="error" style="display: none;" class="neg" role="alert" @if(session()->has('error')) data-show @endif>
-    @icon('danger') <span>{!! nl2br(htmlentities(session()->get('error'))) !!}</span>
+    @icon('danger') <span>{!! nl2br(htmlentities(session()->get('error'))) !!}</span><div class="dismiss">@icon('close')</div>
 </div>
