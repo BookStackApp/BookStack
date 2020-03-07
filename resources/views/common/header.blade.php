b58110000d818e6b5c7c955ae35ef7a58516fa64
Statium <statium@inbox.lv>
2020-03-07 14:43:42 +0500

    Code refactoring
    Removed extra spaces displayed in the header of the login and registration link.

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -25,7 +25,7 @@
         </div>
 
         <div class="text-right">
-            <nav class="header-links" >
+            <nav class="header-links">
                 <div class="links text-center">
                     @if (hasAppAccess())
                         <a class="hide-over-l" href="{{ url('/search') }}">@icon('search'){{ trans('common.search') }}</a>
@@ -43,9 +43,9 @@
 
                     @if(!signedInUser())
                         @if(setting('registration-enabled') && config('auth.method') === 'standard')
-                            <a href="{{ url('/register') }}">@icon('new-user') {{ trans('auth.sign_up') }}</a>
+                            <a href="{{ url('/register') }}">@icon('new-user'){{ trans('auth.sign_up') }}</a>
                         @endif
-                        <a href="{{ url('/login') }}">@icon('login') {{ trans('auth.log_in') }}</a>
+                        <a href="{{ url('/login') }}">@icon('login'){{ trans('auth.log_in') }}</a>
                     @endif
                 </div>
                 @if(signedInUser())
