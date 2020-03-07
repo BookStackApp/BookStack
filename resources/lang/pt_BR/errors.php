94235393fa72afbca1784376d0a459232a5ff7b5
Dan Brown <ssddanbrown@googlemail.com>
2020-03-07 18:49:01 +0000

    New translations errors.php (Portuguese, Brazilian)

--- resources/lang/pt_BR/errors.php
+++ resources/lang/pt_BR/errors.php
@@ -97,6 +97,6 @@ return [
     'api_user_token_expired' => 'O token de autenticação expirou',
 
     // Settings & Maintenance
-    'maintenance_test_email_failure' => 'Error thrown when sending a test email:',
+    'maintenance_test_email_failure' => 'Erro encontrado ao enviar um e-mail de teste:',
 
 ];
