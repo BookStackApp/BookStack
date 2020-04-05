68753476a3d9987950832e5352828efbd76d3863
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:39:41 +0100

    New translations activities.php (Slovak)

--- resources/lang/sk/activities.php
+++ resources/lang/sk/activities.php
@@ -7,33 +7,33 @@ return [
 
     // Pages
     'page_create'                 => 'vytvoril stránku',
-    'page_create_notification'    => 'Stránka úspešne vytvorená',
+    'page_create_notification'    => 'Page Successfully Created',
     'page_update'                 => 'aktualizoval stránku',
-    'page_update_notification'    => 'Stránka úspešne aktualizovaná',
+    'page_update_notification'    => 'Page Successfully Updated',
     'page_delete'                 => 'odstránil stránku',
-    'page_delete_notification'    => 'Stránka úspešne odstránená',
+    'page_delete_notification'    => 'Page Successfully Deleted',
     'page_restore'                => 'obnovil stránku',
-    'page_restore_notification'   => 'Stránka úspešne obnovená',
+    'page_restore_notification'   => 'Page Successfully Restored',
     'page_move'                   => 'presunul stránku',
 
     // Chapters
     'chapter_create'              => 'vytvoril kapitolu',
-    'chapter_create_notification' => 'Kapitola úspešne vytvorená',
+    'chapter_create_notification' => 'Chapter Successfully Created',
     'chapter_update'              => 'aktualizoval kapitolu',
-    'chapter_update_notification' => 'Kapitola úspešne aktualizovaná',
+    'chapter_update_notification' => 'Chapter Successfully Updated',
     'chapter_delete'              => 'odstránil kapitolu',
-    'chapter_delete_notification' => 'Kapitola úspešne odstránená',
+    'chapter_delete_notification' => 'Chapter Successfully Deleted',
     'chapter_move'                => 'presunul kapitolu',
 
     // Books
     'book_create'                 => 'vytvoril knihu',
-    'book_create_notification'    => 'Kniha úspešne vytvorená',
+    'book_create_notification'    => 'Book Successfully Created',
     'book_update'                 => 'aktualizoval knihu',
-    'book_update_notification'    => 'Kniha úspešne aktualizovaná',
+    'book_update_notification'    => 'Book Successfully Updated',
     'book_delete'                 => 'odstránil knihu',
-    'book_delete_notification'    => 'Kniha úspešne odstránená',
+    'book_delete_notification'    => 'Book Successfully Deleted',
     'book_sort'                   => 'zoradil knihu',
-    'book_sort_notification'      => 'Kniha úspešne znovu zoradená',
+    'book_sort_notification'      => 'Book Successfully Re-sorted',
 
     // Bookshelves
     'bookshelf_create'            => 'created Bookshelf',
