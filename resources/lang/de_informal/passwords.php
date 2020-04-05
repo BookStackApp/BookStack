f34fa8696abb34342b114e85da3c951032323a97
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:40:24 +0100

    New translations passwords.php (German Informal)

--- resources/lang/de_informal/passwords.php
+++ resources/lang/de_informal/passwords.php
@@ -6,10 +6,10 @@
  */
 return [
 
-    'password' => 'Passwörter müssen aus mindestens sechs Zeichen bestehen und mit der eingegebenen Wiederholung übereinstimmen.',
-    'user' => "Es wurde kein Benutzer mit dieser E-Mail-Adresse gefunden.",
-    'token' => 'Dieser Link zum Zurücksetzen des Passwortes ist ungültig!',
-    'sent' => 'Der Link zum Zurücksetzen Ihres Passwortes wurde Ihnen per E-Mail zugesendet.',
-    'reset' => 'Ihr Passwort wurde zurückgesetzt!',
+    'password' => 'Passwords must be at least eight characters and match the confirmation.',
+    'user' => "We can't find a user with that e-mail address.",
+    'token' => 'This password reset token is invalid.',
+    'sent' => 'We have e-mailed your password reset link!',
+    'reset' => 'Your password has been reset!',
 
 ];
