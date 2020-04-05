ada4e4c5991fbebda700c2077a68849a0bc5c522
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:39:34 +0100

    New translations activities.php (Russian)

--- resources/lang/ru/activities.php
+++ resources/lang/ru/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'создал страницу',
-    'page_create_notification'    => 'Страница успешно создана',
+    'page_create_notification'    => 'Page Successfully Created',
     'page_update'                 => 'обновил страницу',
-    'page_update_notification'    => 'Страница успешно обновлена',
+    'page_update_notification'    => 'Page Successfully Updated',
     'page_delete'                 => 'удалил страницу',
-    'page_delete_notification'    => 'Страница успешно удалена',
+    'page_delete_notification'    => 'Page Successfully Deleted',
     'page_restore'                => 'восстановил страницу',
-    'page_restore_notification'   => 'Страница успешно восстановлена',
+    'page_restore_notification'   => 'Page Successfully Restored',
     'page_move'                   => 'переместил страницу',
 
     // Chapters
     'chapter_create'              => 'создал главу',
-    'chapter_create_notification' => 'Глава успешно создана',
+    'chapter_create_notification' => 'Chapter Successfully Created',
     'chapter_update'              => 'обновил главу',
-    'chapter_update_notification' => 'Глава успешно обновлена',
+    'chapter_update_notification' => 'Chapter Successfully Updated',
     'chapter_delete'              => 'удалил главу',
-    'chapter_delete_notification' => 'Глава успешно удалена',
+    'chapter_delete_notification' => 'Chapter Successfully Deleted',
     'chapter_move'                => 'переместил главу',
 
     // Books
     'book_create'                 => 'создал книгу',
-    'book_create_notification'    => 'Книга успешно создана',
+    'book_create_notification'    => 'Book Successfully Created',
     'book_update'                 => 'обновил книгу',
-    'book_update_notification'    => 'Книга успешно обновлена',
+    'book_update_notification'    => 'Book Successfully Updated',
     'book_delete'                 => 'удалил книгу',
-    'book_delete_notification'    => 'Книга успешно удалена',
+    'book_delete_notification'    => 'Book Successfully Deleted',
     'book_sort'                   => 'отсортировал книгу',
-    'book_sort_notification'      => 'Книга успешно отсортирована',
+    'book_sort_notification'      => 'Book Successfully Re-sorted',
 
     // Bookshelves
-    'bookshelf_create'            => 'создал полку',
-    'bookshelf_create_notification'    => 'Полка успешно создана',
-    'bookshelf_update'                 => 'обновил полку',
-    'bookshelf_update_notification'    => 'Полка успешно обновлена',
-    'bookshelf_delete'                 => 'удалил полку',
-    'bookshelf_delete_notification'    => 'Полка успешно удалена',
+    'bookshelf_create'            => 'создал книжную полку',
+    'bookshelf_create_notification'    => 'Bookshelf Successfully Created',
+    'bookshelf_update'                 => 'обновил книжную полку',
+    'bookshelf_update_notification'    => 'Bookshelf Successfully Updated',
+    'bookshelf_delete'                 => 'удалил книжную полку',
+    'bookshelf_delete_notification'    => 'Bookshelf Successfully Deleted',
 
     // Other
     'commented_on'                => 'прокомментировал',
