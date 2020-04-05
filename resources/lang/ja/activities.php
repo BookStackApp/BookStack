2c1d6159f5647c75ebb09cf131d83d5bc488aa85
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:43:10 +0100

    New translations activities.php (Japanese)

--- resources/lang/ja/activities.php
+++ resources/lang/ja/activities.php
@@ -7,33 +7,33 @@ return [
 
     // Pages
     'page_create'                 => 'がページを作成:',
-    'page_create_notification'    => 'ページを作成しました',
+    'page_create_notification'    => 'Page Successfully Created',
     'page_update'                 => 'がページを更新:',
-    'page_update_notification'    => 'ページを更新しました',
+    'page_update_notification'    => 'Page Successfully Updated',
     'page_delete'                 => 'がページを削除:',
-    'page_delete_notification'    => 'ページを削除しました',
+    'page_delete_notification'    => 'Page Successfully Deleted',
     'page_restore'                => 'がページを復元:',
-    'page_restore_notification'   => 'ページを復元しました',
+    'page_restore_notification'   => 'Page Successfully Restored',
     'page_move'                   => 'がページを移動:',
 
     // Chapters
     'chapter_create'              => 'がチャプターを作成:',
-    'chapter_create_notification' => 'チャプターを作成しました',
+    'chapter_create_notification' => 'Chapter Successfully Created',
     'chapter_update'              => 'がチャプターを更新:',
-    'chapter_update_notification' => 'チャプターを更新しました',
+    'chapter_update_notification' => 'Chapter Successfully Updated',
     'chapter_delete'              => 'がチャプターを削除:',
-    'chapter_delete_notification' => 'チャプターを削除しました',
+    'chapter_delete_notification' => 'Chapter Successfully Deleted',
     'chapter_move'                => 'がチャプターを移動:',
 
     // Books
     'book_create'                 => 'がブックを作成:',
-    'book_create_notification'    => 'ブックを作成しました',
+    'book_create_notification'    => 'Book Successfully Created',
     'book_update'                 => 'がブックを更新:',
-    'book_update_notification'    => 'ブックを更新しました',
+    'book_update_notification'    => 'Book Successfully Updated',
     'book_delete'                 => 'がブックを削除:',
-    'book_delete_notification'    => 'ブックを削除しました',
+    'book_delete_notification'    => 'Book Successfully Deleted',
     'book_sort'                   => 'がブックの並び順を変更:',
-    'book_sort_notification'      => '並び順を変更しました',
+    'book_sort_notification'      => 'Book Successfully Re-sorted',
 
     // Bookshelves
     'bookshelf_create'            => 'created Bookshelf',
