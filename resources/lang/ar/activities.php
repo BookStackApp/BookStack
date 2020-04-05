129f140f3e86e9abf5a0ab5c4fd44c9d18f7163e
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:45:29 +0100

    New translations activities.php (Arabic)

--- resources/lang/ar/activities.php
+++ resources/lang/ar/activities.php
@@ -7,33 +7,33 @@ return [
 
     // Pages
     'page_create'                 => 'تم إنشاء صفحة',
-    'page_create_notification'    => 'Page Successfully Created',
+    'page_create_notification'    => 'تم إنشاء الصفحة بنجاح',
     'page_update'                 => 'تم تحديث الصفحة',
-    'page_update_notification'    => 'Page Successfully Updated',
+    'page_update_notification'    => 'تم تحديث الصفحة بنجاح',
     'page_delete'                 => 'تم حذف الصفحة',
-    'page_delete_notification'    => 'Page Successfully Deleted',
+    'page_delete_notification'    => 'تم حذف الصفحة بنجاح',
     'page_restore'                => 'تمت استعادة الصفحة',
-    'page_restore_notification'   => 'Page Successfully Restored',
+    'page_restore_notification'   => 'تمت استعادة الصفحة بنجاح',
     'page_move'                   => 'تم نقل الصفحة',
 
     // Chapters
     'chapter_create'              => 'تم إنشاء فصل',
-    'chapter_create_notification' => 'Chapter Successfully Created',
+    'chapter_create_notification' => 'تم إنشاء فصل بنجاح',
     'chapter_update'              => 'تم تحديث الفصل',
-    'chapter_update_notification' => 'Chapter Successfully Updated',
+    'chapter_update_notification' => 'تم تحديث الفصل بنجاح',
     'chapter_delete'              => 'تم حذف الفصل',
-    'chapter_delete_notification' => 'Chapter Successfully Deleted',
+    'chapter_delete_notification' => 'تم حذف الفصل بنجاح',
     'chapter_move'                => 'تم نقل الفصل',
 
     // Books
     'book_create'                 => 'تم إنشاء كتاب',
-    'book_create_notification'    => 'Book Successfully Created',
+    'book_create_notification'    => 'تم إنشاء كتاب بنجاح',
     'book_update'                 => 'تم تحديث الكتاب',
-    'book_update_notification'    => 'Book Successfully Updated',
+    'book_update_notification'    => 'تم تحديث الكتاب بنجاح',
     'book_delete'                 => 'تم حذف الكتاب',
-    'book_delete_notification'    => 'Book Successfully Deleted',
+    'book_delete_notification'    => 'تم حذف الكتاب بنجاح',
     'book_sort'                   => 'تم سرد الكتاب',
-    'book_sort_notification'      => 'Book Successfully Re-sorted',
+    'book_sort_notification'      => 'تمت إعادة سرد الكتاب بنجاح',
 
     // Bookshelves
     'bookshelf_create'            => 'created Bookshelf',
