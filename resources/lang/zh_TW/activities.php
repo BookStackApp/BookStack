831577462693f5ad84b49a686783fac2afbcebbb
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:36:41 +0100

    New translations activities.php (Chinese Traditional)

--- resources/lang/zh_TW/activities.php
+++ resources/lang/zh_TW/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => '建立了頁面',
-    'page_create_notification'    => '頁面已建立成功',
+    'page_create_notification'    => 'Page Successfully Created',
     'page_update'                 => '更新了頁面',
-    'page_update_notification'    => '頁面已更新成功',
+    'page_update_notification'    => 'Page Successfully Updated',
     'page_delete'                 => '刪除了頁面',
-    'page_delete_notification'    => '頁面已刪除成功',
+    'page_delete_notification'    => 'Page Successfully Deleted',
     'page_restore'                => '恢複了頁面',
-    'page_restore_notification'   => '頁面已恢複成功',
+    'page_restore_notification'   => 'Page Successfully Restored',
     'page_move'                   => '移動了頁面',
 
     // Chapters
     'chapter_create'              => '建立了章節',
-    'chapter_create_notification' => '章節已建立成功',
+    'chapter_create_notification' => 'Chapter Successfully Created',
     'chapter_update'              => '更新了章節',
-    'chapter_update_notification' => '章節已建立成功',
+    'chapter_update_notification' => 'Chapter Successfully Updated',
     'chapter_delete'              => '刪除了章節',
-    'chapter_delete_notification' => '章節已刪除成功',
+    'chapter_delete_notification' => 'Chapter Successfully Deleted',
     'chapter_move'                => '移動了章節',
 
     // Books
     'book_create'                 => '建立了圖書',
-    'book_create_notification'    => '圖書已建立成功',
+    'book_create_notification'    => 'Book Successfully Created',
     'book_update'                 => '更新了圖書',
-    'book_update_notification'    => '圖書已更新成功',
+    'book_update_notification'    => 'Book Successfully Updated',
     'book_delete'                 => '刪除了圖書',
-    'book_delete_notification'    => '圖書已刪除成功',
+    'book_delete_notification'    => 'Book Successfully Deleted',
     'book_sort'                   => '排序了圖書',
-    'book_sort_notification'      => '圖書已重新排序成功',
+    'book_sort_notification'      => 'Book Successfully Re-sorted',
 
     // Bookshelves
     'bookshelf_create'            => '建立了書架',
-    'bookshelf_create_notification'    => '書架已建立成功',
+    'bookshelf_create_notification'    => 'Bookshelf Successfully Created',
     'bookshelf_update'                 => '更新了書架',
-    'bookshelf_update_notification'    => '書架已更新成功',
+    'bookshelf_update_notification'    => 'Bookshelf Successfully Updated',
     'bookshelf_delete'                 => '刪除了書架',
-    'bookshelf_delete_notification'    => '書架已刪除成功',
+    'bookshelf_delete_notification'    => 'Bookshelf Successfully Deleted',
 
     // Other
     'commented_on'                => '評論',
