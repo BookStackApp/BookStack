5f50778c5ab5ce66c8204124ffaad741e179dde5
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:42:58 +0100

    New translations activities.php (Italian)

--- resources/lang/it/activities.php
+++ resources/lang/it/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'ha creato la pagina',
-    'page_create_notification'    => 'Pagina Creata Correttamente',
+    'page_create_notification'    => 'Page Successfully Created',
     'page_update'                 => 'ha aggiornato la pagina',
-    'page_update_notification'    => 'Pagina Aggiornata Correttamente',
+    'page_update_notification'    => 'Page Successfully Updated',
     'page_delete'                 => 'ha eliminato la pagina',
-    'page_delete_notification'    => 'Pagina Eliminata Correttamente',
+    'page_delete_notification'    => 'Page Successfully Deleted',
     'page_restore'                => 'ha ripristinato la pagina',
-    'page_restore_notification'   => 'Pagina Ripristinata Correttamente',
+    'page_restore_notification'   => 'Page Successfully Restored',
     'page_move'                   => 'ha mosso la pagina',
 
     // Chapters
     'chapter_create'              => 'ha creato il capitolo',
-    'chapter_create_notification' => 'Capitolo Creato Correttamente',
+    'chapter_create_notification' => 'Chapter Successfully Created',
     'chapter_update'              => 'ha aggiornato il capitolo',
-    'chapter_update_notification' => 'Capitolo Aggiornato Correttamente',
+    'chapter_update_notification' => 'Chapter Successfully Updated',
     'chapter_delete'              => 'ha eliminato il capitolo',
-    'chapter_delete_notification' => 'Capitolo Eliminato Correttamente',
+    'chapter_delete_notification' => 'Chapter Successfully Deleted',
     'chapter_move'                => 'ha spostato il capitolo',
 
     // Books
     'book_create'                 => 'ha creato il libro',
-    'book_create_notification'    => 'Libro Creato Correttamente',
+    'book_create_notification'    => 'Book Successfully Created',
     'book_update'                 => 'ha aggiornato il libro',
-    'book_update_notification'    => 'Libro Aggiornato Correttamente',
+    'book_update_notification'    => 'Book Successfully Updated',
     'book_delete'                 => 'ha eliminato il libro',
-    'book_delete_notification'    => 'Libro Eliminato Correttamente',
+    'book_delete_notification'    => 'Book Successfully Deleted',
     'book_sort'                   => 'ha ordinato il libro',
-    'book_sort_notification'      => 'Libro Riordinato Correttamente',
+    'book_sort_notification'      => 'Book Successfully Re-sorted',
 
     // Bookshelves
     'bookshelf_create'            => 'ha creato la Libreria',
-    'bookshelf_create_notification'    => 'Libreria Creata Correttamente',
+    'bookshelf_create_notification'    => 'Bookshelf Successfully Created',
     'bookshelf_update'                 => 'ha aggiornato la libreria',
-    'bookshelf_update_notification'    => 'Libreria Aggiornata Correttamente',
+    'bookshelf_update_notification'    => 'Bookshelf Successfully Updated',
     'bookshelf_delete'                 => 'ha eliminato la libreria',
-    'bookshelf_delete_notification'    => 'Libreria Eliminata Correttamente',
+    'bookshelf_delete_notification'    => 'Bookshelf Successfully Deleted',
 
     // Other
     'commented_on'                => 'ha commentato in',
