0c0e281b0c59c48e6f2d84abb4cf0c6aea9d56b1
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:46:07 +0100

    New translations activities.php (Czech)

--- resources/lang/cs/activities.php
+++ resources/lang/cs/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'vytvořená stránka',
-    'page_create_notification'    => 'Page Successfully Created',
+    'page_create_notification'    => 'Stránka byla úspěšně vytvořena',
     'page_update'                 => 'aktualizovaná stránka',
-    'page_update_notification'    => 'Page Successfully Updated',
+    'page_update_notification'    => 'Stránka byla úspěšně aktualizována',
     'page_delete'                 => 'smazaná stránka',
-    'page_delete_notification'    => 'Page Successfully Deleted',
+    'page_delete_notification'    => 'Stránka byla úspěšně smazána',
     'page_restore'                => 'renovovaná stránka',
-    'page_restore_notification'   => 'Page Successfully Restored',
+    'page_restore_notification'   => 'Stránka byla úspěšně renovována',
     'page_move'                   => 'přesunutá stránka',
 
     // Chapters
     'chapter_create'              => 'vytvořená kapitola',
-    'chapter_create_notification' => 'Chapter Successfully Created',
+    'chapter_create_notification' => 'Kapitola byla úspěšně vytvořena',
     'chapter_update'              => 'aktualizovaná kapitola',
-    'chapter_update_notification' => 'Chapter Successfully Updated',
+    'chapter_update_notification' => 'Kapitola byla úspěšně aktualizována',
     'chapter_delete'              => 'smazaná kapitola',
-    'chapter_delete_notification' => 'Chapter Successfully Deleted',
+    'chapter_delete_notification' => 'Kapitola byla úspěšně smazána',
     'chapter_move'                => 'přesunutá kapitola',
 
     // Books
     'book_create'                 => 'vytvořená kniha',
-    'book_create_notification'    => 'Book Successfully Created',
+    'book_create_notification'    => 'Kniha byla úspěšně vytvořena',
     'book_update'                 => 'aktualizovaná kniha',
-    'book_update_notification'    => 'Book Successfully Updated',
+    'book_update_notification'    => 'Kniha byla úspěšně aktualizována',
     'book_delete'                 => 'smazaná kniha',
-    'book_delete_notification'    => 'Book Successfully Deleted',
+    'book_delete_notification'    => 'Kniha byla úspěšně smazána',
     'book_sort'                   => 'seřazená kniha',
-    'book_sort_notification'      => 'Book Successfully Re-sorted',
+    'book_sort_notification'      => 'Kniha byla úspěšně seřazena',
 
     // Bookshelves
     'bookshelf_create'            => 'vytvořená knihovna',
-    'bookshelf_create_notification'    => 'Bookshelf Successfully Created',
+    'bookshelf_create_notification'    => 'Knihovna úspěšně vytvořena',
     'bookshelf_update'                 => 'aktualizovaná knihovna',
-    'bookshelf_update_notification'    => 'Bookshelf Successfully Updated',
+    'bookshelf_update_notification'    => 'Knihovna byla úspěšně aktualizována',
     'bookshelf_delete'                 => 'smazaná knihovna',
-    'bookshelf_delete_notification'    => 'Bookshelf Successfully Deleted',
+    'bookshelf_delete_notification'    => 'Knihovna byla úspěšně smazána',
 
     // Other
     'commented_on'                => 'okomentováno v',
