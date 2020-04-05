9e2e95c01e5e88b2ca09657ae83362d23417cc55
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:37:37 +0100

    New translations activities.php (Hungarian)

--- resources/lang/hu/activities.php
+++ resources/lang/hu/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'létrehozta az oldalt:',
-    'page_create_notification'    => 'Oldal sikeresen létrehozva',
+    'page_create_notification'    => 'Page Successfully Created',
     'page_update'                 => 'frissítette az oldalt:',
-    'page_update_notification'    => 'Oldal sikeresen frissítve',
+    'page_update_notification'    => 'Page Successfully Updated',
     'page_delete'                 => 'törölte az oldalt:',
-    'page_delete_notification'    => 'Oldal sikeresen törölve',
+    'page_delete_notification'    => 'Page Successfully Deleted',
     'page_restore'                => 'visszaállította az oldalt:',
-    'page_restore_notification'   => 'Oldal sikeresen visszaállítva',
+    'page_restore_notification'   => 'Page Successfully Restored',
     'page_move'                   => 'áthelyezte az oldalt:',
 
     // Chapters
     'chapter_create'              => 'létrehozta a fejezetet:',
-    'chapter_create_notification' => 'Fejezet sikeresen létrehozva',
+    'chapter_create_notification' => 'Chapter Successfully Created',
     'chapter_update'              => 'frissítette a fejezetet:',
-    'chapter_update_notification' => 'Fejezet sikeresen frissítve',
+    'chapter_update_notification' => 'Chapter Successfully Updated',
     'chapter_delete'              => 'törölte a fejezetet:',
-    'chapter_delete_notification' => 'Fejezet sikeresen törölve',
+    'chapter_delete_notification' => 'Chapter Successfully Deleted',
     'chapter_move'                => 'áthelyezte a fejezetet:',
 
     // Books
     'book_create'                 => 'létrehozott egy könyvet:',
-    'book_create_notification'    => 'Könyv sikeresen létrehozva',
+    'book_create_notification'    => 'Book Successfully Created',
     'book_update'                 => 'frissítette a könyvet:',
-    'book_update_notification'    => 'Könyv sikeresen frissítve',
+    'book_update_notification'    => 'Book Successfully Updated',
     'book_delete'                 => 'törölte a könyvet:',
-    'book_delete_notification'    => 'Könyv sikeresen törölve',
+    'book_delete_notification'    => 'Book Successfully Deleted',
     'book_sort'                   => 'átrendezte a könyvet:',
-    'book_sort_notification'      => 'Könyv sikeresen újrarendezve',
+    'book_sort_notification'      => 'Book Successfully Re-sorted',
 
     // Bookshelves
     'bookshelf_create'            => 'létrehozta a könyvespolcot:',
-    'bookshelf_create_notification'    => 'Könyvespolc sikeresen létrehozva',
+    'bookshelf_create_notification'    => 'Bookshelf Successfully Created',
     'bookshelf_update'                 => 'frissítette a könyvespolcot:',
-    'bookshelf_update_notification'    => 'Könyvespolc sikeresen frissítve',
+    'bookshelf_update_notification'    => 'Bookshelf Successfully Updated',
     'bookshelf_delete'                 => 'törölte a könyvespolcot:',
-    'bookshelf_delete_notification'    => 'Könyvespolc sikeresen törölve',
+    'bookshelf_delete_notification'    => 'Bookshelf Successfully Deleted',
 
     // Other
     'commented_on'                => 'megjegyzést fűzött hozzá:',
