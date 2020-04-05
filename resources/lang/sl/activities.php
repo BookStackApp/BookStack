b5b15a27fd4db517053844bfb3b1f0302047e048
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:41:21 +0100

    New translations activities.php (Slovenian)

--- resources/lang/sl/activities.php
+++ resources/lang/sl/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'ustvarjena stran',
-    'page_create_notification'    => 'Zapis uspešno ustvarjen',
+    'page_create_notification'    => 'Page Successfully Created',
     'page_update'                 => 'nadgrajena stran',
-    'page_update_notification'    => 'Uspešno posodobljeno',
+    'page_update_notification'    => 'Page Successfully Updated',
     'page_delete'                 => 'izbrisana stran',
-    'page_delete_notification'    => 'Uspešno izbrisano',
+    'page_delete_notification'    => 'Page Successfully Deleted',
     'page_restore'                => 'obnovljena stran',
-    'page_restore_notification'   => 'Uspešna obnovitev',
+    'page_restore_notification'   => 'Page Successfully Restored',
     'page_move'                   => 'premaknjena stran',
 
     // Chapters
     'chapter_create'              => 'ustvarjeno poglavje',
-    'chapter_create_notification' => 'Zapis uspešno ustvarjen',
+    'chapter_create_notification' => 'Chapter Successfully Created',
     'chapter_update'              => 'nadgradi poglavje',
-    'chapter_update_notification' => 'Uspešno posodobljeno',
+    'chapter_update_notification' => 'Chapter Successfully Updated',
     'chapter_delete'              => 'izbrisano poglavje',
-    'chapter_delete_notification' => 'Uspešno izbrisano',
+    'chapter_delete_notification' => 'Chapter Successfully Deleted',
     'chapter_move'                => 'premaknjeno poglavje',
 
     // Books
     'book_create'                 => 'knjiga ustvarjena',
-    'book_create_notification'    => 'Knjiga Uspešno Usvarjena',
+    'book_create_notification'    => 'Book Successfully Created',
     'book_update'                 => 'knjiga posodobljena',
-    'book_update_notification'    => 'Uspešno posodobljeno',
+    'book_update_notification'    => 'Book Successfully Updated',
     'book_delete'                 => 'izbrisana knjiga',
-    'book_delete_notification'    => 'Uspešno izbrisano',
+    'book_delete_notification'    => 'Book Successfully Deleted',
     'book_sort'                   => 'razvrščena knjiga',
-    'book_sort_notification'      => 'Knjiga Uspešno Razvrščena',
+    'book_sort_notification'      => 'Book Successfully Re-sorted',
 
     // Bookshelves
     'bookshelf_create'            => 'knjižna polica izdelana',
-    'bookshelf_create_notification'    => 'Knjižna Polica Izdelana',
+    'bookshelf_create_notification'    => 'Bookshelf Successfully Created',
     'bookshelf_update'                 => 'knjižna polica posodobljena',
-    'bookshelf_update_notification'    => 'Knjižna Polica Uspešno Posodobljena',
+    'bookshelf_update_notification'    => 'Bookshelf Successfully Updated',
     'bookshelf_delete'                 => 'knjižna polica izbrisana',
-    'bookshelf_delete_notification'    => 'Knjižna Polica Uspešno Izbrisana',
+    'bookshelf_delete_notification'    => 'Bookshelf Successfully Deleted',
 
     // Other
     'commented_on'                => 'komentar na',
