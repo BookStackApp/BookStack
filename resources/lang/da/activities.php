9ee2e8c2577eded8d005e497b65ef7d8fd7aa47e
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:37:02 +0100

    New translations activities.php (Danish)

--- resources/lang/da/activities.php
+++ resources/lang/da/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'oprettede side',
-    'page_create_notification'    => 'Siden blev oprettet',
+    'page_create_notification'    => 'Page Successfully Created',
     'page_update'                 => 'opdaterede side',
-    'page_update_notification'    => 'Siden blev opdateret',
+    'page_update_notification'    => 'Page Successfully Updated',
     'page_delete'                 => 'slettede side',
-    'page_delete_notification'    => 'Siden blev slettet',
+    'page_delete_notification'    => 'Page Successfully Deleted',
     'page_restore'                => 'gendannede side',
-    'page_restore_notification'   => 'Siden blev gendannet',
+    'page_restore_notification'   => 'Page Successfully Restored',
     'page_move'                   => 'flyttede side',
 
     // Chapters
     'chapter_create'              => 'oprettede kapitel',
-    'chapter_create_notification' => 'Kapitel blev oprettet',
+    'chapter_create_notification' => 'Chapter Successfully Created',
     'chapter_update'              => 'opdaterede kapitel',
-    'chapter_update_notification' => 'Kapitel blev opdateret',
+    'chapter_update_notification' => 'Chapter Successfully Updated',
     'chapter_delete'              => 'slettede kapitel',
-    'chapter_delete_notification' => 'Kapitel blev slettet',
+    'chapter_delete_notification' => 'Chapter Successfully Deleted',
     'chapter_move'                => 'flyttede kapitel',
 
     // Books
     'book_create'                 => 'oprettede bog',
-    'book_create_notification'    => 'Bogen blev oprettet',
+    'book_create_notification'    => 'Book Successfully Created',
     'book_update'                 => 'opdaterede bog',
-    'book_update_notification'    => 'Bogen blev opdateret',
+    'book_update_notification'    => 'Book Successfully Updated',
     'book_delete'                 => 'slettede bog',
-    'book_delete_notification'    => 'Bogen blev slettet',
+    'book_delete_notification'    => 'Book Successfully Deleted',
     'book_sort'                   => 'sorterede bogen',
-    'book_sort_notification'      => 'Bogen blev re-sorteret',
+    'book_sort_notification'      => 'Book Successfully Re-sorted',
 
     // Bookshelves
     'bookshelf_create'            => 'oprettede bogreol',
-    'bookshelf_create_notification'    => 'Bogreolen blev oprettet',
+    'bookshelf_create_notification'    => 'Bookshelf Successfully Created',
     'bookshelf_update'                 => 'opdaterede bogreolen',
-    'bookshelf_update_notification'    => 'Bogreolen blev opdateret',
+    'bookshelf_update_notification'    => 'Bookshelf Successfully Updated',
     'bookshelf_delete'                 => 'slettede bogreol',
-    'bookshelf_delete_notification'    => 'Bogreolen blev opdateret',
+    'bookshelf_delete_notification'    => 'Bookshelf Successfully Deleted',
 
     // Other
     'commented_on'                => 'kommenterede til',
