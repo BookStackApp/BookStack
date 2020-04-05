bc604f082adcba1b8b309dcee029c6992ed1b67a
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:40:54 +0100

    New translations activities.php (Turkish)

--- resources/lang/tr/activities.php
+++ resources/lang/tr/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'saya oluşturuldu',
-    'page_create_notification'    => 'Sayfa Başarıyla Oluşturuldu',
+    'page_create_notification'    => 'Page Successfully Created',
     'page_update'                 => 'sayfa güncellendi',
-    'page_update_notification'    => 'Sayfa Başarıyla Güncellendi',
+    'page_update_notification'    => 'Page Successfully Updated',
     'page_delete'                 => 'sayfa silindi',
-    'page_delete_notification'    => 'Sayfa Başarıyla Silindi',
+    'page_delete_notification'    => 'Page Successfully Deleted',
     'page_restore'                => 'sayfa kurtarıldı',
-    'page_restore_notification'   => 'Sayfa Başarıyla Kurtarıldı',
+    'page_restore_notification'   => 'Page Successfully Restored',
     'page_move'                   => 'sayfa taşındı',
 
     // Chapters
     'chapter_create'              => 'bölüm oluşturuldu',
-    'chapter_create_notification' => 'Bölüm Başarıyla Oluşturuldu',
+    'chapter_create_notification' => 'Chapter Successfully Created',
     'chapter_update'              => 'bölüm güncellendi',
-    'chapter_update_notification' => 'Bölüm Başarıyla Güncellendi',
+    'chapter_update_notification' => 'Chapter Successfully Updated',
     'chapter_delete'              => 'bölüm silindi',
-    'chapter_delete_notification' => 'Bölüm Başarıyla Silindi',
+    'chapter_delete_notification' => 'Chapter Successfully Deleted',
     'chapter_move'                => 'bölüm taşındı',
 
     // Books
     'book_create'                 => 'kitap oluşturuldu',
-    'book_create_notification'    => 'Kitap Başarıyla Oluşturuldu',
+    'book_create_notification'    => 'Book Successfully Created',
     'book_update'                 => 'kitap güncellendi',
-    'book_update_notification'    => 'Kitap Başarıyla Güncellendi',
+    'book_update_notification'    => 'Book Successfully Updated',
     'book_delete'                 => 'kitap silindi',
-    'book_delete_notification'    => 'Kitap Başarıyla Silindi',
+    'book_delete_notification'    => 'Book Successfully Deleted',
     'book_sort'                   => 'kitap düzenlendi',
-    'book_sort_notification'      => 'Kitap Başarıyla Yeniden Sıralandı',
+    'book_sort_notification'      => 'Book Successfully Re-sorted',
 
     // Bookshelves
     'bookshelf_create'            => 'kitaplık oluşturuldu',
-    'bookshelf_create_notification'    => 'Kitaplık Başarıyla Oluşturuldu',
+    'bookshelf_create_notification'    => 'Bookshelf Successfully Created',
     'bookshelf_update'                 => 'kitaplık güncellendi',
-    'bookshelf_update_notification'    => 'Kitaplık Başarıyla Güncellendi',
+    'bookshelf_update_notification'    => 'Bookshelf Successfully Updated',
     'bookshelf_delete'                 => 'kitaplık silindi',
-    'bookshelf_delete_notification'    => 'Kitaplık Başarıyla Silindi',
+    'bookshelf_delete_notification'    => 'Bookshelf Successfully Deleted',
 
     // Other
     'commented_on'                => 'yorum yaptı',
