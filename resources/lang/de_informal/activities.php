bd60e6083e03851e25c14592129e4358485ecab6
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:40:14 +0100

    New translations activities.php (German Informal)

--- resources/lang/de_informal/activities.php
+++ resources/lang/de_informal/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'erstellt Seite',
-    'page_create_notification'    => 'Die Seite wurde erfolgreich erstellt.',
+    'page_create_notification'    => 'Page Successfully Created',
     'page_update'                 => 'aktualisiert Seite',
-    'page_update_notification'    => 'Die Seite wurde erfolgreich aktualisiert.',
+    'page_update_notification'    => 'Page Successfully Updated',
     'page_delete'                 => 'löscht Seite',
-    'page_delete_notification'    => 'Die Seite wurde erfolgreich gelöscht.',
+    'page_delete_notification'    => 'Page Successfully Deleted',
     'page_restore'                => 'stellt Seite wieder her',
-    'page_restore_notification'   => 'Die Seite wurde erfolgreich wiederhergestellt.',
+    'page_restore_notification'   => 'Page Successfully Restored',
     'page_move'                   => 'verschiebt Seite',
 
     // Chapters
     'chapter_create'              => 'erstellt Kapitel',
-    'chapter_create_notification' => 'Das Kapitel wurde erfolgreich erstellt.',
+    'chapter_create_notification' => 'Chapter Successfully Created',
     'chapter_update'              => 'aktualisiert Kapitel',
-    'chapter_update_notification' => 'Das Kapitel wurde erfolgreich aktualisiert.',
+    'chapter_update_notification' => 'Chapter Successfully Updated',
     'chapter_delete'              => 'löscht Kapitel',
-    'chapter_delete_notification' => 'Das Kapitel wurde erfolgreich gelöscht.',
+    'chapter_delete_notification' => 'Chapter Successfully Deleted',
     'chapter_move'                => 'verschiebt Kapitel',
 
     // Books
     'book_create'                 => 'erstellt Buch',
-    'book_create_notification'    => 'Das Buch wurde erfolgreich erstellt.',
+    'book_create_notification'    => 'Book Successfully Created',
     'book_update'                 => 'aktualisiert Buch',
-    'book_update_notification'    => 'Das Buch wurde erfolgreich aktualisiert.',
+    'book_update_notification'    => 'Book Successfully Updated',
     'book_delete'                 => 'löscht Buch',
-    'book_delete_notification'    => 'Das Buch wurde erfolgreich gelöscht.',
+    'book_delete_notification'    => 'Book Successfully Deleted',
     'book_sort'                   => 'sortiert Buch',
-    'book_sort_notification'      => 'Das Buch wurde erfolgreich umsortiert.',
+    'book_sort_notification'      => 'Book Successfully Re-sorted',
 
     // Bookshelves
     'bookshelf_create'            => 'erstellt Bücherregal',
-    'bookshelf_create_notification'    => 'Das Bücherregal wurde erfolgreich erstellt',
+    'bookshelf_create_notification'    => 'Bookshelf Successfully Created',
     'bookshelf_update'                 => 'aktualisiert Bücherregal',
-    'bookshelf_update_notification'    => 'Das Bücherregal wurde erfolgreich aktualisiert',
+    'bookshelf_update_notification'    => 'Bookshelf Successfully Updated',
     'bookshelf_delete'                 => 'löscht Bücherregal',
-    'bookshelf_delete_notification'    => 'Das Bücherregal wurde erfolgreich gelöscht',
+    'bookshelf_delete_notification'    => 'Bookshelf Successfully Deleted',
 
     // Other
     'commented_on'                => 'kommentiert',
