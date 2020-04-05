9dc02de087faa6c7dc32899c0c15337ff42f945f
Dan Brown <ssddanbrown@googlemail.com>
2020-04-05 13:42:24 +0100

    New translations activities.php (Korean)

--- resources/lang/ko/activities.php
+++ resources/lang/ko/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => '문서 만들기',
-    'page_create_notification'    => '문서 만듦',
+    'page_create_notification'    => 'Page Successfully Created',
     'page_update'                 => '문서 수정',
-    'page_update_notification'    => '문서 수정함',
+    'page_update_notification'    => 'Page Successfully Updated',
     'page_delete'                 => '문서 지우기',
-    'page_delete_notification'    => '문서 지움',
+    'page_delete_notification'    => 'Page Successfully Deleted',
     'page_restore'                => '문서 복원',
-    'page_restore_notification'   => '문서 복원함',
+    'page_restore_notification'   => 'Page Successfully Restored',
     'page_move'                   => '문서 옮기기',
 
     // Chapters
     'chapter_create'              => '챕터 만들기',
-    'chapter_create_notification' => '챕터 만듦',
+    'chapter_create_notification' => 'Chapter Successfully Created',
     'chapter_update'              => '챕터 바꾸기',
-    'chapter_update_notification' => '챕터 바꿈',
+    'chapter_update_notification' => 'Chapter Successfully Updated',
     'chapter_delete'              => '챕터 지우기',
-    'chapter_delete_notification' => '챕터 지움',
+    'chapter_delete_notification' => 'Chapter Successfully Deleted',
     'chapter_move'                => '챕터 옮기기',
 
     // Books
     'book_create'                 => '책자 만들기',
-    'book_create_notification'    => '책자 만듦',
+    'book_create_notification'    => 'Book Successfully Created',
     'book_update'                 => '책자 바꾸기',
-    'book_update_notification'    => '책자 바꿈',
+    'book_update_notification'    => 'Book Successfully Updated',
     'book_delete'                 => '책자 지우기',
-    'book_delete_notification'    => '책자 지움',
+    'book_delete_notification'    => 'Book Successfully Deleted',
     'book_sort'                   => '책자 정렬',
-    'book_sort_notification'      => '책자 정렬함',
+    'book_sort_notification'      => 'Book Successfully Re-sorted',
 
     // Bookshelves
     'bookshelf_create'            => '서가 만들기',
-    'bookshelf_create_notification'    => '서가 만듦',
+    'bookshelf_create_notification'    => 'Bookshelf Successfully Created',
     'bookshelf_update'                 => '서가 바꾸기',
-    'bookshelf_update_notification'    => '서가 바꿈',
+    'bookshelf_update_notification'    => 'Bookshelf Successfully Updated',
     'bookshelf_delete'                 => '서가 지우기',
-    'bookshelf_delete_notification'    => '서가 지움',
+    'bookshelf_delete_notification'    => 'Bookshelf Successfully Deleted',
 
     // Other
     'commented_on'                => '댓글 쓰기',
