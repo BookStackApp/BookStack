7b8fe5fbc69a6f49c2acff938f1da48c28e19ecd
Dan Brown <ssddanbrown@googlemail.com>
2020-04-10 16:05:17 +0100

    Added book-export endpoints to the API

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,10 @@ Route::get('books/{id}', 'BooksApiController@read');
 Route::put('books/{id}', 'BooksApiController@update');
 Route::delete('books/{id}', 'BooksApiController@delete');
 
+Route::get('books/{id}/export/html', 'BooksExportApiController@exportHtml');
+Route::get('books/{id}/export/pdf', 'BooksExportApiController@exportPdf');
+Route::get('books/{id}/export/plaintext', 'BooksExportApiController@exportPlainText');
+
 Route::get('shelves', 'BookshelfApiController@list');
 Route::post('shelves', 'BookshelfApiController@create');
 Route::get('shelves/{id}', 'BookshelfApiController@read');
