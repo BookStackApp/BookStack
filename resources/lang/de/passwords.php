3957525e33df43c1cafffd40c9ad33dba5fb87b4
Dan Brown <ssddanbrown@googlemail.com>
2020-04-10 14:11:12 +0100

    New translations passwords.php (German)

--- resources/lang/de/passwords.php
+++ resources/lang/de/passwords.php
@@ -8,7 +8,7 @@ return [
 
     'password' => 'Passwörter müssen aus mindestens sechs Zeichen bestehen und mit der eingegebenen Wiederholung übereinstimmen.',
     'user' => "Es wurde kein Benutzer mit dieser E-Mail-Adresse gefunden.",
-    'token' => 'Dieser Link zum Zurücksetzen des Passwortes ist ungültig!',
+    'token' => 'The password reset token is invalid for this email address.',
     'sent' => 'Der Link zum Zurücksetzen Ihres Passwortes wurde Ihnen per E-Mail zugesendet.',
     'reset' => 'Ihr Passwort wurde zurückgesetzt!',
 
