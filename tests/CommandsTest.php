413cac23aee584b0831f3e3fba79e91ee6fac7cf
Dan Brown <ssddanbrown@googlemail.com>
2020-05-01 23:41:47 +0100

    Added command to regenerate comment content

--- tests/CommandsTest.php
+++ tests/CommandsTest.php
@@ -1,5 +1,7 @@
 <?php namespace Tests;
 
+use BookStack\Actions\Comment;
+use BookStack\Actions\CommentRepo;
 use BookStack\Auth\Permissions\JointPermission;
 use BookStack\Entities\Bookshelf;
 use BookStack\Entities\Page;
@@ -193,5 +195,27 @@ class CommandsTest extends TestCase
 
         $this->expectException(RuntimeException::class);
         $this->artisan('bookstack:update-url https://cats.example.com');
+    }
+
+    public function test_regenerate_comment_content_command()
+    {
+        Comment::query()->forceCreate([
+            'html' => 'some_old_content',
+            'text' => 'some_fresh_content',
+        ]);
+
+        $this->assertDatabaseHas('comments', [
+            'html' => 'some_old_content',
+        ]);
+
+        $exitCode = \Artisan::call('bookstack:regenerate-comment-content');
+        $this->assertTrue($exitCode === 0, 'Command executed successfully');
+
+        $this->assertDatabaseMissing('comments', [
+            'html' => 'some_old_content',
+        ]);
+        $this->assertDatabaseHas('comments', [
+            'html' => "<p>some_fresh_content</p>\n",
+        ]);
     }
 }
