c2c0433bc93664f08f867925f1757726517668df
Dan Brown <ssddanbrown@googlemail.com>
2020-05-17 13:30:06 +0100

    New translations passwords.php (Bulgarian)

---
+++ resources/lang/bg/passwords.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Password Reminder Language Lines
+ * The following language lines are the default lines which match reasons
+ * that are given by the password broker for a password update attempt has failed.
+ */
+return [
+
+    'password' => 'Passwords must be at least eight characters and match the confirmation.',
+    'user' => "We can't find a user with that e-mail address.",
+    'token' => 'The password reset token is invalid for this email address.',
+    'sent' => 'We have e-mailed your password reset link!',
+    'reset' => 'Your password has been reset!',
+
+];
