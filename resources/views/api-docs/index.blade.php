082f02ff811aeacb197d482ad3af4a3f9cd6771d
Dan Brown <ssddanbrown@googlemail.com>
2020-05-23 00:53:13 +0100

    Updated listing endpoints to be clickable in api docs

--- resources/views/api-docs/index.blade.php
+++ resources/views/api-docs/index.blade.php
@@ -189,7 +189,11 @@
                             <h6 class="text-uppercase text-muted float right">{{ $endpoint['controller_method_kebab'] }}</h6>
                             <h5 id="{{ $endpoint['name'] }}" class="text-mono mb-m">
                                 <span class="api-method" data-method="{{ $endpoint['method'] }}">{{ $endpoint['method'] }}</span>
-                                {{ url($endpoint['uri']) }}
+                                @if($endpoint['controller_method_kebab'] === 'list')
+                                    <a style="color: inherit;" target="_blank" href="{{ url($endpoint['uri']) }}">{{ url($endpoint['uri']) }}</a>
+                                @else
+                                    {{ url($endpoint['uri']) }}
+                                @endif
                             </h5>
                             <p class="mb-m">{{ $endpoint['description'] ?? '' }}</p>
                             @if($endpoint['body_params'] ?? false)
