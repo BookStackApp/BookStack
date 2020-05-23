71e7dd5894b855b0a6ed58fd520af49a521681c8
Dan Brown <ssddanbrown@googlemail.com>
2020-05-23 12:56:31 +0100

    Removed failing URL test
    - Was found that the test was not testing the actual situation anyway.
    - A work-around in the request creation, within testing, just happened
     to result in the desired outcome.
    
    For reference: https://github.com/laravel/framework/pull/32345

--- tests/Unit/UrlTest.php
+++ tests/Unit/UrlTest.php
@@ -5,17 +5,6 @@ use Tests\TestCase;
 class UrlTest extends TestCase
 {
 
-    public function test_request_url_takes_custom_url_into_account()
-    {
-        config()->set('app.url', 'http://example.com/bookstack');
-        $this->get('/');
-        $this->assertEquals('http://example.com/bookstack', request()->getUri());
-
-        config()->set('app.url', 'http://example.com/docs/content');
-        $this->get('/');
-        $this->assertEquals('http://example.com/docs/content', request()->getUri());
-    }
-
     public function test_url_helper_takes_custom_url_into_account()
     {
         $this->runWithEnv('APP_URL', 'http://example.com/bookstack', function() {
