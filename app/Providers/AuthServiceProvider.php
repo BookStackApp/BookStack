ebca9ca054df224a8bc181194a438c31dad35f83
Jasper Weyne <jasperweyne@gmail.com>
2020-07-09 18:00:16 +0200

    Add OpenIdService to OpenIdSessionGuard constructor call

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -9,6 +9,7 @@ use BookStack\Auth\Access\Guards\LdapSessionGuard;
 use BookStack\Auth\Access\Guards\Saml2SessionGuard;
 use BookStack\Auth\Access\Guards\OpenIdSessionGuard;
 use BookStack\Auth\Access\LdapService;
+use BookStack\Auth\Access\OpenIdService;
 use BookStack\Auth\Access\RegistrationService;
 use BookStack\Auth\UserRepo;
 use Illuminate\Support\ServiceProvider;
@@ -53,6 +54,7 @@ class AuthServiceProvider extends ServiceProvider
                 $name,
                 $provider,
                 $this->app['session.store'],
+                $app[OpenIdService::class],
                 $app[RegistrationService::class]
             );
         });
