da5e3604fd611dbed4ce22ec63132d431aa12c18
Dan Brown <ssddanbrown@googlemail.com>
2020-07-25 00:36:48 +0100

    New translations common.php (Chinese Simplified)

--- resources/lang/zh_CN/common.php
+++ resources/lang/zh_CN/common.php
@@ -33,6 +33,7 @@ return [
     'copy' => '复制',
     'reply' => '回复',
     'delete' => '删除',
+    'delete_confirm' => 'Confirm Deletion',
     'search' => '搜索',
     'search_clear' => '清除搜索',
     'reset' => '重置',
