fd3de47e8ae3477924fa610e4f55c49f0617bced
Dan Brown <ssddanbrown@googlemail.com>
2020-07-25 00:37:24 +0100

    New translations components.php (Spanish)

--- resources/lang/es/components.php
+++ resources/lang/es/components.php
@@ -15,7 +15,7 @@ return [
     'image_load_more' => 'Cargar más',
     'image_image_name' => 'Nombre de imagen',
     'image_delete_used' => 'Esta imagen está siendo utilizada en las páginas mostradas a continuación.',
-    'image_delete_confirm' => 'Haga click de nuevo para confirmar que quiere borrar esta imagen.',
+    'image_delete_confirm_text' => 'Are you sure you want to delete this image?',
     'image_select_image' => 'Seleccionar Imagen',
     'image_dropzone' => 'Arrastre las imágenes o hacer click aquí para Subir',
     'images_deleted' => 'Imágenes borradas',
