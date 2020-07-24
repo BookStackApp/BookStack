a31bec8aebf30945764fd5789eaf96e652629c93
Dan Brown <ssddanbrown@googlemail.com>
2020-07-25 00:37:43 +0100

    New translations components.php (Hungarian)

--- resources/lang/hu/components.php
+++ resources/lang/hu/components.php
@@ -15,7 +15,7 @@ return [
     'image_load_more' => 'Több betöltése',
     'image_image_name' => 'Kép neve',
     'image_delete_used' => 'Ez a kép a lenti oldalakon van használatban.',
-    'image_delete_confirm' => 'A kép törléséhez ismét rá kell kattintani a törlésre.',
+    'image_delete_confirm_text' => 'Are you sure you want to delete this image?',
     'image_select_image' => 'Kép kiválasztása',
     'image_dropzone' => 'Képek feltöltése ejtéssel vagy kattintással',
     'images_deleted' => 'Képek törölve',
