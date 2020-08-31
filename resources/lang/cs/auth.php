9c457d9ffe32a7c21ddeba32e211730b921b72c0
Jakub Bouček <pan@jakubboucek.cz>
2020-08-31 17:34:46 +0200

    Fix Czech translations (email -> e-mail)
    In Czech language "email" does not means "email" but "enamel paint", correct is "e-mail".
    See in Wikipedia:
    - https://cs.wikipedia.org/wiki/E-mail
    - https://cs.wikipedia.org/wiki/Email_(barva)

--- resources/lang/cs/auth.php
+++ resources/lang/cs/auth.php
@@ -24,7 +24,7 @@ return [
     'password_hint' => 'Musí mít víc než 7 znaků',
     'forgot_password' => 'Zapomněli jste heslo?',
     'remember_me' => 'Neodhlašovat',
-    'ldap_email_hint' => 'Zadejte email, který chcete přiřadit k tomuto účtu.',
+    'ldap_email_hint' => 'Zadejte e-mail, který chcete přiřadit k tomuto účtu.',
     'create_account' => 'Vytvořit účet',
     'already_have_account' => 'Máte už založený účet?',
     'dont_have_account' => 'Nemáte učet?',
@@ -33,37 +33,37 @@ return [
     'social_registration_text' => 'Registrovat a přihlásit se přes jinou službu',
 
     'register_thanks' => 'Díky za registraci!',
-    'register_confirm' => 'Zkontrolujte prosím váš email a klikněte na potvrzovací tlačítko pro dokončení registrace do :appName.',
+    'register_confirm' => 'Zkontrolujte prosím váš e-mail a klikněte na potvrzovací tlačítko pro dokončení registrace do :appName.',
     'registrations_disabled' => 'Registrace jsou momentálně pozastaveny',
-    'registration_email_domain_invalid' => 'Registrace z této emailové domény nejsou povoleny.',
+    'registration_email_domain_invalid' => 'Registrace z této e-mailové domény nejsou povoleny.',
     'register_success' => 'Díky za registraci! Jste registrovaní a přihlášení.',
 
 
     // Password Reset
     'reset_password' => 'Resetovat heslo',
-    'reset_password_send_instructions' => 'Zadejte vaší emailovou adresu a bude vám zaslán odkaz na resetování hesla.',
+    'reset_password_send_instructions' => 'Zadejte vaší e-mailovou adresu a bude vám zaslán odkaz na resetování hesla.',
     'reset_password_send_button' => 'Poslat odkaz pro reset hesla',
-    'reset_password_sent' => 'A password reset link will be sent to :email if that email address is found in the system.',
+    'reset_password_sent' => 'A password reset link will be sent to :email if that e-mail address is found in the system.',
     'reset_password_success' => 'Vaše heslo bylo úspěšně resetováno.',
     'email_reset_subject' => 'Reset hesla do :appName',
-    'email_reset_text' => 'Tento email jste obdrželi, protože jsme dostali žádost o resetování vašeho hesla k účtu v :appName.',
+    'email_reset_text' => 'Tento e-mail jste obdrželi, protože jsme dostali žádost o resetování vašeho hesla k účtu v :appName.',
     'email_reset_not_requested' => 'Pokud jste o reset vašeho hesla nežádali, prostě tento dopis smažte a je to.',
 
 
     // Email Confirmation
-    'email_confirm_subject' => 'Potvrďte vaši emailovou adresu pro :appName',
+    'email_confirm_subject' => 'Potvrďte vaši e-mailovou adresu pro :appName',
     'email_confirm_greeting' => 'Díky že jste se přidali do :appName!',
-    'email_confirm_text' => 'Prosíme potvrďte funkčnost vaší emailové adresy kliknutím na tlačítko níže:',
-    'email_confirm_action' => 'Potvrdit emailovou adresu',
-    'email_confirm_send_error' => 'Potvrzení emailové adresy je vyžadováno, ale systém vám nedokázal odeslat email. Kontaktujte správce aby to dal do kupy a potvrzovací email vám dorazil.',
-    'email_confirm_success' => 'Vaše emailová adresa byla potvrzena!',
-    'email_confirm_resent' => 'Email s žádostí o potvrzení vaší emailové adresy byl odeslán. Podívejte se do příchozí pošty.',
+    'email_confirm_text' => 'Prosíme potvrďte funkčnost vaší e-mailové adresy kliknutím na tlačítko níže:',
+    'email_confirm_action' => 'Potvrdit e-mailovou adresu',
+    'email_confirm_send_error' => 'Potvrzení e-mailové adresy je vyžadováno, ale systém vám nedokázal odeslat e-mail. Kontaktujte správce aby to dal do kupy a potvrzovací e-mail vám dorazil.',
+    'email_confirm_success' => 'Vaše e-mailová adresa byla potvrzena!',
+    'email_confirm_resent' => 'E-mail s žádostí o potvrzení vaší e-mailové adresy byl odeslán. Podívejte se do příchozí pošty.',
 
-    'email_not_confirmed' => 'Emailová adresa nebyla potvrzena',
-    'email_not_confirmed_text' => 'Vaše emailová adresa nebyla dosud potvrzena.',
-    'email_not_confirmed_click_link' => 'Klikněte na odkaz v emailu který jsme vám zaslali ihned po registraci.',
-    'email_not_confirmed_resend' => 'Pokud nemůžete nalézt email v příchozí poště, můžete si jej nechat poslat znovu pomocí formuláře níže.',
-    'email_not_confirmed_resend_button' => 'Znovu poslat email pro potvrzení emailové adresy',
+    'email_not_confirmed' => 'E-mailová adresa nebyla potvrzena',
+    'email_not_confirmed_text' => 'Vaše e-mailová adresa nebyla dosud potvrzena.',
+    'email_not_confirmed_click_link' => 'Klikněte na odkaz v e-mailu který jsme vám zaslali ihned po registraci.',
+    'email_not_confirmed_resend' => 'Pokud nemůžete nalézt e-mail v příchozí poště, můžete si jej nechat poslat znovu pomocí formuláře níže.',
+    'email_not_confirmed_resend_button' => 'Znovu poslat e-mail pro potvrzení e-mailové adresy',
 
     // User Invite
     'user_invite_email_subject' => 'Byl jste pozván do :appName!',
