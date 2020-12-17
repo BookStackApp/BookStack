fe9b70ced1aba7f6a6d061ff715c90b45daecac9
Dan Brown <ssddanbrown@googlemail.com>
2020-12-17 02:48:31 +0000

    New translations entities.php (Chinese Simplified)

--- resources/lang/zh_CN/entities.php
+++ resources/lang/zh_CN/entities.php
@@ -146,7 +146,7 @@ return [
     'chapters_create' => '创建章节',
     'chapters_delete' => '删除章节',
     'chapters_delete_named' => '删除章节「:chapterName」',
-    'chapters_delete_explain' => '这将删除章节「:chapterName」。所有的页面将被删除并添加到其所在的书籍。',
+    'chapters_delete_explain' => 'This will delete the chapter with the name \':chapterName\'. All pages that exist within this chapter will also be deleted.',
     'chapters_delete_confirm' => '您确定要删除此章节吗？',
     'chapters_edit' => '编辑章节',
     'chapters_edit_named' => '编辑章节「:chapterName」',
