34eff6404e7c184a28d09f3ecb44f08ac8ed79ac
Dan Brown <ssddanbrown@googlemail.com>
2021-01-02 16:08:40 +0000

    New translations activities.php (Spanish, Argentina)

--- resources/lang/es_AR/activities.php
+++ resources/lang/es_AR/activities.php
@@ -45,5 +45,5 @@ return [
 
     // Other
     'commented_on'                => 'comentado',
-    'permissions_update'          => 'updated permissions',
+    'permissions_update'          => 'permisos actualizados',
 ];
