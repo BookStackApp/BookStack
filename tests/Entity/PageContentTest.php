ae9675c98d5988bbfa8061d58d5c4c53f1d39d72
Dan Brown <ssddanbrown@googlemail.com>
2021-01-09 19:04:23 +0000

    Added in table + tasklist markdown rendering
    For parity with markdown-it renderer.
    Added tests to cover.
    For #2452

--- tests/Entity/PageContentTest.php
+++ tests/Entity/PageContentTest.php
@@ -420,4 +420,45 @@ class PageContentTest extends TestCase
         $page->refresh();
         $this->assertEquals('"Hello & welcome"', $page->text);
     }
+
+    public function test_page_markdown_table_rendering()
+    {
+        $this->asEditor();
+        $page = Page::query()->first();
+
+        $content = '| Syntax      | Description |
+| ----------- | ----------- |
+| Header      | Title       |
+| Paragraph   | Text        |';
+        $this->put($page->getUrl(), [
+            'name' => $page->name,  'markdown' => $content,
+            'html' => '', 'summary' => ''
+        ]);
+
+        $page->refresh();
+        $this->assertStringContainsString('</tbody>', $page->html);
+
+        $pageView = $this->get($page->getUrl());
+        $pageView->assertElementExists('.page-content table tbody td');
+    }
+
+    public function test_page_markdown_task_list_rendering()
+    {
+        $this->asEditor();
+        $page = Page::query()->first();
+
+        $content = '- [ ] Item a
+- [x] Item b';
+        $this->put($page->getUrl(), [
+            'name' => $page->name,  'markdown' => $content,
+            'html' => '', 'summary' => ''
+        ]);
+
+        $page->refresh();
+        $this->assertStringContainsString('input', $page->html);
+        $this->assertStringContainsString('type="checkbox"', $page->html);
+
+        $pageView = $this->get($page->getUrl());
+        $pageView->assertElementExists('.page-content input[type=checkbox]');
+    }
 }
