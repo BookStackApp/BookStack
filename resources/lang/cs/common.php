3bb49754e29877939c2f603e92a0fe4a7058e27e
Dan Brown <ssddanbrown@googlemail.com>
2021-01-31 00:38:04 +0000

    New translations common.php (Czech)

--- resources/lang/cs/common.php
+++ resources/lang/cs/common.php
@@ -77,4 +77,9 @@ return [
     // Email Content
     'email_action_help' => 'Pokud se vám nedaří kliknout na tlačítko „:actionText“, zkopírujte a vložte níže uvedenou URL do vašeho webového prohlížeče:',
     'email_rights' => 'Všechna práva vyhrazena',
+
+    // Footer Link Options
+    // Not directly used but available for convenience to users.
+    'privacy_policy' => 'Privacy Policy',
+    'terms_of_service' => 'Terms of Service',
 ];
