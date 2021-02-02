b2a33582c593285492f0e8fc4096010cd2ab027d
Dan Brown <ssddanbrown@googlemail.com>
2021-02-02 10:30:09 +0000

    New translations common.php (Chinese Simplified)

--- resources/lang/zh_CN/common.php
+++ resources/lang/zh_CN/common.php
@@ -80,6 +80,6 @@ return [
 
     // Footer Link Options
     // Not directly used but available for convenience to users.
-    'privacy_policy' => 'Privacy Policy',
-    'terms_of_service' => 'Terms of Service',
+    'privacy_policy' => '隐私政策',
+    'terms_of_service' => '服务条款',
 ];
