80a1ccde0859eed1c2771603dbf32278047620bc
Dan Brown <ssddanbrown@googlemail.com>
2021-02-05 09:59:55 +0000

    New translations auth.php (Latvian)

--- resources/lang/lv/auth.php
+++ resources/lang/lv/auth.php
@@ -7,22 +7,22 @@
 return [
 
     'failed' => 'Šie reģistrācijas dati neatbilst mūsu ierakstiem.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'throttle' => 'Pārāk daudz pieteikšanās mēģinājumu. Lūdzu, mēģiniet vēlreiz pēc :seconds seconds.',
 
     // Login & Register
     'sign_up' => 'Reģistrēties',
     'log_in' => 'Ielogoties',
-    'log_in_with' => 'Login with :socialDriver',
-    'sign_up_with' => 'Sign up with :socialDriver',
+    'log_in_with' => 'Ielogoties ar :socialDriver',
+    'sign_up_with' => 'Pieteikties ar :socialDriver',
     'logout' => 'Iziet',
 
-    'name' => 'Name',
+    'name' => 'Vārds',
     'username' => 'Lietotājvārds',
     'email' => 'E-pasts',
     'password' => 'Parole',
     'password_confirm' => 'Apstiprināt paroli',
-    'password_hint' => 'Must be over 7 characters',
-    'forgot_password' => 'Forgot Password?',
+    'password_hint' => 'Jābūt vismaz 8 rakstzīmēm',
+    'forgot_password' => 'Aizmirsta parole?',
     'remember_me' => 'Atcerēties mani',
     'ldap_email_hint' => 'Please enter an email to use for this account.',
     'create_account' => 'Izveidot profilu',
@@ -32,27 +32,27 @@ return [
     'social_registration' => 'Social Registration',
     'social_registration_text' => 'Register and sign in using another service.',
 
-    'register_thanks' => 'Thanks for registering!',
+    'register_thanks' => 'Paldies par reģistrāciju!',
     'register_confirm' => 'Please check your email and click the confirmation button to access :appName.',
-    'registrations_disabled' => 'Registrations are currently disabled',
-    'registration_email_domain_invalid' => 'That email domain does not have access to this application',
+    'registrations_disabled' => 'Reģistrācija ir izslēgta',
+    'registration_email_domain_invalid' => 'E-pasta domēnam nav piekļuves pie šīs aplikācijas',
     'register_success' => 'Thanks for signing up! You are now registered and signed in.',
 
 
     // Password Reset
-    'reset_password' => 'Reset Password',
+    'reset_password' => 'Atiestatīt paroli',
     'reset_password_send_instructions' => 'Enter your email below and you will be sent an email with a password reset link.',
-    'reset_password_send_button' => 'Send Reset Link',
-    'reset_password_sent' => 'A password reset link will be sent to :email if that email address is found in the system.',
-    'reset_password_success' => 'Your password has been successfully reset.',
-    'email_reset_subject' => 'Reset your :appName password',
-    'email_reset_text' => 'You are receiving this email because we received a password reset request for your account.',
-    'email_reset_not_requested' => 'If you did not request a password reset, no further action is required.',
+    'reset_password_send_button' => 'Nosūtīt atjaunošanas saiti',
+    'reset_password_sent' => 'Paroles atiestatīšanas saite tiks nosūtīta uz :email, ja šāds e-pasts eksistē.',
+    'reset_password_success' => 'Jūsu parole ir veiksmīgi atiestatīta.',
+    'email_reset_subject' => 'Atiestatīt :appName paroli',
+    'email_reset_text' => 'Jūs saņemat šo e-pastu, jo mēs saņēmām Jūsu profila paroles atiestatīšanas pieprasījumu.',
+    'email_reset_not_requested' => 'Ja Jūs nepieprasījāt paroles atiestatīšanu, tad tālākas darbības nav nepieciešamas.',
 
 
     // Email Confirmation
-    'email_confirm_subject' => 'Confirm your email on :appName',
-    'email_confirm_greeting' => 'Thanks for joining :appName!',
+    'email_confirm_subject' => 'Apstiprinat savu :appName e-pastu',
+    'email_confirm_greeting' => 'Paldies, ka pievienojāties :appName!',
     'email_confirm_text' => 'Please confirm your email address by clicking the button below:',
     'email_confirm_action' => 'Apstiprināt e-pastu',
     'email_confirm_send_error' => 'Email confirmation required but the system could not send the email. Contact the admin to ensure email is set up correctly.',
@@ -69,9 +69,9 @@ return [
     'user_invite_email_subject' => 'You have been invited to join :appName!',
     'user_invite_email_greeting' => 'An account has been created for you on :appName.',
     'user_invite_email_text' => 'Click the button below to set an account password and gain access:',
-    'user_invite_email_action' => 'Set Account Password',
-    'user_invite_page_welcome' => 'Welcome to :appName!',
-    'user_invite_page_text' => 'To finalise your account and gain access you need to set a password which will be used to log-in to :appName on future visits.',
-    'user_invite_page_confirm_button' => 'Confirm Password',
-    'user_invite_success' => 'Password set, you now have access to :appName!'
+    'user_invite_email_action' => 'Iestatīt profila paroli',
+    'user_invite_page_welcome' => 'Sveicināti :appName!',
+    'user_invite_page_text' => 'Lai pabeigtu profila izveidi un piekļūtu :appName ir jāizveido parole.',
+    'user_invite_page_confirm_button' => 'Apstiprināt paroli',
+    'user_invite_success' => 'Parole iestatīta, tagad varat piekļūt :appName!'
 ];
