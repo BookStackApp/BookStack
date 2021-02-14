6670899e0fa9663aa7da199840174101088dd31b
Benedikt Volke <benedikt.volke@mennekes.de>
2021-02-14 11:40:24 +0100

    Add translation string

--- resources/lang/en/entities.php
+++ resources/lang/en/entities.php
@@ -60,6 +60,7 @@ return [
     'search_permissions_set' => 'Permissions set',
     'search_created_by_me' => 'Created by me',
     'search_updated_by_me' => 'Updated by me',
+    'search_owned_by_me' => 'Owned by me',
     'search_date_options' => 'Date Options',
     'search_updated_before' => 'Updated before',
     'search_updated_after' => 'Updated after',
