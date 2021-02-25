c086d013da8fe5b2baef465de237c36dc6d21c8f
Dan Brown <ssddanbrown@googlemail.com>
2021-02-25 22:37:10 +0000

    New translations passwords.php (Portuguese)

--- resources/lang/pt/passwords.php
+++ resources/lang/pt/passwords.php
@@ -6,10 +6,10 @@
  */
 return [
 
-    'password' => 'Passwords must be at least eight characters and match the confirmation.',
-    'user' => "We can't find a user with that e-mail address.",
-    'token' => 'The password reset token is invalid for this email address.',
-    'sent' => 'We have e-mailed your password reset link!',
-    'reset' => 'Your password has been reset!',
+    'password' => 'Senhas devem ter ao menos oito caracteres e ser iguais à confirmação.',
+    'user' => "Não pudemos encontrar um usuário com o e-mail fornecido.",
+    'token' => 'O token de redefinição de senha é inválido para este endereço de e-mail.',
+    'sent' => 'Enviamos o link de redefinição de senha para o seu e-mail!',
+    'reset' => 'Sua senha foi redefinida com sucesso!',
 
 ];
