d1b7f03f060e5bbc03f16302640ecc9788cec4e6
bridgeyuwa <torkumajonathanyuwa@gmail.com>
2021-03-04 00:51:58 +0100

    Update kernel to schedule sitemap generation weekly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+      $schedule->command('sitemap:generate')->weekly();
     }
 
     /**
