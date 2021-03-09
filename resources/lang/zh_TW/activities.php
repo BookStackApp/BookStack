7cadbeeaef2fb3e085c3ab62735b265b7c28ad91
Dan Brown <ssddanbrown@googlemail.com>
2021-03-09 02:27:22 +0000

    New translations activities.php (Chinese Traditional)

--- resources/lang/zh_TW/activities.php
+++ resources/lang/zh_TW/activities.php
@@ -28,7 +28,7 @@ return [
     // Books
     'book_create'                 => '已建立書本',
     'book_create_notification'    => '書本已建立成功',
-    'book_update'                 => '更新了圖書',
+    'book_update'                 => '已更新書本',
     'book_update_notification'    => '書本已更新成功',
     'book_delete'                 => '已刪除書本',
     'book_delete_notification'    => '書本已刪除成功',
