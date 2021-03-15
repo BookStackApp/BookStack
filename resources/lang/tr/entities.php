0fd70b20584d224d1d6957632ccdc4588fe2e0f0
Dan Brown <ssddanbrown@googlemail.com>
2021-03-15 18:37:09 +0000

    New translations entities.php (Turkish)

--- resources/lang/tr/entities.php
+++ resources/lang/tr/entities.php
@@ -60,6 +60,7 @@ return [
     'search_permissions_set' => 'İzinler ayarlanmış',
     'search_created_by_me' => 'Oluşturduklarım',
     'search_updated_by_me' => 'Güncellediklerim',
+    'search_owned_by_me' => 'Owned by me',
     'search_date_options' => 'Tarih Seçenekleri',
     'search_updated_before' => 'Önce güncellendi',
     'search_updated_after' => 'Sonra güncellendi',
