fce2da9365146abcf35b908f9595f79eb3d8e269
Dan Brown <ssddanbrown@googlemail.com>
2021-03-15 18:37:30 +0000

    New translations entities.php (Czech)

--- resources/lang/cs/entities.php
+++ resources/lang/cs/entities.php
@@ -60,6 +60,7 @@ return [
     'search_permissions_set' => 'Sada oprávnění',
     'search_created_by_me' => 'Vytvořeno mnou',
     'search_updated_by_me' => 'Aktualizováno mnou',
+    'search_owned_by_me' => 'Owned by me',
     'search_date_options' => 'Možnosti data',
     'search_updated_before' => 'Aktualizováno před',
     'search_updated_after' => 'Aktualizováno po',
