597f6a85d68b1a86de475691b9cbfe0c31644632
Dan Brown <ssddanbrown@googlemail.com>
2021-04-25 14:56:35 +0100

    New translations passwords.php (Catalan)

--- resources/lang/ca/passwords.php
+++ resources/lang/ca/passwords.php
@@ -7,9 +7,9 @@
 return [
 
     'password' => 'Les contrasenyes han de tenir com a mínim vuit caràcters i la confirmació ha de coincidir.',
-    'user' => "No s'ha trobat cap usuari amb aquesta adreça electrònica.",
-    'token' => 'El testimoni de reinicialització de la contrasenya no és vàlid per a aquesta adreça electrònica.',
-    'sent' => 'Us hem enviat un enllaç per a restablir la contrasenya!',
-    'reset' => 'S\'ha restablert la vostra contrasenya!',
+    'user' => "No s'ha trobat cap usuari amb aquest correu electrònic.",
+    'token' => 'El token de restabliment de contrasenya no és vàlid per aquest correu electrònic.',
+    'sent' => 'T\'hem enviat un enllaç per a restablir la contrasenya!',
+    'reset' => 'S\'ha restablert la teva contrasenya!',
 
 ];
