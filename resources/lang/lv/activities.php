9982620fa27f81408bed4c2472d7e11626901c8e
Dan Brown <ssddanbrown@googlemail.com>
2021-05-23 14:47:26 +0100

    New translations activities.php (Latvian)

--- resources/lang/lv/activities.php
+++ resources/lang/lv/activities.php
@@ -43,6 +43,10 @@ return [
     'bookshelf_delete'                 => 'izdzēsa plauktu',
     'bookshelf_delete_notification'    => 'Plaukts Veiksmīgi Dzēsts',
 
+    // Favourites
+    'favourite_add_notification' => '":name" has been added to your favourites',
+    'favourite_remove_notification' => '":name" has been removed from your favourites',
+
     // Other
     'commented_on'                => 'komentēts',
     'permissions_update'          => 'atjaunoja atļaujas',
