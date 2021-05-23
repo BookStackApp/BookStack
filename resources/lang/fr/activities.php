ee6aec6da35dd9e9d1503e89467df8e28c8d1a92
Dan Brown <ssddanbrown@googlemail.com>
2021-05-23 14:47:55 +0100

    New translations activities.php (French)

--- resources/lang/fr/activities.php
+++ resources/lang/fr/activities.php
@@ -43,6 +43,10 @@ return [
     'bookshelf_delete'                 => 'a supprimé l\'étagère',
     'bookshelf_delete_notification'    => 'Étagère supprimée avec succès',
 
+    // Favourites
+    'favourite_add_notification' => '":name" has been added to your favourites',
+    'favourite_remove_notification' => '":name" has been removed from your favourites',
+
     // Other
     'commented_on'                => 'a commenté',
     'permissions_update'          => 'mettre à jour les autorisations',
