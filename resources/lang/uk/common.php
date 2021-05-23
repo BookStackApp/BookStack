2ff8401d9fe6d72e3129a6fe50e14f591d664b51
Dan Brown <ssddanbrown@googlemail.com>
2021-05-23 14:47:00 +0100

    New translations common.php (Ukrainian)

--- resources/lang/uk/common.php
+++ resources/lang/uk/common.php
@@ -40,6 +40,8 @@ return [
     'remove' => 'Видалити',
     'add' => 'Додати',
     'fullscreen' => 'На весь екран',
+    'favourite' => 'Favourite',
+    'unfavourite' => 'Unfavourite',
 
     // Sort Options
     'sort_options' => 'Параметри сортування',
