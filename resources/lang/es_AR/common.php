3154303597c7b8020358114530e67e9f1ca9acba
Dan Brown <ssddanbrown@googlemail.com>
2021-05-23 14:47:57 +0100

    New translations common.php (Spanish, Argentina)

--- resources/lang/es_AR/common.php
+++ resources/lang/es_AR/common.php
@@ -40,6 +40,8 @@ return [
     'remove' => 'Remover',
     'add' => 'Agregar',
     'fullscreen' => 'Pantalla completa',
+    'favourite' => 'Favourite',
+    'unfavourite' => 'Unfavourite',
 
     // Sort Options
     'sort_options' => 'Opciones de Orden',
