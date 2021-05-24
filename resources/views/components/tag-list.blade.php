3eaf03a7ac77d3d2765541659312d45e10890824
Dan Brown <ssddanbrown@googlemail.com>
2021-05-24 16:12:09 +0100

    Reviewed tag in seach work
    - Refactored some tag code bits while reviewing.
    - Updated tag design in search listing to be more subtle.
    - Moved tags out of entity-list-item-basic template and instead moved
      them into entity-list-item, below the existing content.
    - Tweaked existing tag colors a little.
    - Changed tag icon to be more tag-like.
    - Added tag-on-search test case.
    
    Review of #2487, Related to #2462

--- resources/views/components/tag-list.blade.php
+++ resources/views/components/tag-list.blade.php
@@ -1,11 +1,11 @@
 @foreach($entity->tags as $tag)
     <div class="tag-item primary-background-light">
-        @if($disableLinks ?? false)
+        @if($linked ?? true)
+            <div class="tag-name"><a href="{{ $tag->nameUrl() }}">@icon('tag'){{ $tag->name }}</a></div>
+            @if($tag->value) <div class="tag-value"><a href="{{ $tag->valueUrl() }}">{{$tag->value}}</a></div> @endif
+        @else
             <div class="tag-name"><span>@icon('tag'){{ $tag->name }}</span></div>
             @if($tag->value) <div class="tag-value"><span>{{$tag->value}}</span></div> @endif
-        @else
-            <div class="tag-name"><a href="{{ url('/search?term=%5B' . urlencode($tag->name) .'%5D') }}">@icon('tag'){{ $tag->name }}</a></div>
-            @if($tag->value) <div class="tag-value"><a href="{{ url('/search?term=%5B' . urlencode($tag->name) .'%3D' . urlencode($tag->value) . '%5D') }}">{{$tag->value}}</a></div> @endif
         @endif
     </div>
 @endforeach
--- resources/views/partials/entity-list-item-basic.blade.php
+++ resources/views/partials/entity-list-item-basic.blade.php
@@ -2,9 +2,6 @@
 <a href="{{ $entity->getUrl() }}" class="{{$type}} {{$type === 'page' && $entity->draft ? 'draft' : ''}} {{$classes ?? ''}} entity-list-item" data-entity-type="{{$type}}" data-entity-id="{{$entity->id}}">
     <span role="presentation" class="icon text-{{$type}}">@icon($type)</span>
     <div class="content">
-            @if($entity->tags->count() > 0 and $showTags ?? false)
-                    @include('components.tag-list', ['entity' => $entity, 'disableLinks' => True ])
-            @endif
             <h4 class="entity-list-item-name break-text">{{ $entity->name }}</h4>
             {{ $slot ?? '' }}
     </div>
