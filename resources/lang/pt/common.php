c5af69ec60282359dc5a22361d2e29899082d922
Dan Brown <ssddanbrown@googlemail.com>
2021-05-29 12:55:26 +0100

    New translations common.php (Portuguese)

--- resources/lang/pt/common.php
+++ resources/lang/pt/common.php
@@ -42,6 +42,8 @@ return [
     'fullscreen' => 'Ecrã completo',
     'favourite' => 'Favourite',
     'unfavourite' => 'Unfavourite',
+    'next' => 'Next',
+    'previous' => 'Previous',
 
     // Sort Options
     'sort_options' => 'Opções de Ordenação',
