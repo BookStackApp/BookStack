7b38029cd7d85a81d639b0f4f453e10521f595f9
Dan Brown <ssddanbrown@googlemail.com>
2021-05-30 00:02:32 +0100

    Aligned export and revision shown date format
    As raised in #2771

--- resources/views/partials/entity-export-meta.blade.php
+++ resources/views/partials/entity-export-meta.blade.php
@@ -4,13 +4,13 @@
     @endif
 
     @icon('star'){!! trans('entities.meta_created' . ($entity->createdBy ? '_name' : ''), [
-        'timeLength' => $entity->created_at->toDayDateTimeString(),
+        'timeLength' => $entity->created_at->formatLocalized('%e %B %Y %H:%M:%S'),
         'user' => e($entity->createdBy->name ?? ''),
         ]) !!}
     <br>
 
     @icon('edit'){!! trans('entities.meta_updated' . ($entity->updatedBy ? '_name' : ''), [
-            'timeLength' => $entity->updated_at->toDayDateTimeString(),
+            'timeLength' => $entity->updated_at->formatLocalized('%e %B %Y %H:%M:%S'),
             'user' => e($entity->updatedBy->name ?? '')
         ]) !!}
 </div>
