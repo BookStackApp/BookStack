eecd644f144b9d2f7c64dec75e91348a3866737d
Dan Brown <ssddanbrown@googlemail.com>
2021-05-29 12:55:44 +0100

    New translations common.php (Italian)

--- resources/lang/it/common.php
+++ resources/lang/it/common.php
@@ -42,6 +42,8 @@ return [
     'fullscreen' => 'Schermo intero',
     'favourite' => 'Favourite',
     'unfavourite' => 'Unfavourite',
+    'next' => 'Next',
+    'previous' => 'Previous',
 
     // Sort Options
     'sort_options' => 'Opzioni Ordinamento',
