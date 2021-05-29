b893a2a8358b299e389d83c95e0f9c5753ca7099
Dan Brown <ssddanbrown@googlemail.com>
2021-05-29 12:55:50 +0100

    New translations common.php (Bulgarian)

--- resources/lang/bg/common.php
+++ resources/lang/bg/common.php
@@ -42,6 +42,8 @@ return [
     'fullscreen' => 'Пълен екран',
     'favourite' => 'Favourite',
     'unfavourite' => 'Unfavourite',
+    'next' => 'Next',
+    'previous' => 'Previous',
 
     // Sort Options
     'sort_options' => 'Опции за сортиране',
