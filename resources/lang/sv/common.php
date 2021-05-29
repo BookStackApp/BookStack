4601edea728e034ad67816019f9fbaaf870f1e12
Dan Brown <ssddanbrown@googlemail.com>
2021-05-29 12:55:37 +0100

    New translations common.php (Swedish)

--- resources/lang/sv/common.php
+++ resources/lang/sv/common.php
@@ -42,6 +42,8 @@ return [
     'fullscreen' => 'Helskärm',
     'favourite' => 'Favourite',
     'unfavourite' => 'Unfavourite',
+    'next' => 'Next',
+    'previous' => 'Previous',
 
     // Sort Options
     'sort_options' => 'Sorteringsalternativ',
