93fef7d7632b8b7963abd0c2b2010e2e39e4927f
Dan Brown <ssddanbrown@googlemail.com>
2021-05-29 12:55:27 +0100

    New translations common.php (Norwegian Bokmal)

--- resources/lang/nb/common.php
+++ resources/lang/nb/common.php
@@ -42,6 +42,8 @@ return [
     'fullscreen' => 'Fullskjerm',
     'favourite' => 'Favourite',
     'unfavourite' => 'Unfavourite',
+    'next' => 'Next',
+    'previous' => 'Previous',
 
     // Sort Options
     'sort_options' => 'Sorteringsalternativer',
