a1046fdb539b6f4a1887b0f1255ce98142aee2c8
Dan Brown <ssddanbrown@googlemail.com>
2021-06-22 22:26:24 +0100

    New translations entities.php (Spanish)

--- resources/lang/es/entities.php
+++ resources/lang/es/entities.php
@@ -36,7 +36,7 @@ return [
     'export_html' => 'Archivo web',
     'export_pdf' => 'Archivo PDF',
     'export_text' => 'Archivo de texto',
-    'export_md' => 'Markdown File',
+    'export_md' => 'Archivo Markdown',
 
     // Permissions and restrictions
     'permissions' => 'Permisos',
