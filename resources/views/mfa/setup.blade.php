ebd79678e06ca673c5dde970d3a8fd9752cfead4
Dan Brown <ssddanbrown@googlemail.com>
2021-06-28 22:02:45 +0100

    Started barebones work of MFA system

---
+++ resources/views/mfa/setup.blade.php
@@ -0,0 +1,16 @@
+@extends('simple-layout')
+
+@section('body')
+    <div class="container small py-xl">
+
+        <div class="card content-wrap auto-height">
+            <h1 class="list-heading">Setup Multi-Factor Authentication</h1>
+            <p>
+                Setup multi-factor authentication as an extra layer of security
+                for your user account.
+                To use multi-factor authentication you'll need a mobile application
+                that supports TOTP such as Google Authenticator, Authy or Microsoft Authenticator.
+            </p>
+        </div>
+    </div>
+@stop
