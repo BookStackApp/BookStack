a97bf4c0a147159a7518fdc9f1d4311fb5d52422
Dan Brown <ssddanbrown@googlemail.com>
2021-07-03 19:55:24 +0100

    New translations common.php (Latvian)

--- resources/lang/lv/common.php
+++ resources/lang/lv/common.php
@@ -60,7 +60,7 @@ return [
     'no_activity' => 'Nav skatāmu darbību',
     'no_items' => 'Vienumi nav pieejami',
     'back_to_top' => 'Uz augšu',
-    'skip_to_main_content' => 'Skip to main content',
+    'skip_to_main_content' => 'Pāriet uz saturu',
     'toggle_details' => 'Rādīt aprakstu',
     'toggle_thumbnails' => 'Iezīmēt sīkatēlus',
     'details' => 'Sīkāka informācija',
