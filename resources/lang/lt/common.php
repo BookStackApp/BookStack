a3ad840bdd1f5680650ec97ef1222c9c91d6cdae
Francesco Franchina <cescus92@gmail.com>
2021-08-03 14:02:01 +0200

    Adding Lithuanian language

---
+++ resources/lang/lt/common.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Common elements found throughout many areas of BookStack.
+ */
+return [
+
+    // Buttons
+    'cancel' => 'Atšaukti',
+    'confirm' => 'Patvirtinti',
+    'back' => 'Grįžti',
+    'save' => 'Išsaugoti',
+    'continue' => 'Praleisti',
+    'select' => 'Pasirinkti',
+    'toggle_all' => 'Perjungti visus',
+    'more' => 'Daugiau',
+
+    // Form Labels
+    'name' => 'Pavadinimas',
+    'description' => 'Apibūdinimas',
+    'role' => 'Vaidmuo',
+    'cover_image' => 'Viršelio nuotrauka',
+    'cover_image_description' => 'Ši nuotrauka turi būti maždaug 440x250px.',
+    
+    // Actions
+    'actions' => 'Veiksmai',
+    'view' => 'Rodyti',
+    'view_all' => 'Rodyti visus',
+    'create' => 'Sukurti',
+    'update' => 'Atnaujinti',
+    'edit' => 'Redaguoti',
+    'sort' => 'Rūšiuoti',
+    'move' => 'Perkelti',
+    'copy' => 'Kopijuoti',
+    'reply' => 'Atsakyti',
+    'delete' => 'Ištrinti',
+    'delete_confirm' => 'Patvirtinti ištrynimą',
+    'search' => 'Paieška',
+    'search_clear' => 'Išvalyti paiešką',
+    'reset' => 'Atsatyti',
+    'remove' => 'Pašalinti',
+    'add' => 'Pridėti',
+    'fullscreen' => 'Visas ekranas',
+
+    // Sort Options
+    'sort_options' => 'Rūšiuoti pasirinkimus',
+    'sort_direction_toggle' => 'Rūšiuoti krypties perjungimus',
+    'sort_ascending' => 'Rūšiuoti didėjančia tvarka',
+    'sort_descending' => 'Rūšiuoti mažėjančia tvarka',
+    'sort_name' => 'Pavadinimas',
+    'sort_default' => 'Numatytas',
+    'sort_created_at' => 'Sukurta data',
+    'sort_updated_at' => 'Atnaujinta data',
+
+    // Misc
+    'deleted_user' => 'Ištrinti naudotoją',
+    'no_activity' => 'Nėra veiklų',
+    'no_items' => 'Nėra elementų',
+    'back_to_top' => 'Grįžti į pradžią',
+    'toggle_details' => 'Perjungti detales',
+    'toggle_thumbnails' => 'Perjungti miniatūras',
+    'details' => 'Detalės',
+    'grid_view' => 'Tinklelio vaizdas',
+    'list_view' => 'Sąrašas',
+    'default' => 'Numatytas',
+    'breadcrumb' => 'Duonos rėžis',
+
+    // Header
+    'header_menu_expand' => 'Plėsti antraštės meniu',
+    'profile_menu' => 'Profilio meniu',
+    'view_profile' => 'Rodyti porofilį',
+    'edit_profile' => 'Redaguoti profilį',
+    'dark_mode' => 'Tamsus rėžimas',
+    'light_mode' => 'Šviesus rėžimas',
+
+    // Layout tabs
+    'tab_info' => 'Informacija',
+    'tab_info_label' => 'Skirtukas: Rodyti antrinę informaciją',
+    'tab_content' => 'Turinys',
+    'tab_content_label' => 'Skirtukas: Rodyti pirminę informaciją',
+
+    // Email Content
+    'email_action_help' => 'Jeigu kyla problemų spaudžiant :actionText: mygtuką, nukopijuokite ir įklijuokite URL į savo naršyklę.',
+    'email_rights' => 'Visos teisės rezervuotos',
+
+    // Footer Link Options
+    // Not directly used but available for convenience to users.
+    'privacy_policy' => 'Privatumo politika',
+    'terms_of_service' => 'Paslaugų teikimo paslaugos',
+];
