8223499ad6fe2110035743b0a91b5c4379034666
Dan Brown <ssddanbrown@googlemail.com>
2021-08-11 04:56:23 +0100

    New translations activities.php (Chinese Traditional)

--- resources/lang/zh_TW/activities.php
+++ resources/lang/zh_TW/activities.php
@@ -44,8 +44,8 @@ return [
     'bookshelf_delete_notification'    => '書架已刪除成功',
 
     // Favourites
-    'favourite_add_notification' => '":name" has been added to your favourites',
-    'favourite_remove_notification' => '":name" has been removed from your favourites',
+    'favourite_add_notification' => '":name" 已加入到你的最愛',
+    'favourite_remove_notification' => '":name" 已從你的最愛移除',
 
     // Other
     'commented_on'                => '評論',
