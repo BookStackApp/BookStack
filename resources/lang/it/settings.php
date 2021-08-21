76e1753ae1332b5fb27bce161c93455b636158b1
Dan Brown <ssddanbrown@googlemail.com>
2021-08-21 15:58:36 +0100

    New translations settings.php (Italian)

--- resources/lang/it/settings.php
+++ resources/lang/it/settings.php
@@ -138,6 +138,7 @@ return [
     'role_details' => 'Dettagli Ruolo',
     'role_name' => 'Nome Ruolo',
     'role_desc' => 'Breve Descrizione del Ruolo',
+    'role_mfa_enforced' => 'Requires Multi-Factor Authentication',
     'role_external_auth_id' => 'ID Autenticazione Esterna',
     'role_system' => 'Permessi di Sistema',
     'role_manage_users' => 'Gestire gli utenti',
@@ -204,6 +205,10 @@ return [
     'users_api_tokens_create' => 'Crea Token',
     'users_api_tokens_expires' => 'Scade',
     'users_api_tokens_docs' => 'Documentazione API',
+    'users_mfa' => 'Multi-Factor Authentication',
+    'users_mfa_desc' => 'Setup multi-factor authentication as an extra layer of security for your user account.',
+    'users_mfa_x_methods' => ':count method configured|:count methods configured',
+    'users_mfa_configure' => 'Configure Methods',
 
     // API Tokens
     'user_api_token_create' => 'Crea Token API',
