5a2e6f9cfe175ac503924fa27647024403ef4934
Dan Brown <ssddanbrown@googlemail.com>
2021-08-21 15:56:56 +0100

    New translations activities.php (Bosnian)

--- resources/lang/bs/activities.php
+++ resources/lang/bs/activities.php
@@ -47,6 +47,10 @@ return [
     'favourite_add_notification' => '":name" je dodan u tvoje favorite',
     'favourite_remove_notification' => '":name" je uklonjen iz tvojih favorita',
 
+    // MFA
+    'mfa_setup_method_notification' => 'Multi-factor method successfully configured',
+    'mfa_remove_method_notification' => 'Multi-factor method successfully removed',
+
     // Other
     'commented_on'                => 'je komentarisao/la na',
     'permissions_update'          => 'je ažurirao/la dozvole',
