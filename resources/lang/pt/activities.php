93a7fe19d1f8d7b16f06c3487599499a51e8031c
Dan Brown <ssddanbrown@googlemail.com>
2021-08-21 15:58:19 +0100

    New translations activities.php (Portuguese)

--- resources/lang/pt/activities.php
+++ resources/lang/pt/activities.php
@@ -47,6 +47,10 @@ return [
     'favourite_add_notification' => '":name" foi adicionado aos seus favoritos',
     'favourite_remove_notification' => '":name" foi removido dos seus favoritos',
 
+    // MFA
+    'mfa_setup_method_notification' => 'Multi-factor method successfully configured',
+    'mfa_remove_method_notification' => 'Multi-factor method successfully removed',
+
     // Other
     'commented_on'                => 'comentado a',
     'permissions_update'          => 'permissões atualizadas',
