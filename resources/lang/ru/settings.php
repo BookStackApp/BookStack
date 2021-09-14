4256361d94cee54b322920a81bf33cf1a6ac3917
Dan Brown <ssddanbrown@googlemail.com>
2021-09-14 10:28:34 +0100

    New translations settings.php (Russian)

--- resources/lang/ru/settings.php
+++ resources/lang/ru/settings.php
@@ -209,7 +209,7 @@ return [
     'users_mfa' => 'Двухфакторная аутентификация',
     'users_mfa_desc' => 'Setup multi-factor authentication as an extra layer of security for your user account.',
     'users_mfa_x_methods' => ':count method configured|:count methods configured',
-    'users_mfa_configure' => 'Configure Methods',
+    'users_mfa_configure' => 'Настройка методов',
 
     // API Tokens
     'user_api_token_create' => 'Создать токен',
