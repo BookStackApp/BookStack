44967e6fc44e20cf0390f4abfce274350fcf0854
Dan Brown <ssddanbrown@googlemail.com>
2021-09-26 22:25:09 +0100

    New translations activities.php (Slovak)

--- resources/lang/sk/activities.php
+++ resources/lang/sk/activities.php
@@ -8,7 +8,7 @@ return [
     // Pages
     'page_create'                 => 'vytvoril(a) stránku',
     'page_create_notification'    => 'Stránka úspešne vytvorená',
-    'page_update'                 => 'aktualizoval stránku',
+    'page_update'                 => 'aktualizoval(a) stránku',
     'page_update_notification'    => 'Stránka úspešne aktualizovaná',
     'page_delete'                 => 'odstránil(a) stránku',
     'page_delete_notification'    => 'Stránka úspešne odstránená',
@@ -48,8 +48,8 @@ return [
     'favourite_remove_notification' => '":name" bol odstránený z obľúbených',
 
     // MFA
-    'mfa_setup_method_notification' => 'Multi-factor method successfully configured',
-    'mfa_remove_method_notification' => 'Multi-factor method successfully removed',
+    'mfa_setup_method_notification' => 'Viacúrovňový spôsob overenia úspešne nastavený',
+    'mfa_remove_method_notification' => 'Viacúrovňový spôsob overenia úspešne odstránený',
 
     // Other
     'commented_on'                => 'komentoval(a)',
