43a0916105a20f39494c6d6946315050453dda7b
Dan Brown <ssddanbrown@googlemail.com>
2021-10-04 20:37:36 +0100

    New translations entities.php (Portuguese)

--- resources/lang/pt/entities.php
+++ resources/lang/pt/entities.php
@@ -234,6 +234,7 @@ return [
     'pages_initial_name' => 'Nova Página',
     'pages_editing_draft_notification' => 'Você está atualmente a editar um rascunho que foi guardado pela última vez a :timeDiff.',
     'pages_draft_edited_notification' => 'Esta página entretanto já foi atualizada. É recomendado que você descarte este rascunho.',
+    'pages_draft_page_changed_since_creation' => 'This page has been updated since this draft was created. It is recommended that you discard this draft or take care not to overwrite any page changes.',
     'pages_draft_edit_active' => [
         'start_a' => ':count usuários iniciaram a edição dessa página',
         'start_b' => ':userName iniciou a edição desta página',
