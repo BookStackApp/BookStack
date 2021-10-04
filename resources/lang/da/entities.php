10dd33b360f0605716a11ce5ee30242eaa030fc3
Dan Brown <ssddanbrown@googlemail.com>
2021-10-04 20:37:46 +0100

    New translations entities.php (Danish)

--- resources/lang/da/entities.php
+++ resources/lang/da/entities.php
@@ -234,6 +234,7 @@ return [
     'pages_initial_name' => 'Ny side',
     'pages_editing_draft_notification' => 'Du redigerer en kladde der sidst var gemt :timeDiff.',
     'pages_draft_edited_notification' => 'Siden har været opdateret siden da. Det er anbefalet at du kasserer denne kladde.',
+    'pages_draft_page_changed_since_creation' => 'This page has been updated since this draft was created. It is recommended that you discard this draft or take care not to overwrite any page changes.',
     'pages_draft_edit_active' => [
         'start_a' => ':count brugerer har begyndt at redigere denne side',
         'start_b' => ':userName er begyndt at redigere denne side',
