53e588a34016976fc19ffd7ce4fc7ae9d1b9ae1c
Dan Brown <ssddanbrown@googlemail.com>
2021-10-16 11:05:34 +0100

    New translations passwords.php (Estonian)

--- resources/lang/et/passwords.php
+++ resources/lang/et/passwords.php
@@ -6,10 +6,10 @@
  */
 return [
 
-    'password' => 'Passwords must be at least eight characters and match the confirmation.',
-    'user' => "We can't find a user with that e-mail address.",
-    'token' => 'The password reset token is invalid for this email address.',
-    'sent' => 'We have e-mailed your password reset link!',
-    'reset' => 'Your password has been reset!',
+    'password' => 'Paroolides peab olema vähemalt kaheksa tähemärki ja nad peavad omavahel ühtima.',
+    'user' => "Sellise e-posti aadressiga kasutajat ei leitud.",
+    'token' => 'Parooli taastamise link ei kehti selle e-posti aadressiga.',
+    'sent' => 'Parooli taastamise link saadeti e-postiga!',
+    'reset' => 'Parool on taastatud!',
 
 ];
