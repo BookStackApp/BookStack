37d30f7f80af656193801d2cfbd3f967f4e62b90
Dan Brown <ssddanbrown@googlemail.com>
2021-10-18 11:42:50 +0100

    Added base64 image extraction to markdown page content
    - Included tests to cover.
    - Manually tested via API update and interface page update.
    
    Closes #2898

--- app/Entities/Models/Page.php
+++ app/Entities/Models/Page.php
@@ -28,7 +28,7 @@ class Page extends BookChild
     public static $listAttributes = ['name', 'id', 'slug', 'book_id', 'chapter_id', 'draft', 'template', 'text', 'created_at', 'updated_at', 'priority'];
     public static $contentAttributes = ['name', 'id', 'slug', 'book_id', 'chapter_id', 'draft', 'template', 'html', 'text', 'created_at', 'updated_at', 'priority'];
 
-    protected $fillable = ['name', 'priority', 'markdown'];
+    protected $fillable = ['name', 'priority'];
 
     public $textField = 'text';
 
