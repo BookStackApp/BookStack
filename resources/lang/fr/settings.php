a27fb3144258ccd08e6ab5787ae3fdf3cdaf7dd4
Dan Brown <ssddanbrown@googlemail.com>
2021-10-27 06:37:59 +0100

    New translations settings.php (French)

--- resources/lang/fr/settings.php
+++ resources/lang/fr/settings.php
@@ -248,7 +248,7 @@ return [
         'de_informal' => 'Deutsch (Du)',
         'es' => 'Español',
         'es_AR' => 'Español Argentina',
-        'et' => 'Eesti keel',
+        'et' => 'Estonien',
         'fr' => 'Français',
         'he' => 'Hébreu',
         'hr' => 'Hrvatski',
