f7b6326f11c07f73c74aadf87b775f71d25b6da1
Dan Brown <ssddanbrown@googlemail.com>
2021-10-28 16:41:54 +0100

    Temporarily moved back config path

--- app/Application.php
+++ app/Application.php
@@ -13,11 +13,12 @@ class Application extends \Illuminate\Foundation\Application
      */
     public function configPath($path = '')
     {
-        return $this->basePath
-            . DIRECTORY_SEPARATOR
-            . 'app'
-            . DIRECTORY_SEPARATOR
-            . 'Config'
-            . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+        return parent::configPath($path);
+//        return $this->basePath
+//            . DIRECTORY_SEPARATOR
+//            . 'app'
+//            . DIRECTORY_SEPARATOR
+//            . 'Config'
+//            . ($path ? DIRECTORY_SEPARATOR . $path : $path);
     }
 }
