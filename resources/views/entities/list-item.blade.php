bc472ca2d7f0f01b035cb17a414c9e7eef9a5576
Dan Brown <ssddanbrown@googlemail.com>
2021-11-08 15:24:49 +0000

    Improved relation loading during search
    Relations now loaded during back-end query phase instead of being lazy
    loaded one-by-one within views.
    
    Reduced queries in testing from ~60 to ~20.
    
    Need to check other areas list-item.php's "showPath" option is used to
    ensure relations are properly loaded for those listings.

--- resources/views/entities/list-item.blade.php
+++ resources/views/entities/list-item.blade.php
@@ -3,9 +3,9 @@
 <div class="entity-item-snippet">
 
     @if($showPath ?? false)
-        @if($entity->book_id)
+        @if($entity->relationLoaded('book') && $entity->book)
             <span class="text-book">{{ $entity->book->getShortName(42) }}</span>
-            @if($entity->chapter_id)
+            @if($entity->relationLoaded('chapter') && $entity->chapter)
                 <span class="text-muted entity-list-item-path-sep">@icon('chevron-right')</span> <span class="text-chapter">{{ $entity->chapter->getShortName(42) }}</span>
             @endif
         @endif
