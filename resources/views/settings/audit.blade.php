ac8e57a78de7eababe3dba90aa3e2dfcdb207cc5
Zero <john.royer@gmail.com>
2021-12-07 17:29:41 +0800

    Fix UI error of IP searching input box

--- resources/views/settings/audit.blade.php
+++ resources/views/settings/audit.blade.php
@@ -51,7 +51,7 @@
 
                 <div class="form-group ml-auto">
                     <label for="ip">{{ trans('settings.audit_table_ip') }}</label>
-                    @include('form.text', ['name' => 'ip', 'model' => $listDetails['ip'] ? \BookStack\Actions\Activity::query()->where(['ip' => $listDetails['ip']])->first() : null])
+                    @include('form.text', ['name' => 'ip', 'model' => (object) $listDetails])
                     <input type="submit" style="display: none">
                 </div>
             </form>
