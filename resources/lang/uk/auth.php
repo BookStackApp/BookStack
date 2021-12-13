4ddea48e76a352740cf5dec2b27c6bd5d046de6d
Dan Brown <ssddanbrown@googlemail.com>
2021-12-13 06:17:40 +0000

    New translations auth.php (Ukrainian)

--- resources/lang/uk/auth.php
+++ resources/lang/uk/auth.php
@@ -74,10 +74,10 @@ return [
     'user_invite_success_login' => 'Password set, you should now be able to login using your set password to access :appName!',
 
     // Multi-factor Authentication
-    'mfa_setup' => 'Setup Multi-Factor Authentication',
+    'mfa_setup' => 'Налаштувати двофакторну автентифікацію',
     'mfa_setup_desc' => 'Setup multi-factor authentication as an extra layer of security for your user account.',
     'mfa_setup_configured' => 'Already configured',
-    'mfa_setup_reconfigure' => 'Reconfigure',
+    'mfa_setup_reconfigure' => 'Переналаштувати',
     'mfa_setup_remove_confirmation' => 'Are you sure you want to remove this multi-factor authentication method?',
     'mfa_setup_action' => 'Setup',
     'mfa_backup_codes_usage_limit_warning' => 'You have less than 5 backup codes remaining, Please generate and store a new set before you run out of codes to prevent being locked out of your account.',
