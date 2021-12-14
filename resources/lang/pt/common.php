6bdbc08e5b0e681e53547cd1f8305f8e9ab36e1e
Dan Brown <ssddanbrown@googlemail.com>
2021-12-14 01:56:36 +0000

    New translations common.php (Portuguese)

--- resources/lang/pt/common.php
+++ resources/lang/pt/common.php
@@ -45,8 +45,8 @@ return [
     'unfavourite' => 'Retirar Favorito',
     'next' => 'Próximo',
     'previous' => 'Anterior',
-    'filter_active' => 'Active Filter:',
-    'filter_clear' => 'Clear Filter',
+    'filter_active' => 'Filtro Ativo:',
+    'filter_clear' => 'Limpar Filtro',
 
     // Sort Options
     'sort_options' => 'Opções de Ordenação',
