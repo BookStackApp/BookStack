ba961d46cbc45915cd3afe56b9e9745e48f7c957
Dan Brown <ssddanbrown@googlemail.com>
2021-12-18 11:48:23 +0000

    New translations common.php (Korean)

--- resources/lang/ko/common.php
+++ resources/lang/ko/common.php
@@ -71,6 +71,9 @@ return [
     'list_view' => '목록 보기',
     'default' => '기본 설정',
     'breadcrumb' => '탐색 경로',
+    'status' => 'Status',
+    'status_active' => 'Active',
+    'status_inactive' => 'Inactive',
 
     // Header
     'header_menu_expand' => 'Expand Header Menu',
