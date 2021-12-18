fcfb7a45b73d4f28508472f99a75314c73c094de
Dan Brown <ssddanbrown@googlemail.com>
2021-12-18 11:48:21 +0000

    New translations common.php (Hungarian)

--- resources/lang/hu/common.php
+++ resources/lang/hu/common.php
@@ -71,6 +71,9 @@ return [
     'list_view' => 'Lista nézet',
     'default' => 'Alapértelmezés szerinti',
     'breadcrumb' => 'Morzsa',
+    'status' => 'Status',
+    'status_active' => 'Active',
+    'status_inactive' => 'Inactive',
 
     // Header
     'header_menu_expand' => 'Expand Header Menu',
