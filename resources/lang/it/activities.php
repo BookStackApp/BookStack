28afd3c6e3ca7e12768e534182ed202ad57fcc2c
Dan Brown <ssddanbrown@googlemail.com>
2021-12-18 11:47:48 +0000

    New translations activities.php (Italian)

--- resources/lang/it/activities.php
+++ resources/lang/it/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'pagina creata',
-    'page_create_notification'    => 'Pagina Creata Correttamente',
+    'page_create_notification'    => 'Page successfully created',
     'page_update'                 => 'ha aggiornato la pagina',
-    'page_update_notification'    => 'Pagina Aggiornata Correttamente',
+    'page_update_notification'    => 'Page successfully updated',
     'page_delete'                 => 'ha eliminato la pagina',
-    'page_delete_notification'    => 'Pagina Eliminata Correttamente',
+    'page_delete_notification'    => 'Page successfully deleted',
     'page_restore'                => 'ha ripristinato la pagina',
-    'page_restore_notification'   => 'Pagina Ripristinata Correttamente',
+    'page_restore_notification'   => 'Page successfully restored',
     'page_move'                   => 'ha mosso la pagina',
 
     // Chapters
     'chapter_create'              => 'ha creato il capitolo',
-    'chapter_create_notification' => 'Capitolo Creato Correttamente',
+    'chapter_create_notification' => 'Chapter successfully created',
     'chapter_update'              => 'ha aggiornato il capitolo',
-    'chapter_update_notification' => 'Capitolo Aggiornato Correttamente',
+    'chapter_update_notification' => 'Chapter successfully updated',
     'chapter_delete'              => 'ha eliminato il capitolo',
-    'chapter_delete_notification' => 'Capitolo Eliminato Correttamente',
+    'chapter_delete_notification' => 'Chapter successfully deleted',
     'chapter_move'                => 'ha spostato il capitolo',
 
     // Books
     'book_create'                 => 'ha creato il libro',
-    'book_create_notification'    => 'Libro Creato Correttamente',
+    'book_create_notification'    => 'Book successfully created',
     'book_update'                 => 'ha aggiornato il libro',
-    'book_update_notification'    => 'Libro Aggiornato Correttamente',
+    'book_update_notification'    => 'Book successfully updated',
     'book_delete'                 => 'ha eliminato il libro',
-    'book_delete_notification'    => 'Libro Eliminato Correttamente',
+    'book_delete_notification'    => 'Book successfully deleted',
     'book_sort'                   => 'ha ordinato il libro',
-    'book_sort_notification'      => 'Libro Riordinato Correttamente',
+    'book_sort_notification'      => 'Book successfully re-sorted',
 
     // Bookshelves
-    'bookshelf_create'            => 'ha creato la Libreria',
-    'bookshelf_create_notification'    => 'Libreria Creata Correttamente',
+    'bookshelf_create'            => 'created bookshelf',
+    'bookshelf_create_notification'    => 'Bookshelf successfully created',
     'bookshelf_update'                 => 'ha aggiornato la libreria',
-    'bookshelf_update_notification'    => 'Libreria Aggiornata Correttamente',
+    'bookshelf_update_notification'    => 'Bookshelf successfully updated',
     'bookshelf_delete'                 => 'ha eliminato la libreria',
-    'bookshelf_delete_notification'    => 'Libreria Eliminata Correttamente',
+    'bookshelf_delete_notification'    => 'Bookshelf successfully deleted',
 
     // Favourites
     'favourite_add_notification' => '":name" è stato aggiunto ai tuoi preferiti',
@@ -50,6 +50,14 @@ return [
     // MFA
     'mfa_setup_method_notification' => 'Metodo multi-fattore impostato con successo',
     'mfa_remove_method_notification' => 'Metodo multi-fattore rimosso con successo',
+
+    // Webhooks
+    'webhook_create' => 'created webhook',
+    'webhook_create_notification' => 'Webhook successfully created',
+    'webhook_update' => 'updated webhook',
+    'webhook_update_notification' => 'Webhook successfully updated',
+    'webhook_delete' => 'deleted webhook',
+    'webhook_delete_notification' => 'Webhook successfully deleted',
 
     // Other
     'commented_on'                => 'ha commentato in',
