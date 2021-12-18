b92e7d27dc7b710f11b90163dd1889b5d753b94c
Dan Brown <ssddanbrown@googlemail.com>
2021-12-18 11:48:34 +0000

    New translations common.php (Vietnamese)

--- resources/lang/vi/common.php
+++ resources/lang/vi/common.php
@@ -71,6 +71,9 @@ return [
     'list_view' => 'Hiển thị dạng danh sách',
     'default' => 'Mặc định',
     'breadcrumb' => 'Đường dẫn liên kết',
+    'status' => 'Status',
+    'status_active' => 'Active',
+    'status_inactive' => 'Inactive',
 
     // Header
     'header_menu_expand' => 'Expand Header Menu',
