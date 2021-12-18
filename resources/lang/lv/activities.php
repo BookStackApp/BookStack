a2ddb228602e389958fafb2cc3cc59e0729f194f
Dan Brown <ssddanbrown@googlemail.com>
2021-12-18 11:47:21 +0000

    New translations activities.php (Latvian)

--- resources/lang/lv/activities.php
+++ resources/lang/lv/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'izveidoja lapu',
-    'page_create_notification'    => 'Lapa Veiksmīgi Izveidota',
+    'page_create_notification'    => 'Page successfully created',
     'page_update'                 => 'atjaunoja lapu',
-    'page_update_notification'    => 'Lapa Veiksmīgi Atjaunota',
+    'page_update_notification'    => 'Page successfully updated',
     'page_delete'                 => 'izdzēsa lapu',
-    'page_delete_notification'    => 'Lapa Veiksmīgi Dzēsta',
+    'page_delete_notification'    => 'Page successfully deleted',
     'page_restore'                => 'atjaunoja lapu',
-    'page_restore_notification'   => 'Lapa Veiksmīgi Atjaunota',
+    'page_restore_notification'   => 'Page successfully restored',
     'page_move'                   => 'pārvietoja lapu',
 
     // Chapters
     'chapter_create'              => 'izveidoja nodaļu',
-    'chapter_create_notification' => 'Nodaļa Veiksmīgi Izveidota',
+    'chapter_create_notification' => 'Chapter successfully created',
     'chapter_update'              => 'atjaunoja nodaļu',
-    'chapter_update_notification' => 'Nodaļa Veiksmīgi Atjaunota',
+    'chapter_update_notification' => 'Chapter successfully updated',
     'chapter_delete'              => 'izdzēsa nodaļu',
-    'chapter_delete_notification' => 'Nodaļa Veiksmīgi Dzēsta',
+    'chapter_delete_notification' => 'Chapter successfully deleted',
     'chapter_move'                => 'pārvietoja nodaļu',
 
     // Books
     'book_create'                 => 'izveidoja grāmatu',
-    'book_create_notification'    => 'Grāmata Veiksmīgi Izveidota',
+    'book_create_notification'    => 'Book successfully created',
     'book_update'                 => 'atjaunoja grāmatu',
-    'book_update_notification'    => 'Grāmata Veiksmīgi Atjaunota',
+    'book_update_notification'    => 'Book successfully updated',
     'book_delete'                 => 'izdzēsa grāmatu',
-    'book_delete_notification'    => 'Grāmata Veiksmīgi Dzēsta',
+    'book_delete_notification'    => 'Book successfully deleted',
     'book_sort'                   => 'kārtoja grāmatu',
-    'book_sort_notification'      => 'Grāmata Veiksmīgi Pārkārtota',
+    'book_sort_notification'      => 'Book successfully re-sorted',
 
     // Bookshelves
-    'bookshelf_create'            => 'izveidoja Plauktu',
-    'bookshelf_create_notification'    => 'Plaukts Veiksmīgi Izveidots',
+    'bookshelf_create'            => 'created bookshelf',
+    'bookshelf_create_notification'    => 'Bookshelf successfully created',
     'bookshelf_update'                 => 'atjaunoja plauktu',
-    'bookshelf_update_notification'    => 'Plaukts Veiksmīgi Atjaunots',
+    'bookshelf_update_notification'    => 'Bookshelf successfully updated',
     'bookshelf_delete'                 => 'izdzēsa plauktu',
-    'bookshelf_delete_notification'    => 'Plaukts Veiksmīgi Dzēsts',
+    'bookshelf_delete_notification'    => 'Bookshelf successfully deleted',
 
     // Favourites
     'favourite_add_notification' => '":name" ir pievienots jūsu favorītiem',
@@ -50,6 +50,14 @@ return [
     // MFA
     'mfa_setup_method_notification' => '2FA funkcija aktivizēta',
     'mfa_remove_method_notification' => '2FA funkcija noņemta',
+
+    // Webhooks
+    'webhook_create' => 'created webhook',
+    'webhook_create_notification' => 'Webhook successfully created',
+    'webhook_update' => 'updated webhook',
+    'webhook_update_notification' => 'Webhook successfully updated',
+    'webhook_delete' => 'deleted webhook',
+    'webhook_delete_notification' => 'Webhook successfully deleted',
 
     // Other
     'commented_on'                => 'komentēts',
