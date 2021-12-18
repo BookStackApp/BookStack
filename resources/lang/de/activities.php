e5b1325c7506dbd0edb0560fa6460754e3d90256
Dan Brown <ssddanbrown@googlemail.com>
2021-12-18 11:47:44 +0000

    New translations activities.php (German)

--- resources/lang/de/activities.php
+++ resources/lang/de/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'hat die Seite erstellt',
-    'page_create_notification'    => 'Die Seite wurde erfolgreich erstellt',
+    'page_create_notification'    => 'Page successfully created',
     'page_update'                 => 'hat die Seite aktualisiert',
-    'page_update_notification'    => 'Die Seite wurde erfolgreich aktualisiert',
+    'page_update_notification'    => 'Page successfully updated',
     'page_delete'                 => 'hat die Seite gelöscht',
-    'page_delete_notification'    => 'Die Seite wurde erfolgreich gelöscht',
+    'page_delete_notification'    => 'Page successfully deleted',
     'page_restore'                => 'hat die Seite wiederhergestellt',
-    'page_restore_notification'   => 'Die Seite wurde erfolgreich wiederhergestellt',
+    'page_restore_notification'   => 'Page successfully restored',
     'page_move'                   => 'hat die Seite verschoben',
 
     // Chapters
     'chapter_create'              => 'hat das Kapitel erstellt',
-    'chapter_create_notification' => 'Das Kapitel wurde erfolgreich erstellt',
+    'chapter_create_notification' => 'Chapter successfully created',
     'chapter_update'              => 'hat das Kapitel geändert',
-    'chapter_update_notification' => 'Das Kapitel wurde erfolgreich aktualisiert',
+    'chapter_update_notification' => 'Chapter successfully updated',
     'chapter_delete'              => 'hat das Kapitel gelöscht',
-    'chapter_delete_notification' => 'Das Kapitel wurde erfolgreich gelöscht',
+    'chapter_delete_notification' => 'Chapter successfully deleted',
     'chapter_move'                => 'hat das Kapitel verschoben',
 
     // Books
     'book_create'                 => 'hat das Buch erstellt',
-    'book_create_notification'    => 'Das Buch wurde erfolgreich erstellt',
+    'book_create_notification'    => 'Book successfully created',
     'book_update'                 => 'hat das Buch aktualisiert',
-    'book_update_notification'    => 'Das Buch wurde erfolgreich aktualisiert',
+    'book_update_notification'    => 'Book successfully updated',
     'book_delete'                 => 'hat das Buch gelöscht',
-    'book_delete_notification'    => 'Das Buch wurde erfolgreich gelöscht',
+    'book_delete_notification'    => 'Book successfully deleted',
     'book_sort'                   => 'hat die Buch-Sortierung geändert',
-    'book_sort_notification'      => 'Das Buch wurde erfolgreich umsortiert',
+    'book_sort_notification'      => 'Book successfully re-sorted',
 
     // Bookshelves
-    'bookshelf_create'            => 'hat das Bücherregal erstellt',
-    'bookshelf_create_notification'    => 'Das Bücherregal wurde erfolgreich erstellt',
+    'bookshelf_create'            => 'created bookshelf',
+    'bookshelf_create_notification'    => 'Bookshelf successfully created',
     'bookshelf_update'                 => 'hat das Bücherregal geändert',
-    'bookshelf_update_notification'    => 'Das Bücherregal wurde erfolgreich geändert',
+    'bookshelf_update_notification'    => 'Bookshelf successfully updated',
     'bookshelf_delete'                 => 'hat das Bücherregal gelöscht',
-    'bookshelf_delete_notification'    => 'Das Bücherregal wurde erfolgreich gelöscht',
+    'bookshelf_delete_notification'    => 'Bookshelf successfully deleted',
 
     // Favourites
     'favourite_add_notification' => '":name" wurde zu deinen Favoriten hinzugefügt',
@@ -50,6 +50,14 @@ return [
     // MFA
     'mfa_setup_method_notification' => 'Multi-Faktor-Methode erfolgreich konfiguriert',
     'mfa_remove_method_notification' => 'Multi-Faktor-Methode erfolgreich entfernt',
+
+    // Webhooks
+    'webhook_create' => 'created webhook',
+    'webhook_create_notification' => 'Webhook successfully created',
+    'webhook_update' => 'updated webhook',
+    'webhook_update_notification' => 'Webhook successfully updated',
+    'webhook_delete' => 'deleted webhook',
+    'webhook_delete_notification' => 'Webhook successfully deleted',
 
     // Other
     'commented_on'                => 'hat einen Kommentar hinzugefügt',
