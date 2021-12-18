4b4d9910148ee12dd0588d9449f90a2b3b86563e
Dan Brown <ssddanbrown@googlemail.com>
2021-12-18 11:47:55 +0000

    New translations activities.php (Russian)

--- resources/lang/ru/activities.php
+++ resources/lang/ru/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'создал страницу',
-    'page_create_notification'    => 'Страница успешно создана',
+    'page_create_notification'    => 'Page successfully created',
     'page_update'                 => 'обновил страницу',
-    'page_update_notification'    => 'Страница успешно обновлена',
+    'page_update_notification'    => 'Page successfully updated',
     'page_delete'                 => 'удалил страницу',
-    'page_delete_notification'    => 'Страница успешно удалена',
+    'page_delete_notification'    => 'Page successfully deleted',
     'page_restore'                => 'восстановил страницу',
-    'page_restore_notification'   => 'Страница успешно восстановлена',
+    'page_restore_notification'   => 'Page successfully restored',
     'page_move'                   => 'переместил страницу',
 
     // Chapters
     'chapter_create'              => 'создал главу',
-    'chapter_create_notification' => 'Глава успешно создана',
+    'chapter_create_notification' => 'Chapter successfully created',
     'chapter_update'              => 'обновил главу',
-    'chapter_update_notification' => 'Глава успешно обновлена',
+    'chapter_update_notification' => 'Chapter successfully updated',
     'chapter_delete'              => 'удалил главу',
-    'chapter_delete_notification' => 'Глава успешно удалена',
+    'chapter_delete_notification' => 'Chapter successfully deleted',
     'chapter_move'                => 'переместил главу',
 
     // Books
     'book_create'                 => 'создал книгу',
-    'book_create_notification'    => 'Книга успешно создана',
+    'book_create_notification'    => 'Book successfully created',
     'book_update'                 => 'обновил книгу',
-    'book_update_notification'    => 'Книга успешно обновлена',
+    'book_update_notification'    => 'Book successfully updated',
     'book_delete'                 => 'удалил книгу',
-    'book_delete_notification'    => 'Книга успешно удалена',
+    'book_delete_notification'    => 'Book successfully deleted',
     'book_sort'                   => 'отсортировал книгу',
-    'book_sort_notification'      => 'Книга успешно отсортирована',
+    'book_sort_notification'      => 'Book successfully re-sorted',
 
     // Bookshelves
-    'bookshelf_create'            => 'создал полку',
-    'bookshelf_create_notification'    => 'Полка успешно создана',
+    'bookshelf_create'            => 'created bookshelf',
+    'bookshelf_create_notification'    => 'Bookshelf successfully created',
     'bookshelf_update'                 => 'обновил полку',
-    'bookshelf_update_notification'    => 'Полка успешно обновлена',
+    'bookshelf_update_notification'    => 'Bookshelf successfully updated',
     'bookshelf_delete'                 => 'удалил полку',
-    'bookshelf_delete_notification'    => 'Полка успешно удалена',
+    'bookshelf_delete_notification'    => 'Bookshelf successfully deleted',
 
     // Favourites
     'favourite_add_notification' => '":name" добавлено в избранное',
@@ -50,6 +50,14 @@ return [
     // MFA
     'mfa_setup_method_notification' => 'Двухфакторный метод авторизации успешно настроен',
     'mfa_remove_method_notification' => 'Двухфакторный метод авторизации успешно удален',
+
+    // Webhooks
+    'webhook_create' => 'created webhook',
+    'webhook_create_notification' => 'Webhook successfully created',
+    'webhook_update' => 'updated webhook',
+    'webhook_update_notification' => 'Webhook successfully updated',
+    'webhook_delete' => 'deleted webhook',
+    'webhook_delete_notification' => 'Webhook successfully deleted',
 
     // Other
     'commented_on'                => 'прокомментировал',
