49786903f54e85ec074f8fbe39fcccbf50eaf17a
Dan Brown <ssddanbrown@googlemail.com>
2021-12-18 11:47:12 +0000

    New translations activities.php (Estonian)

--- resources/lang/et/activities.php
+++ resources/lang/et/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'lisas lehe',
-    'page_create_notification'    => 'Leht on lisatud',
+    'page_create_notification'    => 'Page successfully created',
     'page_update'                 => 'muutis lehte',
-    'page_update_notification'    => 'Leht on muudetud',
+    'page_update_notification'    => 'Page successfully updated',
     'page_delete'                 => 'kustutas lehe',
-    'page_delete_notification'    => 'Leht on kustutatud',
+    'page_delete_notification'    => 'Page successfully deleted',
     'page_restore'                => 'taastas lehe',
-    'page_restore_notification'   => 'Leht on taastatud',
+    'page_restore_notification'   => 'Page successfully restored',
     'page_move'                   => 'liigutas lehte',
 
     // Chapters
     'chapter_create'              => 'lisas peatüki',
-    'chapter_create_notification' => 'Peatükk on lisatud',
+    'chapter_create_notification' => 'Chapter successfully created',
     'chapter_update'              => 'muutis peatükki',
-    'chapter_update_notification' => 'Peatükk on muudetud',
+    'chapter_update_notification' => 'Chapter successfully updated',
     'chapter_delete'              => 'kustutas peatüki',
-    'chapter_delete_notification' => 'Peatükk on kustutatud',
+    'chapter_delete_notification' => 'Chapter successfully deleted',
     'chapter_move'                => 'liigutas peatükki',
 
     // Books
     'book_create'                 => 'lisas raamatu',
-    'book_create_notification'    => 'Raamat on lisatud',
+    'book_create_notification'    => 'Book successfully created',
     'book_update'                 => 'muutis raamatut',
-    'book_update_notification'    => 'Raamat on muudetud',
+    'book_update_notification'    => 'Book successfully updated',
     'book_delete'                 => 'kustutas raamatu',
-    'book_delete_notification'    => 'Raamat on kustutatud',
+    'book_delete_notification'    => 'Book successfully deleted',
     'book_sort'                   => 'sorteeris raamatut',
-    'book_sort_notification'      => 'Raamat on sorteeritud',
+    'book_sort_notification'      => 'Book successfully re-sorted',
 
     // Bookshelves
-    'bookshelf_create'            => 'lisas riiuli',
-    'bookshelf_create_notification'    => 'Riiul on lisatud',
+    'bookshelf_create'            => 'created bookshelf',
+    'bookshelf_create_notification'    => 'Bookshelf successfully created',
     'bookshelf_update'                 => 'muutis riiulit',
-    'bookshelf_update_notification'    => 'Riiul on muudetud',
+    'bookshelf_update_notification'    => 'Bookshelf successfully updated',
     'bookshelf_delete'                 => 'kustutas riiuli',
-    'bookshelf_delete_notification'    => 'Riiul on kustutatud',
+    'bookshelf_delete_notification'    => 'Bookshelf successfully deleted',
 
     // Favourites
     'favourite_add_notification' => '":name" lisati su lemmikute hulka',
@@ -50,6 +50,14 @@ return [
     // MFA
     'mfa_setup_method_notification' => 'Mitmeastmeline autentimine seadistatud',
     'mfa_remove_method_notification' => 'Mitmeastmeline autentimine eemaldatud',
+
+    // Webhooks
+    'webhook_create' => 'created webhook',
+    'webhook_create_notification' => 'Webhook successfully created',
+    'webhook_update' => 'updated webhook',
+    'webhook_update_notification' => 'Webhook successfully updated',
+    'webhook_delete' => 'deleted webhook',
+    'webhook_delete_notification' => 'Webhook successfully deleted',
 
     // Other
     'commented_on'                => 'kommenteeris lehte',
