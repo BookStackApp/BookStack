2bd72250a61797787a0321a5e0c7926ca6f178b8
Dan Brown <ssddanbrown@googlemail.com>
2021-12-21 19:25:51 +0000

    New translations activities.php (French)

--- resources/lang/fr/activities.php
+++ resources/lang/fr/activities.php
@@ -7,41 +7,41 @@ return [
 
     // Pages
     'page_create'                 => 'a créé la page',
-    'page_create_notification'    => 'Page successfully created',
+    'page_create_notification'    => 'Page créée avec succès',
     'page_update'                 => 'a modifié la page',
-    'page_update_notification'    => 'Page successfully updated',
+    'page_update_notification'    => 'Page modifiée avec succès',
     'page_delete'                 => 'a supprimé la page',
-    'page_delete_notification'    => 'Page successfully deleted',
+    'page_delete_notification'    => 'Page supprimée avec succès',
     'page_restore'                => 'a restauré la page',
-    'page_restore_notification'   => 'Page successfully restored',
+    'page_restore_notification'   => 'Page restaurée avec succès',
     'page_move'                   => 'a déplacé la page',
 
     // Chapters
     'chapter_create'              => 'a créé le chapitre',
-    'chapter_create_notification' => 'Chapter successfully created',
+    'chapter_create_notification' => 'Chapitre créé avec succès',
     'chapter_update'              => 'a modifié le chapitre',
-    'chapter_update_notification' => 'Chapter successfully updated',
+    'chapter_update_notification' => 'Chapitre modifié avec succès',
     'chapter_delete'              => 'a supprimé le chapitre',
-    'chapter_delete_notification' => 'Chapter successfully deleted',
+    'chapter_delete_notification' => 'Chapitre supprimé avec succès',
     'chapter_move'                => 'a déplacé le chapitre',
 
     // Books
     'book_create'                 => 'a créé un livre',
-    'book_create_notification'    => 'Book successfully created',
+    'book_create_notification'    => 'Livre créé avec succès',
     'book_update'                 => 'a modifié le livre',
-    'book_update_notification'    => 'Book successfully updated',
+    'book_update_notification'    => 'Livre modifié avec succès',
     'book_delete'                 => 'a supprimé un livre',
-    'book_delete_notification'    => 'Book successfully deleted',
+    'book_delete_notification'    => 'Livre supprimé avec succès',
     'book_sort'                   => 'a réordonné le livre',
-    'book_sort_notification'      => 'Book successfully re-sorted',
+    'book_sort_notification'      => 'Livre restauré avec succès',
 
     // Bookshelves
     'bookshelf_create'            => 'created bookshelf',
-    'bookshelf_create_notification'    => 'Bookshelf successfully created',
+    'bookshelf_create_notification'    => 'Étagère créée avec succès',
     'bookshelf_update'                 => 'a modifié l\'étagère',
     'bookshelf_update_notification'    => 'Bookshelf successfully updated',
     'bookshelf_delete'                 => 'a supprimé l\'étagère',
-    'bookshelf_delete_notification'    => 'Bookshelf successfully deleted',
+    'bookshelf_delete_notification'    => 'Étagère supprimée avec succès',
 
     // Favourites
     'favourite_add_notification' => '":name" a été ajouté dans vos favoris',
