96372f3ef51f6e919bc91158aecbe8acd2fa7029
Dan Brown <ssddanbrown@googlemail.com>
2022-01-03 20:27:15 +0000

    New translations common.php (Spanish, Argentina)

--- resources/lang/es_AR/common.php
+++ resources/lang/es_AR/common.php
@@ -74,6 +74,7 @@ return [
     'status' => 'Estado',
     'status_active' => 'Activo',
     'status_inactive' => 'Inactive',
+    'never' => 'Never',
 
     // Header
     'header_menu_expand' => 'Expandir el Menú de Cabecera',
