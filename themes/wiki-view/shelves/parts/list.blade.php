55c21ff90208a6814bb655af9e72a9ea636fc67c
thething <atillzcomputers@gmail.com>
2022-01-03 10:20:04 -0500

    Update list.blade.php

--- themes/wiki-view/shelves/parts/list.blade.php
+++ themes/wiki-view/shelves/parts/list.blade.php
@@ -3,7 +3,11 @@
     <div class="grid half v-center">
         <h1 class="list-heading">{{ trans('entities.shelves') }}</h1>
         <div class="text-right">
-            @include('entities.sort', ['options' => $sortOptions, 'order' => $order, 'sort' => $sort, 'type' => 'bookshelves'])
+            @include('entities.sort', [
+            'options' => $sortOptions, 
+            'order' => $order, 
+            'sort' => $sort, 
+            'type' => 'bookshelves'])
         </div>
     </div>
 
