07c8876e22ccfc9de29abdffc4917a198e0f17d7
Dan Brown <ssddanbrown@googlemail.com>
2022-01-14 14:55:07 +0000

    Imported marks from example schema for customization

--- resources/js/editor/schema-marks.js
+++ resources/js/editor/schema-marks.js
@@ -1,6 +1,55 @@
-import {schema as basicSchema} from "prosemirror-schema-basic";
+const link = {
+    attrs: {
+        href: {},
+        title: {default: null}
+    },
+    inclusive: false,
+    parseDOM: [{
+        tag: "a[href]", getAttrs: function getAttrs(dom) {
+            return {href: dom.getAttribute("href"), title: dom.getAttribute("title")}
+        }
+    }],
+    toDOM: function toDOM(node) {
+        const ref = node.attrs;
+        const href = ref.href;
+        const title = ref.title;
+        return ["a", {href: href, title: title}, 0]
+    }
+};
 
-const baseMarks = basicSchema.spec.marks;
+const em = {
+    parseDOM: [{tag: "i"}, {tag: "em"}, {style: "font-style=italic"}],
+    toDOM() {
+        return ["em", 0]
+    }
+};
+
+const strong = {
+    parseDOM: [{tag: "strong"},
+        // This works around a Google Docs misbehavior where
+        // pasted content will be inexplicably wrapped in `<b>`
+        // tags with a font-weight normal.
+        {
+            tag: "b", getAttrs: function (node) {
+                return node.style.fontWeight != "normal" && null;
+            }
+        },
+        {
+            style: "font-weight", getAttrs: function (value) {
+                return /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null;
+            }
+        }],
+    toDOM() {
+        return ["strong", 0]
+    }
+};
+
+const code = {
+    parseDOM: [{tag: "code"}],
+    toDOM() {
+        return ["code", 0]
+    }
+};
 
 const underline = {
     parseDOM: [{tag: "u"}, {style: "text-decoration=underline"}],
@@ -60,13 +109,17 @@ const background_color = {
     }
 };
 
-const marks = baseMarks.append({
+const marks = {
+    link,
+    em,
+    strong,
+    code,
     underline,
     strike,
     superscript,
     subscript,
     text_color,
     background_color,
-});
+};
 
 export default marks;
