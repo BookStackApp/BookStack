78c3fed4ca7bb8f5b483df30d5bffc85381fb7e7
ististyle <53050102+ististyle@users.noreply.github.com>
2022-02-08 12:19:40 +0900

    Update activities.php

--- resources/lang/ko/activities.php
+++ resources/lang/ko/activities.php
@@ -7,59 +7,59 @@ return [
 
     // Pages
     'page_create'                 => '문서 만들기',
-    'page_create_notification'    => '문서를 만들었습니다.',
+    'page_create_notification'    => '문서 생성함',
     'page_update'                 => '문서 수정',
-    'page_update_notification'    => '문서를 수정했습니다.',
+    'page_update_notification'    => '문서 수정함',
     'page_delete'                 => '문서 지우기',
-    'page_delete_notification'    => '문서를 삭제했습니다.',
+    'page_delete_notification'    => '문서 삭제함',
     'page_restore'                => '문서 복원',
-    'page_restore_notification'   => '문서를 복원했습니다.',
+    'page_restore_notification'   => '문서 복원함',
     'page_move'                   => '문서 이동',
 
     // Chapters
     'chapter_create'              => '챕터 만들기',
-    'chapter_create_notification' => '챕터를 만들었습니다.',
+    'chapter_create_notification' => '챕터 생성함',
     'chapter_update'              => '챕터 수정',
-    'chapter_update_notification' => '챕터를 수정했습니다.',
+    'chapter_update_notification' => '챕터 수정함',
     'chapter_delete'              => '챕터 지우기',
-    'chapter_delete_notification' => '챕터를 삭제했습니다.',
+    'chapter_delete_notification' => '챕터 삭제함',
     'chapter_move'                => '챕터 이동',
 
     // Books
     'book_create'                 => '책 만들기',
-    'book_create_notification'    => '책을 만들었습니다.',
+    'book_create_notification'    => '책 생성함',
     'book_update'                 => '책 수정',
-    'book_update_notification'    => '책을 수정했습니다.',
+    'book_update_notification'    => '책 수정함',
     'book_delete'                 => '책 지우기',
-    'book_delete_notification'    => '책을 삭제했습니다.',
+    'book_delete_notification'    => '책 삭제함',
     'book_sort'                   => '책 정렬',
-    'book_sort_notification'      => '책 정렬을 바꿨습니다.',
+    'book_sort_notification'      => '책 정렬 바꿈',
 
     // Bookshelves
     'bookshelf_create'            => '책꽂이 만들기',
-    'bookshelf_create_notification'    => '책꽂이을 만들었습니다.',
+    'bookshelf_create_notification'    => '책꽂이 생성함',
     'bookshelf_update'                 => '책꽂이 수정',
-    'bookshelf_update_notification'    => '책꽂이을 수정했습니다.',
+    'bookshelf_update_notification'    => '책꽂이 수정함',
     'bookshelf_delete'                 => '책꽂이 지우기',
-    'bookshelf_delete_notification'    => '책꽂이를 삭제했습니다.',
+    'bookshelf_delete_notification'    => '책꽂이 삭제함',
 
     // Favourites
-    'favourite_add_notification' => '":name"을 북마크에 추가했습니다.',
-    'favourite_remove_notification' => '":name"을 북마크에서 지웠습니다.',
+    'favourite_add_notification' => '":name" 북마크에 추가함',
+    'favourite_remove_notification' => '":name" 북마크에서 삭제함',
 
     // MFA
-    'mfa_setup_method_notification' => '다중 인증을 설정했습니다.',
-    'mfa_remove_method_notification' => '다중 인증을 해제했습니다.',
+    'mfa_setup_method_notification' => '다중 인증 설정함',
+    'mfa_remove_method_notification' => '다중 인증 해제함',
 
     // Webhooks
     'webhook_create' => '웹 훅 만들기',
-    'webhook_create_notification' => '웹 훅을 만들었습니다.',
+    'webhook_create_notification' => '웹 훅 생성함',
     'webhook_update' => '웹 훅 수정하기',
-    'webhook_update_notification' => '웹 훅을 수정했습니다.',
+    'webhook_update_notification' => '웹 훅 수정함',
     'webhook_delete' => '웹 훅 지우기',
-    'webhook_delete_notification' => '웹 훅을 지웠습니다.',
+    'webhook_delete_notification' => '웹 훅 삭제함',
 
     // Other
     'commented_on'                => '댓글 쓰기',
-    'permissions_update'          => '권한을 수정했습니다.',
+    'permissions_update'          => '권한 수정함',
 ];
