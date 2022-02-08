a56edd96f128482175bf98fb028c5435cd847ab7
Dan Brown <ssddanbrown@googlemail.com>
2022-02-08 13:35:39 +0000

    New translations activities.php (Swedish)

--- resources/lang/sv/activities.php
+++ resources/lang/sv/activities.php
@@ -59,6 +59,10 @@ return [
     'webhook_delete' => 'deleted webhook',
     'webhook_delete_notification' => 'Webhook successfully deleted',
 
+    // Users
+    'user_update_notification' => 'User successfully updated',
+    'user_delete_notification' => 'User successfully removed',
+
     // Other
     'commented_on'                => 'kommenterade',
     'permissions_update'          => 'uppdaterade behörigheter',
