f5bf9c987125ffa1da36b31741713dc4b5466dee
Dan Brown <ssddanbrown@googlemail.com>
2022-02-11 14:09:12 +0000

    New translations passwords.php (Hungarian)

--- resources/lang/hu/passwords.php
+++ resources/lang/hu/passwords.php
@@ -6,9 +6,9 @@
  */
 return [
 
-    'password' => 'A jelszónak legalább hat karakterből kell állnia és egyeznie kell a megerősítéssel.',
+    'password' => 'A jelszónak legalább hat karakterből kell állnia, és egyeznie kell a megerősítéssel.',
     'user' => "Nem található felhasználó ezzel az e-mail címmel.",
-    'token' => 'The password reset token is invalid for this email address.',
+    'token' => 'A jelszó visszaállító biztonsági kód nem érvényes ehhez az e-mail címhez.',
     'sent' => 'E-mailben elküldtük a jelszó visszaállító hivatkozást!',
     'reset' => 'A jelszó visszaállítva!',
 
