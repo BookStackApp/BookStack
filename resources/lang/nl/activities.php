2b4fc5b267d6b06455ddaee74b626f38b397d27e
Dan Brown <ssddanbrown@googlemail.com>
2022-02-14 00:07:15 +0000

    New translations activities.php (Dutch)

--- resources/lang/nl/activities.php
+++ resources/lang/nl/activities.php
@@ -6,41 +6,41 @@
 return [
 
     // Pages
-    'page_create'                 => 'gemaakte pagina',
+    'page_create'                 => 'aangemaakte pagina',
     'page_create_notification'    => 'Pagina succesvol aangemaakt',
-    'page_update'                 => 'wijzigde pagina',
+    'page_update'                 => 'pagina bijgewerkt',
     'page_update_notification'    => 'Pagina succesvol bijgewerkt',
     'page_delete'                 => 'verwijderde pagina',
     'page_delete_notification'    => 'Pagina met succes verwijderd',
-    'page_restore'                => 'herstelde pagina',
+    'page_restore'                => 'pagina hersteld',
     'page_restore_notification'   => 'Pagina met succes hersteld',
-    'page_move'                   => 'verplaatste pagina',
+    'page_move'                   => 'pagina verplaatst',
 
     // Chapters
-    'chapter_create'              => 'maakte hoofdstuk',
+    'chapter_create'              => 'hoofdstuk aangemaakt',
     'chapter_create_notification' => 'Hoofdstuk succesvol aangemaakt',
-    'chapter_update'              => 'wijzigde hoofdstuk',
+    'chapter_update'              => 'hoofdstuk bijgewerkt',
     'chapter_update_notification' => 'Hoofdstuk succesvol bijgewerkt',
-    'chapter_delete'              => 'verwijderde hoofdstuk',
+    'chapter_delete'              => 'hoofdstuk verwijderd',
     'chapter_delete_notification' => 'Hoofdstuk met succes verwijderd',
-    'chapter_move'                => 'verplaatste hoofdstuk',
+    'chapter_move'                => 'hoofdstuk verplaatst',
 
     // Books
-    'book_create'                 => 'maakte boek',
+    'book_create'                 => 'boek aangemaakt',
     'book_create_notification'    => 'Boek succesvol aangemaakt',
-    'book_update'                 => 'wijzigde boek',
+    'book_update'                 => 'boek bijgewerkt',
     'book_update_notification'    => 'Boek succesvol bijgewerkt',
-    'book_delete'                 => 'verwijderde boek',
+    'book_delete'                 => 'boek verwijderd',
     'book_delete_notification'    => 'Boek met succes verwijderd',
-    'book_sort'                   => 'sorteerde boek',
+    'book_sort'                   => 'boek gesorteerd',
     'book_sort_notification'      => 'Boek succesvol opnieuw gesorteerd',
 
     // Bookshelves
     'bookshelf_create'            => 'boekenplank aangemaakt',
     'bookshelf_create_notification'    => 'Boekenplank succesvol aangemaakt',
-    'bookshelf_update'                 => 'wijzigde boekenplank',
+    'bookshelf_update'                 => 'boekenplank bijgewerkt',
     'bookshelf_update_notification'    => 'Boekenplank succesvol bijgewerkt',
-    'bookshelf_delete'                 => 'verwijderde boekenplank',
+    'bookshelf_delete'                 => 'boekenplank verwijderd',
     'bookshelf_delete_notification'    => 'Boekenplank met succes verwijderd',
 
     // Favourites
@@ -48,8 +48,8 @@ return [
     'favourite_remove_notification' => '":name" is verwijderd uit je favorieten',
 
     // MFA
-    'mfa_setup_method_notification' => 'Multi-factor methode succesvol geconfigureerd',
-    'mfa_remove_method_notification' => 'Multi-factor methode succesvol verwijderd',
+    'mfa_setup_method_notification' => 'Meervoudige verificatie methode is succesvol geconfigureerd',
+    'mfa_remove_method_notification' => 'Meervoudige verificatie methode is succesvol verwijderd',
 
     // Webhooks
     'webhook_create' => 'webhook aangemaakt',
@@ -65,5 +65,5 @@ return [
 
     // Other
     'commented_on'                => 'reageerde op',
-    'permissions_update'          => 'bijgewerkte machtigingen',
+    'permissions_update'          => 'machtigingen bijgewerkt',
 ];
