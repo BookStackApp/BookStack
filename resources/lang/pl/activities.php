644e3b976753a97b4fcc58ec33b6762b8ccc2718
Dan Brown <ssddanbrown@googlemail.com>
2022-02-15 10:19:16 +0000

    New translations activities.php (Polish)

--- resources/lang/pl/activities.php
+++ resources/lang/pl/activities.php
@@ -6,41 +6,41 @@
 return [
 
     // Pages
-    'page_create'                 => 'utworzono stronę',
+    'page_create'                 => 'utworzył stronę',
     'page_create_notification'    => 'Strona została utworzona',
-    'page_update'                 => 'zaktualizowano stronę',
+    'page_update'                 => 'zaktualizował stronę',
     'page_update_notification'    => 'Strona zaktualizowana pomyślnie',
-    'page_delete'                 => 'usunięto stronę',
+    'page_delete'                 => 'usunął stronę',
     'page_delete_notification'    => 'Strona została usunięta',
-    'page_restore'                => 'przywrócono stronę',
+    'page_restore'                => 'przywrócił stronę',
     'page_restore_notification'   => 'Strona przywrócona pomyślnie',
-    'page_move'                   => 'przeniesiono stronę',
+    'page_move'                   => 'przeniósł stronę',
 
     // Chapters
-    'chapter_create'              => 'utworzono rozdział',
+    'chapter_create'              => 'utworzył rozdział',
     'chapter_create_notification' => 'Rozdział utworzony pomyślnie',
-    'chapter_update'              => 'zaktualizowano rozdział',
+    'chapter_update'              => 'zaktualizował rozdział',
     'chapter_update_notification' => 'Rozdział zaktualizowany pomyślnie',
-    'chapter_delete'              => 'usunięto rozdział',
+    'chapter_delete'              => 'usunął rozdział',
     'chapter_delete_notification' => 'Rozdział usunięty pomyślnie',
-    'chapter_move'                => 'przeniesiono rozdział',
+    'chapter_move'                => 'przeniósł rozdział',
 
     // Books
-    'book_create'                 => 'utworzono książkę',
+    'book_create'                 => 'utworzył książkę',
     'book_create_notification'    => 'Książka utworzona pomyślnie',
-    'book_update'                 => 'zaktualizowano książkę',
+    'book_update'                 => 'zaktualizował książkę',
     'book_update_notification'    => 'Książka zaktualizowana pomyślnie',
-    'book_delete'                 => 'usunięto książkę',
+    'book_delete'                 => 'usunął książkę',
     'book_delete_notification'    => 'Książka usunięta pomyślnie',
-    'book_sort'                   => 'posortowano książkę',
+    'book_sort'                   => 'posortował książkę',
     'book_sort_notification'      => 'Książka przywrócona pomyślnie',
 
     // Bookshelves
     'bookshelf_create'            => 'utworzył półkę',
     'bookshelf_create_notification'    => 'Półka utworzona pomyślnie',
-    'bookshelf_update'                 => 'zaktualizowano półkę',
+    'bookshelf_update'                 => 'zaktualizował półkę',
     'bookshelf_update_notification'    => 'Półka zaktualizowana pomyślnie',
-    'bookshelf_delete'                 => 'usunięto półkę',
+    'bookshelf_delete'                 => 'usunął półkę',
     'bookshelf_delete_notification'    => 'Półka usunięta pomyślnie',
 
     // Favourites
