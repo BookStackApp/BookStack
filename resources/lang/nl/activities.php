e53371e64cdc2d01ba21a76e6d074de18fbd9cbe
Dan Brown <ssddanbrown@googlemail.com>
2022-02-16 10:34:49 +0000

    New translations activities.php (Dutch)

--- resources/lang/nl/activities.php
+++ resources/lang/nl/activities.php
@@ -6,14 +6,14 @@
 return [
 
     // Pages
-    'page_create'                 => 'aangemaakte pagina',
+    'page_create'                 => 'maakte pagina',
     'page_create_notification'    => 'Pagina succesvol aangemaakt',
     'page_update'                 => 'pagina bijgewerkt',
     'page_update_notification'    => 'Pagina succesvol bijgewerkt',
     'page_delete'                 => 'verwijderde pagina',
-    'page_delete_notification'    => 'Pagina met succes verwijderd',
+    'page_delete_notification'    => 'Pagina succesvol verwijderd',
     'page_restore'                => 'pagina hersteld',
-    'page_restore_notification'   => 'Pagina met succes hersteld',
+    'page_restore_notification'   => 'Pagina succesvol hersteld',
     'page_move'                   => 'pagina verplaatst',
 
     // Chapters
@@ -22,7 +22,7 @@ return [
     'chapter_update'              => 'hoofdstuk bijgewerkt',
     'chapter_update_notification' => 'Hoofdstuk succesvol bijgewerkt',
     'chapter_delete'              => 'hoofdstuk verwijderd',
-    'chapter_delete_notification' => 'Hoofdstuk met succes verwijderd',
+    'chapter_delete_notification' => 'Hoofdstuk succesvol verwijderd',
     'chapter_move'                => 'hoofdstuk verplaatst',
 
     // Books
@@ -31,7 +31,7 @@ return [
     'book_update'                 => 'boek bijgewerkt',
     'book_update_notification'    => 'Boek succesvol bijgewerkt',
     'book_delete'                 => 'boek verwijderd',
-    'book_delete_notification'    => 'Boek met succes verwijderd',
+    'book_delete_notification'    => 'Boek succesvol verwijderd',
     'book_sort'                   => 'boek gesorteerd',
     'book_sort_notification'      => 'Boek succesvol opnieuw gesorteerd',
 
@@ -41,14 +41,14 @@ return [
     'bookshelf_update'                 => 'boekenplank bijgewerkt',
     'bookshelf_update_notification'    => 'Boekenplank succesvol bijgewerkt',
     'bookshelf_delete'                 => 'boekenplank verwijderd',
-    'bookshelf_delete_notification'    => 'Boekenplank met succes verwijderd',
+    'bookshelf_delete_notification'    => 'Boekenplank succesvol verwijderd',
 
     // Favourites
     'favourite_add_notification' => '":name" is toegevoegd aan je favorieten',
     'favourite_remove_notification' => '":name" is verwijderd uit je favorieten',
 
     // MFA
-    'mfa_setup_method_notification' => 'Meervoudige verificatie methode is succesvol geconfigureerd',
+    'mfa_setup_method_notification' => 'Multi-factor methode is succesvol geconfigureerd',
     'mfa_remove_method_notification' => 'Meervoudige verificatie methode is succesvol verwijderd',
 
     // Webhooks
@@ -57,11 +57,11 @@ return [
     'webhook_update' => 'webhook bijgewerkt',
     'webhook_update_notification' => 'Webhook succesvol bijgewerkt',
     'webhook_delete' => 'webhook verwijderd',
-    'webhook_delete_notification' => 'Webhook met succes verwijderd',
+    'webhook_delete_notification' => 'Webhook succesvol verwijderd',
 
     // Users
     'user_update_notification' => 'Gebruiker succesvol bijgewerkt',
-    'user_delete_notification' => 'Gebruiker met succes verwijderd',
+    'user_delete_notification' => 'Gebruiker succesvol verwijderd',
 
     // Other
     'commented_on'                => 'reageerde op',
