a57e8a5109834cc292041ec946293474fa2789e0
Dan Brown <ssddanbrown@googlemail.com>
2022-03-18 11:19:47 +0000

    New translations passwords.php (Basque)

--- resources/lang/eu/passwords.php
+++ resources/lang/eu/passwords.php
@@ -8,7 +8,7 @@ return [
 
     'password' => 'Pasahitzak gutxienez sei karaktere izan behar ditu eta baldintzak bete behar ditu.',
     'user' => "Ezin izan dugu erabiltzailerik topatu posta-helbide honekin.",
-    'token' => 'The password reset token is invalid for this email address.',
+    'token' => 'Pasahitza berritzeko token hau ez da baliagarria email helbide honentzat.',
     'sent' => 'Zure pasahitza berrezartzeko esteka email bidez bidali dizugu!',
     'reset' => 'Zure pasahitza berrezarri da!',
 
