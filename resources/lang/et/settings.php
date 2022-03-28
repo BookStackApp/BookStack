106c04ed01722b1387581c77f96d82271b6dba04
Dan Brown <ssddanbrown@googlemail.com>
2022-03-28 11:35:21 +0100

    New translations settings.php (Estonian)

--- resources/lang/et/settings.php
+++ resources/lang/et/settings.php
@@ -10,6 +10,8 @@ return [
     'settings' => 'Seaded',
     'settings_save' => 'Salvesta seaded',
     'settings_save_success' => 'Seaded salvestatud',
+    'system_version' => 'System Version',
+    'categories' => 'Categories',
 
     // App Settings
     'app_customization' => 'Kohandamine',
