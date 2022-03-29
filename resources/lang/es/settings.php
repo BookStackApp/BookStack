59742bbb204071f7ce1d32ed08cf524af0e1c3f2
Dan Brown <ssddanbrown@googlemail.com>
2022-03-29 16:21:00 +0100

    New translations settings.php (Spanish)

--- resources/lang/es/settings.php
+++ resources/lang/es/settings.php
@@ -10,8 +10,8 @@ return [
     'settings' => 'Ajustes',
     'settings_save' => 'Guardar ajustes',
     'settings_save_success' => 'Ajustes guardados',
-    'system_version' => 'System Version',
-    'categories' => 'Categories',
+    'system_version' => 'Versión del Sistema',
+    'categories' => 'Categorías',
 
     // App Settings
     'app_customization' => 'Personalización',
