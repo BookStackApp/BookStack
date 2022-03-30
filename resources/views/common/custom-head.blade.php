da4308bb0f692056ffc14ddb0b5881b0291b894e
Dan Brown <ssddanbrown@googlemail.com>
2022-03-30 19:15:24 +0100

    Fixed settings redirect issue and custom head display
    - Fixed issue where redirect for `/settings` view would not be ran
      through base url generator so would not create a correct path in some
      cases. Now routed through controller with normal redirect.
    - Fixed custom head content being active on settings pages due to route
      name changes, for when viewing settings, in last release.
    
    Fixes #3356 and #3355

--- resources/views/common/custom-head.blade.php
+++ resources/views/common/custom-head.blade.php
@@ -1,6 +1,6 @@
 @inject('headContent', 'BookStack\Theming\CustomHtmlHeadContentProvider')
 
-@if(setting('app-custom-head') && \Route::currentRouteName() !== 'settings')
+@if(setting('app-custom-head') && !request()->routeIs('settings.category'))
 <!-- Start: custom user content -->
 {!! $headContent->forWeb() !!}
 <!-- End: custom user content -->
