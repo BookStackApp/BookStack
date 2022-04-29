8fb016d1bfb7cb18000630ed1fedf1281c373f20
Dan Brown <ssddanbrown@googlemail.com>
2022-04-29 15:40:38 +0100

    New Crowdin updates (#3384)

---
+++ resources/lang/uz/components.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Text used in custom JavaScript driven components.
+ */
+return [
+
+    // Image Manager
+    'image_select' => 'Rasmni tanlash',
+    'image_all' => 'Barchasi',
+    'image_all_title' => 'Barcha rasmlarni ko\'rish',
+    'image_book_title' => 'Ushbu kitobga yuklangan barcha rasmlarni ko\'rish',
+    'image_page_title' => 'Ushbu sahifaga yuklangan barcha rasmlarni ko\'rish',
+    'image_search_hint' => 'Rasmni nomi bo\'yicha izlash',
+    'image_uploaded' => ':uploadedDate sanada yuklangan',
+    'image_load_more' => 'Yana yuklash',
+    'image_image_name' => 'Rasm nomi',
+    'image_delete_used' => 'This image is used in the pages below.',
+    'image_delete_confirm_text' => 'Are you sure you want to delete this image?',
+    'image_select_image' => 'Rasmni tanlash',
+    'image_dropzone' => 'Drop images or click here to upload',
+    'images_deleted' => 'Images Deleted',
+    'image_preview' => 'Image Preview',
+    'image_upload_success' => 'Image uploaded successfully',
+    'image_update_success' => 'Image details successfully updated',
+    'image_delete_success' => 'Image successfully deleted',
+    'image_upload_remove' => 'Olib tashlash',
+
+    // Code Editor
+    'code_editor' => 'Kodni tahrirlash',
+    'code_language' => 'Kod tili',
+    'code_content' => 'Kod matni',
+    'code_session_history' => 'Sessiya tarixi',
+    'code_save' => 'Kodni saqlash',
+];
