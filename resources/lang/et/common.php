24915c891dd01bff3a45091566b174c33016941d
Dan Brown <ssddanbrown@googlemail.com>
2022-05-30 17:12:54 +0100

    New translations common.php (Estonian)

--- resources/lang/et/common.php
+++ resources/lang/et/common.php
@@ -47,6 +47,8 @@ return [
     'previous' => 'Eelmine',
     'filter_active' => 'Aktiivne filter:',
     'filter_clear' => 'Tühjenda filter',
+    'download' => 'Download',
+    'open_in_tab' => 'Open in Tab',
 
     // Sort Options
     'sort_options' => 'Sorteerimise valikud',
