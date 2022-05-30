01fa3bda7b65918f148676221ef86a99c9b2832d
Dan Brown <ssddanbrown@googlemail.com>
2022-05-30 17:13:04 +0100

    New translations common.php (French)

--- resources/lang/fr/common.php
+++ resources/lang/fr/common.php
@@ -47,6 +47,8 @@ return [
     'previous' => 'Précédent',
     'filter_active' => 'Filtre actif :',
     'filter_clear' => 'Effacer le filtre',
+    'download' => 'Download',
+    'open_in_tab' => 'Open in Tab',
 
     // Sort Options
     'sort_options' => 'Options de tri',
