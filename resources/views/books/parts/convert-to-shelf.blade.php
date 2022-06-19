f145ffc93004707257b03773c70255e73909c9ed
Dan Brown <ssddanbrown@googlemail.com>
2022-06-19 16:23:18 +0100

    Extracted conversion text to translation file

--- resources/views/books/parts/convert-to-shelf.blade.php
+++ resources/views/books/parts/convert-to-shelf.blade.php
@@ -1,27 +1,18 @@
 <div class="content-wrap card auto-height">
-    <h2 class="list-heading">Convert to Shelf</h2>
+    <h2 class="list-heading">{{ trans('entities.convert_to_shelf') }}</h2>
     <p>
-        You can convert this book to a new shelf with the same contents.
-        Chapters contained within this book will be converted to new books.
-
-        If this book contains any pages, that are not in a chapter, this book will be renamed
-        and contain such pages, and this book will become part of the new shelf.
-
+        {{ trans('entities.convert_to_shelf_contents_desc') }}
         <br><br>
-
-        Any permissions set on this book will be copied to the new shelf and to all new child books
-        that don't have their own permissions enforced.
-
-        Note that permissions on shelves do not auto-cascade to content within, as they do for books.
+        {{ trans('entities.convert_to_shelf_permissions_desc') }}
     </p>
     <div class="text-right">
         <div component="dropdown" class="dropdown-container">
-            <button refs="dropdown@toggle" class="button outline" aria-haspopup="true" aria-expanded="false">Convert Book</button>
+            <button refs="dropdown@toggle" class="button outline" aria-haspopup="true" aria-expanded="false">{{ trans('entities.convert_book') }}</button>
             <ul refs="dropdown@menu" class="dropdown-menu" role="menu">
                 <li class="px-m py-s text-small text-muted">
-                    Are you sure you want to convert this book?
+                    {{ trans('entities.convert_book_confirm') }}
                     <br>
-                    This cannot be as easily undone.
+                    {{ trans('entities.convert_undo_warning') }}
                 </li>
                 <li>
                     <form action="{{ $book->getUrl('/convert-to-shelf') }}" method="POST">
