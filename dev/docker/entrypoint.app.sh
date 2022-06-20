0ceb54f776dea1c8d7973c06035d425a2a1e2cc2
Stefan Simon <stefan.simon@bytepoets.com>
2022-06-20 13:40:17 +0200

    Run migration without command prompt

--- dev/docker/entrypoint.app.sh
+++ dev/docker/entrypoint.app.sh
@@ -9,7 +9,7 @@ if [[ -n "$1" ]]; then
 else
     composer install
     wait-for-it db:3306 -t 45
-    php artisan migrate --database=mysql
+    php artisan migrate --database=mysql --force
     chown -R www-data:www-data storage
     exec apache2-foreground
 fi
