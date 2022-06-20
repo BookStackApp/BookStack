1e73a6049649597281b0f14f90a56fa8fe3fa074
Dan Brown <ssddanbrown@googlemail.com>
2022-06-20 13:01:31 +0100

    New translations entities.php (Polish)

--- resources/lang/pl/entities.php
+++ resources/lang/pl/entities.php
@@ -355,4 +355,16 @@ return [
     'copy_consider_images' => 'Pliki obrazów znajdujących się na stronie nie będą zduplikowane i oryginalne obrazy zachowają swój związek ze stroną, do której zostały pierwotnie przesłane.',
     'copy_consider_attachments' => 'Załączniki strony nie zostaną skopiowane.',
     'copy_consider_access' => 'Zmiana lokalizacji, właściciela lub uprawnień może spowodować, że ta zawartość będzie dostępna dla tych, którzy wcześniej nie mieli dostępu.',
+
+    // Conversions
+    'convert_to_shelf' => 'Convert to Shelf',
+    'convert_to_shelf_contents_desc' => 'You can convert this book to a new shelf with the same contents. Chapters contained within this book will be converted to new books. If this book contains any pages, that are not in a chapter, this book will be renamed and contain such pages, and this book will become part of the new shelf.',
+    'convert_to_shelf_permissions_desc' => 'Any permissions set on this book will be copied to the new shelf and to all new child books that don\'t have their own permissions enforced. Note that permissions on shelves do not auto-cascade to content within, as they do for books.',
+    'convert_book' => 'Convert Book',
+    'convert_book_confirm' => 'Are you sure you want to convert this book?',
+    'convert_undo_warning' => 'This cannot be as easily undone.',
+    'convert_to_book' => 'Convert to Book',
+    'convert_to_book_desc' => 'You can convert this chapter to a new book with the same contents. Any permissions set on this chapter will be copied to the new book but any inherited permissions, from the parent book, will not be copied which could lead to a change of access control.',
+    'convert_chapter' => 'Convert Chapter',
+    'convert_chapter_confirm' => 'Are you sure you want to convert this chapter?',
 ];
