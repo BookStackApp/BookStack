cef16052a861bd05bd97c6cb6d7514b7f5b5c9e1
Dan Brown <ssddanbrown@googlemail.com>
2022-06-21 15:46:03 +0100

    New translations auth.php (Slovenian)

--- resources/lang/sl/auth.php
+++ resources/lang/sl/auth.php
@@ -38,6 +38,11 @@ return [
     'registration_email_domain_invalid' => 'Ta e-poštna domena nima dostopa do te aplikacije',
     'register_success' => 'Hvala za registracijo! Sedaj ste registrirani in prijavljeni.',
 
+    // Login auto-initiation
+    'auto_init_starting' => 'Attempting Login',
+    'auto_init_starting_desc' => 'We\'re contacting your authentication system to start the login process. If there\'s no progress after 5 seconds you can try clicking the link below.',
+    'auto_init_start_link' => 'Proceed with authentication',
+
     // Password Reset
     'reset_password' => 'Ponastavi geslo',
     'reset_password_send_instructions' => 'Spodaj vpišite vaš e-poštni naslov in prejeli boste e-pošto s povezavo za ponastavitev gesla.',
