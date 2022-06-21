f221354b74c82215a2a0a70b0301f76a1b016524
Dan Brown <ssddanbrown@googlemail.com>
2022-06-21 15:45:53 +0100

    New translations auth.php (Spanish, Argentina)

--- resources/lang/es_AR/auth.php
+++ resources/lang/es_AR/auth.php
@@ -38,6 +38,11 @@ return [
     'registration_email_domain_invalid' => 'Este dominio de correo electrónico no tiene acceso a esta aplicación',
     'register_success' => '¡Gracias por registrarse! Ahora se encuentra registrado y ha accedido a la aplicación.',
 
+    // Login auto-initiation
+    'auto_init_starting' => 'Attempting Login',
+    'auto_init_starting_desc' => 'We\'re contacting your authentication system to start the login process. If there\'s no progress after 5 seconds you can try clicking the link below.',
+    'auto_init_start_link' => 'Proceed with authentication',
+
     // Password Reset
     'reset_password' => 'Restablecer la contraseña',
     'reset_password_send_instructions' => 'Introduzca su correo electrónico a continuación y se le enviará un correo electrónico con un enlace para la restauración',
