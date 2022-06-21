cef6b766f270af93d2acacfebf2b353a0f85c05a
Dan Brown <ssddanbrown@googlemail.com>
2022-06-21 15:45:43 +0100

    New translations auth.php (Portuguese, Brazilian)

--- resources/lang/pt_BR/auth.php
+++ resources/lang/pt_BR/auth.php
@@ -38,6 +38,11 @@ return [
     'registration_email_domain_invalid' => 'O domínio de e-mail usado não tem acesso permitido a essa aplicação',
     'register_success' => 'Obrigado por se cadastrar! Você agora encontra-se cadastrado(a) e logado(a).',
 
+    // Login auto-initiation
+    'auto_init_starting' => 'Attempting Login',
+    'auto_init_starting_desc' => 'We\'re contacting your authentication system to start the login process. If there\'s no progress after 5 seconds you can try clicking the link below.',
+    'auto_init_start_link' => 'Proceed with authentication',
+
     // Password Reset
     'reset_password' => 'Redefinir Senha',
     'reset_password_send_instructions' => 'Insira seu e-mail abaixo e uma mensagem com o link de redefinição de senha lhe será enviada.',
