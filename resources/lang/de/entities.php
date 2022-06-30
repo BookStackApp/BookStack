341853924fb52db1d206b0de7d32ad6326bc3523
Dan Brown <ssddanbrown@googlemail.com>
2022-06-30 09:36:18 +0100

    New translations entities.php (German)

--- resources/lang/de/entities.php
+++ resources/lang/de/entities.php
@@ -357,7 +357,7 @@ return [
     'copy_consider_access' => 'Eine Veränderung der Position, Besitzers oder Berechtigungen könnte dafür sorgen, dass Unberechtigte, Zugriff bekommen.',
 
     // Conversions
-    'convert_to_shelf' => 'Convert to Shelf',
+    'convert_to_shelf' => 'In Regal umwandeln',
     'convert_to_shelf_contents_desc' => 'You can convert this book to a new shelf with the same contents. Chapters contained within this book will be converted to new books. If this book contains any pages, that are not in a chapter, this book will be renamed and contain such pages, and this book will become part of the new shelf.',
     'convert_to_shelf_permissions_desc' => 'Any permissions set on this book will be copied to the new shelf and to all new child books that don\'t have their own permissions enforced. Note that permissions on shelves do not auto-cascade to content within, as they do for books.',
     'convert_book' => 'Convert Book',
