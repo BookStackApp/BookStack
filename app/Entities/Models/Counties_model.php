f0de1e20122d8dec0584e17fed8c969c9f456985
unknown <kiprop@dukapepe.tech>
2022-07-12 11:05:55 +0300

    cancer levels design and ratings

--- app/Entities/Models/Counties_model.php
+++ app/Entities/Models/Counties_model.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Counties_model extends Model
 {
-    protected $tableName = 'counties';
+    //protected $tableName = 'counties';
     use HasFactory;
 }
