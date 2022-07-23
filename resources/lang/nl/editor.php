2354ce49ba322395d938e3a170433414bd741857
Dan Brown <ssddanbrown@googlemail.com>
2022-07-23 11:43:08 +0100

    New translations editor.php (Dutch)

--- resources/lang/nl/editor.php
+++ resources/lang/nl/editor.php
@@ -157,7 +157,7 @@ return [
     'about' => 'Over de bewerker',
     'about_title' => 'Over de WYSIWYG Bewerker',
     'editor_license' => 'Bewerker Licentie & Copyright',
-    'editor_tiny_license' => 'Deze bewerker is gebouwd met :tinyLink, dat aangeboden wordt via een LGPL v2.1 licentie.',
+    'editor_tiny_license' => 'This editor is built using :tinyLink which is provided under the MIT license.',
     'editor_tiny_license_link' => 'De copyright- en licentiegegevens van TinyMCE vindt u hier.',
     'save_continue' => 'Pagina opslaan en verdergaan',
     'callouts_cycle' => '(Blijf drukken om door de types te wisselen)',
