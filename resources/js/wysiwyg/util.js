5d2d473f22bf3fefc22e478bdc25b53e6e29cd83
Dan Brown <ssddanbrown@googlemail.com>
2022-07-23 11:18:03 +0100

    Ensured wysiwyg details contents are wrapped in block elements
    Fixes issue where inline-only content would disappear when unwrapping a
    details block element.

---
+++ resources/js/wysiwyg/util.js
@@ -0,0 +1,19 @@
+
+
+export const blockElementTypes = [
+    'p',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'div',
+    'blockquote',
+    'pre',
+    'code-block',
+    'details',
+    'ul',
+    'ol',
+    'table'
+];
