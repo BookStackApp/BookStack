8a0a56f0866abe6273fee76d1c1e77b243597de7
Dan Brown <ssddanbrown@googlemail.com>
2022-07-24 12:39:35 +0100

    New translations entities.php (French)

--- resources/lang/fr/entities.php
+++ resources/lang/fr/entities.php
@@ -88,7 +88,7 @@ return [
     'shelves_save' => 'Enregistrer l\'étagère',
     'shelves_books' => 'Livres sur cette étagère',
     'shelves_add_books' => 'Ajouter des livres sur cette étagère',
-    'shelves_drag_books' => 'Déposez des livres ici pour les ajouter a cette étagère',
+    'shelves_drag_books' => 'Drag books below to add them to this shelf',
     'shelves_empty_contents' => 'Aucun livre n\'a été assigné à cette étagère',
     'shelves_edit_and_assign' => 'Modifier cette étagère pour y ajouter des livres',
     'shelves_edit_named' => 'Modifier l\'étagère :name',
