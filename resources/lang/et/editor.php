017c7659e50a004a343e59eefa9b69b2c4da6bc9
Dan Brown <ssddanbrown@googlemail.com>
2022-07-25 16:03:40 +0100

    New translations editor.php (Estonian)

--- resources/lang/et/editor.php
+++ resources/lang/et/editor.php
@@ -157,7 +157,7 @@ return [
     'about' => 'Redaktori info',
     'about_title' => 'Info WYSIWYG redaktori kohta',
     'editor_license' => 'Redaktori litsents ja autoriõigused',
-    'editor_tiny_license' => 'This editor is built using :tinyLink which is provided under the MIT license.',
+    'editor_tiny_license' => 'See redaktor on loodud :tinyLink abil, mis on saadaval MIT litsentsi alusel.',
     'editor_tiny_license_link' => 'TinyMCE autoriõigused ja litsents on saadaval siin.',
     'save_continue' => 'Salvesta leht ja jätka',
     'callouts_cycle' => '(Vajuta, et tüüpide vahel valida)',
