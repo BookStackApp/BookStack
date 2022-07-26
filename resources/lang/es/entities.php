80039c6232535eb143a06b3f5ec76dad6f706357
Dan Brown <ssddanbrown@googlemail.com>
2022-07-26 20:14:59 +0100

    New translations entities.php (Spanish)

--- resources/lang/es/entities.php
+++ resources/lang/es/entities.php
@@ -171,7 +171,7 @@ return [
     'chapters_permissions_active' => 'Permisos de capítulo activos',
     'chapters_permissions_success' => 'Permisos de capítulo actualizados',
     'chapters_search_this' => 'Buscar este capítulo',
-    'chapter_sort_book' => 'Sort Book',
+    'chapter_sort_book' => 'Ordenar libro',
 
     // Pages
     'page' => 'Página',
