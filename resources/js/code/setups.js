d4f2fcdf7908ffa176350468a85e631f0497646c
Dan Brown <ssddanbrown@googlemail.com>
2022-08-02 20:11:02 +0100

    Started codemirror update, In broken state

---
+++ resources/js/code/setups.js
@@ -0,0 +1,32 @@
+
+import {keymap, highlightSpecialChars, drawSelection, highlightActiveLine, dropCursor,
+    rectangularSelection, lineNumbers, highlightActiveLineGutter} from "@codemirror/view"
+import {defaultHighlightStyle, syntaxHighlighting, bracketMatching,
+     foldKeymap} from "@codemirror/language"
+import {defaultKeymap, history, historyKeymap} from "@codemirror/commands"
+import {EditorState} from "@codemirror/state"
+
+import {modesAsStreamLanguages} from "./modes";
+
+
+export function viewer() {
+    return [
+        lineNumbers(),
+        highlightActiveLineGutter(),
+        highlightSpecialChars(),
+        history(),
+        drawSelection(),
+        dropCursor(),
+        syntaxHighlighting(defaultHighlightStyle, {fallback: true}),
+        bracketMatching(),
+        rectangularSelection(),
+        highlightActiveLine(),
+        keymap.of([
+            ...defaultKeymap,
+            ...historyKeymap,
+            ...foldKeymap,
+        ]),
+        EditorState.readOnly.of(true),
+        ...modesAsStreamLanguages(),
+    ];
+}
