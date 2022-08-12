1a86e01d0f923eddcaa0d6181a1d885fc532f816
Dan Brown <ssddanbrown@googlemail.com>
2022-08-12 17:28:22 +0100

    New translations activities.php (Romanian)

--- resources/lang/ro/activities.php
+++ resources/lang/ro/activities.php
@@ -6,45 +6,45 @@
 return [
 
     // Pages
-    'page_create'                 => 'pagina creată',
+    'page_create'                 => 'a creat pagina',
     'page_create_notification'    => 'Pagina creată cu succes',
-    'page_update'                 => 'pagină actualizată',
+    'page_update'                 => 'a actualizat pagina',
     'page_update_notification'    => 'Pagina a fost actualizată cu succes',
-    'page_delete'                 => 'pagină ștearsă',
+    'page_delete'                 => 'a șters pagina',
     'page_delete_notification'    => 'Pagina a fost ștearsă cu succes',
-    'page_restore'                => 'pagină restabilită',
+    'page_restore'                => 'a restabilit pagina',
     'page_restore_notification'   => 'Pagina a fost restaurată cu succes',
-    'page_move'                   => 'pagină mutată',
+    'page_move'                   => 'a mutat pagina',
 
     // Chapters
-    'chapter_create'              => 'capitol creat',
+    'chapter_create'              => 'a creat capitolul',
     'chapter_create_notification' => 'Capitol creat cu succes',
-    'chapter_update'              => 'capitol actualizat',
+    'chapter_update'              => 'a actualizat capitolul',
     'chapter_update_notification' => 'Capitolul a fost actualizat cu succes',
-    'chapter_delete'              => 'capitol șters',
+    'chapter_delete'              => 'a șters capitolul',
     'chapter_delete_notification' => 'Capitolul a fost șters cu succes',
-    'chapter_move'                => 'capitol mutat',
+    'chapter_move'                => 'a mutat capitolul',
 
     // Books
-    'book_create'                 => 'carte creată',
+    'book_create'                 => 'a creat cartea',
     'book_create_notification'    => 'Carte creată cu succes',
-    'book_create_from_chapter'              => 'capitol convertit în carte',
+    'book_create_from_chapter'              => 'a convertit capitolul în carte',
     'book_create_from_chapter_notification' => 'Capitol convertit cu succes într-o carte',
-    'book_update'                 => 'carte actualizată',
+    'book_update'                 => 'a actualizat cartea',
     'book_update_notification'    => 'Carte actualizată cu succes',
-    'book_delete'                 => 'carte ștearsă',
+    'book_delete'                 => 'a șters cartea',
     'book_delete_notification'    => 'Carte ștearsă cu succes',
-    'book_sort'                   => 'carte sortată',
-    'book_sort_notification'      => 'Carte resortată cu succes',
+    'book_sort'                   => 'a sortat cartea',
+    'book_sort_notification'      => 'Carte reordonată cu succes',
 
     // Bookshelves
-    'bookshelf_create'            => 'raft creat',
+    'bookshelf_create'            => 'a creat raftul',
     'bookshelf_create_notification'    => 'Raftul de cărți a fost creat cu succes',
-    'bookshelf_create_from_book'    => 'carte convertită în raft de cărți',
+    'bookshelf_create_from_book'    => 'a convertit cartea în raft de cărți',
     'bookshelf_create_from_book_notification'    => 'Carte transformată cu succes într-un raft',
-    'bookshelf_update'                 => 'raft de cărți actualizat',
+    'bookshelf_update'                 => 'a actualizat raftul de cărți',
     'bookshelf_update_notification'    => 'Raftul de cărți a fost actualizat',
-    'bookshelf_delete'                 => 'raft de cărți șters',
+    'bookshelf_delete'                 => 'a șters raftul',
     'bookshelf_delete_notification'    => 'Raftul de cărți a fost șters',
 
     // Favourites
@@ -56,11 +56,11 @@ return [
     'mfa_remove_method_notification' => 'Metoda multi-factor a fost configurată cu succes',
 
     // Webhooks
-    'webhook_create' => 'webhook creat',
+    'webhook_create' => 'a creat webhook',
     'webhook_create_notification' => 'Webhook creat cu succes',
-    'webhook_update' => 'webhook actualizat',
+    'webhook_update' => 'a actualizat webhook',
     'webhook_update_notification' => 'Webhook actualizat cu succes',
-    'webhook_delete' => 'webhook șters',
+    'webhook_delete' => 'a șters webhook',
     'webhook_delete_notification' => 'Webhook șters cu succes',
 
     // Users
@@ -68,6 +68,6 @@ return [
     'user_delete_notification' => 'Utilizator eliminat cu succes',
 
     // Other
-    'commented_on'                => 'commented on',
-    'permissions_update'          => 'updated permissions',
+    'commented_on'                => 'a comentat la',
+    'permissions_update'          => 'a actualizat permisiunile',
 ];
