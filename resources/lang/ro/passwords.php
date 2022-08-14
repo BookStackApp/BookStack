e387eb17a81acd2c18c6b3efa7524526d6df663c
Dan Brown <ssddanbrown@googlemail.com>
2022-08-14 05:49:59 +0100

    New translations passwords.php (Romanian)

--- resources/lang/ro/passwords.php
+++ resources/lang/ro/passwords.php
@@ -6,10 +6,10 @@
  */
 return [
 
-    'password' => 'Passwords must be at least eight characters and match the confirmation.',
-    'user' => "We can't find a user with that e-mail address.",
-    'token' => 'The password reset token is invalid for this email address.',
-    'sent' => 'We have e-mailed your password reset link!',
-    'reset' => 'Your password has been reset!',
+    'password' => 'Parolele trebuie să aibă cel puțin opt caractere și să corespundă confirmării.',
+    'user' => "Nu putem găsi un utilizator cu această adresă de e-mail.",
+    'token' => 'Token-ul de resetare a parolei nu este valid pentru această adresă de e-mail.',
+    'sent' => 'Am trimis prin e-mail link-ul de resetare a parolei!',
+    'reset' => 'Parola ta a fost resetată!',
 
 ];
