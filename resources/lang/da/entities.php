80db9ad05eb036e878ff1d27cc2d4622371e7281
Dan Brown <ssddanbrown@googlemail.com>
2022-08-29 17:57:34 +0100

    New translations entities.php (Danish)

--- resources/lang/da/entities.php
+++ resources/lang/da/entities.php
@@ -23,6 +23,7 @@ return [
     'meta_updated' => 'Opdateret :timeLength',
     'meta_updated_name' => 'Opdateret :timeLength af :user',
     'meta_owned_name' => 'Ejet af :user',
+    'meta_reference_page_count' => 'Referenced on 1 page|Referenced on :count pages',
     'entity_select' => 'Vælg emne',
     'entity_select_lack_permission' => 'You don\'t have the required permissions to select this item',
     'images' => 'Billeder',
@@ -248,6 +249,7 @@ return [
     'pages_edit_content_link' => 'Redigér indhold',
     'pages_permissions_active' => 'Aktive sidetilladelser',
     'pages_initial_revision' => 'Første udgivelse',
+    'pages_references_update_revision' => 'System auto-update of internal links',
     'pages_initial_name' => 'Ny side',
     'pages_editing_draft_notification' => 'Du redigerer en kladde der sidst var gemt :timeDiff.',
     'pages_draft_edited_notification' => 'Siden har været opdateret siden da. Det er anbefalet at du kasserer denne kladde.',
@@ -369,4 +371,9 @@ return [
     'convert_to_book_desc' => 'You can convert this chapter to a new book with the same contents. Any permissions set on this chapter will be copied to the new book but any inherited permissions, from the parent book, will not be copied which could lead to a change of access control.',
     'convert_chapter' => 'Omdan Kapitel',
     'convert_chapter_confirm' => 'Er du sikker på at du vil omdanne dette kapitel?',
+
+    // References
+    'references' => 'References',
+    'references_none' => 'There are no tracked references to this item.',
+    'references_to_desc' => 'Shown below are all the known pages in the system that link to this item.',
 ];
