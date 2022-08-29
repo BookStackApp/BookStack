2b03368b4000d4fb33c0dcb17e030c3bb701cac5
Dan Brown <ssddanbrown@googlemail.com>
2022-08-29 17:58:12 +0100

    New translations entities.php (Indonesian)

--- resources/lang/id/entities.php
+++ resources/lang/id/entities.php
@@ -23,6 +23,7 @@ return [
     'meta_updated' => 'Diperbaharui :timeLength',
     'meta_updated_name' => 'Diperbaharui :timeLength oleh :user',
     'meta_owned_name' => 'Dimiliki oleh :user',
+    'meta_reference_page_count' => 'Referenced on 1 page|Referenced on :count pages',
     'entity_select' => 'Pilihan Entitas',
     'entity_select_lack_permission' => 'You don\'t have the required permissions to select this item',
     'images' => 'Gambar-gambar',
@@ -248,6 +249,7 @@ return [
     'pages_edit_content_link' => 'Sunting Konten',
     'pages_permissions_active' => 'Izin Halaman Aktif',
     'pages_initial_revision' => 'Penerbitan awal',
+    'pages_references_update_revision' => 'System auto-update of internal links',
     'pages_initial_name' => 'Halaman Baru',
     'pages_editing_draft_notification' => 'Anda sedang menyunting konsep yang terakhir disimpan :timeDiff.',
     'pages_draft_edited_notification' => 'Halaman ini telah diperbarui sejak saat itu. Anda disarankan untuk membuang draf ini.',
@@ -369,4 +371,9 @@ return [
     'convert_to_book_desc' => 'You can convert this chapter to a new book with the same contents. Any permissions set on this chapter will be copied to the new book but any inherited permissions, from the parent book, will not be copied which could lead to a change of access control.',
     'convert_chapter' => 'Convert Chapter',
     'convert_chapter_confirm' => 'Are you sure you want to convert this chapter?',
+
+    // References
+    'references' => 'References',
+    'references_none' => 'There are no tracked references to this item.',
+    'references_to_desc' => 'Shown below are all the known pages in the system that link to this item.',
 ];
