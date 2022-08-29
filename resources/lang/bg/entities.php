6d84c9619231d46c8bf963fa98531f7dd9cd1db2
Dan Brown <ssddanbrown@googlemail.com>
2022-08-29 17:57:28 +0100

    New translations entities.php (Bulgarian)

--- resources/lang/bg/entities.php
+++ resources/lang/bg/entities.php
@@ -23,6 +23,7 @@ return [
     'meta_updated' => 'Актуализирано :timeLength',
     'meta_updated_name' => 'Актуализирано преди :timeLength от :user',
     'meta_owned_name' => 'Притежавано от :user',
+    'meta_reference_page_count' => 'Referenced on 1 page|Referenced on :count pages',
     'entity_select' => 'Избор на обект',
     'entity_select_lack_permission' => 'You don\'t have the required permissions to select this item',
     'images' => 'Изображения',
@@ -248,6 +249,7 @@ return [
     'pages_edit_content_link' => 'Редактиране на съдържанието',
     'pages_permissions_active' => 'Настройките за достъп до страницата са активни',
     'pages_initial_revision' => 'Първо публикуване',
+    'pages_references_update_revision' => 'System auto-update of internal links',
     'pages_initial_name' => 'Нова страница',
     'pages_editing_draft_notification' => 'В момента редактирате чернова, която беше последно обновена :timeDiff.',
     'pages_draft_edited_notification' => 'Тази страница беше актуализирана от тогава. Препоръчително е да изтриете настоящата чернова.',
@@ -369,4 +371,9 @@ return [
     'convert_to_book_desc' => 'You can convert this chapter to a new book with the same contents. Any permissions set on this chapter will be copied to the new book but any inherited permissions, from the parent book, will not be copied which could lead to a change of access control.',
     'convert_chapter' => 'Convert Chapter',
     'convert_chapter_confirm' => 'Are you sure you want to convert this chapter?',
+
+    // References
+    'references' => 'References',
+    'references_none' => 'There are no tracked references to this item.',
+    'references_to_desc' => 'Shown below are all the known pages in the system that link to this item.',
 ];
