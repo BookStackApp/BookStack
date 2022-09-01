6af57bfce6ffc1412e979a7ce7f7a38376cf088d
Dan Brown <ssddanbrown@googlemail.com>
2022-09-01 15:05:32 +0100

    New translations activities.php (Spanish, Argentina)

--- resources/lang/es_AR/activities.php
+++ resources/lang/es_AR/activities.php
@@ -38,14 +38,14 @@ return [
     'book_sort_notification'      => 'Libro reordenado correctamente',
 
     // Bookshelves
-    'bookshelf_create'            => 'estante creado',
-    'bookshelf_create_notification'    => 'Estante creado correctamente',
-    'bookshelf_create_from_book'    => 'libro convertido en estante',
+    'bookshelf_create'            => 'created shelf',
+    'bookshelf_create_notification'    => 'Shelf successfully created',
+    'bookshelf_create_from_book'    => 'converted book to shelf',
     'bookshelf_create_from_book_notification'    => 'Libro convertido en estante con éxito',
-    'bookshelf_update'                 => 'Estante actualizado',
-    'bookshelf_update_notification'    => 'Estante actualizado correctamente',
-    'bookshelf_delete'                 => 'Estante borrado',
-    'bookshelf_delete_notification'    => 'Estante eliminado correctamente',
+    'bookshelf_update'                 => 'updated shelf',
+    'bookshelf_update_notification'    => 'Shelf successfully updated',
+    'bookshelf_delete'                 => 'deleted shelf',
+    'bookshelf_delete_notification'    => 'Shelf successfully deleted',
 
     // Favourites
     'favourite_add_notification' => '".name" se añadió a sus favoritos',
