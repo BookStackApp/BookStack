a29f056202d4481426304e4e39ae721e05c4f14d
Dan Brown <ssddanbrown@googlemail.com>
2022-09-14 18:35:20 +0100

    New translations auth.php (German Informal)

--- resources/lang/de_informal/auth.php
+++ resources/lang/de_informal/auth.php
@@ -39,9 +39,9 @@ return [
     'register_success' => 'Vielen Dank für deine Registrierung! Du bist jetzt registriert und eingeloggt.',
 
     // Login auto-initiation
-    'auto_init_starting' => 'Anmeldeversuche',
-    'auto_init_starting_desc' => 'Wir verbinden und mit Ihrem Authentifizierungssystem um den Anmeldeprozess zu starten. Sollte es nach 5 Sekunden nicht weitergehen, klicken Sie bitte auf den unten stehenden Link.',
-    'auto_init_start_link' => 'Mit Authentifizierung fortfahren',
+    'auto_init_starting' => 'Versuche Anmeldung',
+    'auto_init_starting_desc' => 'Wir kontaktieren dein Authentifizierungssystem, um den Anmeldevorgang zu starten. Wenn nach 5 Sekunden kein Fortschritt zu sehen ist, kannst du versuchen, auf den unten stehenden Link zu klicken.',
+    'auto_init_start_link' => 'Mit der Authentifizierung fortfahren',
 
     // Password Reset
     'reset_password' => 'Passwort vergessen',
