d9d1c2f9eb6f0c80cdcc07891195f89f9a42c62d
Dan Brown <ssddanbrown@googlemail.com>
2022-09-28 11:10:06 +0100

    Applied permissions to revision action visibility
    Related to #3723

--- resources/views/pages/parts/revision-table-row.blade.php
+++ resources/views/pages/parts/revision-table-row.blade.php
@@ -30,40 +30,46 @@
             <a target="_blank" rel="noopener" href="{{ $revision->page->getUrl() }}"><i>{{ trans('entities.pages_revisions_current') }}</i></a>
         @else
             <a href="{{ $revision->getUrl() }}" target="_blank" rel="noopener">{{ trans('entities.pages_revisions_preview') }}</a>
-            <span class="text-muted">&nbsp;|&nbsp;</span>
-            <div component="dropdown" class="dropdown-container">
-                <a refs="dropdown@toggle" href="#" aria-haspopup="true" aria-expanded="false">{{ trans('entities.pages_revisions_restore') }}</a>
-                <ul refs="dropdown@menu" class="dropdown-menu" role="menu">
-                    <li class="px-m py-s"><small class="text-muted">{{trans('entities.revision_restore_confirm')}}</small></li>
-                    <li>
-                        <form action="{{ $revision->getUrl('/restore') }}" method="POST">
-                            {!! csrf_field() !!}
-                            <input type="hidden" name="_method" value="PUT">
-                            <button type="submit" class="text-primary icon-item">
-                                @icon('history')
-                                <div>{{ trans('entities.pages_revisions_restore') }}</div>
-                            </button>
-                        </form>
-                    </li>
-                </ul>
-            </div>
-            <span class="text-muted">&nbsp;|&nbsp;</span>
-            <div component="dropdown" class="dropdown-container">
-                <a refs="dropdown@toggle" href="#" aria-haspopup="true" aria-expanded="false">{{ trans('common.delete') }}</a>
-                <ul refs="dropdown@menu" class="dropdown-menu" role="menu">
-                    <li class="px-m py-s"><small class="text-muted">{{trans('entities.revision_delete_confirm')}}</small></li>
-                    <li>
-                        <form action="{{ $revision->getUrl('/delete/') }}" method="POST">
-                            {!! csrf_field() !!}
-                            <input type="hidden" name="_method" value="DELETE">
-                            <button type="submit" class="text-neg icon-item">
-                                @icon('delete')
-                                <div>{{ trans('common.delete') }}</div>
-                            </button>
-                        </form>
-                    </li>
-                </ul>
-            </div>
+
+            @if(userCan('page-update', $revision->page))
+                <span class="text-muted">&nbsp;|&nbsp;</span>
+                <div component="dropdown" class="dropdown-container">
+                    <a refs="dropdown@toggle" href="#" aria-haspopup="true" aria-expanded="false">{{ trans('entities.pages_revisions_restore') }}</a>
+                    <ul refs="dropdown@menu" class="dropdown-menu" role="menu">
+                        <li class="px-m py-s"><small class="text-muted">{{trans('entities.revision_restore_confirm')}}</small></li>
+                        <li>
+                            <form action="{{ $revision->getUrl('/restore') }}" method="POST">
+                                {!! csrf_field() !!}
+                                <input type="hidden" name="_method" value="PUT">
+                                <button type="submit" class="text-primary icon-item">
+                                    @icon('history')
+                                    <div>{{ trans('entities.pages_revisions_restore') }}</div>
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            @endif
+
+            @if(userCan('page-delete', $revision->page))
+                <span class="text-muted">&nbsp;|&nbsp;</span>
+                <div component="dropdown" class="dropdown-container">
+                    <a refs="dropdown@toggle" href="#" aria-haspopup="true" aria-expanded="false">{{ trans('common.delete') }}</a>
+                    <ul refs="dropdown@menu" class="dropdown-menu" role="menu">
+                        <li class="px-m py-s"><small class="text-muted">{{trans('entities.revision_delete_confirm')}}</small></li>
+                        <li>
+                            <form action="{{ $revision->getUrl('/delete/') }}" method="POST">
+                                {!! csrf_field() !!}
+                                <input type="hidden" name="_method" value="DELETE">
+                                <button type="submit" class="text-neg icon-item">
+                                    @icon('delete')
+                                    <div>{{ trans('common.delete') }}</div>
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            @endif
         @endif
     </td>
 </tr>
