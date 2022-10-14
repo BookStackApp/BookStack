a0cfd486deb01a528ae57570062b4b331c1b4b0c
Dan Brown <email@danb.me>
2022-10-14 17:51:34 +0100

    New translations entities.php (Vietnamese)

--- resources/lang/vi/entities.php
+++ resources/lang/vi/entities.php
@@ -42,10 +42,14 @@ return [
 
     // Permissions and restrictions
     'permissions' => 'Quyền',
-    'permissions_intro' => 'Một khi được bật, các quyền này sẽ được ưu tiên trên hết tất cả các quyền hạn khác.',
-    'permissions_enable' => 'Bật quyền hạn tùy chỉnh',
+    'permissions_desc' => 'Set permissions here to override the default permissions provided by user roles.',
+    'permissions_book_cascade' => 'Permissions set on books will automatically cascade to child chapters and pages, unless they have their own permissions defined.',
+    'permissions_chapter_cascade' => 'Permissions set on chapters will automatically cascade to child pages, unless they have their own permissions defined.',
     'permissions_save' => 'Lưu quyền hạn',
     'permissions_owner' => 'Chủ sở hữu',
+    'permissions_role_everyone_else' => 'Everyone Else',
+    'permissions_role_everyone_else_desc' => 'Set permissions for all roles not specifically overridden.',
+    'permissions_role_override' => 'Override permissions for role',
 
     // Search
     'search_results' => 'Kết quả Tìm kiếm',
