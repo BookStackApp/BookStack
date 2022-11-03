e0043964faaae622b435f0e1291696b35cbf0fd9
Dan Brown <email@danb.me>
2022-11-03 14:59:40 +0000

    New translations settings.php (Bulgarian)

--- resources/lang/bg/settings.php
+++ resources/lang/bg/settings.php
@@ -133,6 +133,11 @@ return [
     // Role Settings
     'roles' => 'Роли',
     'role_user_roles' => 'Потребителски роли',
+    'roles_index_desc' => 'Roles are used to group users & provide system permission to their members. When a user is a member of multiple roles the privileges granted will stack and the user will inherit all abilities.',
+    'roles_x_users_assigned' => '1 user assigned|:count users assigned',
+    'roles_x_permissions_provided' => '1 permission|:count permissions',
+    'roles_assigned_users' => 'Assigned Users',
+    'roles_permissions_provided' => 'Provided Permissions',
     'role_create' => 'Създай нова роля',
     'role_create_success' => 'Ролята беше успешно създадена',
     'role_delete' => 'Изтрий роля',
@@ -172,6 +177,7 @@ return [
 
     // Users
     'users' => 'Потребители',
+    'users_index_desc' => 'Create & manage individual user accounts within the system. User accounts are used for login and attribution of content & activity. Access permissions are primarily role-based but user content ownership, among other factors, may also affect permissions & access.',
     'user_profile' => 'Потребителски профил',
     'users_add_new' => 'Добави нов потребител',
     'users_search' => 'Търси Потребители',
@@ -241,6 +247,8 @@ return [
 
     // Webhooks
     'webhooks' => 'Уебкука',
+    'webhooks_index_desc' => 'Webhooks are a way to send data to external URLs when certain actions and events occur within the system which allows event-based integration with external platforms such as messaging or notification systems.',
+    'webhooks_x_trigger_events' => '1 trigger event|:count trigger events',
     'webhooks_create' => 'Създай нова уебкука',
     'webhooks_none_created' => 'Няма създадени уебкуки.',
     'webhooks_edit' => 'Редактирай уебкука',
