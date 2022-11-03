78f7c2e82f7c6d4b5acc1cb601e8f23cd2e07a67
Dan Brown <email@danb.me>
2022-11-03 15:00:24 +0000

    New translations settings.php (Czech)

--- resources/lang/cs/settings.php
+++ resources/lang/cs/settings.php
@@ -133,6 +133,11 @@ return [
     // Role Settings
     'roles' => 'Role',
     'role_user_roles' => 'Uživatelské role',
+    'roles_index_desc' => 'Roles are used to group users & provide system permission to their members. When a user is a member of multiple roles the privileges granted will stack and the user will inherit all abilities.',
+    'roles_x_users_assigned' => '1 user assigned|:count users assigned',
+    'roles_x_permissions_provided' => '1 permission|:count permissions',
+    'roles_assigned_users' => 'Assigned Users',
+    'roles_permissions_provided' => 'Provided Permissions',
     'role_create' => 'Vytvořit novou roli',
     'role_create_success' => 'Role byla vytvořena',
     'role_delete' => 'Odstranit roli',
@@ -172,6 +177,7 @@ return [
 
     // Users
     'users' => 'Uživatelé',
+    'users_index_desc' => 'Create & manage individual user accounts within the system. User accounts are used for login and attribution of content & activity. Access permissions are primarily role-based but user content ownership, among other factors, may also affect permissions & access.',
     'user_profile' => 'Profil uživatele',
     'users_add_new' => 'Přidat nového uživatele',
     'users_search' => 'Vyhledávání uživatelů',
@@ -241,6 +247,8 @@ return [
 
     // Webhooks
     'webhooks' => 'Webhooky',
+    'webhooks_index_desc' => 'Webhooks are a way to send data to external URLs when certain actions and events occur within the system which allows event-based integration with external platforms such as messaging or notification systems.',
+    'webhooks_x_trigger_events' => '1 trigger event|:count trigger events',
     'webhooks_create' => 'Vytvořit nový webhook',
     'webhooks_none_created' => 'Žádné webhooky nebyly doposud vytvořeny.',
     'webhooks_edit' => 'Upravit webhook',
