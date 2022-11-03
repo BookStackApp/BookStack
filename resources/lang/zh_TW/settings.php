899dbb5310bfb196a72934d9dfabbc0185d77287
Dan Brown <email@danb.me>
2022-11-03 14:59:47 +0000

    New translations settings.php (Chinese Traditional)

--- resources/lang/zh_TW/settings.php
+++ resources/lang/zh_TW/settings.php
@@ -133,6 +133,11 @@ return [
     // Role Settings
     'roles' => '角色',
     'role_user_roles' => '使用者角色',
+    'roles_index_desc' => 'Roles are used to group users & provide system permission to their members. When a user is a member of multiple roles the privileges granted will stack and the user will inherit all abilities.',
+    'roles_x_users_assigned' => '1 user assigned|:count users assigned',
+    'roles_x_permissions_provided' => '1 permission|:count permissions',
+    'roles_assigned_users' => 'Assigned Users',
+    'roles_permissions_provided' => 'Provided Permissions',
     'role_create' => '建立新角色',
     'role_create_success' => '角色建立成功',
     'role_delete' => '刪除角色',
@@ -172,6 +177,7 @@ return [
 
     // Users
     'users' => '使用者',
+    'users_index_desc' => 'Create & manage individual user accounts within the system. User accounts are used for login and attribution of content & activity. Access permissions are primarily role-based but user content ownership, among other factors, may also affect permissions & access.',
     'user_profile' => '使用者個人資料',
     'users_add_new' => '新增使用者',
     'users_search' => '搜尋使用者',
@@ -241,6 +247,8 @@ return [
 
     // Webhooks
     'webhooks' => 'Webhooks',
+    'webhooks_index_desc' => 'Webhooks are a way to send data to external URLs when certain actions and events occur within the system which allows event-based integration with external platforms such as messaging or notification systems.',
+    'webhooks_x_trigger_events' => '1 trigger event|:count trigger events',
     'webhooks_create' => '建立 Webhook',
     'webhooks_none_created' => '沒有已建立的 Webhook',
     'webhooks_edit' => '設置 Webhook',
