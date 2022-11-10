89b7715bab5d26383d24214639dbadd0fc2fd982
Dan Brown <email@danb.me>
2022-11-10 14:32:53 +0000

    New translations preferences.php (Spanish)

--- resources/lang/es/preferences.php
+++ resources/lang/es/preferences.php
@@ -5,14 +5,14 @@
  */
 
 return [
-    'shortcuts' => 'Shortcuts',
-    'shortcuts_interface' => 'Interface Keyboard Shortcuts',
-    'shortcuts_toggle_desc' => 'Here you can enable or disable keyboard system interface shortcuts, used for navigation and actions.',
-    'shortcuts_customize_desc' => 'You can customize each of the shortcuts below. Just press your desired key combination after selecting the input for a shortcut.',
-    'shortcuts_toggle_label' => 'Keyboard shortcuts enabled',
-    'shortcuts_section_navigation' => 'Navigation',
-    'shortcuts_section_actions' => 'Common Actions',
-    'shortcuts_save' => 'Save Shortcuts',
+    'shortcuts' => 'Accesos rápidos',
+    'shortcuts_interface' => 'Atajos del Teclado de la Interfaz',
+    'shortcuts_toggle_desc' => 'Aquí puede activar o desactivar los accesos rápidos de la interfaz, utilizados para la navegación y las acciones.',
+    'shortcuts_customize_desc' => 'Puede personalizar cada uno de los accesos directos a continuación. Simplemente pulse la combinación de teclas deseada después de seleccionar la entrada para un acceso directo.',
+    'shortcuts_toggle_label' => 'Accesos directos habilitados',
+    'shortcuts_section_navigation' => 'Navegación',
+    'shortcuts_section_actions' => 'Acciones comunes',
+    'shortcuts_save' => 'Guardar atajos',
     'shortcuts_overlay_desc' => 'Note: When shortcuts are enabled a helper overlay is available via pressing "?" which will highlight the available shortcuts for actions currently visible on the screen.',
     'shortcuts_update_success' => 'Shortcut preferences have been updated!',
 ];
