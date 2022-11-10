4269b60f262d221b6d2db14ed20c09a9f5d2b482
Dan Brown <email@danb.me>
2022-11-10 13:35:05 +0000

    New translations entities.php (Persian)

--- resources/lang/fa/entities.php
+++ resources/lang/fa/entities.php
@@ -50,6 +50,7 @@ return [
     'permissions_role_everyone_else' => 'سایر کاربران',
     'permissions_role_everyone_else_desc' => 'مجوزها را برای نقش‌هایی تنظیم کنید که به طور خاص لغو نشده‌اند.',
     'permissions_role_override' => 'لغو مجوز برای نقش',
+    'permissions_inherit_defaults' => 'Inherit defaults',
 
     // Search
     'search_results' => 'نتایج جستجو',
