2ec2b67336beff2663915cdaf332a52e27b305fd
Dan Brown <email@danb.me>
2022-11-19 02:30:50 +0000

    New translations preferences.php (Portuguese, Brazilian)

--- resources/lang/pt_BR/preferences.php
+++ resources/lang/pt_BR/preferences.php
@@ -5,14 +5,14 @@
  */
 
 return [
-    'shortcuts' => 'Shortcuts',
-    'shortcuts_interface' => 'Interface Keyboard Shortcuts',
-    'shortcuts_toggle_desc' => 'Here you can enable or disable keyboard system interface shortcuts, used for navigation and actions.',
-    'shortcuts_customize_desc' => 'You can customize each of the shortcuts below. Just press your desired key combination after selecting the input for a shortcut.',
-    'shortcuts_toggle_label' => 'Keyboard shortcuts enabled',
-    'shortcuts_section_navigation' => 'Navigation',
-    'shortcuts_section_actions' => 'Common Actions',
-    'shortcuts_save' => 'Save Shortcuts',
-    'shortcuts_overlay_desc' => 'Note: When shortcuts are enabled a helper overlay is available via pressing "?" which will highlight the available shortcuts for actions currently visible on the screen.',
-    'shortcuts_update_success' => 'Shortcut preferences have been updated!',
+    'shortcuts' => 'Atalhos',
+    'shortcuts_interface' => 'Atalhos de Teclado da Interface',
+    'shortcuts_toggle_desc' => 'Aqui você pode habilitar ou desabilitar os atalhos da interface do sistema de teclado, usados para navegação e ações.',
+    'shortcuts_customize_desc' => 'Você pode personalizar cada um dos atalhos abaixo. Basta pressionar a combinação de teclas desejada após selecionar a entrada para um atalho.',
+    'shortcuts_toggle_label' => 'Atalhos de teclado ativados',
+    'shortcuts_section_navigation' => 'Navegação',
+    'shortcuts_section_actions' => 'Ações Comuns',
+    'shortcuts_save' => 'Salvar Atalhos',
+    'shortcuts_overlay_desc' => 'Observação: quando os atalhos estão ativados, uma sobreposição auxiliar está disponível pressionando "?" que destacará os atalhos disponíveis para ações atualmente visíveis na tela.',
+    'shortcuts_update_success' => 'As preferências de atalho foram atualizadas!',
 ];
