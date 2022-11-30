a6a82f734e4a03b02fc46e9a92373fc428296c47
Dan Brown <email@danb.me>
2022-11-30 13:01:45 +0000

    New translations preferences.php (German)

--- resources/lang/de/preferences.php
+++ resources/lang/de/preferences.php
@@ -9,7 +9,7 @@ return [
     'shortcuts_interface' => 'Interface Keyboard Shortcuts',
     'shortcuts_toggle_desc' => 'Here you can enable or disable keyboard system interface shortcuts, used for navigation and actions.',
     'shortcuts_customize_desc' => 'You can customize each of the shortcuts below. Just press your desired key combination after selecting the input for a shortcut.',
-    'shortcuts_toggle_label' => 'Keyboard shortcuts enabled',
+    'shortcuts_toggle_label' => 'Tastaturkürzel aktiviert',
     'shortcuts_section_navigation' => 'Navigation',
     'shortcuts_section_actions' => 'Gemeinsame Aktionen',
     'shortcuts_save' => 'Tastenkürzel speichern',
