489631244a33ffdf38c00d87f8cef425247a9b6a
Dan Brown <email@danb.me>
2022-12-01 07:08:52 +0000

    New translations preferences.php (Greek)

--- resources/lang/el/preferences.php
+++ resources/lang/el/preferences.php
@@ -11,8 +11,8 @@ return [
     'shortcuts_customize_desc' => 'Μπορείτε να προσαρμόσετε κάθε μία από τις παρακάτω συντομεύσεις. Απλά πατήστε το επιθυμητό συνδυασμό πλήκτρων μετά την επιλογή της εισόδου για μια συντόμευση.',
     'shortcuts_toggle_label' => 'Ενεργοποίηση συντομεύσεων πληκτρολογίου',
     'shortcuts_section_navigation' => 'Πλοήγηση',
-    'shortcuts_section_actions' => 'Common Actions',
+    'shortcuts_section_actions' => 'Κοινές ενέργειες',
     'shortcuts_save' => 'Αποθήκευση Συντομεύσεων',
-    'shortcuts_overlay_desc' => 'Note: When shortcuts are enabled a helper overlay is available via pressing "?" which will highlight the available shortcuts for actions currently visible on the screen.',
+    'shortcuts_overlay_desc' => 'Σημείωση: Όταν οι συντομεύσεις είναι ενεργοποιημένες μια βοηθητική επικάλυψη είναι διαθέσιμη πατώντας "?" που θα τονίσει τις διαθέσιμες συντομεύσεις για ενέργειες που είναι ορατές στην οθόνη.',
     'shortcuts_update_success' => 'Οι προτιμήσεις σας αποθηκεύτηκαν!',
 ];
