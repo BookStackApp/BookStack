1ece4c655348902f4d1004af6a66aa565d3a39cb
Dan Brown <email@danb.me>
2022-12-04 23:45:12 +0000

    New translations preferences.php (German Informal)

--- resources/lang/de_informal/preferences.php
+++ resources/lang/de_informal/preferences.php
@@ -7,12 +7,12 @@
 return [
     'shortcuts' => 'Kürzel',
     'shortcuts_interface' => 'Oberflächen-Tastaturkürzel',
-    'shortcuts_toggle_desc' => 'Hier kannst Du Tastaturkürzel für die Systemoberfläche für Navigation und Aktionen aktivieren oder deaktivieren.',
-    'shortcuts_customize_desc' => 'Unten kannst Du alle Tastenkürzel anpassen. Drücke einfach die gewünschte Tastenkombination, nachdem Du die Eingabe für eine Tastenkombination ausgewählt hast.',
+    'shortcuts_toggle_desc' => 'Hier kannst du Tastaturkürzel für die Systemoberfläche für Navigation und Aktionen aktivieren oder deaktivieren.',
+    'shortcuts_customize_desc' => 'Unten kannst du alle Tastenkürzel anpassen. Drücke einfach die gewünschte Tastenkombination, nachdem du die Eingabe für eine Tastenkombination ausgewählt hast.',
     'shortcuts_toggle_label' => 'Tastaturkürzel aktiviert',
     'shortcuts_section_navigation' => 'Navigation',
     'shortcuts_section_actions' => 'Häufige Aktionen',
     'shortcuts_save' => 'Tastenkürzel speichern',
-    'shortcuts_overlay_desc' => 'Hinweis: Wenn Tastenkürzel aktiviert sind, ist ein Hilfsoverlay durch Drücken von "?" verfügbar, welches die verfügbaren Tastenkürzel für Aktionen hervorhebt, die aktuell auf dem Bildschirm sichtbar sind.',
+    'shortcuts_overlay_desc' => 'Hinweis: Wenn Tastenkürzel aktiviert sind, ist ein Hilfefähnchen durch Drücken von "?" verfügbar, welches die verfügbaren Tastenkürzel für Aktionen hervorhebt, die aktuell auf dem Bildschirm sichtbar sind.',
     'shortcuts_update_success' => 'Tastenkürzel Einstellungen wurden aktualisiert!',
 ];
