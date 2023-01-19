2529063809fd4328ebc0b43c0470c0d3ee1c2576
Pankaj Lagu <pankaj.lagu@shreemeera.org>
2023-01-19 23:31:09 +0530

    Remove vertical and horizontal page scroll for welcome page on desktop layout

--- resources/tailwind-views/welcome/default.blade.php
+++ resources/tailwind-views/welcome/default.blade.php
@@ -1,73 +1,70 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.lang') }}"
-      dir="{{ config('app.rtl') ? 'rtl' : 'ltr' }}"
-      class="{{ setting()->getForCurrentUser('dark-mode-enabled') ? 'dark-mode ' : '' }}">
+<html lang="{{ config('app.lang') }}" dir="{{ config('app.rtl') ? 'rtl' : 'ltr' }}" class="{{ setting()->getForCurrentUser('dark-mode-enabled') ? 'dark-mode ' : '' }}">
+
 <head>
-    <title>{{ isset($pageTitle) ? $pageTitle . ' | ' : '' }}{{ setting('app-name') }}</title>
+		<title>{{ isset($pageTitle) ? $pageTitle . ' | ' : '' }}{{ setting('app-name') }}</title>
 
-    <!-- Meta -->
-    <meta name="viewport" content="width=device-width">
-    <meta name="token" content="{{ csrf_token() }}">
-    <meta name="base-url" content="{{ url('/') }}">
-    <meta charset="utf-8">
+		<!-- Meta -->
+		<meta name="viewport" content="width=device-width">
+		<meta name="token" content="{{ csrf_token() }}">
+		<meta name="base-url" content="{{ url('/') }}">
+		<meta charset="utf-8">
 
-    <!-- Social Cards Meta -->
-    <meta property="og:title" content="{{ isset($pageTitle) ? $pageTitle . ' | ' : '' }}{{ setting('app-name') }}">
-    <meta property="og:url" content="{{ url()->current() }}">
-    @stack('social-meta')
+		<!-- Social Cards Meta -->
+		<meta property="og:title" content="{{ isset($pageTitle) ? $pageTitle . ' | ' : '' }}{{ setting('app-name') }}">
+		<meta property="og:url" content="{{ url()->current() }}">
+		@stack('social-meta')
 
-    <!-- Styles and Fonts -->
-	<link rel="stylesheet" href="{{ versioned_asset('dist/styles.css') }}">
-	<link rel="stylesheet" href="{{ versioned_asset('dist/tailwind-compiled.css') }}"> 
-    {{-- <link rel="stylesheet" media="print" href="{{ versioned_asset('dist/print-styles.css') }}"> --}}
+		<!-- Styles and Fonts -->
+		<link rel="stylesheet" href="{{ versioned_asset('dist/styles.css') }}">
+		<link rel="stylesheet" href="{{ versioned_asset('dist/tailwind-compiled.css') }}">
+		{{-- <link rel="stylesheet" media="print" href="{{ versioned_asset('dist/print-styles.css') }}"> --}}
 
 
-    @yield('head')
+		@yield('head')
 
-    <!-- Custom Styles & Head Content -->
-    {{-- @include('common.custom-styles')
+		<!-- Custom Styles & Head Content -->
+		{{-- @include('common.custom-styles')
     @include('common.custom-head') --}}
 
-    @stack('head')
+		@stack('head')
 
-    <!-- Translations for JS -->
-    @stack('translations')
+		<!-- Translations for JS -->
+		@stack('translations')
 </head>
-<body
-    @if(setting()->getForCurrentUser('ui-shortcuts-enabled', false))
-        component="shortcuts"
-        option:shortcuts:key-map="{{ \BookStack\Settings\UserShortcutMap::fromUserPreferences()->toJson() }}"
-    @endif
-      class="@stack('body-class') smf-bg-white smf-h-screen smf-w-screen">
 
-    @include('layouts.parts.base-body-start')
-    {{-- @include('common.skip-to-content') --}}
-    {{-- @include('common.notifications') --}}
-    {{-- @include('welcome.parts.header') --}}
-	<header>
-		@include('welcome.parts.navbar')
-	</header>
-	
-    
-	{{-- <div id="content" components="@yield('content-components')" class="block  smf-container"> --}}
+<body @if (setting()->getForCurrentUser('ui-shortcuts-enabled', false)) component="shortcuts"
+        option:shortcuts:key-map="{{ \BookStack\Settings\UserShortcutMap::fromUserPreferences()->toJson() }}" @endif class="@stack('body-class') smf-bg-white">
+
+		@include('layouts.parts.base-body-start')
+		{{-- @include('common.skip-to-content') --}}
+		{{-- @include('common.notifications') --}}
+		{{-- @include('welcome.parts.header') --}}
+		<header>
+				@include('welcome.parts.navbar')
+		</header>
+
+
+		{{-- <div id="content" components="@yield('content-components')" class="block  smf-container"> --}}
 		@include('welcome.parts.body')
 		@include('welcome.parts.features')
-    {{-- </div> --}}
+		{{-- </div> --}}
 
-	@include('common.footer')
+		@include('common.footer')
 
-    <div component="back-to-top" class="back-to-top print-hidden">
-        <div class="inner">
-            @icon('chevron-up') <span>{{ trans('common.back_to_top') }}</span>
-        </div>
-    </div>
+		<div component="back-to-top" class="back-to-top print-hidden">
+				<div class="inner">
+						@icon('chevron-up') <span>{{ trans('common.back_to_top') }}</span>
+				</div>
+		</div>
 
 
-    @yield('bottom')
-    <script src="{{ versioned_asset('dist/app.js') }}" nonce="{{ $cspNonce }}"></script>
+		@yield('bottom')
+		<script src="{{ versioned_asset('dist/app.js') }}" nonce="{{ $cspNonce }}"></script>
 		<script src="{{ versioned_asset('dist/flowbite.min.js') }}" nonce="{{ $cspNonce }}"></script>
-    @yield('scripts')
+		@yield('scripts')
 
-    @include('layouts.parts.base-body-end')
+		@include('layouts.parts.base-body-end')
 </body>
+
 </html>
