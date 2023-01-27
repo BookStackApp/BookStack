40e112fc5b4896c52d5aa09ea2ad2a9da6eccfa1
Dan Brown <ssddanbrown@googlemail.com>
2023-01-27 13:26:58 +0000

    Extracted text & added dropdown for book sort move actions
    Primarily styling and testing left to do.

--- resources/views/books/parts/sort-box-actions.blade.php
+++ resources/views/books/parts/sort-box-actions.blade.php
@@ -1,12 +1,23 @@
 <div class="sort-box-actions">
-    <button type="button" data-move="up">Move Up</button>
-    <button type="button" data-move="down">Move Down</button>
-    <button type="button" data-move="prev_book">Move To Previous Book</button>
-    <button type="button" data-move="next_book">Move To Next Book</button>
-    <button type="button" data-move="prev_chapter">Move Into Previous Chapter</button>
-    <button type="button" data-move="next_chapter">Move Into Next Chapter</button>
-    <button type="button" data-move="book_start">Move To Start of Book</button>
-    <button type="button" data-move="book_end">Move To End of Book</button>
-    <button type="button" data-move="before_chapter">Move To Before Chapter</button>
-    <button type="button" data-move="after_chapter">Move To After Chapter</button>
+    <button type="button" data-move="up" title="{{ trans('entities.books_sort_move_up') }}">@icon('chevron-up')</button>
+    <button type="button" data-move="down"
+            title="{{ trans('entities.books_sort_move_down') }}">@icon('chevron-down')</button>
+    <div class="dropdown-container" component="dropdown">
+        <button refs="dropdown@toggle"
+                title="{{ trans('common.more') }}"
+                aria-haspopup="true"
+                aria-expanded="false">
+            @icon('more')
+        </button>
+        <div refs="dropdown@menu" class="dropdown-menu" role="menu">
+            <button type="button" class="text-item" data-move="prev_book">{{ trans('entities.books_sort_move_prev_book') }}</button>
+            <button type="button" class="text-item" data-move="next_book">{{ trans('entities.books_sort_move_next_book') }}</button>
+            <button type="button" class="text-item" data-move="prev_chapter">{{ trans('entities.books_sort_move_prev_chapter') }}</button>
+            <button type="button" class="text-item" data-move="next_chapter">{{ trans('entities.books_sort_move_next_chapter') }}</button>
+            <button type="button" class="text-item" data-move="book_start">{{ trans('entities.books_sort_move_book_start') }}</button>
+            <button type="button" class="text-item" data-move="book_end">{{ trans('entities.books_sort_move_book_end') }}</button>
+            <button type="button" class="text-item" data-move="before_chapter">{{ trans('entities.books_sort_move_before_chapter') }}</button>
+            <button type="button" class="text-item" data-move="after_chapter">{{ trans('entities.books_sort_move_after_chapter') }}</button>
+        </div>
+    </div>
 </div>
