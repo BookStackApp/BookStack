73afe0c16bb6e63c2b931f7ae319d19814910b22
Dan Brown <email@danb.me>
2023-02-19 16:17:44 +0000

    New translations activities.php (Italian)

--- lang/it/activities.php
+++ lang/it/activities.php
@@ -67,6 +67,11 @@ return [
     'user_update_notification' => 'Utente aggiornato con successo',
     'user_delete_notification' => 'Utente rimosso con successo',
 
+    // Roles
+    'role_create_notification' => 'Role successfully created',
+    'role_update_notification' => 'Role successfully updated',
+    'role_delete_notification' => 'Role successfully deleted',
+
     // Other
     'commented_on'                => 'ha commentato in',
     'permissions_update'          => 'autorizzazioni aggiornate',
