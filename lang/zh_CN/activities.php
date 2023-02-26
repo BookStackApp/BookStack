36632e8749200ad836b5785db5a3be04e1fb7ab6
Dan Brown <email@danb.me>
2023-02-26 14:05:19 +0000

    New translations activities.php (Chinese Simplified)

--- lang/zh_CN/activities.php
+++ lang/zh_CN/activities.php
@@ -68,9 +68,9 @@ return [
     'user_delete_notification' => '成功移除用户',
 
     // Roles
-    'role_create_notification' => 'Role successfully created',
-    'role_update_notification' => 'Role successfully updated',
-    'role_delete_notification' => 'Role successfully deleted',
+    'role_create_notification' => '角色创建成功',
+    'role_update_notification' => '角色更新成功',
+    'role_delete_notification' => '角色删除成功',
 
     // Other
     'commented_on'                => '评论',
