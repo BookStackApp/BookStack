a22e7cbc79da79e0d86c8b08ce0fcfd63fa7ce6f
Dan Brown <ssddanbrown@googlemail.com>
2023-04-18 15:08:17 +0100

    CM6: Further fixes/improvements after testing
    - Updated event naming to be "cm6" when codemirror-specific.
    - Removed cm block border in md editor to prevent double bordering.
    - Updated copy handling to fallback to execCommand.

--- resources/js/services/clipboard.js
+++ resources/js/services/clipboard.js
@@ -51,8 +51,20 @@ export class Clipboard {
     }
 }
 
-export function copyTextToClipboard(text) {
-    return navigator.clipboard.writeText(text);
+export async function copyTextToClipboard(text) {
+    if (window.isSecureContext && navigator.clipboard) {
+        await navigator.clipboard.writeText(text);
+        return;
+    }
+
+    // Backup option where we can't use the navigator.clipboard API
+    const tempInput = document.createElement("textarea");
+    tempInput.style = "position: absolute; left: -1000px; top: -1000px;";
+    tempInput.value = text;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
 }
 
 export default Clipboard;
