c09e74fa7ce26415fc15ee0ff0bb04a6e7e8723b
jasonF1000 <116643854+jasonF1000@users.noreply.github.com>
2023-04-24 14:38:03 +0200

    changed app/Entities/Tools/PageContent.php to accept nested include levels. Tested it and it works.

--- app/Entities/Tools/PageContent.php
+++ app/Entities/Tools/PageContent.php
@@ -301,11 +301,13 @@ class PageContent
             $content = HtmlContentFilter::removeScripts($content);
         }
 
-        if ($blankIncludes) {
-            $content = $this->blankPageIncludes($content);
-        } else {
-            $content = $this->parsePageIncludes($content);
-        }
+        for ($includeDepth = 0; $includeDepth <= 3; $includeDepth++){
+            if ($blankIncludes) {                                
+                $content = $this->blankPageIncludes($content);
+            } else {                                              
+                $content = $this->parsePageIncludes($content);
+            }                                                     
+        } 
 
         return $content;
     }
