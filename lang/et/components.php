c7738f2456de6ea512cac73843e4245ecf6011cf
Dan Brown <email@danb.me>
2023-04-27 13:56:38 +0100

    New translations components.php (Estonian)

--- lang/et/components.php
+++ lang/et/components.php
@@ -6,6 +6,9 @@ return [
 
     // Image Manager
     'image_select' => 'Pildifaili valik',
+    'image_upload' => 'Upload Image',
+    'image_intro' => 'Here you can select and manage images that have been previously uploaded to the system.',
+    'image_intro_upload' => 'Upload a new image by dragging an image file into this window, or by using the "Upload Image" button above.',
     'image_all' => 'Kõik',
     'image_all_title' => 'Vaata kõiki pildifaile',
     'image_book_title' => 'Vaata sellesse raamatusse laaditud pildifaile',
@@ -18,12 +21,12 @@ return [
     'image_delete_confirm_text' => 'Kas oled kindel, et soovid selle pildifaili kustutada?',
     'image_select_image' => 'Vali pildifail',
     'image_dropzone' => 'Üleslaadimiseks lohista pildid või klõpsa siin',
+    'image_dropzone_drop' => 'Drop images here to upload',
     'images_deleted' => 'Pildifailid kustutatud',
     'image_preview' => 'Pildi eelvaade',
     'image_upload_success' => 'Pildifail üles laaditud',
     'image_update_success' => 'Pildifaili andmed muudetud',
     'image_delete_success' => 'Pildifail kustutatud',
-    'image_upload_remove' => 'Eemalda',
 
     // Code Editor
     'code_editor' => 'Muuda koodi',
