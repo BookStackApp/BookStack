7e462ce885bda0fa709c2e870c47d23d029e7b28
Dan Brown <email@danb.me>
2023-04-27 13:56:01 +0100

    New translations components.php (Russian)

--- lang/ru/components.php
+++ lang/ru/components.php
@@ -6,6 +6,9 @@ return [
 
     // Image Manager
     'image_select' => 'Выбрать изображение',
+    'image_upload' => 'Upload Image',
+    'image_intro' => 'Here you can select and manage images that have been previously uploaded to the system.',
+    'image_intro_upload' => 'Upload a new image by dragging an image file into this window, or by using the "Upload Image" button above.',
     'image_all' => 'Все',
     'image_all_title' => 'Просмотр всех изображений',
     'image_book_title' => 'Просмотр всех изображений, загруженных в эту книгу',
@@ -18,12 +21,12 @@ return [
     'image_delete_confirm_text' => 'Вы уверены, что хотите удалить это изображение?',
     'image_select_image' => 'Выбрать изображение',
     'image_dropzone' => 'Перетащите изображение или кликните для загрузки',
+    'image_dropzone_drop' => 'Drop images here to upload',
     'images_deleted' => 'Изображения удалены',
     'image_preview' => 'Предпросмотр изображения',
     'image_upload_success' => 'Изображение успешно загружено',
     'image_update_success' => 'Детали изображения успешно обновлены',
     'image_delete_success' => 'Изображение успешно удалено',
-    'image_upload_remove' => 'Удалить изображение',
 
     // Code Editor
     'code_editor' => 'Изменить код',
