230ce0fdcc89ddd1bacdde1cb2efd68ce1fe5d61
Dan Brown <email@danb.me>
2023-04-27 13:55:29 +0100

    New translations components.php (Basque)

--- lang/eu/components.php
+++ lang/eu/components.php
@@ -6,6 +6,9 @@ return [
 
     // Image Manager
     'image_select' => 'Irudia aukeratu',
+    'image_upload' => 'Upload Image',
+    'image_intro' => 'Here you can select and manage images that have been previously uploaded to the system.',
+    'image_intro_upload' => 'Upload a new image by dragging an image file into this window, or by using the "Upload Image" button above.',
     'image_all' => 'Guztiak',
     'image_all_title' => 'Irudi guztiak ikusi',
     'image_book_title' => 'Liburu honetan igotako irudiak ikusi',
@@ -18,12 +21,12 @@ return [
     'image_delete_confirm_text' => 'Ziur irudi hau ezabatu nahi duzula?',
     'image_select_image' => 'Irudia aukeratu',
     'image_dropzone' => 'Irudiak bota edo klikatu hemen igotzeko',
+    'image_dropzone_drop' => 'Drop images here to upload',
     'images_deleted' => 'Ezabatutako irudiak',
     'image_preview' => 'Irudiaren aurrebista',
     'image_upload_success' => 'Irudia zuzen eguneratu da',
     'image_update_success' => 'Irudiaren xehetasunak egioki eguneratu dira',
     'image_delete_success' => 'Irudia ondo ezabatu da',
-    'image_upload_remove' => 'Ezabatu',
 
     // Code Editor
     'code_editor' => 'Kodea editatu',
