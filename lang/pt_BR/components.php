82d70a1291d5bbb22a5f4e727ebe8454a790704c
Dan Brown <email@danb.me>
2023-04-27 13:56:25 +0100

    New translations components.php (Portuguese, Brazilian)

--- lang/pt_BR/components.php
+++ lang/pt_BR/components.php
@@ -6,6 +6,9 @@ return [
 
     // Image Manager
     'image_select' => 'Selecionar Imagem',
+    'image_upload' => 'Upload Image',
+    'image_intro' => 'Here you can select and manage images that have been previously uploaded to the system.',
+    'image_intro_upload' => 'Upload a new image by dragging an image file into this window, or by using the "Upload Image" button above.',
     'image_all' => 'Todas',
     'image_all_title' => 'Visualizar todas as imagens',
     'image_book_title' => 'Visualizar imagens relacionadas a esse livro',
@@ -18,12 +21,12 @@ return [
     'image_delete_confirm_text' => 'Tem certeza de que deseja excluir essa imagem?',
     'image_select_image' => 'Selecionar Imagem',
     'image_dropzone' => 'Arraste imagens ou clique aqui para fazer upload',
+    'image_dropzone_drop' => 'Drop images here to upload',
     'images_deleted' => 'Imagens Excluídas',
     'image_preview' => 'Pré-Visualização de Imagem',
     'image_upload_success' => 'Upload de imagem efetuado com sucesso',
     'image_update_success' => 'Detalhes da imagem atualizados com sucesso',
     'image_delete_success' => 'Imagem excluída com sucesso',
-    'image_upload_remove' => 'Remover',
 
     // Code Editor
     'code_editor' => 'Editar Código',
