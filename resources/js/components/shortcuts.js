3ec5cda424befc6733ef14036a5a79dd19d38db8
Dan Brown <ssddanbrown@googlemail.com>
2023-05-08 14:28:03 +0100

    Prevented shorcuts activating when in codemirror areas
    For #4227

--- resources/js/components/shortcuts.js
+++ resources/js/components/shortcuts.js
@@ -25,7 +25,7 @@ export class Shortcuts extends Component {
 
     setupListeners() {
         window.addEventListener('keydown', event => {
-            if (event.target.closest('input, select, textarea')) {
+            if (event.target.closest('input, select, textarea, .cm-editor')) {
                 return;
             }
 
