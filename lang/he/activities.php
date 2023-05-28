ca48bc994d8f78913ac59c7a808f570a3978da6f
Dan Brown <email@danb.me>
2023-05-28 18:34:03 +0100

    New translations activities.php (Hebrew)

--- lang/he/activities.php
+++ lang/he/activities.php
@@ -28,24 +28,24 @@ return [
     // Books
     'book_create'                 => 'ספר נוצר',
     'book_create_notification'    => 'ספר נוצר בהצלחה',
-    'book_create_from_chapter'              => 'converted chapter to book',
-    'book_create_from_chapter_notification' => 'Chapter successfully converted to a book',
+    'book_create_from_chapter'              => 'המר פרק לספר',
+    'book_create_from_chapter_notification' => 'הפרק הומר בהצלחה לספר',
     'book_update'                 => 'ספר הועדכן',
     'book_update_notification'    => 'ספר התעדכן בהצלחה',
     'book_delete'                 => 'ספר נמחק',
     'book_delete_notification'    => 'ספר נמחק בהצלחה',
-    'book_sort'                   => 'sorted book',
-    'book_sort_notification'      => 'Book successfully re-sorted',
+    'book_sort'                   => 'ספר ממויין',
+    'book_sort_notification'      => 'ספר מויין מחדש בהצלחה',
 
     // Bookshelves
-    'bookshelf_create'            => 'created shelf',
-    'bookshelf_create_notification'    => 'Shelf successfully created',
-    'bookshelf_create_from_book'    => 'converted book to shelf',
+    'bookshelf_create'            => 'מדף נוצר',
+    'bookshelf_create_notification'    => 'המדף נוצר בהצלחה',
+    'bookshelf_create_from_book'    => 'המר ספר למדף',
     'bookshelf_create_from_book_notification'    => 'הספר הוסב בהצלחה למדף',
-    'bookshelf_update'                 => 'updated shelf',
-    'bookshelf_update_notification'    => 'Shelf successfully updated',
-    'bookshelf_delete'                 => 'deleted shelf',
-    'bookshelf_delete_notification'    => 'Shelf successfully deleted',
+    'bookshelf_update'                 => 'מדף עודכן',
+    'bookshelf_update_notification'    => 'מדף עודכן בהצלחה',
+    'bookshelf_delete'                 => 'מדף שנמחק',
+    'bookshelf_delete_notification'    => 'מדף נמחק בהצלחה',
 
     // Favourites
     'favourite_add_notification' => '":name" has been added to your favourites',
@@ -57,22 +57,22 @@ return [
 
     // Webhooks
     'webhook_create' => 'created webhook',
-    'webhook_create_notification' => 'Webhook successfully created',
+    'webhook_create_notification' => 'Webhook נוצר בהצלחה',
     'webhook_update' => 'updated webhook',
     'webhook_update_notification' => 'Webhook successfully updated',
     'webhook_delete' => 'deleted webhook',
     'webhook_delete_notification' => 'Webhook successfully deleted',
 
     // Users
-    'user_update_notification' => 'User successfully updated',
-    'user_delete_notification' => 'User successfully removed',
+    'user_update_notification' => 'משתמש עודכן בהצלחה',
+    'user_delete_notification' => 'משתמש הוסר בהצלחה',
 
     // Roles
-    'role_create_notification' => 'Role successfully created',
-    'role_update_notification' => 'Role successfully updated',
-    'role_delete_notification' => 'Role successfully deleted',
+    'role_create_notification' => 'תפקיד נוצר בהצלחה',
+    'role_update_notification' => 'תפקיד עודכן בהצלחה',
+    'role_delete_notification' => 'תפקיד נמחק בהצלחה',
 
     // Other
-    'commented_on'                => 'commented on',
-    'permissions_update'          => 'updated permissions',
+    'commented_on'                => 'הגיב/ה על',
+    'permissions_update'          => 'הרשאות עודכנו',
 ];
