dd08eefa0187270b32268b566c262be472331e20
Dan Brown <email@danb.me>
2023-05-29 17:20:51 +0100

    New translations errors.php (Romanian)

--- lang/ro/errors.php
+++ lang/ro/errors.php
@@ -49,6 +49,7 @@ return [
     // Drawing & Images
     'image_upload_error' => 'A apărut o eroare la încărcarea imaginii',
     'image_upload_type_error' => 'Tipul de imagine încărcat nu este valid',
+    'image_upload_replace_type' => 'Image file replacements must be of the same type',
     'drawing_data_not_found' => 'Drawing data could not be loaded. The drawing file might no longer exist or you may not have permission to access it.',
 
     // Attachments
