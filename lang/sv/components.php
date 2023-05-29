e58d65255271ffefd94ee5eda29115afb16db0d2
Dan Brown <email@danb.me>
2023-05-29 17:20:41 +0100

    New translations components.php (Swedish)

--- lang/sv/components.php
+++ lang/sv/components.php
@@ -6,6 +6,8 @@ return [
 
     // Image Manager
     'image_select' => 'Val av bild',
+    'image_list' => 'Image List',
+    'image_details' => 'Image Details',
     'image_upload' => 'Upload Image',
     'image_intro' => 'Here you can select and manage images that have been previously uploaded to the system.',
     'image_intro_upload' => 'Upload a new image by dragging an image file into this window, or by using the "Upload Image" button above.',
@@ -15,6 +17,9 @@ return [
     'image_page_title' => 'Visa bilder som laddats upp till den aktuella sidan',
     'image_search_hint' => 'Sök efter bildens namn',
     'image_uploaded' => 'Laddades upp :uploadedDate',
+    'image_uploaded_by' => 'Uploaded by :userName',
+    'image_uploaded_to' => 'Uploaded to :pageLink',
+    'image_updated' => 'Updated :updateDate',
     'image_load_more' => 'Ladda fler',
     'image_image_name' => 'Bildnamn',
     'image_delete_used' => 'Den här bilden används på nedanstående sidor.',
@@ -27,6 +32,8 @@ return [
     'image_upload_success' => 'Bilden har laddats upp',
     'image_update_success' => 'Bildens uppgifter har ändrats',
     'image_delete_success' => 'Bilden har tagits bort',
+    'image_replace' => 'Replace Image',
+    'image_replace_success' => 'Image file successfully updated',
 
     // Code Editor
     'code_editor' => 'Redigera kod',
