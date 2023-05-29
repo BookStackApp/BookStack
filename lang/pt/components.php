a9833aa817e11e40bc44f29d3c455415ecc6f5c9
Dan Brown <email@danb.me>
2023-05-29 17:20:37 +0100

    New translations components.php (Portuguese)

--- lang/pt/components.php
+++ lang/pt/components.php
@@ -6,6 +6,8 @@ return [
 
     // Image Manager
     'image_select' => 'Selecionar Imagem',
+    'image_list' => 'Image List',
+    'image_details' => 'Image Details',
     'image_upload' => 'Upload Image',
     'image_intro' => 'Here you can select and manage images that have been previously uploaded to the system.',
     'image_intro_upload' => 'Upload a new image by dragging an image file into this window, or by using the "Upload Image" button above.',
@@ -15,6 +17,9 @@ return [
     'image_page_title' => 'Visualizar imagens relacionadas a esta página',
     'image_search_hint' => 'Pesquisar imagem por nome',
     'image_uploaded' => 'Adicionada em :uploadedDate',
+    'image_uploaded_by' => 'Uploaded by :userName',
+    'image_uploaded_to' => 'Uploaded to :pageLink',
+    'image_updated' => 'Updated :updateDate',
     'image_load_more' => 'Carregar Mais',
     'image_image_name' => 'Nome da Imagem',
     'image_delete_used' => 'Esta imagem é utilizada nas páginas abaixo.',
@@ -27,6 +32,8 @@ return [
     'image_upload_success' => 'Carregamento da imagem efetuado com sucesso',
     'image_update_success' => 'Detalhes da imagem atualizados com sucesso',
     'image_delete_success' => 'Imagem eliminada com sucesso',
+    'image_replace' => 'Replace Image',
+    'image_replace_success' => 'Image file successfully updated',
 
     // Code Editor
     'code_editor' => 'Editar Código',
