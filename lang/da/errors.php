1527e4f6fd86af5134dc10ae85dd8e7d75ada87f
Dan Brown <email@danb.me>
2023-05-29 17:20:58 +0100

    New translations errors.php (Danish)

--- lang/da/errors.php
+++ lang/da/errors.php
@@ -49,6 +49,7 @@ return [
     // Drawing & Images
     'image_upload_error' => 'Der opstod en fejl ved upload af billedet',
     'image_upload_type_error' => 'Billedtypen, der uploades, er ugyldig',
+    'image_upload_replace_type' => 'Image file replacements must be of the same type',
     'drawing_data_not_found' => 'Drawing data could not be loaded. The drawing file might no longer exist or you may not have permission to access it.',
 
     // Attachments
