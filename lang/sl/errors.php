82e69a0024ae461b9305afe636bd82b7302bf3b7
Dan Brown <email@danb.me>
2023-05-29 17:21:18 +0100

    New translations errors.php (Slovenian)

--- lang/sl/errors.php
+++ lang/sl/errors.php
@@ -49,6 +49,7 @@ return [
     // Drawing & Images
     'image_upload_error' => 'Prišlo je do napake med nalaganjem slike',
     'image_upload_type_error' => 'Napačen tip (format) slike',
+    'image_upload_replace_type' => 'Image file replacements must be of the same type',
     'drawing_data_not_found' => 'Drawing data could not be loaded. The drawing file might no longer exist or you may not have permission to access it.',
 
     // Attachments
