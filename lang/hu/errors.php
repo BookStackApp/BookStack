9a30369923e8418aa976ca8aa9bb177660dc98ef
Dan Brown <email@danb.me>
2023-05-29 17:21:02 +0100

    New translations errors.php (Hungarian)

--- lang/hu/errors.php
+++ lang/hu/errors.php
@@ -49,6 +49,7 @@ return [
     // Drawing & Images
     'image_upload_error' => 'Hiba történt a kép feltöltése közben',
     'image_upload_type_error' => 'A feltöltött kép típusa érvénytelen',
+    'image_upload_replace_type' => 'Image file replacements must be of the same type',
     'drawing_data_not_found' => 'Drawing data could not be loaded. The drawing file might no longer exist or you may not have permission to access it.',
 
     // Attachments
