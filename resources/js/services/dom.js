88785aa71b1da3c33928c3bcd67e77b2572c5208
Dan Brown <ssddanbrown@googlemail.com>
2023-05-31 16:38:20 +0100

    Page display pointer: Considerably improved accessibility
    - Updated pointer to move within content DOM so that you can back-focus
      into the pointer if desired.
    - Added new "Section select mode" which toggles focusabiltiy for main
      content sections, with ability to show pointer via enter press on
      these.
    - Updated pointer with proper input/button labelling.
    
    Tested via orca screen reader on Firefox/Fedora/Gnome.
    For #3975

--- resources/js/services/dom.js
+++ resources/js/services/dom.js
@@ -75,22 +75,41 @@ export function onSelect(elements, callback) {
 }
 
 /**
- * Listen to enter press on the given element(s).
+ * Listen to key press on the given element(s).
+ * @param {String} key
  * @param {HTMLElement|Array} elements
  * @param {function} callback
  */
-export function onEnterPress(elements, callback) {
+function onKeyPress(key, elements, callback) {
     if (!Array.isArray(elements)) {
         elements = [elements];
     }
 
     const listener = event => {
-        if (event.key === 'Enter') {
+        if (event.key === key) {
             callback(event);
         }
     };
 
-    elements.forEach(e => e.addEventListener('keypress', listener));
+    elements.forEach(e => e.addEventListener('keydown', listener));
+}
+
+/**
+ * Listen to enter press on the given element(s).
+ * @param {HTMLElement|Array} elements
+ * @param {function} callback
+ */
+export function onEnterPress(elements, callback) {
+    onKeyPress('Enter', elements, callback);
+}
+
+/**
+ * Listen to escape press on the given element(s).
+ * @param {HTMLElement|Array} elements
+ * @param {function} callback
+ */
+export function onEscapePress(elements, callback) {
+    onKeyPress('Escape', elements, callback);
 }
 
 /**
