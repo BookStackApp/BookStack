39f03c5ffe6d22395c4420e6ef130973526f25f1
Dan Brown <email@danb.me>
2023-06-12 16:51:26 +0100

    New translations activities.php (Romanian)

--- lang/ro/activities.php
+++ lang/ro/activities.php
@@ -15,6 +15,7 @@ return [
     'page_restore'                => 'a restabilit pagina',
     'page_restore_notification'   => 'Pagina a fost restaurată cu succes',
     'page_move'                   => 'a mutat pagina',
+    'page_move_notification'      => 'Page successfully moved',
 
     // Chapters
     'chapter_create'              => 'a creat capitolul',
@@ -24,6 +25,7 @@ return [
     'chapter_delete'              => 'a șters capitolul',
     'chapter_delete_notification' => 'Capitolul a fost șters cu succes',
     'chapter_move'                => 'a mutat capitolul',
+    'chapter_move_notification' => 'Chapter successfully moved',
 
     // Books
     'book_create'                 => 'a creat cartea',
@@ -47,13 +49,29 @@ return [
     'bookshelf_delete'                 => 'raft șters',
     'bookshelf_delete_notification'    => 'Raftul a fost șters cu succes',
 
+    // Revisions
+    'revision_restore' => 'restored revision',
+    'revision_delete' => 'deleted revision',
+    'revision_delete_notification' => 'Revision successfully deleted',
+
     // Favourites
     'favourite_add_notification' => '":name" a fost adăugat la favorite',
     'favourite_remove_notification' => '":name" a fost eliminat din favorite',
 
-    // MFA
+    // Auth
+    'auth_login' => 'logged in',
+    'auth_register' => 'registered as new user',
+    'auth_password_reset_request' => 'requested user password reset',
+    'auth_password_reset_update' => 'reset user password',
+    'mfa_setup_method' => 'configured MFA method',
     'mfa_setup_method_notification' => 'Metoda multi-factor a fost configurată cu succes',
+    'mfa_remove_method' => 'removed MFA method',
     'mfa_remove_method_notification' => 'Metoda multi-factor a fost configurată cu succes',
+
+    // Settings
+    'settings_update' => 'updated settings',
+    'settings_update_notification' => 'Settings successfully updated',
+    'maintenance_action_run' => 'ran maintenance action',
 
     // Webhooks
     'webhook_create' => 'a creat webhook',
@@ -64,13 +82,33 @@ return [
     'webhook_delete_notification' => 'Webhook șters cu succes',
 
     // Users
+    'user_create' => 'created user',
+    'user_create_notification' => 'User successfully created',
+    'user_update' => 'updated user',
     'user_update_notification' => 'Utilizator actualizat cu succes',
+    'user_delete' => 'deleted user',
     'user_delete_notification' => 'Utilizator eliminat cu succes',
 
+    // API Tokens
+    'api_token_create' => 'created api token',
+    'api_token_create_notification' => 'API token successfully created',
+    'api_token_update' => 'updated api token',
+    'api_token_update_notification' => 'API token successfully updated',
+    'api_token_delete' => 'deleted api token',
+    'api_token_delete_notification' => 'API token successfully deleted',
+
     // Roles
+    'role_create' => 'created role',
     'role_create_notification' => 'Role successfully created',
+    'role_update' => 'updated role',
     'role_update_notification' => 'Role successfully updated',
+    'role_delete' => 'deleted role',
     'role_delete_notification' => 'Role successfully deleted',
+
+    // Recycle Bin
+    'recycle_bin_empty' => 'emptied recycle bin',
+    'recycle_bin_restore' => 'restored from recycle bin',
+    'recycle_bin_destroy' => 'removed from recycle bin',
 
     // Other
     'commented_on'                => 'a comentat la',
