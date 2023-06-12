cbf900ac8a6cb906292929eee047f91df177392f
Dan Brown <email@danb.me>
2023-06-12 16:49:34 +0100

    New translations activities.php (German)

--- lang/de/activities.php
+++ lang/de/activities.php
@@ -15,6 +15,7 @@ return [
     'page_restore'                => 'hat die Seite wiederhergestellt',
     'page_restore_notification'   => 'Seite erfolgreich wiederhergestellt',
     'page_move'                   => 'Seite verschoben',
+    'page_move_notification'      => 'Page successfully moved',
 
     // Chapters
     'chapter_create'              => 'erstellte Kapitel',
@@ -24,6 +25,7 @@ return [
     'chapter_delete'              => 'löschte Kapitel',
     'chapter_delete_notification' => 'Kapitel erfolgreich gelöscht',
     'chapter_move'                => 'verschob Kapitel',
+    'chapter_move_notification' => 'Chapter successfully moved',
 
     // Books
     'book_create'                 => 'erstellte Buch',
@@ -47,13 +49,29 @@ return [
     'bookshelf_delete'                 => 'löschte Regal',
     'bookshelf_delete_notification'    => 'Regal erfolgreich gelöscht',
 
+    // Revisions
+    'revision_restore' => 'restored revision',
+    'revision_delete' => 'deleted revision',
+    'revision_delete_notification' => 'Revision successfully deleted',
+
     // Favourites
     'favourite_add_notification' => '":name" wurde zu Ihren Favoriten hinzugefügt',
     'favourite_remove_notification' => '":name" wurde aus Ihren Favoriten entfernt',
 
-    // MFA
+    // Auth
+    'auth_login' => 'logged in',
+    'auth_register' => 'registered as new user',
+    'auth_password_reset_request' => 'requested user password reset',
+    'auth_password_reset_update' => 'reset user password',
+    'mfa_setup_method' => 'configured MFA method',
     'mfa_setup_method_notification' => 'Multi-Faktor-Methode erfolgreich konfiguriert',
+    'mfa_remove_method' => 'removed MFA method',
     'mfa_remove_method_notification' => 'Multi-Faktor-Methode erfolgreich entfernt',
+
+    // Settings
+    'settings_update' => 'updated settings',
+    'settings_update_notification' => 'Settings successfully updated',
+    'maintenance_action_run' => 'ran maintenance action',
 
     // Webhooks
     'webhook_create' => 'erstellte Webhook',
@@ -64,13 +82,33 @@ return [
     'webhook_delete_notification' => 'Webhook erfolgreich gelöscht',
 
     // Users
+    'user_create' => 'created user',
+    'user_create_notification' => 'User successfully created',
+    'user_update' => 'updated user',
     'user_update_notification' => 'Benutzer erfolgreich aktualisiert',
+    'user_delete' => 'deleted user',
     'user_delete_notification' => 'Benutzer erfolgreich entfernt',
 
+    // API Tokens
+    'api_token_create' => 'created api token',
+    'api_token_create_notification' => 'API token successfully created',
+    'api_token_update' => 'updated api token',
+    'api_token_update_notification' => 'API token successfully updated',
+    'api_token_delete' => 'deleted api token',
+    'api_token_delete_notification' => 'API token successfully deleted',
+
     // Roles
+    'role_create' => 'created role',
     'role_create_notification' => 'Rolle erfolgreich angelegt',
+    'role_update' => 'updated role',
     'role_update_notification' => 'Rolle erfolgreich aktualisiert',
+    'role_delete' => 'deleted role',
     'role_delete_notification' => 'Rolle erfolgreich gelöscht',
+
+    // Recycle Bin
+    'recycle_bin_empty' => 'emptied recycle bin',
+    'recycle_bin_restore' => 'restored from recycle bin',
+    'recycle_bin_destroy' => 'removed from recycle bin',
 
     // Other
     'commented_on'                => 'hat einen Kommentar hinzugefügt',
