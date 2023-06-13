0332cc73e906b1a6e352ce7f3d43285c8357bdfe
Dan Brown <email@danb.me>
2023-06-13 15:20:18 +0100

    New translations errors.php (Croatian)

--- lang/hr/errors.php
+++ lang/hr/errors.php
@@ -58,6 +58,7 @@ return [
 
     // Pages
     'page_draft_autosave_fail' => 'Problem sa spremanjem nacrta. Osigurajte stabilnu internetsku vezu.',
+    'page_draft_delete_fail' => 'Failed to delete page draft and fetch current page saved content',
     'page_custom_home_deletion' => 'Stranica označena kao naslovnica ne može se izbrisati',
 
     // Entities
