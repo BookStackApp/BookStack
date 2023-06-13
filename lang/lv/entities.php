e9fc2eafe970d0ccae05e8c755d83929316cf637
Dan Brown <email@danb.me>
2023-06-13 15:19:39 +0100

    New translations entities.php (Latvian)

--- lang/lv/entities.php
+++ lang/lv/entities.php
@@ -213,6 +213,7 @@ return [
     'pages_editing_page' => 'Labo lapu',
     'pages_edit_draft_save_at' => 'Melnraksts saglabāts ',
     'pages_edit_delete_draft' => 'Dzēst melnrakstu',
+    'pages_edit_delete_draft_confirm' => 'Are you sure you want to delete your draft page changes? All of your changes, since the last full save, will be lost and the editor will be updated with the latest page non-draft save state.',
     'pages_edit_discard_draft' => 'Atmest malnrakstu',
     'pages_edit_switch_to_markdown' => 'Pārslēgties uz Markdown redaktoru',
     'pages_edit_switch_to_markdown_clean' => '(Iztīrītais saturs)',
@@ -285,7 +286,8 @@ return [
         'time_b' => 'pēdējās :minCount minūtēs',
         'message' => ':start :time. Esat uzmanīgi, lai neaizstātu viens otra izmaiņas!',
     ],
-    'pages_draft_discarded' => 'Melnraksts ir atcelts, redaktors ir atjaunināts ar pašreizējo lapas saturu',
+    'pages_draft_discarded' => 'Draft discarded! The editor has been updated with the current page content',
+    'pages_draft_deleted' => 'Draft deleted! The editor has been updated with the current page content',
     'pages_specific' => 'Konkrēta lapa',
     'pages_is_template' => 'Lapas šablons',
 
