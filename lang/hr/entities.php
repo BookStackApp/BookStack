6073b6efc235a8da821cd0a552d76690f5834346
Dan Brown <email@danb.me>
2023-06-13 15:19:36 +0100

    New translations entities.php (Croatian)

--- lang/hr/entities.php
+++ lang/hr/entities.php
@@ -213,6 +213,7 @@ return [
     'pages_editing_page' => 'Uređivanje stranice',
     'pages_edit_draft_save_at' => 'Nacrt spremljen kao',
     'pages_edit_delete_draft' => 'Izbriši nacrt',
+    'pages_edit_delete_draft_confirm' => 'Are you sure you want to delete your draft page changes? All of your changes, since the last full save, will be lost and the editor will be updated with the latest page non-draft save state.',
     'pages_edit_discard_draft' => 'Odbaci nacrt',
     'pages_edit_switch_to_markdown' => 'Switch to Markdown Editor',
     'pages_edit_switch_to_markdown_clean' => '(Clean Content)',
@@ -285,7 +286,8 @@ return [
         'time_b' => 'u zadnjih :minCount minuta',
         'message' => ':start :time. Pripazite na uzajamna ažuriranja!',
     ],
-    'pages_draft_discarded' => 'Nacrt je odbijen jer je uređivač ažurirao postoječi sadržaj',
+    'pages_draft_discarded' => 'Draft discarded! The editor has been updated with the current page content',
+    'pages_draft_deleted' => 'Draft deleted! The editor has been updated with the current page content',
     'pages_specific' => 'Predlošci stranice',
     'pages_is_template' => 'Predložak stranice',
 
