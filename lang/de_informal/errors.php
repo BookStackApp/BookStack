b6a5acd1abd61ebb3210f1aa08660e1bef1b3162
Dan Brown <email@danb.me>
2023-06-13 15:20:25 +0100

    New translations errors.php (German Informal)

--- lang/de_informal/errors.php
+++ lang/de_informal/errors.php
@@ -58,6 +58,7 @@ return [
 
     // Pages
     'page_draft_autosave_fail' => 'Fehler beim Speichern des Entwurfs. Stelle sicher, dass du mit dem Internet verbunden bist, bevor du den Entwurf dieser Seite speicherst.',
+    'page_draft_delete_fail' => 'Failed to delete page draft and fetch current page saved content',
     'page_custom_home_deletion' => 'Eine als Startseite gesetzte Seite kann nicht gelöscht werden.',
 
     // Entities
