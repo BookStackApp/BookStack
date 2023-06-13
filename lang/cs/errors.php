1b695160a99335e3f66fc452d0897a3a97fce17e
Dan Brown <email@danb.me>
2023-06-13 15:19:51 +0100

    New translations errors.php (Czech)

--- lang/cs/errors.php
+++ lang/cs/errors.php
@@ -58,6 +58,7 @@ return [
 
     // Pages
     'page_draft_autosave_fail' => 'Nepovedlo se uložit koncept. Než stránku uložíte, ujistěte se, že jste připojeni k internetu.',
+    'page_draft_delete_fail' => 'Failed to delete page draft and fetch current page saved content',
     'page_custom_home_deletion' => 'Nelze odstranit tuto stránku, protože je nastavena jako uvítací stránka',
 
     // Entities
