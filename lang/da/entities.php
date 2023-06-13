64b593d2dfc6ea3c59e050cd9d88fbff65e7177b
Dan Brown <email@danb.me>
2023-06-13 15:19:11 +0100

    New translations entities.php (Danish)

--- lang/da/entities.php
+++ lang/da/entities.php
@@ -213,6 +213,7 @@ return [
     'pages_editing_page' => 'Redigerer side',
     'pages_edit_draft_save_at' => 'Kladde gemt ved ',
     'pages_edit_delete_draft' => 'Slet kladde',
+    'pages_edit_delete_draft_confirm' => 'Are you sure you want to delete your draft page changes? All of your changes, since the last full save, will be lost and the editor will be updated with the latest page non-draft save state.',
     'pages_edit_discard_draft' => 'Kassér kladde',
     'pages_edit_switch_to_markdown' => 'Skift til Markdown redigering',
     'pages_edit_switch_to_markdown_clean' => '(Clean Content)',
@@ -285,7 +286,8 @@ return [
         'time_b' => 'i de sidste :minCount minutter',
         'message' => ':start : time. Pas på ikke at overskrive hinandens opdateringer!',
     ],
-    'pages_draft_discarded' => 'Kladde kasseret, editoren er blevet opdateret med aktuelt sideindhold',
+    'pages_draft_discarded' => 'Draft discarded! The editor has been updated with the current page content',
+    'pages_draft_deleted' => 'Draft deleted! The editor has been updated with the current page content',
     'pages_specific' => 'Specifik side',
     'pages_is_template' => 'Sideskabelon',
 
