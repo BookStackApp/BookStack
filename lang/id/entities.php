557b92f6db67ec878c294d0173115487098ec2c7
Dan Brown <email@danb.me>
2023-06-13 15:19:33 +0100

    New translations entities.php (Indonesian)

--- lang/id/entities.php
+++ lang/id/entities.php
@@ -213,6 +213,7 @@ return [
     'pages_editing_page' => 'Mengedit Draf',
     'pages_edit_draft_save_at' => 'Draf disimpan pada ',
     'pages_edit_delete_draft' => 'Hapus Draf',
+    'pages_edit_delete_draft_confirm' => 'Are you sure you want to delete your draft page changes? All of your changes, since the last full save, will be lost and the editor will be updated with the latest page non-draft save state.',
     'pages_edit_discard_draft' => 'Buang Draf',
     'pages_edit_switch_to_markdown' => 'Switch to Markdown Editor',
     'pages_edit_switch_to_markdown_clean' => '(Clean Content)',
@@ -285,7 +286,8 @@ return [
         'time_b' => 'di akhir :minCount menit',
         'message' => ':start :time. Berhati-hatilah untuk tidak menimpa pembaruan satu sama lain!',
     ],
-    'pages_draft_discarded' => 'Konsep dibuang, Penyunting telah diperbarui dengan konten halaman saat ini',
+    'pages_draft_discarded' => 'Draft discarded! The editor has been updated with the current page content',
+    'pages_draft_deleted' => 'Draft deleted! The editor has been updated with the current page content',
     'pages_specific' => 'Halaman Tertentu',
     'pages_is_template' => 'Template Halaman',
 
