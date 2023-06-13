9953483ca2bbea05b7eab513d052b4b1b9ecdb02
Dan Brown <email@danb.me>
2023-06-13 15:19:09 +0100

    New translations entities.php (Catalan)

--- lang/ca/entities.php
+++ lang/ca/entities.php
@@ -213,6 +213,7 @@ return [
     'pages_editing_page' => 'Esteu editant la pàgina',
     'pages_edit_draft_save_at' => 'Esborrany desat ',
     'pages_edit_delete_draft' => 'Suprimeix l\'esborrany',
+    'pages_edit_delete_draft_confirm' => 'Are you sure you want to delete your draft page changes? All of your changes, since the last full save, will be lost and the editor will be updated with the latest page non-draft save state.',
     'pages_edit_discard_draft' => 'Descarta l\'esborrany',
     'pages_edit_switch_to_markdown' => 'Switch to Markdown Editor',
     'pages_edit_switch_to_markdown_clean' => '(Clean Content)',
@@ -285,7 +286,8 @@ return [
         'time_b' => 'en els darrers :minCount minuts',
         'message' => ':start :time. Aneu amb compte de no trepitjar-vos les actualitzacions entre vosaltres!',
     ],
-    'pages_draft_discarded' => 'S\'ha descartat l\'esborrany, l\'editor s\'ha actualitzat amb el contingut actual de la pàgina',
+    'pages_draft_discarded' => 'Draft discarded! The editor has been updated with the current page content',
+    'pages_draft_deleted' => 'Draft deleted! The editor has been updated with the current page content',
     'pages_specific' => 'Una pàgina específica',
     'pages_is_template' => 'Plantilla de pàgina',
 
