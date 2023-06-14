9ddd81fcf4895c1678690ac600e6c5b9c27951ca
Thomas Kuschan <mail@thomaskuschan.de>
2023-06-14 11:52:22 +0200

    Move handling of ApiAuthException to handler
    Remove handling of exception from ApiAuthenticate middleware into the application exception handler.
    Remove unnecessary UnauthorizedException and make ApiAuthException compatible with HttpExceptionInterface.

--- app/Exceptions/ApiAuthException.php
+++ app/Exceptions/ApiAuthException.php
@@ -2,6 +2,39 @@
 
 namespace BookStack\Exceptions;
 
-class ApiAuthException extends UnauthorizedException
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ApiAuthException extends \Exception implements HttpExceptionInterface
 {
+    protected int $status;
+
+    /**
+     * @var array<mixed>
+     */
+    protected array $headers = [];
+
+    /**
+     * ApiAuthException constructor.
+     */
+    public function __construct(string $message, int $statusCode = 401)
+    {
+        $this->status = $statusCode;
+        parent::__construct($message, $statusCode);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getStatusCode(): int
+    {
+        return $this->status;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
 }
