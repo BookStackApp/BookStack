c35080d6cef5b5600cb4f93ca2fd25d0f31a4cd2
Thomas Kuschan <mail@thomaskuschan.de>
2023-06-14 14:09:52 +0200

    Modify HttpFetchException handle to log exception
    Within the flow of HttpFetchException, the actual exception from curl is preserved and logged. Make HttpFetchException a pretty exception for when it is shown to users.

--- app/Exceptions/HttpFetchException.php
+++ app/Exceptions/HttpFetchException.php
@@ -2,8 +2,22 @@
 
 namespace BookStack\Exceptions;
 
-use Exception;
-
-class HttpFetchException extends Exception
+class HttpFetchException extends PrettyException
 {
+    /**
+     * Construct exception within BookStack\Uploads\HttpFetcher.
+     */
+    public function __construct(string $message = '', int $code = 0, ?\Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        if ($previous) {
+            $this->setDetails($previous->getMessage());
+        }
+    }
+
+    public function getStatusCode(): int
+    {
+        return 500;
+    }
 }
