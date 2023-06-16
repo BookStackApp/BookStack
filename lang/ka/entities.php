74098389c9cdce8776303484f545f82fe4a7ef5e
Dan Brown <email@danb.me>
2023-06-16 14:07:04 +0100

    New translations entities.php (Georgian)

--- lang/ka/entities.php
+++ lang/ka/entities.php
@@ -371,6 +371,7 @@ return [
     'comment_updated_success' => 'Comment updated',
     'comment_delete_confirm' => 'Are you sure you want to delete this comment?',
     'comment_in_reply_to' => 'In reply to :commentId',
+    'comment_editor_explain' => 'Here are the comments that have been left on this page. Comments can be added & managed when viewing the saved page.',
 
     // Revision
     'revision_delete_confirm' => 'Are you sure you want to delete this revision?',
