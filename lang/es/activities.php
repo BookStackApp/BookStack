dd3f03de46cc69b3471c56f69e4e316e4a879624
Dan Brown <email@danb.me>
2023-07-19 10:26:45 +0100

    New translations activities.php (Spanish)

--- lang/es/activities.php
+++ lang/es/activities.php
@@ -110,7 +110,12 @@ return [
     'recycle_bin_restore' => 'restaurado de la papelera de reciclaje',
     'recycle_bin_destroy' => 'eliminado de la papelera de reciclaje',
 
-    // Other
+    // Comments
     'commented_on'                => 'comentada el',
+    'comment_create'              => 'added comment',
+    'comment_update'              => 'updated comment',
+    'comment_delete'              => 'deleted comment',
+
+    // Other
     'permissions_update'          => 'permisos actualizados',
 ];
