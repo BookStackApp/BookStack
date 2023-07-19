1c922be4c7b0e32c57511e8875d479b99b763f75
Dan Brown <ssddanbrown@googlemail.com>
2023-07-19 10:11:53 +0100

    Comments: Added text for new activity types

--- lang/en/activities.php
+++ lang/en/activities.php
@@ -110,7 +110,12 @@ return [
     'recycle_bin_restore' => 'restored from recycle bin',
     'recycle_bin_destroy' => 'removed from recycle bin',
 
-    // Other
+    // Comments
     'commented_on'                => 'commented on',
+    'comment_create'              => 'added comment',
+    'comment_update'              => 'updated comment',
+    'comment_delete'              => 'deleted comment',
+
+    // Other
     'permissions_update'          => 'updated permissions',
 ];
