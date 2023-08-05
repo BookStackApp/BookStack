ecab2c8e42ae11485bb3c0fcce1a51df1bc6b118
Dan Brown <ssddanbrown@googlemail.com>
2023-08-05 14:19:23 +0100

    Notifications: Added logic and classes for remaining notification types

--- app/Activity/Notifications/Handlers/PageCreationNotificationHandler.php
+++ app/Activity/Notifications/Handlers/PageCreationNotificationHandler.php
@@ -7,38 +7,17 @@ use BookStack\Activity\Notifications\Messages\PageCreationNotification;
 use BookStack\Activity\Tools\EntityWatchers;
 use BookStack\Activity\WatchLevels;
 use BookStack\Entities\Models\Page;
-use BookStack\Permissions\PermissionApplicator;
 use BookStack\Users\Models\User;
 
-class PageCreationNotificationHandler implements NotificationHandler
+class PageCreationNotificationHandler extends BaseNotificationHandler
 {
     public function handle(string $activityType, Loggable|string $detail, User $user): void
     {
         if (!($detail instanceof Page)) {
             throw new \InvalidArgumentException("Detail for page create notifications must be a page");
         }
-        // No user-level preferences to care about here.
-        // Possible Scenarios:
-        // ✅ User watching parent chapter
-        // ✅ User watching parent book
-        // ❌ User ignoring parent book
-        // ❌ User ignoring parent chapter
-        // ❌ User watching parent book, ignoring chapter
-        // ✅ User watching parent book, watching chapter
-        // ❌ User ignoring parent book, ignoring chapter
-        // ✅ User ignoring parent book, watching chapter
 
-        // Get all relevant watchers
         $watchers = new EntityWatchers($detail, WatchLevels::NEW);
-        $users = User::query()->whereIn('id', $watchers->getWatcherUserIds())->get();
-
-        // TODO - Clean this up, likely abstract to base class
-        // TODO - Prevent sending to current user
-        $permissions = app()->make(PermissionApplicator::class);
-        foreach ($users as $user) {
-            if ($user->can('receive-notifications') && $permissions->checkOwnableUserAccess($detail, 'view')) {
-                $user->notify(new PageCreationNotification($detail, $user));
-            }
-        }
+        $this->sendNotificationToUserIds(PageCreationNotification::class, $watchers->getWatcherUserIds(), $user, $detail);
     }
 }
--- app/Activity/Notifications/Handlers/PageUpdateNotificationHandler.php
+++ app/Activity/Notifications/Handlers/PageUpdateNotificationHandler.php
@@ -3,12 +3,31 @@
 namespace BookStack\Activity\Notifications\Handlers;
 
 use BookStack\Activity\Models\Loggable;
+use BookStack\Activity\Notifications\Messages\PageUpdateNotification;
+use BookStack\Activity\Tools\EntityWatchers;
+use BookStack\Activity\WatchLevels;
+use BookStack\Entities\Models\Page;
+use BookStack\Settings\UserNotificationPreferences;
 use BookStack\Users\Models\User;
 
-class PageUpdateNotificationHandler implements NotificationHandler
+class PageUpdateNotificationHandler extends BaseNotificationHandler
 {
     public function handle(string $activityType, Loggable|string $detail, User $user): void
     {
-        // TODO
+        if (!($detail instanceof Page)) {
+            throw new \InvalidArgumentException("Detail for page update notifications must be a page");
+        }
+
+        $watchers = new EntityWatchers($detail, WatchLevels::UPDATES);
+        $watcherIds = $watchers->getWatcherUserIds();
+
+        if (!$watchers->isUserIgnoring($detail->owned_by) && $detail->ownedBy) {
+            $userNotificationPrefs = new UserNotificationPreferences($detail->ownedBy);
+            if ($userNotificationPrefs->notifyOnOwnPageChanges()) {
+                $watcherIds[] = $detail->owned_by;
+            }
+        }
+
+        $this->sendNotificationToUserIds(PageUpdateNotification::class, $watcherIds, $user, $detail);
     }
 }
