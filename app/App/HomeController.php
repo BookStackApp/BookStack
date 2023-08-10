3d8056e9c90755b8e8627b1d519a99e46192a40e
JonatanRek <gamerclass7@gmail.com>
2023-08-10 17:02:31 +0200

    Move Manifest Definition to Separate Config File

--- app/App/HomeController.php
+++ app/App/HomeController.php
@@ -147,61 +147,10 @@ class HomeController extends Controller
     */
     public function manifest()
     {   
-        $manifest = [
-            "name" => (config('app.name' | 'BookStack') ??'BookStack' ), 
-            "short_name" => "bookstack", 
-            "start_url" => "./", 
-            "scope" => ".", 
-            "display" => "standalone", 
-            "background_color" => setting('app-color'), 
-            "description" =>( config('app.name' | 'BookStack') ??'BookStack'), 
-            "categories" => [
-                "productivity", 
-                "lifestyle" 
-            ], 
-            "launch_handler" => [
-                "client_mode" => "focus-existing" 
-            ], 
-            "orientation" => "portrait", 
-            "icons" => [
-                [
-                    "src" => "/icon-64.png", 
-                    "sizes" => "64x64", 
-                    "type" => "image/png" 
-                ], 
-                [
-                    "src" => "/icon-32.png", 
-                    "sizes" => "32x32", 
-                    "type" => "image/png" 
-                ], 
-                [
-                    "src" => "/icon-128.png", 
-                    "sizes" => "128x128", 
-                    "type" => "image/png" 
-                ], 
-                [
-                    "src" => "icon-180.png", 
-                    "sizes" => "180x180", 
-                    "type" => "image/png" 
-                ], 
-                [
-                    "src" => "icon.png", 
-                    "sizes" => "256x256", 
-                    "type" => "image/png" 
-                ], 
-                [
-                    "src" => "icon.ico", 
-                    "sizes" => "48x48", 
-                    "type" => "image/vnd.microsoft.icon" 
-                ], 
-                [
-                    "src" => "favicon.ico", 
-                    "sizes" => "48x48", 
-                    "type" => "image/vnd.microsoft.icon" 
-                ],
-            ],
-        ]; 
+        $manifest =  config('manifest');
         
+        $manifest["background_color"] = setting('app-color');
+
         return response()->json($manifest);
     }
 }
