371779205a29816b90fcf31bb6dba609aeb192fc
Dan Brown <ssddanbrown@googlemail.com>
2023-08-14 17:29:12 +0100

    Notifications: Added new preferences view and access control
    - Added general user preferences view and updated link in profile menu
      to suit.
    - Made notification permission required for notification preferences
      view, added test to cover.

--- resources/views/users/preferences/notifications.blade.php
+++ resources/views/users/preferences/notifications.blade.php
@@ -11,33 +11,36 @@
                 <h1 class="list-heading">{{ trans('preferences.notifications') }}</h1>
                 <p class="text-small text-muted">{{ trans('preferences.notifications_desc') }}</p>
 
-                <div class="toggle-switch-list">
-                    <div>
-                        @include('form.toggle-switch', [
-                            'name' => 'preferences[own-page-changes]',
-                            'value' => $preferences->notifyOnOwnPageChanges(),
-                            'label' => trans('preferences.notifications_opt_own_page_changes'),
-                        ])
+                <div class="flex-container-row wrap justify-space-between pb-m">
+                    <div class="toggle-switch-list min-width-l">
+                        <div>
+                            @include('form.toggle-switch', [
+                                'name' => 'preferences[own-page-changes]',
+                                'value' => $preferences->notifyOnOwnPageChanges(),
+                                'label' => trans('preferences.notifications_opt_own_page_changes'),
+                            ])
+                        </div>
+                        <div>
+                            @include('form.toggle-switch', [
+                                'name' => 'preferences[own-page-comments]',
+                                'value' => $preferences->notifyOnOwnPageComments(),
+                                'label' => trans('preferences.notifications_opt_own_page_comments'),
+                            ])
+                        </div>
+                        <div>
+                            @include('form.toggle-switch', [
+                                'name' => 'preferences[comment-replies]',
+                                'value' => $preferences->notifyOnCommentReplies(),
+                                'label' => trans('preferences.notifications_opt_comment_replies'),
+                            ])
+                        </div>
                     </div>
-                    <div>
-                        @include('form.toggle-switch', [
-                            'name' => 'preferences[own-page-comments]',
-                            'value' => $preferences->notifyOnOwnPageComments(),
-                            'label' => trans('preferences.notifications_opt_own_page_comments'),
-                        ])
-                    </div>
-                    <div>
-                        @include('form.toggle-switch', [
-                            'name' => 'preferences[comment-replies]',
-                            'value' => $preferences->notifyOnCommentReplies(),
-                            'label' => trans('preferences.notifications_opt_comment_replies'),
-                        ])
+
+                    <div class="mt-auto">
+                        <button class="button">{{ trans('preferences.notifications_save') }}</button>
                     </div>
                 </div>
 
-                <div class="form-group text-right">
-                    <button class="button">{{ trans('preferences.notifications_save') }}</button>
-                </div>
             </form>
         </section>
 
