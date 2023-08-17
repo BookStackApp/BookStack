682cd63400d7bda461548d9b3ec512c20ac5a041
Dan Brown <email@danb.me>
2023-08-17 22:32:57 +0100

    New translations notifications.php (Dutch)

--- lang/nl/notifications.php
+++ lang/nl/notifications.php
@@ -4,23 +4,23 @@
  */
 return [
 
-    'new_comment_subject' => 'New comment on page: :pageName',
-    'new_comment_intro' => 'A user has commented on a page in :appName:',
-    'new_page_subject' => 'New page: :pageName',
-    'new_page_intro' => 'A new page has been created in :appName:',
-    'updated_page_subject' => 'Updated page: :pageName',
-    'updated_page_intro' => 'A page has been updated in :appName:',
-    'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
+    'new_comment_subject' => 'Nieuwe opmerking op pagina: :pageName',
+    'new_comment_intro' => 'Een gebruiker heeft gereageerd op een pagina in :appName:',
+    'new_page_subject' => 'Nieuwe pagina: :pageName',
+    'new_page_intro' => 'Een nieuwe pagina is gemaakt in :appName:',
+    'updated_page_subject' => 'Aangepaste pagina: :pageName',
+    'updated_page_intro' => 'Een pagina werd aangepast in :appName:',
+    'updated_page_debounce' => 'Om een stortvloed aan meldingen te voorkomen, zul je een tijdje geen meldingen ontvangen voor verdere bewerkingen van deze pagina door dezelfde redacteur.',
 
-    'detail_page_name' => 'Page Name:',
-    'detail_commenter' => 'Commenter:',
-    'detail_comment' => 'Comment:',
-    'detail_created_by' => 'Created By:',
-    'detail_updated_by' => 'Updated By:',
+    'detail_page_name' => 'Pagina Naam:',
+    'detail_commenter' => 'Reageerder:',
+    'detail_comment' => 'Opmerking:',
+    'detail_created_by' => 'Gemaakt Door:',
+    'detail_updated_by' => 'Aangepast Door:',
 
-    'action_view_comment' => 'View Comment',
-    'action_view_page' => 'View Page',
+    'action_view_comment' => 'Bekijk Opmerking',
+    'action_view_page' => 'Bekijk Pagina',
 
-    'footer_reason' => 'This notification was sent to you because :link cover this type of activity for this item.',
-    'footer_reason_link' => 'your notification preferences',
+    'footer_reason' => 'Deze melding is naar u verzonden omdat :link dit type activiteit voor dit artikel dekt.',
+    'footer_reason_link' => 'je meldingsvoorkeuren',
 ];
