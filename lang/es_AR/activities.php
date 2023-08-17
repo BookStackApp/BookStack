44d1495470eb2548b40d4d2a704439410edda8e0
Dan Brown <email@danb.me>
2023-08-17 21:18:58 +0100

    New translations activities.php (Spanish, Argentina)

--- lang/es_AR/activities.php
+++ lang/es_AR/activities.php
@@ -58,6 +58,9 @@ return [
     'favourite_add_notification' => '".name" se añadió a sus favoritos',
     'favourite_remove_notification' => '".name" se eliminó de sus favoritos',
 
+    // Watching
+    'watch_update_level_notification' => 'Watch preferences successfully updated',
+
     // Auth
     'auth_login' => 'sesión iniciada',
     'auth_register' => 'registrado como usuario nuevo',
@@ -112,9 +115,9 @@ return [
 
     // Comments
     'commented_on'                => 'comentado',
-    'comment_create'              => 'added comment',
-    'comment_update'              => 'updated comment',
-    'comment_delete'              => 'deleted comment',
+    'comment_create'              => 'comentario añadido',
+    'comment_update'              => 'comentario actualizado',
+    'comment_delete'              => 'comentario borrado',
 
     // Other
     'permissions_update'          => 'permisos actualizados',
