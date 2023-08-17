53dd19d66115cdc7db5e2db19c8f69d9075e8fee
Dan Brown <email@danb.me>
2023-08-17 21:19:33 +0100

    New translations activities.php (Uzbek)

--- lang/uz/activities.php
+++ lang/uz/activities.php
@@ -58,6 +58,9 @@ return [
     'favourite_add_notification' => '":name" sevimlilaringizga qo\'shildi',
     'favourite_remove_notification' => '":name" sevimlilaringizdan olib tashlandi',
 
+    // Watching
+    'watch_update_level_notification' => 'Watch preferences successfully updated',
+
     // Auth
     'auth_login' => 'logged in',
     'auth_register' => 'registered as new user',
