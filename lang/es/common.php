61070cb5e85b91c1ab5a6e1018dc9cd1b1ba3bda
Dan Brown <email@danb.me>
2023-08-18 13:33:05 +0100

    New translations common.php (Spanish)

--- lang/es/common.php
+++ lang/es/common.php
@@ -42,7 +42,7 @@ return [
     'remove' => 'Remover',
     'add' => 'Añadir',
     'configure' => 'Configurar',
-    'manage' => 'Manage',
+    'manage' => 'Gestionar',
     'fullscreen' => 'Pantalla completa',
     'favourite' => 'Añadir a favoritos',
     'unfavourite' => 'Eliminar de favoritos',
