dabf7e0a9a8c3104cb843e3ba8ae79c51871395b
Dan Brown <email@danb.me>
2023-08-20 23:08:59 +0100

    New translations notifications.php (Swedish)

--- lang/sv/notifications.php
+++ lang/sv/notifications.php
@@ -18,7 +18,7 @@ return [
     'detail_created_by' => 'Created By:',
     'detail_updated_by' => 'Updated By:',
 
-    'action_view_comment' => 'View Comment',
+    'action_view_comment' => 'Visa kommentar',
     'action_view_page' => 'View Page',
 
     'footer_reason' => 'This notification was sent to you because :link cover this type of activity for this item.',
