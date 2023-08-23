48b4fc8bb22a46c034e901c02e5ee70d0a871702
Dan Brown <email@danb.me>
2023-08-23 08:58:05 +0100

    New translations notifications.php (Norwegian Bokmal)

--- lang/nb/notifications.php
+++ lang/nb/notifications.php
@@ -4,23 +4,23 @@
  */
 return [
 
-    'new_comment_subject' => 'New comment on page: :pageName',
-    'new_comment_intro' => 'A user has commented on a page in :appName:',
-    'new_page_subject' => 'New page: :pageName',
-    'new_page_intro' => 'A new page has been created in :appName:',
-    'updated_page_subject' => 'Updated page: :pageName',
-    'updated_page_intro' => 'A page has been updated in :appName:',
+    'new_comment_subject' => 'Ny kommentar på siden: :pageName',
+    'new_comment_intro' => 'En bruker har kommentert en side i :appName:',
+    'new_page_subject' => 'Ny side: :pageName',
+    'new_page_intro' => 'En ny side er opprettet i :appName:',
+    'updated_page_subject' => 'Oppdatert side: :pageName',
+    'updated_page_intro' => 'En side er oppdatert i :appName:',
     'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
 
-    'detail_page_name' => 'Page Name:',
+    'detail_page_name' => 'Sidenavn:',
     'detail_commenter' => 'Commenter:',
-    'detail_comment' => 'Comment:',
-    'detail_created_by' => 'Created By:',
-    'detail_updated_by' => 'Updated By:',
+    'detail_comment' => 'Kommentar:',
+    'detail_created_by' => 'Opprettet av:',
+    'detail_updated_by' => 'Oppdatert av:',
 
-    'action_view_comment' => 'View Comment',
-    'action_view_page' => 'View Page',
+    'action_view_comment' => 'Vis kommentar',
+    'action_view_page' => 'Se side',
 
     'footer_reason' => 'This notification was sent to you because :link cover this type of activity for this item.',
-    'footer_reason_link' => 'your notification preferences',
+    'footer_reason_link' => 'dine varslingsinnstillinger',
 ];
