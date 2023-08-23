1421eeea3ac9f6f926df310f1905fa4b9256c641
Dan Brown <email@danb.me>
2023-08-23 19:38:31 +0100

    New translations entities.php (Korean)

--- lang/ko/entities.php
+++ lang/ko/entities.php
@@ -239,6 +239,8 @@ return [
     'pages_md_insert_drawing' => '드로잉 추가',
     'pages_md_show_preview' => 'Show preview',
     'pages_md_sync_scroll' => 'Sync preview scroll',
+    'pages_drawing_unsaved' => 'Unsaved Drawing Found',
+    'pages_drawing_unsaved_confirm' => 'Unsaved drawing data was found from a previous failed drawing save attempt. Would you like to restore and continue editing this unsaved drawing?',
     'pages_not_in_chapter' => '챕터에 있는 문서가 아닙니다.',
     'pages_move' => '문서 이동하기',
     'pages_copy' => '문서 복제',
