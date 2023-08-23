28e708903632b2d0383547baf7169a09143c19dd
Dan Brown <email@danb.me>
2023-08-23 19:38:28 +0100

    New translations entities.php (Italian)

--- lang/it/entities.php
+++ lang/it/entities.php
@@ -239,6 +239,8 @@ return [
     'pages_md_insert_drawing' => 'Inserisci Disegno',
     'pages_md_show_preview' => 'Visualizza anteprima',
     'pages_md_sync_scroll' => 'Sincronizza scorrimento anteprima',
+    'pages_drawing_unsaved' => 'Unsaved Drawing Found',
+    'pages_drawing_unsaved_confirm' => 'Unsaved drawing data was found from a previous failed drawing save attempt. Would you like to restore and continue editing this unsaved drawing?',
     'pages_not_in_chapter' => 'La pagina non è in un capitolo',
     'pages_move' => 'Muovi Pagina',
     'pages_copy' => 'Copia Pagina',
