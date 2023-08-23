a470f346a9999b5b06c8b2cdd78def9e450b09aa
Dan Brown <email@danb.me>
2023-08-23 19:38:49 +0100

    New translations entities.php (Latvian)

--- lang/lv/entities.php
+++ lang/lv/entities.php
@@ -239,6 +239,8 @@ return [
     'pages_md_insert_drawing' => 'Ievietot zīmējumu',
     'pages_md_show_preview' => 'Rādīt priekšskatu',
     'pages_md_sync_scroll' => 'Sync preview scroll',
+    'pages_drawing_unsaved' => 'Unsaved Drawing Found',
+    'pages_drawing_unsaved_confirm' => 'Unsaved drawing data was found from a previous failed drawing save attempt. Would you like to restore and continue editing this unsaved drawing?',
     'pages_not_in_chapter' => 'Lapa nav nodaļā',
     'pages_move' => 'Pārvietot lapu',
     'pages_copy' => 'Kopēt lapu',
