674445b21a4ce04154611257e2d714a82b5b1f2a
Dan Brown <email@danb.me>
2023-08-23 19:38:27 +0100

    New translations entities.php (Hungarian)

--- lang/hu/entities.php
+++ lang/hu/entities.php
@@ -239,6 +239,8 @@ return [
     'pages_md_insert_drawing' => 'Rajz beillesztése',
     'pages_md_show_preview' => 'Show preview',
     'pages_md_sync_scroll' => 'Sync preview scroll',
+    'pages_drawing_unsaved' => 'Unsaved Drawing Found',
+    'pages_drawing_unsaved_confirm' => 'Unsaved drawing data was found from a previous failed drawing save attempt. Would you like to restore and continue editing this unsaved drawing?',
     'pages_not_in_chapter' => 'Az oldal nincs fejezetben',
     'pages_move' => 'Oldal áthelyezése',
     'pages_copy' => 'Oldal másolása',
