b01012a33a6a2df84f557277c92a211db4a3ed6d
Dan Brown <email@danb.me>
2023-08-23 19:38:29 +0100

    New translations entities.php (Japanese)

--- lang/ja/entities.php
+++ lang/ja/entities.php
@@ -239,6 +239,8 @@ return [
     'pages_md_insert_drawing' => '描画を追加',
     'pages_md_show_preview' => 'プレビューを表示',
     'pages_md_sync_scroll' => 'プレビューとスクロールを同期',
+    'pages_drawing_unsaved' => 'Unsaved Drawing Found',
+    'pages_drawing_unsaved_confirm' => 'Unsaved drawing data was found from a previous failed drawing save attempt. Would you like to restore and continue editing this unsaved drawing?',
     'pages_not_in_chapter' => 'チャプターが設定されていません',
     'pages_move' => 'ページを移動',
     'pages_copy' => 'ページをコピー',
