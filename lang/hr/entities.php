b90442e30c5ff07c1c8e572e41d31e606a2ee12f
Dan Brown <email@danb.me>
2023-08-23 19:38:47 +0100

    New translations entities.php (Croatian)

--- lang/hr/entities.php
+++ lang/hr/entities.php
@@ -239,6 +239,8 @@ return [
     'pages_md_insert_drawing' => 'Umetni crtež',
     'pages_md_show_preview' => 'Prikaži pregled',
     'pages_md_sync_scroll' => 'Sinkroniziraj pomicanje pregleda',
+    'pages_drawing_unsaved' => 'Unsaved Drawing Found',
+    'pages_drawing_unsaved_confirm' => 'Unsaved drawing data was found from a previous failed drawing save attempt. Would you like to restore and continue editing this unsaved drawing?',
     'pages_not_in_chapter' => 'Stranica nije u poglavlju',
     'pages_move' => 'Premjesti stranicu',
     'pages_copy' => 'Kopiraj stranicu',
