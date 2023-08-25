73d92a0f3726ca3a3abb00e456f1b00204c60c40
Dan Brown <email@danb.me>
2023-08-25 15:12:43 +0100

    New translations notifications.php (Russian)

--- lang/ru/notifications.php
+++ lang/ru/notifications.php
@@ -12,14 +12,14 @@ return [
     'updated_page_intro' => 'A page has been updated in :appName:',
     'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
 
-    'detail_page_name' => 'Page Name:',
-    'detail_commenter' => 'Commenter:',
-    'detail_comment' => 'Comment:',
-    'detail_created_by' => 'Created By:',
-    'detail_updated_by' => 'Updated By:',
+    'detail_page_name' => 'Имя страницы:',
+    'detail_commenter' => 'Комментатор:',
+    'detail_comment' => 'Комментарий:',
+    'detail_created_by' => 'Создано:',
+    'detail_updated_by' => 'Обновлено:',
 
-    'action_view_comment' => 'View Comment',
-    'action_view_page' => 'View Page',
+    'action_view_comment' => 'Просмотреть комментарий',
+    'action_view_page' => 'Посмотреть страницу',
 
     'footer_reason' => 'This notification was sent to you because :link cover this type of activity for this item.',
     'footer_reason_link' => 'your notification preferences',
