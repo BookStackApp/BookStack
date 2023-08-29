8db32513558b0f698b8a5840ab63d740669c20a2
Dan Brown <email@danb.me>
2023-08-29 09:49:58 +0100

    New translations notifications.php (Norwegian Bokmal)

--- lang/nb/notifications.php
+++ lang/nb/notifications.php
@@ -10,10 +10,10 @@ return [
     'new_page_intro' => 'En ny side er opprettet i :appName:',
     'updated_page_subject' => 'Oppdatert side: :pageName',
     'updated_page_intro' => 'En side er oppdatert i :appName:',
-    'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
+    'updated_page_debounce' => 'For å forhindre mange varslinger, vil du ikke få nye varslinger for endringer på denne siden fra samme forfatter.',
 
     'detail_page_name' => 'Sidenavn:',
-    'detail_commenter' => 'Commenter:',
+    'detail_commenter' => 'Kommentar fra:',
     'detail_comment' => 'Kommentar:',
     'detail_created_by' => 'Opprettet av:',
     'detail_updated_by' => 'Oppdatert av:',
@@ -21,6 +21,6 @@ return [
     'action_view_comment' => 'Vis kommentar',
     'action_view_page' => 'Se side',
 
-    'footer_reason' => 'This notification was sent to you because :link cover this type of activity for this item.',
+    'footer_reason' => 'Denne meldingen ble sendt til deg fordi :link dekker denne typen aktivitet for dette elementet.',
     'footer_reason_link' => 'dine varslingsinnstillinger',
 ];
