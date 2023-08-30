41001f3b2f9b0a5ecd4006c112072de5faa741d8
Dan Brown <email@danb.me>
2023-08-30 12:30:21 +0100

    New translations errors.php (Greek)

--- lang/el/errors.php
+++ lang/el/errors.php
@@ -111,4 +111,6 @@ return [
     // Settings & Maintenance
     'maintenance_test_email_failure' => 'Σφάλμα κατά την αποστολή δοκιμαστικού email:',
 
+    // HTTP errors
+    'http_ssr_url_no_match' => 'The URL does not match the configured allowed SSR hosts',
 ];
