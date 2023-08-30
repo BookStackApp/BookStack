cd9236f10cdd05c4ac359a72233fb5530594d0e6
Dan Brown <email@danb.me>
2023-08-30 12:30:18 +0100

    New translations errors.php (Czech)

--- lang/cs/errors.php
+++ lang/cs/errors.php
@@ -111,4 +111,6 @@ return [
     // Settings & Maintenance
     'maintenance_test_email_failure' => 'Při posílání testovacího e-mailu nastala chyba:',
 
+    // HTTP errors
+    'http_ssr_url_no_match' => 'The URL does not match the configured allowed SSR hosts',
 ];
