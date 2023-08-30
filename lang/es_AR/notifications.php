a2bc660a3aa153624778adc387732a3b594ed085
Dan Brown <email@danb.me>
2023-08-30 12:29:00 +0100

    New translations notifications.php (Spanish, Argentina)

--- lang/es_AR/notifications.php
+++ lang/es_AR/notifications.php
@@ -4,23 +4,23 @@
  */
 return [
 
-    'new_comment_subject' => 'New comment on page: :pageName',
-    'new_comment_intro' => 'A user has commented on a page in :appName:',
-    'new_page_subject' => 'New page: :pageName',
-    'new_page_intro' => 'A new page has been created in :appName:',
-    'updated_page_subject' => 'Updated page: :pageName',
-    'updated_page_intro' => 'A page has been updated in :appName:',
-    'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
+    'new_comment_subject' => 'Nuevo comentario en la página: :pageName',
+    'new_comment_intro' => 'Un usuario ha comentado en una página de :appName:',
+    'new_page_subject' => 'Nueva página: :pageName',
+    'new_page_intro' => 'Una nueva página ha sido creada en :appName:',
+    'updated_page_subject' => 'Página actualizada: :pageName',
+    'updated_page_intro' => 'Una página ha sido actualizada en :appName:',
+    'updated_page_debounce' => 'Para prevenir notificaciones en masa, durante un tiempo no se enviarán notificaciones para futuras ediciones de esta página por el mismo editor.',
 
-    'detail_page_name' => 'Page Name:',
-    'detail_commenter' => 'Commenter:',
-    'detail_comment' => 'Comment:',
-    'detail_created_by' => 'Created By:',
-    'detail_updated_by' => 'Updated By:',
+    'detail_page_name' => 'Nombre de página:',
+    'detail_commenter' => 'Autor del comentario:',
+    'detail_comment' => 'Comentario:',
+    'detail_created_by' => 'Creado por:',
+    'detail_updated_by' => 'Actualizado por:',
 
-    'action_view_comment' => 'View Comment',
-    'action_view_page' => 'View Page',
+    'action_view_comment' => 'Ver comentario',
+    'action_view_page' => 'Ver página',
 
-    'footer_reason' => 'This notification was sent to you because :link cover this type of activity for this item.',
-    'footer_reason_link' => 'your notification preferences',
+    'footer_reason' => 'Esta notificación fue enviada porque :link cubre este tipo de actividad para este artículo.',
+    'footer_reason_link' => 'sus preferencias de notificación',
 ];
