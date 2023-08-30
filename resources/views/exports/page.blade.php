7999edec35b391fad5983f464f25b7e7b455f3c8
Sascha <mib.jelinek@gmail.com>
2023-08-30 13:20:03 +0200

    Update page.blade.php
    Included the page content and the tags from the entity tag-export

--- resources/views/exports/page.blade.php
+++ resources/views/exports/page.blade.php
@@ -3,7 +3,40 @@
 @section('title', $page->name)
 
 @section('content')
-    @include('pages.parts.page-display')
+    <div dir="auto">
+
+        <h1 class="break-text" id="bkmrk-page-title">{{$page->name}}</h1>
+        @php
+            $toc = (new \BookStack\Entities\Tools\PageContent($page))->getNavigation($page->html);
+        @endphp
+
+        @if(!empty($toc))
+            <hr>
+            <div style="color: #888;"><strong>Page Contents</strong></div>
+            <ul>
+                @foreach($toc as $tocEntry)
+                    <li style="margin-left: {{ ($tocEntry['level'] - 1) * 16  }}px;"><a href="{{ $tocEntry['link'] }}">{{ $tocEntry['text'] }}</a></li>
+                @endforeach
+            </ul>
+            <hr>
+        @endif
+
+        @if($page->tags->count() > 0)
+            <div style="color: #888;"><strong>Tags</strong></div>
+            <div class="text-muted text-small">
+            <ul>
+                @foreach($page->tags as $tag)
+                    <li>@include('entities.tag-export', ['tag' => $tag])</li>
+                @endforeach
+            </ul>
+            </div>
+            <hr>
+        @endif
+
+        <div style="clear:left;"></div>
+
+        {!! $page->renderedHTML ?? $page->html !!}
+    </div>
 
     <hr>
 
