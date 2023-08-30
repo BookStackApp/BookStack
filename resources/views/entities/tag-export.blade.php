2401b47a8c3c696d4cd9598ebbb6fbc57a0145bb
Sascha <mib.jelinek@gmail.com>
2023-08-30 13:04:18 +0200

    Create tag-export.blade.php
    An entity which allow to include tags in a PDF export

---
+++ resources/views/entities/tag-export.blade.php
@@ -0,0 +1,4 @@
+<div class="tag-item primary-background-light">
+    <div class="tag-name"><span><strong>{{ $tag->name }}</strong>
+    @if($tag->value) - {{$tag->value}}</span></div> @endif
+</div>
