15d91145a8dfd7ecd3560c5afe87f15de4985872
Dan Brown <email@danb.me>
2023-08-31 17:42:39 +0100

    New translations notifications.php (Japanese)

--- lang/ja/notifications.php
+++ lang/ja/notifications.php
@@ -9,18 +9,18 @@ return [
     'new_page_subject' => 'New page: :pageName',
     'new_page_intro' => 'A new page has been created in :appName:',
     'updated_page_subject' => 'Updated page: :pageName',
-    'updated_page_intro' => 'A page has been updated in :appName:',
-    'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
+    'updated_page_intro' => ':appName: でページが更新されました',
+    'updated_page_debounce' => '大量の通知を防ぐために、しばらくの間は同じユーザがこのページをさらに編集しても通知は送信されません。',
 
-    'detail_page_name' => 'Page Name:',
-    'detail_commenter' => 'Commenter:',
-    'detail_comment' => 'Comment:',
-    'detail_created_by' => 'Created By:',
-    'detail_updated_by' => 'Updated By:',
+    'detail_page_name' => 'ページ名：',
+    'detail_commenter' => 'コメントユーザ：',
+    'detail_comment' => 'コメント：',
+    'detail_created_by' => '作成ユーザ：',
+    'detail_updated_by' => '更新ユーザ：',
 
-    'action_view_comment' => 'View Comment',
-    'action_view_page' => 'View Page',
+    'action_view_comment' => 'コメントを表示',
+    'action_view_page' => 'ページを表示',
 
     'footer_reason' => 'This notification was sent to you because :link cover this type of activity for this item.',
-    'footer_reason_link' => 'your notification preferences',
+    'footer_reason_link' => '通知設定',
 ];
