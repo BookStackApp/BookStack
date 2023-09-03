e44c7cc200a392a7c6dc0e81fb03c26fc5f1f554
Dan Brown <email@danb.me>
2023-09-03 19:02:07 +0100

    New translations preferences.php (Estonian)

--- lang/et/preferences.php
+++ lang/et/preferences.php
@@ -5,7 +5,7 @@
  */
 
 return [
-    'preferences' => 'Preferences',
+    'preferences' => 'Eelistused',
 
     'shortcuts' => 'Kiirklahvid',
     'shortcuts_interface' => 'Kasutajaliidese kiirklahvid',
@@ -17,17 +17,17 @@ return [
     'shortcuts_save' => 'Salvesta kiirklahvid',
     'shortcuts_overlay_desc' => 'Märkus: Kui kiirklahvid on sisse lülitatud, saab "?" vajutades kuvada abiinfo, mis märgib ära kõigi hetkel ekraanil nähtavate tegevuste kiirklahvid.',
     'shortcuts_update_success' => 'Kiirklahvide eelistused on salvestatud!',
-    'shortcuts_overview_desc' => 'Manage keyboard shortcuts you can use to navigate the system user interface.',
+    'shortcuts_overview_desc' => 'Halda klaviatuuri kiirklahve süsteemi kasutajaliideses navigeerimiseks.',
 
-    'notifications' => 'Notification Preferences',
-    'notifications_desc' => 'Control the email notifications you receive when certain activity is performed within the system.',
-    'notifications_opt_own_page_changes' => 'Notify upon changes to pages I own',
-    'notifications_opt_own_page_comments' => 'Notify upon comments on pages I own',
-    'notifications_opt_comment_replies' => 'Notify upon replies to my comments',
-    'notifications_save' => 'Save Preferences',
-    'notifications_update_success' => 'Notification preferences have been updated!',
-    'notifications_watched' => 'Watched & Ignored Items',
-    'notifications_watched_desc' => ' Below are the items that have custom watch preferences applied. To update your preferences for these, view the item then find the watch options in the sidebar.',
+    'notifications' => 'Teavituste eelistused',
+    'notifications_desc' => 'Halda e-posti teavitusi, mis saadetakse teatud tegevuste puhul.',
+    'notifications_opt_own_page_changes' => 'Teavita muudatustest minu lehtedel',
+    'notifications_opt_own_page_comments' => 'Teavita kommentaaridest minu lehtedel',
+    'notifications_opt_comment_replies' => 'Teavita vastustest minu kommentaaridele',
+    'notifications_save' => 'Salvesta eelistused',
+    'notifications_update_success' => 'Teavituste eelistused on salvestatud!',
+    'notifications_watched' => 'Jälgitud ja ignoreeritud objektid',
+    'notifications_watched_desc' => ' Allpool on objektid, millele on määratud kohaldatud jälgimise eelistused. Eelistuste muutmiseks ava vastav objekt ning leia jälgimise valikud külgmenüüs.',
 
-    'profile_overview_desc' => ' Manage your user profile details including preferred language and authentication options.',
+    'profile_overview_desc' => ' Halda oma kasutajaprofiili andmeid, kaasa arvatud keele eelistust ja autentimisvalikuid.',
 ];
