67e45aa32cc5aef1d81095c76bd25a7af58ada69
Dan Brown <email@danb.me>
2023-09-08 18:32:53 +0100

    New translations errors.php (Russian)

--- lang/ru/errors.php
+++ lang/ru/errors.php
@@ -112,5 +112,5 @@ return [
     'maintenance_test_email_failure' => 'Ошибка при отправке тестового письма:',
 
     // HTTP errors
-    'http_ssr_url_no_match' => 'The URL does not match the configured allowed SSR hosts',
+    'http_ssr_url_no_match' => 'URL-адрес не соответствует настроенным разрешенным хостам SSR',
 ];
