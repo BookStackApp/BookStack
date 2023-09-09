6ff3de1bd4e6ea053530fd1b0b4e16a4e4790ea0
Dan Brown <email@danb.me>
2023-09-09 11:56:40 +0100

    New translations notifications.php (Persian)

--- lang/fa/notifications.php
+++ lang/fa/notifications.php
@@ -4,10 +4,10 @@
  */
 return [
 
-    'new_comment_subject' => 'New comment on page: :pageName',
-    'new_comment_intro' => 'A user has commented on a page in :appName:',
-    'new_page_subject' => 'New page: :pageName',
-    'new_page_intro' => 'A new page has been created in :appName:',
+    'new_comment_subject' => 'نظر جدید در صفحه: :pageName',
+    'new_comment_intro' => 'یک کاربر در بر روی صفحه‌ای نظر ثبت کرده است :appName:',
+    'new_page_subject' => 'صفحه جدید: :pageName',
+    'new_page_intro' => 'یک صفحه جدید ایجاد شده است در :appName:',
     'updated_page_subject' => 'Updated page: :pageName',
     'updated_page_intro' => 'A page has been updated in :appName:',
     'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
