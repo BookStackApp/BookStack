2a2f893fcc045cd1bf7c70d0f40edfed389b1dce
JonatanRek <gamerclass7@gmail.com>
2023-09-22 11:18:10 +0200

    Formating Fixes

--- app/App/HomeController.php
+++ app/App/HomeController.php
@@ -18,41 +18,41 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     /**
-    * Display the homepage.
-    */
+     * Display the homepage.
+     */
     public function index(Request $request, ActivityQueries $activities)
     {
         $activity = $activities->latest(10);
         $draftPages = [];
-        
+
         if ($this->isSignedIn()) {
             $draftPages = Page::visible()
-            ->where('draft', '=', true)
-            ->where('created_by', '=', user()->id)
-            ->orderBy('updated_at', 'desc')
-            ->with('book')
-            ->take(6)
-            ->get();
+                ->where('draft', '=', true)
+                ->where('created_by', '=', user()->id)
+                ->orderBy('updated_at', 'desc')
+                ->with('book')
+                ->take(6)
+                ->get();
         }
-        
+
         $recentFactor = count($draftPages) > 0 ? 0.5 : 1;
         $recents = $this->isSignedIn() ?
-        (new RecentlyViewed())->run(12 * $recentFactor, 1)
-        : Book::visible()->orderBy('created_at', 'desc')->take(12 * $recentFactor)->get();
+            (new RecentlyViewed())->run(12 * $recentFactor, 1)
+            : Book::visible()->orderBy('created_at', 'desc')->take(12 * $recentFactor)->get();
         $favourites = (new TopFavourites())->run(6);
         $recentlyUpdatedPages = Page::visible()->with('book')
-        ->where('draft', false)
-        ->orderBy('updated_at', 'desc')
-        ->take($favourites->count() > 0 ? 5 : 10)
-        ->select(Page::$listAttributes)
-        ->get();
-        
+            ->where('draft', false)
+            ->orderBy('updated_at', 'desc')
+            ->take($favourites->count() > 0 ? 5 : 10)
+            ->select(Page::$listAttributes)
+            ->get();
+
         $homepageOptions = ['default', 'books', 'bookshelves', 'page'];
         $homepageOption = setting('app-homepage-type', 'default');
         if (!in_array($homepageOption, $homepageOptions)) {
             $homepageOption = 'default';
         }
-        
+
         $commonData = [
             'activity'             => $activity,
             'recents'              => $recents,
@@ -60,7 +60,7 @@ class HomeController extends Controller
             'draftPages'           => $draftPages,
             'favourites'           => $favourites,
         ];
-        
+
         // Add required list ordering & sorting for books & shelves views.
         if ($homepageOption === 'bookshelves' || $homepageOption === 'books') {
             $key = $homepageOption;
@@ -70,27 +70,27 @@ class HomeController extends Controller
                 'created_at' => trans('common.sort_created_at'),
                 'updated_at' => trans('common.sort_updated_at'),
             ]);
-            
+
             $commonData = array_merge($commonData, [
                 'view'        => $view,
                 'listOptions' => $listOptions,
             ]);
         }
-        
+
         if ($homepageOption === 'bookshelves') {
             $shelves = app()->make(BookshelfRepo::class)->getAllPaginated(18, $commonData['listOptions']->getSort(), $commonData['listOptions']->getOrder());
             $data = array_merge($commonData, ['shelves' => $shelves]);
-            
+
             return view('home.shelves', $data);
         }
-        
+
         if ($homepageOption === 'books') {
             $books = app()->make(BookRepo::class)->getAllPaginated(18, $commonData['listOptions']->getSort(), $commonData['listOptions']->getOrder());
             $data = array_merge($commonData, ['books' => $books]);
-            
+
             return view('home.books', $data);
         }
-        
+
         if ($homepageOption === 'page') {
             $homepageSetting = setting('app-homepage', '0:');
             $id = intval(explode(':', $homepageSetting)[0]);
@@ -98,63 +98,46 @@ class HomeController extends Controller
             $customHomepage = Page::query()->where('draft', '=', false)->findOrFail($id);
             $pageContent = new PageContent($customHomepage);
             $customHomepage->html = $pageContent->render(false);
-            
+
             return view('home.specific-page', array_merge($commonData, ['customHomepage' => $customHomepage]));
         }
-        
+
         return view('home.default', $commonData);
     }
-    
+
     /**
-    * Show the view for /robots.txt.
-    */
+     * Show the view for /robots.txt.
+     */
     public function robots()
     {
         $sitePublic = setting('app-public', false);
         $allowRobots = config('app.allow_robots');
-        
+
         if ($allowRobots === null) {
             $allowRobots = $sitePublic;
         }
-        
+
         return response()
-        ->view('misc.robots', ['allowRobots' => $allowRobots])
-        ->header('Content-Type', 'text/plain');
+            ->view('misc.robots', ['allowRobots' => $allowRobots])
+            ->header('Content-Type', 'text/plain');
     }
-    
+
     /**
-    * Show the route for 404 responses.
-    */
+     * Show the route for 404 responses.
+     */
     public function notFound()
     {
         return response()->view('errors.404', [], 404);
     }
-    
+
     /**
-    * Serve the application favicon.
-    * Ensures a 'favicon.ico' file exists at the web root location (if writable) to be served
-    * directly by the webserver in the future.
-    */
+     * Serve the application favicon.
+     * Ensures a 'favicon.ico' file exists at the web root location (if writable) to be served
+     * directly by the webserver in the future.
+     */
     public function favicon(FaviconHandler $favicons)
     {
         $exists = $favicons->restoreOriginalIfNotExists();
         return response()->file($exists ? $favicons->getPath() : $favicons->getOriginalPath());
-    }
-    
-    /**
-    * Serve the application manifest.
-    * Ensures a 'manifest.json'
-    */
-    public function manifest()
-    {   
-        $manifest =  config('manifest');
-
-        if (setting()->getForCurrentUser('dark-mode-enabled')){
-            $manifest["background_color"] = setting('app-color-dark');
-        }else{
-            $manifest["background_color"] = setting('app-color');
-        }
-
-        return response()->json($manifest);
     }
 }
