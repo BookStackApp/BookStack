49ed777ed2fc3c7c83408d66acacddf877b257ff
Dan Brown <email@danb.me>
2023-10-01 17:30:48 +0000

    New translations components.php (Portuguese, Brazilian)

--- lang/pt_BR/components.php
+++ lang/pt_BR/components.php
@@ -34,6 +34,8 @@ return [
     'image_delete_success' => 'Imagem excluída com sucesso',
     'image_replace' => 'Substituir imagem',
     'image_replace_success' => 'Arquivo de imagem atualizado com sucesso',
+    'image_rebuild_thumbs' => 'Regenerate Size Variations',
+    'image_rebuild_thumbs_success' => 'Image size variations successfully rebuilt!',
 
     // Code Editor
     'code_editor' => 'Editar Código',
