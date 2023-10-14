4ca0c8b3bd2bd4ded53dc8f3e9afb3d6b2254c6c
Dan Brown <email@danb.me>
2023-10-14 19:11:30 +0100

    New translations entities.php (Dutch)

--- lang/nl/entities.php
+++ lang/nl/entities.php
@@ -295,6 +295,7 @@ return [
     'pages_is_template' => 'Paginasjabloon',
 
     // Editor Sidebar
+    'toggle_sidebar' => 'Toggle Sidebar',
     'page_tags' => 'Pagina Labels',
     'chapter_tags' => 'Hoofdstuk Labels',
     'book_tags' => 'Boek Labels',
