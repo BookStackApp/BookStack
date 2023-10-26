65ac197be440b90deccfd13d4eb8e6bec11e23b0
Sascha <mib.jelinek@gmail.com>
2023-10-26 14:01:38 +0200

    Added book name to the mail template
    added book name
    
    synced with actual file from dev branch
    
    added book name
    
    add book name
    
    added book name
    
    extended with chaptername
    
    extended with chapter name
    
    Update PageUpdateNotification.php
    
    Update notifications.php
    
    Update notifications.php
    
    Update notifications.php
    
    correction of chapter syntax
    
    correction of chapter syntax

--- app/Activity/Notifications/Messages/PageCreationNotification.php
+++ app/Activity/Notifications/Messages/PageCreationNotification.php
@@ -4,6 +4,7 @@ namespace BookStack\Activity\Notifications\Messages;
 
 use BookStack\Activity\Notifications\MessageParts\ListMessageLine;
 use BookStack\Entities\Models\Page;
+use BookStack\Entities\Models\Chapter;
 use BookStack\Users\Models\User;
 use Illuminate\Notifications\Messages\MailMessage;
 
@@ -13,16 +14,32 @@ class PageCreationNotification extends BaseActivityNotification
     {
         /** @var Page $page */
         $page = $this->detail;
+        $book = $page->book;
+        $chapterId = $page->chapter_id;
+        $chapter = $chapterId ? Chapter::find($chapterId) : null;
 
         $locale = $notifiable->getLocale();
+
+        $listMessageData = [
+            $locale->trans('notifications.detail_page_name') => $page->name,
+            '' => '',
+        ];
+
+        if ($chapter) {
+            $listMessageData += [
+                $locale->trans('notifications.detail_chapter_name') => $chapter->name,
+            ];
+        }
+    
+        $listMessageData += [
+            $locale->trans('notifications.detail_book_name') => $book->name,
+            $locale->trans('notifications.detail_created_by') => $this->user->name,
+        ];
 
         return $this->newMailMessage($locale)
             ->subject($locale->trans('notifications.new_page_subject', ['pageName' => $page->getShortName()]))
             ->line($locale->trans('notifications.new_page_intro', ['appName' => setting('app-name')], $locale))
-            ->line(new ListMessageLine([
-                $locale->trans('notifications.detail_page_name') => $page->name,
-                $locale->trans('notifications.detail_created_by') => $this->user->name,
-            ]))
+            ->line(new ListMessageLine($listMessageData))
             ->action($locale->trans('notifications.action_view_page'), $page->getUrl())
             ->line($this->buildReasonFooterLine($locale));
     }
