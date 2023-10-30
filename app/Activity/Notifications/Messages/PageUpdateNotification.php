3f546467fa354bd5de1d75760a03cc5e399117c0
Sascha <mib.jelinek@gmail.com>
2023-10-30 11:34:24 +0100

    extended with chapter name

--- app/Activity/Notifications/Messages/PageUpdateNotification.php
+++ app/Activity/Notifications/Messages/PageUpdateNotification.php
@@ -4,7 +4,7 @@ namespace BookStack\Activity\Notifications\Messages;
 
 use BookStack\Activity\Notifications\MessageParts\ListMessageLine;
 use BookStack\Entities\Models\Page;
-use BookStack\Entities\Models\Book;
+use BookStack\Entities\Models\Chapter;
 use BookStack\Users\Models\User;
 use Illuminate\Notifications\Messages\MailMessage;
 
@@ -14,18 +14,30 @@ class PageUpdateNotification extends BaseActivityNotification
     {
         /** @var Page $page */
         $page = $this->detail;
-        $book = $this->detail;
+        $book = $page->book;
+        $chapterId = $page->chapter_id;
+        $chapter = $chapterId ? Chapter::find($chapterId) : null;
 
         $locale = $notifiable->getLocale();
 
-        return $this->newMailMessage($locale)
+        $listMessageData = [
+             $locale->trans('notifications.detail_page_name') => $page->name,
+            '' => '',
+        ];
+    
+        if ($chapter) {
+            $listMessageData[$locale->trans('notifications.detail_chapter_name') => $chapter->name;
+        }
+    
+        $listMessageData += [
+            $locale->trans('notifications.detail_book_name') => $book->name,
+            $locale->trans('notifications.detail_updated_by') => $this->user->name,
+        ];
+
+        return $this->newMailMessage($language)
             ->subject($locale->trans('notifications.updated_page_subject', ['pageName' => $page->getShortName()]))
             ->line($locale->trans('notifications.updated_page_intro', ['appName' => setting('app-name')]))
-            ->line(new ListMessageLine([
-                $locale->trans('notifications.detail_book_name') => $book->name,
-                $locale->trans('notifications.detail_page_name') => $page->name,
-                $locale->trans('notifications.detail_updated_by') => $this->user->name,
-            ]))
+            ->line(new ListMessageLine($listMessageData))
             ->line($locale->trans('notifications.updated_page_debounce'))
             ->action($locale->trans('notifications.action_view_page'), $page->getUrl())
             ->line($this->buildReasonFooterLine($locale));
