209d73fbc6992bdcbe5d51a105e2c3b5ac6a2aca
Dan Brown <email@danb.me>
2023-11-11 13:42:17 +0000

    New translations auth.php (Catalan)

--- lang/ca/auth.php
+++ lang/ca/auth.php
@@ -7,29 +7,29 @@
 return [
 
     'failed' => 'Les credencials no coincideixen amb les que hi ha emmagatzemades.',
-    'throttle' => 'Massa intents d\'inici de sessió. Torna-ho a provar d\'aquí a :seconds segons.',
+    'throttle' => 'Massa intents d’inici de sessió. Torneu-ho a provar d’aquí a :seconds segons.',
 
     // Login & Register
-    'sign_up' => 'Registra-m\'hi',
+    'sign_up' => 'Registra-m’hi',
     'log_in' => 'Inicia la sessió',
     'log_in_with' => 'Inicia la sessió amb :socialDriver',
-    'sign_up_with' => 'Registra-m\'hi amb :socialDriver',
+    'sign_up_with' => 'Registra-m’hi amb :socialDriver',
     'logout' => 'Tanca la sessió',
 
     'name' => 'Nom',
-    'username' => 'Nom d\'usuari',
+    'username' => 'Nom d’usuari',
     'email' => 'Adreça electrònica',
     'password' => 'Contrasenya',
     'password_confirm' => 'Confirmeu la contrasenya',
-    'password_hint' => 'Must be at least 8 characters',
+    'password_hint' => 'Cal que tingui un mínim de 8 caràcters',
     'forgot_password' => 'Heu oblidat la contrasenya?',
-    'remember_me' => 'Recorda\'m',
+    'remember_me' => 'Recorda’m',
     'ldap_email_hint' => 'Introduïu una adreça electrònica per a aquest compte.',
     'create_account' => 'Crea el compte',
     'already_have_account' => 'Ja teniu un compte?',
     'dont_have_account' => 'No teniu cap compte?',
-    'social_login' => 'Inici de sessió amb xarxes social',
-    'social_registration' => 'Registre social',
+    'social_login' => 'Inici de sessió amb xarxes socials',
+    'social_registration' => 'Registre amb xarxes socials',
     'social_registration_text' => 'Registreu-vos i inicieu la sessió fent servir un altre servei.',
 
     'register_thanks' => 'Gràcies per registrar-vos!',
@@ -39,35 +39,35 @@ return [
     'register_success' => 'Gràcies per registrar-vos! Ja us hi heu registrat i heu iniciat la sessió.',
 
     // Login auto-initiation
-    'auto_init_starting' => 'Attempting Login',
-    'auto_init_starting_desc' => 'We\'re contacting your authentication system to start the login process. If there\'s no progress after 5 seconds you can try clicking the link below.',
-    'auto_init_start_link' => 'Proceed with authentication',
+    'auto_init_starting' => 'S’està provant d’iniciar la sessió',
+    'auto_init_starting_desc' => 'Estem contactant amb el vostre sistema d’autenticació per a començar el procés d’inici de sessió. Si no hi ha cap progrés d’aquí a 5 segons, proveu de fer clic a l’enllaç de sota.',
+    'auto_init_start_link' => 'Continua amb l’autenticació',
 
     // Password Reset
     'reset_password' => 'Restableix la contrasenya',
     'reset_password_send_instructions' => 'Introduïu la vostra adreça electrònica a continuació i us enviarem un correu electrònic amb un enllaç per a restablir la contrasenya.',
-    'reset_password_send_button' => 'Envia l\'enllaç de restabliment',
-    'reset_password_sent' => 'S\'enviarà un enllaç per a restablir la contrasenya a :email, si es troba aquesta adreça al sistema.',
-    'reset_password_success' => 'La vostra contrasenya s\'ha restablert correctament.',
+    'reset_password_send_button' => 'Envia l’enllaç de restabliment',
+    'reset_password_sent' => 'S’enviarà un enllaç per a restablir la contrasenya a :email, si es troba aquesta adreça al sistema.',
+    'reset_password_success' => 'La contrasenya s’ha restablert correctament.',
     'email_reset_subject' => 'Restabliu la contrasenya a :appName',
-    'email_reset_text' => 'Rebeu aquest correu electrònic perquè heu rebut una petició de restabliment de contrasenya per al vostre compte.',
-    'email_reset_not_requested' => 'Si no heu demanat restablir la contrasenya, no cal que prengueu cap acció.',
+    'email_reset_text' => 'Rebeu aquest correu electrònic perquè s’ha fet una petició de restabliment de contrasenya per al vostre compte.',
+    'email_reset_not_requested' => 'Si no heu demanat restablir la contrasenya, no cal que emprengueu cap acció.',
 
     // Email Confirmation
     'email_confirm_subject' => 'Confirmeu la vostra adreça electrònica a :appName',
     'email_confirm_greeting' => 'Gràcies per unir-vos a :appName!',
     'email_confirm_text' => 'Confirmeu la vostra adreça electrònica fent clic al botó a continuació:',
     'email_confirm_action' => 'Confirma el correu',
-    'email_confirm_send_error' => 'Cal confirmar l\'adreça electrònica, però el sistema no ha pogut enviar el correu electrònic. Poseu-vos en contacte amb l\'administrador perquè s\'asseguri que el correu electrònic està ben configurat.',
-    'email_confirm_success' => 'Your email has been confirmed! You should now be able to login using this email address.',
-    'email_confirm_resent' => 'S\'ha tornat a enviar el correu electrònic de confirmació. Reviseu la vostra safata d\'entrada.',
-    'email_confirm_thanks' => 'Thanks for confirming!',
-    'email_confirm_thanks_desc' => 'Please wait a moment while your confirmation is handled. If you are not redirected after 3 seconds press the "Continue" link below to proceed.',
+    'email_confirm_send_error' => 'Cal confirmar l’adreça electrònica, però el sistema no ha pogut enviar el correu electrònic. Poseu-vos en contacte amb l’administrador perquè s’asseguri que el correu electrònic està ben configurat.',
+    'email_confirm_success' => 'Heu confirmat el vostre correu electrònic! Ara hauríeu de poder iniciar la sessió fent servir aquesta adreça electrònica.',
+    'email_confirm_resent' => 'S’ha tornat a enviar el correu electrònic de confirmació. Reviseu la vostra safata d\'entrada.',
+    'email_confirm_thanks' => 'Gràcies per la confirmació!',
+    'email_confirm_thanks_desc' => 'Espereu uns instants mentre gestionem la confirmació. Si no se us redirigeix d’aquí a 3 segons, premeu l’enllaç «Continua» de sota per a continuar.',
 
     'email_not_confirmed' => 'Adreça electrònica no confirmada',
     'email_not_confirmed_text' => 'La vostra adreça electrònica encara no està confirmada.',
-    'email_not_confirmed_click_link' => 'Feu clic a l\'enllaç del correu electrònic que us vam enviar poc després que us registréssiu.',
-    'email_not_confirmed_resend' => 'Si no podeu trobar el correu, podeu tornar a enviar el correu electrònic de confirmació enviant el formulari a continuació.',
+    'email_not_confirmed_click_link' => 'Feu clic a l’enllaç del correu electrònic que us vam enviar poc després que us registréssiu.',
+    'email_not_confirmed_resend' => 'Si no el trobeu, podeu tornar a enviar el correu electrònic de confirmació enviant el formulari a continuació.',
     'email_not_confirmed_resend_button' => 'Torna a enviar el correu de confirmació',
 
     // User Invite
