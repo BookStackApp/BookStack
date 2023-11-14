0be522d16dd0a8171578518d38e0e96c0ba6e17d
Dan Brown <email@danb.me>
2023-11-14 13:59:17 +0000

    New translations common.php (Norwegian Nynorsk)

--- lang/nn/common.php
+++ lang/nn/common.php
@@ -57,26 +57,26 @@ return [
     // Sort Options
     'sort_options' => 'Sorteringsalternativ',
     'sort_direction_toggle' => 'Sorteringsretning',
-    'sort_ascending' => 'Stigende sortering',
-    'sort_descending' => 'Synkende sortering',
-    'sort_name' => 'Navn',
+    'sort_ascending' => 'Stigande sortering',
+    'sort_descending' => 'Synkande sortering',
+    'sort_name' => 'Namn',
     'sort_default' => 'Standard',
-    'sort_created_at' => 'Dato opprettet',
+    'sort_created_at' => 'Dato oppretta',
     'sort_updated_at' => 'Dato oppdatert',
 
     // Misc
-    'deleted_user' => 'Slett bruker',
+    'deleted_user' => 'Slett brukar',
     'no_activity' => 'Ingen aktivitet å vise',
-    'no_items' => 'Ingen ting å vise',
-    'back_to_top' => 'Hopp til toppen',
-    'skip_to_main_content' => 'Gå til hovedinnhold',
-    'toggle_details' => 'Vis/skjul detaljer',
-    'toggle_thumbnails' => 'Vis/skjul miniatyrbilder',
-    'details' => 'Detaljer',
-    'grid_view' => 'Rutenettvisning',
-    'list_view' => 'Listevisning',
+    'no_items' => 'Ingenting å vise',
+    'back_to_top' => 'Tilbake til toppen',
+    'skip_to_main_content' => 'Gå til hovudinnhald',
+    'toggle_details' => 'Vis/skjul detaljar',
+    'toggle_thumbnails' => 'Vis/skjul miniatyrbilete',
+    'details' => 'Detaljar',
+    'grid_view' => 'Rutenettvising',
+    'list_view' => 'Listevising',
     'default' => 'Standard',
-    'breadcrumb' => 'Brødsmuler',
+    'breadcrumb' => 'Brødsmular',
     'status' => 'Status',
     'status_active' => 'Aktiv',
     'status_inactive' => 'Inaktiv',
@@ -84,11 +84,11 @@ return [
     'none' => 'Ingen',
 
     // Header
-    'homepage' => 'Hjemmeside',
+    'homepage' => 'Heimeside',
     'header_menu_expand' => 'Utvid toppmeny',
     'profile_menu' => 'Profilmeny',
     'view_profile' => 'Vis profil',
-    'edit_profile' => 'Endre Profile',
+    'edit_profile' => 'Endre profil',
     'dark_mode' => 'Kveldsmodus',
     'light_mode' => 'Dagmodus',
     'global_search' => 'Globalt søk',
@@ -96,15 +96,15 @@ return [
     // Layout tabs
     'tab_info' => 'Informasjon',
     'tab_info_label' => 'Fane: Vis tilleggsinfo',
-    'tab_content' => 'Innhold',
+    'tab_content' => 'Innhald',
     'tab_content_label' => 'Fane: Vis hovedinnhold',
 
     // Email Content
-    'email_action_help' => 'Om du har problemer med å trykke på «:actionText»-knappen, bruk nettadressen under for å gå direkte dit:',
-    'email_rights' => 'Kopibeskyttet',
+    'email_action_help' => 'Om du har problem med å trykkja på ":actionText"-knappen, bruk nettadressa under for å gå direkte dit:',
+    'email_rights' => 'Kopibeskytta',
 
     // Footer Link Options
     // Not directly used but available for convenience to users.
-    'privacy_policy' => 'Personvernregler',
+    'privacy_policy' => 'Personvernreglar',
     'terms_of_service' => 'Bruksvilkår',
 ];
