0ddfb5a8ee65d3176635197a03b38b836bb2478f
Dan Brown <ssddanbrown@googlemail.com>
2023-12-22 14:57:20 +0000

    Input WYSIWYG: Updated exports to handle HTML descriptions

--- resources/views/exports/chapter.blade.php
+++ resources/views/exports/chapter.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
     <h1 style="font-size: 4.8em">{{$chapter->name}}</h1>
-    <p>{{ $chapter->description }}</p>
+    <div>{!! $chapter->descriptionHtml() !!}</div>
 
     @include('exports.parts.chapter-contents-menu', ['pages' => $pages])
 
