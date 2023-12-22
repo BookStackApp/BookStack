55c133dcb27b1a9185bae8d90f8e66c968f710d2
Dan Brown <email@danb.me>
2023-12-22 16:44:15 +0000

    New translations entities.php (Hungarian)

--- lang/hu/entities.php
+++ lang/hu/entities.php
@@ -23,7 +23,7 @@ return [
     'meta_updated' => 'Frissítve :timeLength',
     'meta_updated_name' => ':user frissítette :timeLength',
     'meta_owned_name' => ':user tulajdona',
-    'meta_reference_page_count' => 'Referenced on :count page|Referenced on :count pages',
+    'meta_reference_count' => 'Referenced by :count item|Referenced by :count items',
     'entity_select' => 'Entitás kiválasztása',
     'entity_select_lack_permission' => 'You don\'t have the required permissions to select this item',
     'images' => 'Képek',
@@ -409,7 +409,7 @@ return [
     // References
     'references' => 'References',
     'references_none' => 'There are no tracked references to this item.',
-    'references_to_desc' => 'Shown below are all the known pages in the system that link to this item.',
+    'references_to_desc' => 'Listed below is all the known content in the system that links to this item.',
 
     // Watch Options
     'watch' => 'Watch',
