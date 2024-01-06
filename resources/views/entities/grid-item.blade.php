76ff9964fb312d14357ed0261e75c75dfa531044
Tan Wei Lok <s2030275@siswa.um.edu.my>
2024-01-06 09:51:42 +0800

    Refactor grid-item.blade.php file

--- resources/views/entities/grid-item.blade.php
+++ resources/views/entities/grid-item.blade.php
@@ -1,29 +1,25 @@
-<div>
-    <a href="{{ $entity->getUrl() }}" class="grid-card" data-entity-type="{{ $entity->getType() }}"
-        data-entity-id="{{ $entity->id }}">
-        <div class="bg-{{ $entity->getType() }} featured-image-container-wrap">
-            <div class="featured-image-container" @if($entity->cover) style="background-image: url('{{
-                $entity->getBookCover() }}')"@endif>
-            </div>
-            @icon($entity->getType())
+<a href="{{ $entity->getUrl() }}" class="grid-card" data-entity-type="{{ $entity->getType() }}"
+    data-entity-id="{{ $entity->id }}">
+    <div class="bg-{{ $entity->getType() }} featured-image-container-wrap">
+        <div class="featured-image-container" @if($entity->cover) style="background-image: url('{{
+            $entity->getBookCover() }}')"@endif>
         </div>
-        <div class="grid-card-content">
-            <h2 class="text-limit-lines-2">{{ $entity->name }}</h2>
-            <p class="text-muted">{{ $entity->getExcerpt(130) }}</p>
-        </div>
-        <div class="grid-card-footer text-muted ">
-            <p>@icon('star')<span title="{{ $entity->created_at->toDayDateTimeString() }}">{{
-                    trans('entities.meta_created', ['timeLength' => $entity->created_at->diffForHumans()]) }}</span></p>
-            <p>@icon('edit')<span title="{{ $entity->updated_at->toDayDateTimeString() }}">{{
-                    trans('entities.meta_updated', ['timeLength' => $entity->updated_at->diffForHumans()]) }}</span></p>
-        </div>
-
-    </a>
-
+        @icon($entity->getType())
+    </div>
+    <div class="grid-card-content">
+        <h2 class="text-limit-lines-2">{{ $entity->name }}</h2>
+        <p class="text-muted">{{ $entity->getExcerpt(130) }}</p>
+    </div>
+    <div class="grid-card-footer text-muted ">
+        <p>@icon('star')<span title="{{ $entity->created_at->toDayDateTimeString() }}">{{
+                trans('entities.meta_created', ['timeLength' => $entity->created_at->diffForHumans()]) }}</span></p>
+        <p>@icon('edit')<span title="{{ $entity->updated_at->toDayDateTimeString() }}">{{
+                trans('entities.meta_updated', ['timeLength' => $entity->updated_at->diffForHumans()]) }}</span></p>
+    </div>
     <div>
         @if($entity->getType() === 'book' || $entity->getType() === 'bookshelf')
             @component('books.parts.context-menu', ['entity' => $entity])
             @endcomponent
         @endif
     </div>
-</div>
+</a>
