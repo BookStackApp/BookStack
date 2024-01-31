cd3c4712c4d32073f53fc54640814b2dc023843a
Dan Brown <email@danb.me>
2024-01-31 13:05:07 +0000

    New translations notifications.php (Hungarian)

--- lang/hu/notifications.php
+++ lang/hu/notifications.php
@@ -4,13 +4,13 @@
  */
 return [
 
-    'new_comment_subject' => 'New comment on page: :pageName',
-    'new_comment_intro' => 'A user has commented on a page in :appName:',
+    'new_comment_subject' => 'Új megjegyzés ezen az oldalon: :pageName',
+    'new_comment_intro' => 'Egy felhasználó hozzászólt egy oldalon itt: :appName:',
     'new_page_subject' => 'New page: :pageName',
     'new_page_intro' => 'A new page has been created in :appName:',
     'updated_page_subject' => 'Updated page: :pageName',
     'updated_page_intro' => 'A page has been updated in :appName:',
-    'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
+    'updated_page_debounce' => 'Az értesítések tömegének elkerülése érdekében egy ideig nem kap értesítést az oldal további szerkesztéseiről ugyanaz a szerkesztő.',
 
     'detail_page_name' => 'Oldal neve:',
     'detail_page_path' => 'Oldal helye:',
