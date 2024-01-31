4078937b40dd94cb9ba77ec82ed06e533321cab2
Dan Brown <email@danb.me>
2024-01-31 10:16:07 +0000

    New translations notifications.php (Hungarian)

--- lang/hu/notifications.php
+++ lang/hu/notifications.php
@@ -12,16 +12,16 @@ return [
     'updated_page_intro' => 'A page has been updated in :appName:',
     'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
 
-    'detail_page_name' => 'Page Name:',
-    'detail_page_path' => 'Page Path:',
-    'detail_commenter' => 'Commenter:',
-    'detail_comment' => 'Comment:',
-    'detail_created_by' => 'Created By:',
-    'detail_updated_by' => 'Updated By:',
+    'detail_page_name' => 'Oldal neve:',
+    'detail_page_path' => 'Oldal helye:',
+    'detail_commenter' => 'Hozzászóló:',
+    'detail_comment' => 'Megjegyzés:',
+    'detail_created_by' => 'Készítette:',
+    'detail_updated_by' => 'Frissítette:',
 
-    'action_view_comment' => 'View Comment',
-    'action_view_page' => 'View Page',
+    'action_view_comment' => 'Hozzászólás megtekintése',
+    'action_view_page' => 'Oldal megtekintése',
 
-    'footer_reason' => 'This notification was sent to you because :link cover this type of activity for this item.',
-    'footer_reason_link' => 'your notification preferences',
+    'footer_reason' => 'Ezt az értesítést azért küldtük, mert a :link lefedi ezt a tevékenységtípust ehhez az elemhez.',
+    'footer_reason_link' => 'értesítési beállításait',
 ];
