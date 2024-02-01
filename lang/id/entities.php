270bfa65c0a0ae064e8df126932925947f8e1f88
Dan Brown <email@danb.me>
2024-02-01 13:02:10 +0000

    New translations entities.php (Indonesian)

--- lang/id/entities.php
+++ lang/id/entities.php
@@ -39,6 +39,9 @@ return [
     'export_pdf' => 'Dokumen PDF',
     'export_text' => 'Dokumen Teks Biasa',
     'export_md' => 'File Markdown',
+    'default_template' => 'Default Page Template',
+    'default_template_explain' => 'Assign a page template that will be used as the default content for all pages created within this item. Keep in mind this will only be used if the page creator has view access to the chosen template page.',
+    'default_template_select' => 'Select a template page',
 
     // Permissions and restrictions
     'permissions' => 'Izin',
@@ -132,9 +135,6 @@ return [
     'books_edit_named' => 'Sunting Buku :bookName',
     'books_form_book_name' => 'Nama Buku',
     'books_save' => 'Simpan Buku',
-    'books_default_template' => 'Default Page Template',
-    'books_default_template_explain' => 'Assign a page template that will be used as the default content for all new pages in this book. Keep in mind this will only be used if the page creator has view access to those chosen template page.',
-    'books_default_template_select' => 'Select a template page',
     'books_permissions' => 'Izin Buku',
     'books_permissions_updated' => 'Izin Buku Diperbarui',
     'books_empty_contents' => 'Tidak ada halaman atau bab yang telah dibuat untuk buku ini.',
@@ -207,7 +207,7 @@ return [
     'pages_delete_draft' => 'Hapus Halaman Draf',
     'pages_delete_success' => 'Halaman dihapus',
     'pages_delete_draft_success' => 'Halaman draf dihapus',
-    'pages_delete_warning_template' => 'This page is in active use as a book default page template. These books will no longer have a default page template assigned after this page is deleted.',
+    'pages_delete_warning_template' => 'This page is in active use as a book or chapter default page template. These books or chapters will no longer have a default page template assigned after this page is deleted.',
     'pages_delete_confirm' => 'Anda yakin ingin menghapus halaman ini?',
     'pages_delete_draft_confirm' => 'Anda yakin ingin menghapus halaman draf ini?',
     'pages_editing_named' => 'Menyunting Halaman :pageName',
