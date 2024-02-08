908fed4e5721b951a5b2fa305196560341787424
Dan Brown <email@danb.me>
2024-02-08 00:17:15 +0000

    New translations notifications.php (Serbian (Cyrillic))

--- lang/sr/notifications.php
+++ lang/sr/notifications.php
@@ -4,23 +4,23 @@
  */
 return [
 
-    'new_comment_subject' => 'New comment on page: :pageName',
-    'new_comment_intro' => 'A user has commented on a page in :appName:',
-    'new_page_subject' => 'New page: :pageName',
-    'new_page_intro' => 'A new page has been created in :appName:',
-    'updated_page_subject' => 'Updated page: :pageName',
-    'updated_page_intro' => 'A page has been updated in :appName:',
+    'new_comment_subject' => 'Нови коментар на станици: :pageName',
+    'new_comment_intro' => 'Корисник је коментарисао на страници у :appName:',
+    'new_page_subject' => 'Нова страница: :pageName',
+    'new_page_intro' => 'Нова страница је креирана у :appName:',
+    'updated_page_subject' => 'Ажурирана страница: :pageName',
+    'updated_page_intro' => 'Страница је ажурирана у :appName:',
     'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
 
-    'detail_page_name' => 'Page Name:',
-    'detail_page_path' => 'Page Path:',
+    'detail_page_name' => 'Назив странице:',
+    'detail_page_path' => 'Путања странице:',
     'detail_commenter' => 'Commenter:',
-    'detail_comment' => 'Comment:',
-    'detail_created_by' => 'Created By:',
-    'detail_updated_by' => 'Updated By:',
+    'detail_comment' => 'Коментар:',
+    'detail_created_by' => 'Креирао/ла:',
+    'detail_updated_by' => 'Отпремио/ла:',
 
-    'action_view_comment' => 'View Comment',
-    'action_view_page' => 'View Page',
+    'action_view_comment' => 'Погледај коментар',
+    'action_view_page' => 'Погледај страницу',
 
     'footer_reason' => 'This notification was sent to you because :link cover this type of activity for this item.',
     'footer_reason_link' => 'your notification preferences',
