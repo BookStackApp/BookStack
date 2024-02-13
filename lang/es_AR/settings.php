d82ca533ddc8425b7a4400ba09099965e87dfba0
Dan Brown <email@danb.me>
2024-02-13 15:59:35 +0000

    New translations settings.php (Spanish, Argentina)

--- lang/es_AR/settings.php
+++ lang/es_AR/settings.php
@@ -109,7 +109,7 @@ return [
     'recycle_bin_contents_empty' => 'La papelera de reciclaje está vacía',
     'recycle_bin_empty' => 'Vaciar Papelera de reciclaje',
     'recycle_bin_empty_confirm' => 'Esto destruirá permanentemente todos los elementos de la papelera de reciclaje, incluyendo el contenido existente en cada elemento. ¿Está seguro de que desea vaciar la papelera de reciclaje?',
-    'recycle_bin_destroy_confirm' => 'Esta acción eliminará permanentemente este elemento del sistema, junto con los elementos secundarios listados a continuación, y no podrá restaurar este contenido. ¿Está seguro de que desea eliminar permanentemente este elemento?',
+    'recycle_bin_destroy_confirm' => 'This action will permanently delete this item from the system, along with any child elements listed below, and you will not be able to restore this content. Are you sure you want to permanently delete this item?',
     'recycle_bin_destroy_list' => 'Elementos a destruir',
     'recycle_bin_restore_list' => 'Elementos a restaurar',
     'recycle_bin_restore_confirm' => 'Esta acción restaurará el elemento eliminado, incluyendo cualquier elemento secundario, a su ubicación original. Si la ubicación original ha sido eliminada, y ahora está en la papelera de reciclaje, el elemento padre también tendrá que ser restaurado.',
