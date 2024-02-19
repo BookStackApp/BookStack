748d523382aa0e87082dbfc6befab4e61d9ab85e
Dan Brown <email@danb.me>
2024-02-19 20:29:08 +0000

    New translations common.php (Norwegian Nynorsk)

--- lang/nn/common.php
+++ lang/nn/common.php
@@ -20,7 +20,7 @@ return [
     'description' => 'Skildring',
     'role' => 'Rolle',
     'cover_image' => 'Framside',
-    'cover_image_description' => 'Bilete bør vere ca. 440x250px.',
+    'cover_image_description' => 'This image should be approximately 440x250px although it will be flexibly scaled & cropped to fit the user interface in different scenarios as required, so actual dimensions for display will differ.',
 
     // Actions
     'actions' => 'Handlingar',
