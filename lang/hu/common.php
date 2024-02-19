98be6d6103e172911efc5398eea45eebcbdfe004
Dan Brown <email@danb.me>
2024-02-19 20:28:47 +0000

    New translations common.php (Hungarian)

--- lang/hu/common.php
+++ lang/hu/common.php
@@ -20,7 +20,7 @@ return [
     'description' => 'Leírás',
     'role' => 'Szerepkör',
     'cover_image' => 'Borítókép',
-    'cover_image_description' => 'A kép méretének kb. 440x250px-nek kell lennie.',
+    'cover_image_description' => 'This image should be approximately 440x250px although it will be flexibly scaled & cropped to fit the user interface in different scenarios as required, so actual dimensions for display will differ.',
 
     // Actions
     'actions' => 'Műveletek',
