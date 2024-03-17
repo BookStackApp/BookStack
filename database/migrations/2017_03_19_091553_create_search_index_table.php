28d6292278b9ce2ab728e62a2b622a0beee62c1c
Dan Brown <ssddanbrown@googlemail.com>
2024-03-17 16:52:19 +0000

    Framework: Addressed deprecations

--- database/migrations/2017_03_19_091553_create_search_index_table.php
+++ database/migrations/2017_03_19_091553_create_search_index_table.php
@@ -25,9 +25,9 @@ return new class extends Migration
         });
 
         $sm = Schema::getConnection()->getDoctrineSchemaManager();
-        $pages = $sm->listTableDetails('pages');
-        $books = $sm->listTableDetails('books');
-        $chapters = $sm->listTableDetails('chapters');
+        $pages = $sm->introspectTable('pages');
+        $books = $sm->introspectTable('books');
+        $chapters = $sm->introspectTable('chapters');
 
         if ($pages->hasIndex('search')) {
             Schema::table('pages', function (Blueprint $table) {
