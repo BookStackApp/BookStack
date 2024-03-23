93a51fad48ecce6788079853684cf7c745d58d9a
Dan Brown <ssddanbrown@googlemail.com>
2024-03-23 22:04:18 +0000

    Licensing: Added links and tests for new licenses endpoint
    For #4907

--- readme.md
+++ readme.md
@@ -158,3 +158,5 @@ Note: This is not an exhaustive list of all libraries and projects that would be
 * [PHPStan](https://phpstan.org/) & [Larastan](https://github.com/nunomaduro/larastan) - _[MIT](https://github.com/phpstan/phpstan/blob/master/LICENSE) and [MIT](https://github.com/nunomaduro/larastan/blob/master/LICENSE.md)_
 * [PHP_CodeSniffer](https://github.com/squizlabs/PHP_CodeSniffer) - _[BSD 3-Clause](https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt)_
 * [JakeArchibald/IDB-Keyval](https://github.com/jakearchibald/idb-keyval) - _[Apache-2.0](https://github.com/jakearchibald/idb-keyval/blob/main/LICENCE)_
+
+For a detailed breakdown of the JavaScript & PHP projects imported & used via NPM & composer package managers, along with their licenses, please see the [dev/licensing/js-library-licenses.txt](dev/licensing/js-library-licenses.txt) and [dev/licensing/php-library-licenses.txt](dev/licensing/php-library-licenses.txt) files. 
--- resources/views/help/licenses.blade.php
+++ resources/views/help/licenses.blade.php
@@ -7,39 +7,36 @@
         <div class="my-l">&nbsp;</div>
 
         <div class="card content-wrap auto-height">
-            <h1 class="list-heading">Licenses</h1>
 
-            <p>
-                This page details license information for BookStack in addition to the projects & libraries that are used within BookStack.
-                Many projects listed may only be used in a development context.
-            </p>
+            <h1 class="list-heading">{{ trans('settings.licenses') }}</h1>
+            <p>{{ trans('settings.licenses_desc') }}</p>
 
             <ul>
-                <li><a href="#bookstack-license">BookStack License</a></li>
-                <li><a href="#php-lib-licenses">PHP Library Licenses</a></li>
-                <li><a href="#js-lib-licenses">JavaScript Library Licenses</a></li>
-                <li><a href="#js-lib-licenses">Other Licenses</a></li>
+                <li><a href="#bookstack-license">{{ trans('settings.licenses_bookstack') }}</a></li>
+                <li><a href="#php-lib-licenses">{{ trans('settings.licenses_php') }}</a></li>
+                <li><a href="#js-lib-licenses">{{ trans('settings.licenses_js') }}</a></li>
+                <li><a href="#other-licenses">{{ trans('settings.licenses_other') }}</a></li>
             </ul>
         </div>
 
         <div id="bookstack-license" class="card content-wrap auto-height">
-            <h3 class="list-heading">BookStack License</h3>
+            <h3 class="list-heading">{{ trans('settings.licenses_bookstack') }}</h3>
             <div style="white-space: pre-wrap;" class="mb-m">{{ $license }}</div>
             <p>BookStack® is a UK registered trade mark of Daniel Brown. </p>
         </div>
 
         <div id="php-lib-licenses" class="card content-wrap auto-height">
-            <h3 class="list-heading">PHP Library Licenses</h3>
+            <h3 class="list-heading">{{ trans('settings.licenses_php') }}</h3>
             <div style="white-space: pre-wrap;">{{ $phpLibData }}</div>
         </div>
 
         <div id="js-lib-licenses" class="card content-wrap auto-height">
-            <h3 class="list-heading">JavaScript Library Licenses</h3>
+            <h3 class="list-heading">{{ trans('settings.licenses_js') }}</h3>
             <div style="white-space: pre-wrap;">{{ $jsLibData }}</div>
         </div>
 
         <div id="other-licenses" class="card content-wrap auto-height">
-            <h3 class="list-heading">Other Licenses</h3>
+            <h3 class="list-heading">{{ trans('settings.licenses_other') }}</h3>
             <div style="white-space: pre-line;">BookStack makes heavy use of PHP:
                 License: PHP License, version 3.01
                 License File: https://www.php.net/license/3_01.txt
