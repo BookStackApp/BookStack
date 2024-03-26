6102523379d61892d7e45e0beea3aa70637d6ebc
Dan Brown <email@danb.me>
2024-03-26 12:17:24 +0000

    New translations entities.php (Russian)

--- lang/ru/entities.php
+++ lang/ru/entities.php
@@ -243,7 +243,7 @@ return [
     'pages_md_insert_drawing' => 'Вставить рисунок',
     'pages_md_show_preview' => 'Предпросмотр',
     'pages_md_sync_scroll' => 'Синхронизировать прокрутку',
-    'pages_drawing_unsaved' => 'Unsaved Drawing Found',
+    'pages_drawing_unsaved' => 'Найден несохраненный чертеж',
     'pages_drawing_unsaved_confirm' => 'Unsaved drawing data was found from a previous failed drawing save attempt. Would you like to restore and continue editing this unsaved drawing?',
     'pages_not_in_chapter' => 'Страница не находится в главе',
     'pages_move' => 'Переместить страницу',
