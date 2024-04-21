ec0f470560f7eba865d1e445b70f4afef3dd83c6
Dan Brown <email@danb.me>
2024-04-21 13:27:48 +0100

    New translations preferences.php (Chinese Traditional)

--- lang/zh_TW/preferences.php
+++ lang/zh_TW/preferences.php
@@ -8,7 +8,7 @@ return [
     'my_account' => '帳號設定',
 
     'shortcuts' => '快捷鍵',
-    'shortcuts_interface' => 'UI Shortcut Preferences',
+    'shortcuts_interface' => '快速鍵設定',
     'shortcuts_toggle_desc' => '您可以在此處啟用或停用鍵盤系統介面快捷鍵，這些快捷鍵用於導覽與操作。',
     'shortcuts_customize_desc' => '您可以自訂下方的每個快捷鍵。只要在選取快捷鍵輸入後按下您想要使用的按鍵組合即可。',
     'shortcuts_toggle_label' => '鍵盤快捷鍵已啟用',
@@ -19,7 +19,7 @@ return [
     'shortcuts_update_success' => '快捷鍵偏好設定已更新！',
     'shortcuts_overview_desc' => 'Manage keyboard shortcuts you can use to navigate the system user interface.',
 
-    'notifications' => 'Notification Preferences',
+    'notifications' => '通知設定',
     'notifications_desc' => 'Control the email notifications you receive when certain activity is performed within the system.',
     'notifications_opt_own_page_changes' => 'Notify upon changes to pages I own',
     'notifications_opt_own_page_comments' => 'Notify upon comments on pages I own',
@@ -32,11 +32,11 @@ return [
     'auth' => 'Access & Security',
     'auth_change_password' => '變更密碼',
     'auth_change_password_desc' => 'Change the password you use to log-in to the application. This must be at least 8 characters long.',
-    'auth_change_password_success' => 'Password has been updated!',
+    'auth_change_password_success' => '密碼已更新',
 
     'profile' => 'Profile Details',
     'profile_desc' => 'Manage the details of your account which represents you to other users, in addition to details that are used for communication and system personalisation.',
-    'profile_view_public' => 'View Public Profile',
+    'profile_view_public' => '檢視公開的個人檔案',
     'profile_name_desc' => 'Configure your display name which will be visible to other users in the system through the activity you perform, and content you own.',
     'profile_email_desc' => 'This email will be used for notifications and, depending on active system authentication, system access.',
     'profile_email_no_permission' => 'Unfortunately you don\'t have permission to change your email address. If you want to change this, you\'d need to ask an administrator to change this for you.',
@@ -44,8 +44,8 @@ return [
     'profile_admin_options' => 'Administrator Options',
     'profile_admin_options_desc' => 'Additional administrator-level options, like those to manage role assignments, can be found for your user account in the "Settings > Users" area of the application.',
 
-    'delete_account' => 'Delete Account',
-    'delete_my_account' => 'Delete My Account',
-    'delete_my_account_desc' => 'This will fully delete your user account from the system. You will not be able to recover this account or revert this action. Content you\'ve created, such as created pages and uploaded images, will remain.',
+    'delete_account' => '刪除帳號',
+    'delete_my_account' => '刪除我的帳號',
+    'delete_my_account_desc' => '將從系統中刪除所有帳號資料，此動作無法復原。你所建立的內容、頁面以及上傳的圖片將會保留。',
     'delete_my_account_warning' => '您確定要刪除您的帳戶嗎？',
 ];
