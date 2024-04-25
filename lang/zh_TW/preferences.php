c16e49c02d3ae6bce9570fff44dad8d25029a7dc
Dan Brown <email@danb.me>
2024-04-25 06:11:17 +0100

    New translations preferences.php (Chinese Traditional)

--- lang/zh_TW/preferences.php
+++ lang/zh_TW/preferences.php
@@ -20,13 +20,13 @@ return [
     'shortcuts_overview_desc' => 'Manage keyboard shortcuts you can use to navigate the system user interface.',
 
     'notifications' => '通知設定',
-    'notifications_desc' => 'Control the email notifications you receive when certain activity is performed within the system.',
-    'notifications_opt_own_page_changes' => 'Notify upon changes to pages I own',
-    'notifications_opt_own_page_comments' => 'Notify upon comments on pages I own',
-    'notifications_opt_comment_replies' => 'Notify upon replies to my comments',
+    'notifications_desc' => '控制在系統有特定活動時，是否要接收電子郵件通知',
+    'notifications_opt_own_page_changes' => '當我的頁面有異動時發送通知',
+    'notifications_opt_own_page_comments' => '當我的頁面有評論時發送通知',
+    'notifications_opt_comment_replies' => '當我的評論有新的回覆時發送通知',
     'notifications_save' => '儲存偏好設定',
     'notifications_update_success' => 'Notification preferences have been updated!',
-    'notifications_watched' => 'Watched & Ignored Items',
+    'notifications_watched' => '追蹤與忽略的項目',
     'notifications_watched_desc' => ' Below are the items that have custom watch preferences applied. To update your preferences for these, view the item then find the watch options in the sidebar.',
 
     'auth' => 'Access & Security',
