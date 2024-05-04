d35c718ef0dae870c7a2e134b30ff18a027e1612
Dan Brown <email@danb.me>
2024-05-04 15:20:05 +0100

    New translations preferences.php (Uzbek)

--- lang/uz/preferences.php
+++ lang/uz/preferences.php
@@ -27,7 +27,7 @@ return [
     'notifications_save' => 'Afzalliklarni saqlash',
     'notifications_update_success' => 'Bildirishnoma sozlamalari yangilandi!',
     'notifications_watched' => 'Koʻrilgan va e\'tiborga olinmagan narsalar',
-    'notifications_watched_desc' => 'Below are the items that have custom watch preferences applied. To update your preferences for these, view the item then find the watch options in the sidebar.',
+    'notifications_watched_desc' => 'Quyida maxsus soat sozlamalari qoʻllaniladigan elementlar mavjud. Bular uchun afzalliklaringizni yangilash uchun elementni koʻring, soʻngra yon paneldagi tomosha parametrlarini toping.',
 
     'auth' => 'Access & Security',
     'auth_change_password' => 'Change Password',
