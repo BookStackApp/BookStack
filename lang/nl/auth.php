452c8882af51f967f947c0f4601b538e1c6975a1
Dan Brown <email@danb.me>
2024-05-27 11:41:55 +0100

    New translations auth.php (Dutch)

--- lang/nl/auth.php
+++ lang/nl/auth.php
@@ -91,7 +91,7 @@ return [
     'mfa_option_totp_title' => 'Mobiele app',
     'mfa_option_totp_desc' => 'Om meervoudige verificatie te gebruiken heb je een mobiele applicatie nodig die TOTP ondersteunt, zoals Google Authenticator, Authy of Microsoft Authenticator.',
     'mfa_option_backup_codes_title' => 'Back-up Codes',
-    'mfa_option_backup_codes_desc' => 'Generates a set of one-time-use backup codes which you\'ll enter on login to verify your identity. Make sure to store these in a safe & secure place.',
+    'mfa_option_backup_codes_desc' => 'Genereert een set met eenmalige back-upcodes die je kan invoeren om je identiteit te bevestigen. Bewaar deze op een veilige plaats.',
     'mfa_gen_confirm_and_enable' => 'Bevestigen en inschakelen',
     'mfa_gen_backup_codes_title' => 'Back-up codes instellen',
     'mfa_gen_backup_codes_desc' => 'Bewaar de onderstaande lijst met codes op een veilige plaats. Bij toegang tot het systeem kun je een van de codes gebruiken als tweede verificatiemechanisme.',
