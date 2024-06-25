cc667b9ffac1d5f8af12eb79b6cf9298c3bd7e18
Dan Brown <ssddanbrown@googlemail.com>
2024-06-25 18:33:29 +0100

    Lexical: Extracted mouse drag tracking to new helper

--- resources/js/wysiwyg/ui/decorators/image.ts
+++ resources/js/wysiwyg/ui/decorators/image.ts
@@ -3,6 +3,7 @@ import {el} from "../../helpers";
 import {$createNodeSelection, $setSelection} from "lexical";
 import {EditorUiContext} from "../framework/core";
 import {ImageNode} from "../../nodes/image";
+import {MouseDragTracker, MouseDragTrackerDistance} from "../framework/helpers/mouse-drag-tracker";
 
 
 export class ImageDecorator extends EditorDecorator {
@@ -15,18 +16,11 @@ export class ImageDecorator extends EditorDecorator {
             class: 'editor-image-decorator',
         }, []);
         let selected = false;
+        let tracker: MouseDragTracker|null = null;
 
         const windowClick = (event: MouseEvent) => {
             if (!decorateEl.contains(event.target as Node) && (Date.now() - this.dragLastMouseUp > 100)) {
                 unselect();
-            }
-        };
-
-        const mouseDown = (event: MouseEvent) => {
-            const handle = (event.target as HTMLElement).closest('.editor-image-decorator-handle') as HTMLElement|null;
-            if (handle) {
-                // handlingResize = true;
-                this.startHandlingResize(handle, event, context);
             }
         };
 
@@ -44,7 +38,7 @@ export class ImageDecorator extends EditorDecorator {
                 return el('div', {class: `editor-image-decorator-handle ${c}`});
             });
             decorateEl.append(...handleElems);
-            decorateEl.addEventListener('mousedown', mouseDown);
+            tracker = this.setupTracker(decorateEl, context);
 
             context.editor.update(() => {
                 const nodeSelection = $createNodeSelection();
@@ -55,10 +49,9 @@ export class ImageDecorator extends EditorDecorator {
 
         const unselect = () => {
             selected = false;
-            // handlingResize = false;
             decorateEl.classList.remove('selected');
             window.removeEventListener('click', windowClick);
-            decorateEl.removeEventListener('mousedown', mouseDown);
+            tracker?.teardown();
             for (const el of handleElems) {
                 el.remove();
             }
@@ -80,62 +73,61 @@ export class ImageDecorator extends EditorDecorator {
         return this.dom;
     }
 
-    startHandlingResize(element: HTMLElement, event: MouseEvent, context: EditorUiContext) {
-        const startingX = event.screenX;
-        const startingY = event.screenY;
-        const node = this.getNode() as ImageNode;
-        let startingWidth = element.clientWidth;
-        let startingHeight = element.clientHeight;
-        let startingRatio = startingWidth / startingHeight;
+    setupTracker(container: HTMLElement, context: EditorUiContext): MouseDragTracker {
+        let startingWidth: number = 0;
+        let startingHeight: number = 0;
+        let startingRatio: number = 0;
         let hasHeight = false;
         let firstChange = true;
-        context.editor.getEditorState().read(() => {
-            startingWidth = node.getWidth() || startingWidth;
-            startingHeight = node.getHeight() || startingHeight;
-            if (node.getHeight()) {
-                hasHeight = true;
+        let node: ImageNode = this.getNode() as ImageNode;
+        let _this = this;
+        let flipXChange: boolean = false;
+        let flipYChange: boolean = false;
+
+        return new MouseDragTracker(container, '.editor-image-decorator-handle', {
+            down(event: MouseEvent, handle: HTMLElement) {
+                context.editor.getEditorState().read(() => {
+                    startingWidth = node.getWidth() || startingWidth;
+                    startingHeight = node.getHeight() || startingHeight;
+                    if (node.getHeight()) {
+                        hasHeight = true;
+                    }
+                    startingRatio = startingWidth / startingHeight;
+                });
+
+                flipXChange = handle.classList.contains('nw') || handle.classList.contains('sw');
+                flipYChange = handle.classList.contains('nw') || handle.classList.contains('ne');
+            },
+            move(event: MouseEvent, handle: HTMLElement, distance: MouseDragTrackerDistance) {
+                let xChange = distance.x;
+                if (flipXChange) {
+                    xChange = 0 - xChange;
+                }
+                let yChange = distance.y;
+                if (flipYChange) {
+                    yChange = 0 - yChange;
+                }
+                const balancedChange = Math.sqrt(Math.pow(Math.abs(xChange), 2) + Math.pow(Math.abs(yChange), 2));
+                const increase = xChange + yChange > 0;
+                const directedChange = increase ? balancedChange : 0-balancedChange;
+                const newWidth = Math.max(5, Math.round(startingWidth + directedChange));
+                let newHeight = 0;
+                if (hasHeight) {
+                    newHeight = newWidth * startingRatio;
+                }
+
+                const updateOptions = firstChange ? {} : {tag: 'history-merge'};
+                context.editor.update(() => {
+                    const node = _this.getNode() as ImageNode;
+                    node.setWidth(newWidth);
+                    node.setHeight(newHeight);
+                }, updateOptions);
+                firstChange = false;
+            },
+            up() {
+                _this.dragLastMouseUp = Date.now();
             }
-            startingRatio = startingWidth / startingHeight;
         });
-
-        const flipXChange = element.classList.contains('nw') || element.classList.contains('sw');
-        const flipYChange = element.classList.contains('nw') || element.classList.contains('ne');
-
-        const mouseMoveListener = (event: MouseEvent) => {
-            let xChange = event.screenX - startingX;
-            if (flipXChange) {
-                xChange = 0 - xChange;
-            }
-            let yChange = event.screenY - startingY;
-            if (flipYChange) {
-                yChange = 0 - yChange;
-            }
-            const balancedChange = Math.sqrt(Math.pow(Math.abs(xChange), 2) + Math.pow(Math.abs(yChange), 2));
-            const increase = xChange + yChange > 0;
-            const directedChange = increase ? balancedChange : 0-balancedChange;
-            const newWidth = Math.max(5, Math.round(startingWidth + directedChange));
-            let newHeight = 0;
-            if (hasHeight) {
-                newHeight = newWidth * startingRatio;
-            }
-
-            const updateOptions = firstChange ? {} : {tag: 'history-merge'};
-            context.editor.update(() => {
-                const node = this.getNode() as ImageNode;
-                node.setWidth(newWidth);
-                node.setHeight(newHeight);
-            }, updateOptions);
-            firstChange = false;
-        };
-
-        const mouseUpListener = (event: MouseEvent) => {
-            window.removeEventListener('mousemove', mouseMoveListener);
-            window.removeEventListener('mouseup', mouseUpListener);
-            this.dragLastMouseUp = Date.now();
-        };
-
-        window.addEventListener('mousemove', mouseMoveListener);
-        window.addEventListener('mouseup', mouseUpListener);
     }
 
 }
