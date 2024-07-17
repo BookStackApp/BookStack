fef7691897820b2022e72c1b75a7d99cc9f4e3f2
Dan Brown <email@danb.me>
2024-07-17 13:25:24 +0100

    New translations notifications.php (Czech)

--- lang/cs/notifications.php
+++ lang/cs/notifications.php
@@ -5,7 +5,7 @@
 return [
 
     'new_comment_subject' => 'Nový komentář na stránce: :pageName',
-    'new_comment_intro' => 'Uživatel(ka) okomentoval(a) stránku v :appName:',
+    'new_comment_intro' => 'Uživatel/ka okomentoval/a stránku v :appName:',
     'new_page_subject' => 'Nová stránka: :pageName',
     'new_page_intro' => 'V :appName byla vytvořena nová stránka:',
     'updated_page_subject' => 'Aktualizovaná stránka: :pageName',
@@ -14,10 +14,10 @@ return [
 
     'detail_page_name' => 'Název stránky:',
     'detail_page_path' => 'Umístění:',
-    'detail_commenter' => 'Komentoval(a):',
+    'detail_commenter' => 'Komentoval/a:',
     'detail_comment' => 'Komentář:',
-    'detail_created_by' => 'Vytvořil(a):',
-    'detail_updated_by' => 'Aktualizoval(a):',
+    'detail_created_by' => 'Vytvořil/a:',
+    'detail_updated_by' => 'Aktualizoval/a:',
 
     'action_view_comment' => 'Zobrazit komentář',
     'action_view_page' => 'Zobrazit stránku',
