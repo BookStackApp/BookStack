7eb2105b6531696e2f7ac4b26edc8d6a8b22b10d
Dan Brown <email@danb.me>
2024-08-22 09:46:07 +0100

    New translations preferences.php (Swedish)

--- lang/sv/preferences.php
+++ lang/sv/preferences.php
@@ -32,11 +32,11 @@ return [
     'auth' => 'Access & Security',
     'auth_change_password' => 'Change Password',
     'auth_change_password_desc' => 'Change the password you use to log-in to the application. This must be at least 8 characters long.',
-    'auth_change_password_success' => 'Password has been updated!',
+    'auth_change_password_success' => 'Lösenordet har uppdaterats!',
 
-    'profile' => 'Profile Details',
+    'profile' => 'Profildetaljer',
     'profile_desc' => 'Manage the details of your account which represents you to other users, in addition to details that are used for communication and system personalisation.',
-    'profile_view_public' => 'View Public Profile',
+    'profile_view_public' => 'Visa publik profil',
     'profile_name_desc' => 'Configure your display name which will be visible to other users in the system through the activity you perform, and content you own.',
     'profile_email_desc' => 'This email will be used for notifications and, depending on active system authentication, system access.',
     'profile_email_no_permission' => 'Unfortunately you don\'t have permission to change your email address. If you want to change this, you\'d need to ask an administrator to change this for you.',
@@ -44,8 +44,8 @@ return [
     'profile_admin_options' => 'Administrator Options',
     'profile_admin_options_desc' => 'Additional administrator-level options, like those to manage role assignments, can be found for your user account in the "Settings > Users" area of the application.',
 
-    'delete_account' => 'Delete Account',
-    'delete_my_account' => 'Delete My Account',
+    'delete_account' => 'Radera konto',
+    'delete_my_account' => 'Radera mitt konto',
     'delete_my_account_desc' => 'This will fully delete your user account from the system. You will not be able to recover this account or revert this action. Content you\'ve created, such as created pages and uploaded images, will remain.',
     'delete_my_account_warning' => 'Are you sure you want to delete your account?',
 ];
