e6642a6b9b9de3762a636592e948f29aecccd798
Dan Brown <email@danb.me>
2024-09-29 16:52:48 +0100

    New translations errors.php (Arabic)

--- lang/ar/errors.php
+++ lang/ar/errors.php
@@ -78,6 +78,7 @@ return [
     // Users
     'users_cannot_delete_only_admin' => 'لا يمكن حذف المشرف الوحيد',
     'users_cannot_delete_guest' => 'لا يمكن حذف المستخدم الضيف',
+    'users_could_not_send_invite' => 'Could not create user since invite email failed to send',
 
     // Roles
     'role_cannot_be_edited' => 'لا يمكن تعديل هذا الدور',
