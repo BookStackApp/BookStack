658bb26d1c8dcef0239586241161533c6e18c853
Dan Brown <email@danb.me>
2024-09-29 16:52:44 +0100

    New translations errors.php (Welsh)

--- lang/cy/errors.php
+++ lang/cy/errors.php
@@ -78,6 +78,7 @@ return [
     // Users
     'users_cannot_delete_only_admin' => 'Ni allwch ddileu\'r unig weinyddwr',
     'users_cannot_delete_guest' => 'Ni allwch ddileu\'r defnyddiwr gwadd',
+    'users_could_not_send_invite' => 'Could not create user since invite email failed to send',
 
     // Roles
     'role_cannot_be_edited' => 'Nid oes modd golygu\'r rôl hon',
