7db1d72f4aa8363aa9688d9910a7daefbd1b334c
Dan Brown <email@danb.me>
2024-09-29 16:52:54 +0100

    New translations errors.php (Greek)

--- lang/el/errors.php
+++ lang/el/errors.php
@@ -78,6 +78,7 @@ return [
     // Users
     'users_cannot_delete_only_admin' => 'Δεν μπορείτε να διαγράψετε τον μοναδικό διαχειριστή',
     'users_cannot_delete_guest' => 'Δεν μπορείτε να διαγράψετε τον επισκέπτη',
+    'users_could_not_send_invite' => 'Could not create user since invite email failed to send',
 
     // Roles
     'role_cannot_be_edited' => 'Αυτός ο ρόλος δεν μπορεί να επεξεργαστεί',
