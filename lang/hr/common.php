29862b5323877fe53061948a75dc0f5c67795f59
Dan Brown <email@danb.me>
2024-09-30 18:12:12 +0100

    New translations common.php (Croatian)

--- lang/hr/common.php
+++ lang/hr/common.php
@@ -107,4 +107,7 @@ return [
     // Not directly used but available for convenience to users.
     'privacy_policy' => 'Politika privatnosti',
     'terms_of_service' => 'Uvjeti korištenja',
+
+    // OpenSearch
+    'opensearch_description' => 'Search :appName',
 ];
