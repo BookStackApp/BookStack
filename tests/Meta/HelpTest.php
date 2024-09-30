514db60617780de889645513ad613d8ff0ba3108
Dan Brown <ssddanbrown@googlemail.com>
2024-09-30 17:07:53 +0100

    Tests: Categorised up meta tests
    Extracted robots.txt tests into its own file to fit into new folder.
    Also tweaked open search tests a tad to specifically check long app
    names.

--- tests/Meta/HelpTest.php
+++ tests/Meta/HelpTest.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Tests;
+namespace Tests\Meta;
+
+use Tests\TestCase;
 
 class HelpTest extends TestCase
 {
--- tests/Meta/OpensearchTest.php
+++ tests/Meta/OpensearchTest.php
@@ -1,12 +1,15 @@
 <?php
 
-namespace Tests;
+namespace Tests\Meta;
+
+use Tests\TestCase;
 
 class OpensearchTest extends TestCase
 {
     public function test_opensearch_endpoint()
     {
-        $appName = setting('app-name');
+        $appName = 'MyAppNameThatsReallyLongLikeThis';
+        setting()->put('app-name', $appName);
         $resultUrl = url('/search') . '?term={searchTerms}';
         $selfUrl = url('/opensearch.xml');
 
@@ -17,14 +20,11 @@ class OpensearchTest extends TestCase
 
         $html->assertElementExists('OpenSearchDescription > ShortName');
         $html->assertElementContains('OpenSearchDescription > ShortName', mb_strimwidth($appName, 0, 16));
+        $html->assertElementNotContains('OpenSearchDescription > ShortName', $appName);
 
         $html->assertElementExists('OpenSearchDescription > Description');
-        $html->assertElementContains('OpenSearchDescription > Description', trans('common.opensearch_description', [
-            'appName' => $appName,
-        ]));
-
+        $html->assertElementContains('OpenSearchDescription > Description', "Search {$appName}");
         $html->assertElementExists('OpenSearchDescription > Image');
-
         $html->assertElementExists('OpenSearchDescription > Url[rel="results"][template="' . htmlspecialchars($resultUrl) . '"]');
         $html->assertElementExists('OpenSearchDescription > Url[rel="self"][template="' . htmlspecialchars($selfUrl) . '"]');
     }
