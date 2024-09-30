ab84536c765b5516d8d6ce87e61357c7e3da45f5
Dan Brown <email@danb.me>
2024-09-30 18:11:41 +0100

    New translations common.php (Danish)

--- lang/da/common.php
+++ lang/da/common.php
@@ -107,4 +107,7 @@ return [
     // Not directly used but available for convenience to users.
     'privacy_policy' => 'Privatlivspolitik',
     'terms_of_service' => 'Tjenestevilkår',
+
+    // OpenSearch
+    'opensearch_description' => 'Search :appName',
 ];
