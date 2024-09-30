5655c93ea1b037ac2a67ac9b5571e6c74b8bf500
Dan Brown <email@danb.me>
2024-09-30 18:12:14 +0100

    New translations common.php (Estonian)

--- lang/et/common.php
+++ lang/et/common.php
@@ -107,4 +107,7 @@ return [
     // Not directly used but available for convenience to users.
     'privacy_policy' => 'Privaatsus',
     'terms_of_service' => 'Kasutustingimused',
+
+    // OpenSearch
+    'opensearch_description' => 'Search :appName',
 ];
