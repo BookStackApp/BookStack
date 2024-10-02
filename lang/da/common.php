54e9fdaeba9e98a7cafb0cfbb44aaa9aca590356
Dan Brown <email@danb.me>
2024-10-02 19:22:00 +0100

    New translations common.php (Danish)

--- lang/da/common.php
+++ lang/da/common.php
@@ -109,5 +109,5 @@ return [
     'terms_of_service' => 'Tjenestevilkår',
 
     // OpenSearch
-    'opensearch_description' => 'Search :appName',
+    'opensearch_description' => 'Søg :appName',
 ];
