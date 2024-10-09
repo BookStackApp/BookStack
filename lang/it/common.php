6a864cbd50dc55b3000502391924c54995943cd1
Dan Brown <email@danb.me>
2024-10-09 17:52:58 +0100

    New translations common.php (Italian)

--- lang/it/common.php
+++ lang/it/common.php
@@ -109,5 +109,5 @@ return [
     'terms_of_service' => 'Condizioni del Servizio',
 
     // OpenSearch
-    'opensearch_description' => 'Search :appName',
+    'opensearch_description' => 'Cerca :appName',
 ];
