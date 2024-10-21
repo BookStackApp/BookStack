64001c0b047c39dc9fadb2f73cbae8f9c5ba3cac
Dan Brown <ssddanbrown@googlemail.com>
2024-10-21 13:59:15 +0100

    ZIP Exports: Added working image handling/inclusion

--- app/Exports/ZipExportFiles.php
+++ app/Exports/ZipExportFiles.php
@@ -4,6 +4,8 @@ namespace BookStack\Exports;
 
 use BookStack\Uploads\Attachment;
 use BookStack\Uploads\AttachmentService;
+use BookStack\Uploads\Image;
+use BookStack\Uploads\ImageService;
 use Illuminate\Support\Str;
 
 class ZipExportFiles
@@ -14,8 +16,15 @@ class ZipExportFiles
      */
     protected array $attachmentRefsById = [];
 
+    /**
+     * References for images by image ID.
+     * @var array<int, string>
+     */
+    protected array $imageRefsById = [];
+
     public function __construct(
         protected AttachmentService $attachmentService,
+        protected ImageService $imageService,
     ) {
     }
 
@@ -30,13 +39,44 @@ class ZipExportFiles
             return $this->attachmentRefsById[$attachment->id];
         }
 
+        $existingFiles = $this->getAllFileNames();
         do {
             $fileName = Str::random(20) . '.' . $attachment->extension;
-        } while (in_array($fileName, $this->attachmentRefsById));
+        } while (in_array($fileName, $existingFiles));
 
         $this->attachmentRefsById[$attachment->id] = $fileName;
 
         return $fileName;
+    }
+
+    /**
+     * Gain a reference to the given image instance.
+     * This is expected to be an image that the user has visibility of,
+     * no permission/access checks are performed here.
+     */
+    public function referenceForImage(Image $image): string
+    {
+        if (isset($this->imageRefsById[$image->id])) {
+            return $this->imageRefsById[$image->id];
+        }
+
+        $existingFiles = $this->getAllFileNames();
+        $extension = pathinfo($image->path, PATHINFO_EXTENSION);
+        do {
+            $fileName = Str::random(20) . '.' . $extension;
+        } while (in_array($fileName, $existingFiles));
+
+        $this->imageRefsById[$image->id] = $fileName;
+
+        return $fileName;
+    }
+
+    protected function getAllFileNames(): array
+    {
+        return array_merge(
+            array_values($this->attachmentRefsById),
+            array_values($this->imageRefsById),
+        );
     }
 
     /**
@@ -50,6 +90,15 @@ class ZipExportFiles
             $attachment = Attachment::query()->find($attachmentId);
             $stream = $this->attachmentService->streamAttachmentFromStorage($attachment);
             $tmpFile = tempnam(sys_get_temp_dir(), 'bszipfile-');
+            $tmpFileStream = fopen($tmpFile, 'w');
+            stream_copy_to_stream($stream, $tmpFileStream);
+            $callback($tmpFile, $ref);
+        }
+
+        foreach ($this->imageRefsById as $imageId => $ref) {
+            $image = Image::query()->find($imageId);
+            $stream = $this->imageService->getImageStream($image);
+            $tmpFile = tempnam(sys_get_temp_dir(), 'bszipimage-');
             $tmpFileStream = fopen($tmpFile, 'w');
             stream_copy_to_stream($stream, $tmpFileStream);
             $callback($tmpFile, $ref);
--- app/Exports/ZipExportReferences.php
+++ app/Exports/ZipExportReferences.php
@@ -3,8 +3,13 @@
 namespace BookStack\Exports;
 
 use BookStack\App\Model;
+use BookStack\Entities\Models\Page;
 use BookStack\Exports\ZipExportModels\ZipExportAttachment;
+use BookStack\Exports\ZipExportModels\ZipExportImage;
+use BookStack\Exports\ZipExportModels\ZipExportModel;
 use BookStack\Exports\ZipExportModels\ZipExportPage;
+use BookStack\Uploads\Attachment;
+use BookStack\Uploads\Image;
 
 class ZipExportReferences
 {
@@ -15,6 +20,9 @@ class ZipExportReferences
 
     /** @var ZipExportAttachment[] */
     protected array $attachments = [];
+
+    /** @var ZipExportImage[] */
+    protected array $images = [];
 
     public function __construct(
         protected ZipReferenceParser $parser,
@@ -34,19 +42,12 @@ class ZipExportReferences
         }
     }
 
-    public function buildReferences(): void
+    public function buildReferences(ZipExportFiles $files): void
     {
-        // TODO - References to images, attachments, other entities
-
         // TODO - Parse page MD & HTML
         foreach ($this->pages as $page) {
-            $page->html = $this->parser->parse($page->html ?? '', function (Model $model): ?string {
-                // TODO - Handle found link to $model
-                //   - Validate we can see/access $model, or/and that it's
-                //     part of the export in progress.
-
-                // TODO - Add images after the above to files
-                return '[CAT]';
+            $page->html = $this->parser->parse($page->html ?? '', function (Model $model) use ($files, $page) {
+                return $this->handleModelReference($model, $page, $files);
             });
             // TODO - markdown
         }
@@ -54,5 +55,46 @@ class ZipExportReferences
 //        dd('end');
         // TODO - Parse chapter desc html
         // TODO - Parse book desc html
+    }
+
+    protected function handleModelReference(Model $model, ZipExportModel $exportModel, ZipExportFiles $files): ?string
+    {
+        // TODO - References to other entities
+
+        // Handle attachment references
+        // No permission check needed here since they would only already exist in this
+        // reference context if already allowed via their entity access.
+        if ($model instanceof Attachment) {
+            if (isset($this->attachments[$model->id])) {
+                return "[[bsexport:attachment:{$model->id}]]";
+            }
+            return null;
+        }
+
+        // Handle image references
+        if ($model instanceof Image) {
+            // Only handle gallery and drawio images
+            if ($model->type !== 'gallery' && $model->type !== 'drawio') {
+                return null;
+            }
+
+            // We don't expect images to be part of book/chapter content
+            if (!($exportModel instanceof ZipExportPage)) {
+                return null;
+            }
+
+            $page = $model->getPage();
+            if ($page && userCan('view', $page)) {
+                if (!isset($this->images[$model->id])) {
+                    $exportImage = ZipExportImage::fromModel($model, $files);
+                    $this->images[$model->id] = $exportImage;
+                    $exportModel->images[] = $exportImage;
+                }
+                return "[[bsexport:image:{$model->id}]]";
+            }
+            return null;
+        }
+
+        return null;
     }
 }
