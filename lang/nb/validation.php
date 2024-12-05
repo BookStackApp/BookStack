7d684620089ac0b3389f23e252a8a053bce72020
Dan Brown <email@danb.me>
2024-12-05 14:07:41 +0000

    New translations validation.php (Norwegian Bokmal)

--- lang/nb/validation.php
+++ lang/nb/validation.php
@@ -105,10 +105,10 @@ return [
     'url'                  => ':attribute format er ugyldig.',
     'uploaded'             => 'kunne ikke lastes opp, tjeneren støtter ikke filer av denne størrelsen.',
 
-    'zip_file' => 'The :attribute needs to reference a file within the ZIP.',
-    'zip_file_mime' => 'The :attribute needs to reference a file of type :validTypes, found :foundType.',
-    'zip_model_expected' => 'Data object expected but ":type" found.',
-    'zip_unique' => 'The :attribute must be unique for the object type within the ZIP.',
+    'zip_file' => 'Attributtet :attribute må henvises til en fil i ZIP.',
+    'zip_file_mime' => 'Attributtet :attribute må referere en fil av typen :validTypes, som ble funnet :foundType.',
+    'zip_model_expected' => 'Data objekt forventet, men ":type" funnet.',
+    'zip_unique' => 'Attributtet :attribute må være unikt for objekttypen i ZIP.',
 
     // Custom validation lines
     'custom' => [
