5d89cb9f6082f901bfa10cd299664819a90af907
Dan Brown <email@danb.me>
2024-12-07 10:14:30 +0000

    New translations notifications.php (Greek)

--- lang/el/notifications.php
+++ lang/el/notifications.php
@@ -4,24 +4,24 @@
  */
 return [
 
-    'new_comment_subject' => 'New comment on page: :pageName',
-    'new_comment_intro' => 'A user has commented on a page in :appName:',
-    'new_page_subject' => 'New page: :pageName',
-    'new_page_intro' => 'A new page has been created in :appName:',
-    'updated_page_subject' => 'Updated page: :pageName',
-    'updated_page_intro' => 'A page has been updated in :appName:',
-    'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
+    'new_comment_subject' => 'Νέο σχόλιο στη σελίδα: :pageName',
+    'new_comment_intro' => 'Ένας χρήστης έχει σχολιάσει σε μια σελίδα στο :appName:',
+    'new_page_subject' => 'Νέα σελίδα: :pageName',
+    'new_page_intro' => 'Μια νέα σελίδα έχει δημιουργηθεί στο :appName:',
+    'updated_page_subject' => 'Ενημερωμένη σελίδα: :pageName',
+    'updated_page_intro' => 'Μια σελίδα έχει ενημερωθεί στο :appName:',
+    'updated_page_debounce' => 'Για να αποτρέψετε μαζικές ειδοποιήσεις, για κάποιο διάστημα δε θα σας αποστέλλονται ειδοποιήσεις για περαιτέρω αλλαγές σε αυτήν τη σελίδα από τον ίδιο συντάκτη.',
 
-    'detail_page_name' => 'Page Name:',
-    'detail_page_path' => 'Page Path:',
-    'detail_commenter' => 'Commenter:',
-    'detail_comment' => 'Comment:',
-    'detail_created_by' => 'Created By:',
-    'detail_updated_by' => 'Updated By:',
+    'detail_page_name' => 'Όνομα σελίδας:',
+    'detail_page_path' => 'Διαδρομή σελίδας:',
+    'detail_commenter' => 'Σχολιαστής:',
+    'detail_comment' => 'Σχόλιο:',
+    'detail_created_by' => 'Δημιουργήθηκε από:',
+    'detail_updated_by' => 'Ενημερώθηκε από:',
 
-    'action_view_comment' => 'View Comment',
-    'action_view_page' => 'View Page',
+    'action_view_comment' => 'Προβολή σχολίου',
+    'action_view_page' => 'Προβολή σελίδας',
 
-    'footer_reason' => 'This notification was sent to you because :link cover this type of activity for this item.',
-    'footer_reason_link' => 'your notification preferences',
+    'footer_reason' => 'Αυτή η ειδοποίηση εστάλη σε εσάς επειδή το :link καλύπτει αυτόν τον τύπο δραστηριότητας για αυτό το αντικείμενο.',
+    'footer_reason_link' => 'προτιμήσεις σας για ειδοποιήσεις',
 ];
