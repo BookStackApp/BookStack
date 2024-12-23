221e2e38ee5b86105aa189861b0fd5d3ee1f66aa
Dan Brown <email@danb.me>
2024-12-23 15:10:50 +0000

    New translations errors.php (Italian)

--- lang/it/errors.php
+++ lang/it/errors.php
@@ -106,16 +106,16 @@ return [
     'back_soon' => 'Tornerà presto online.',
 
     // Import
-    'import_zip_cant_read' => 'Could not read ZIP file.',
-    'import_zip_cant_decode_data' => 'Could not find and decode ZIP data.json content.',
-    'import_zip_no_data' => 'ZIP file data has no expected book, chapter or page content.',
-    'import_validation_failed' => 'Import ZIP failed to validate with errors:',
-    'import_zip_failed_notification' => 'Failed to import ZIP file.',
-    'import_perms_books' => 'You are lacking the required permissions to create books.',
-    'import_perms_chapters' => 'You are lacking the required permissions to create chapters.',
-    'import_perms_pages' => 'You are lacking the required permissions to create pages.',
-    'import_perms_images' => 'You are lacking the required permissions to create images.',
-    'import_perms_attachments' => 'You are lacking the required permission to create attachments.',
+    'import_zip_cant_read' => 'Impossibile leggere il file ZIP.',
+    'import_zip_cant_decode_data' => 'Impossibile trovare e decodificare il contenuto ZIP data.json.',
+    'import_zip_no_data' => 'I dati del file ZIP non hanno il contenuto previsto di libri, capitoli o pagine.',
+    'import_validation_failed' => 'L\'importazione ZIP non è stata convalidata con errori:',
+    'import_zip_failed_notification' => 'Impossibile importare il file ZIP.',
+    'import_perms_books' => 'Non hai i permessi necessari per creare libri.',
+    'import_perms_chapters' => 'Non hai i permessi necessari per creare capitoli.',
+    'import_perms_pages' => 'Non hai i permessi necessari per creare pagine.',
+    'import_perms_images' => 'Non hai i permessi necessari per creare immagini.',
+    'import_perms_attachments' => 'Non hai il permesso necessario per creare allegati.',
 
     // API errors
     'api_no_authorization_found' => 'Nessun token di autorizzazione trovato nella richiesta',
