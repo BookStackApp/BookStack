aec69cd945a4e2e0e06c43888bf45f1726a4f706
Dan Brown <email@danb.me>
2024-12-23 11:16:44 +0000

    New translations errors.php (Spanish, Argentina)

--- lang/es_AR/errors.php
+++ lang/es_AR/errors.php
@@ -106,16 +106,16 @@ return [
     'back_soon' => 'Volverá a estar operativa en corto tiempo.',
 
     // Import
-    'import_zip_cant_read' => 'Could not read ZIP file.',
-    'import_zip_cant_decode_data' => 'Could not find and decode ZIP data.json content.',
-    'import_zip_no_data' => 'ZIP file data has no expected book, chapter or page content.',
-    'import_validation_failed' => 'Import ZIP failed to validate with errors:',
-    'import_zip_failed_notification' => 'Failed to import ZIP file.',
-    'import_perms_books' => 'You are lacking the required permissions to create books.',
-    'import_perms_chapters' => 'You are lacking the required permissions to create chapters.',
-    'import_perms_pages' => 'You are lacking the required permissions to create pages.',
-    'import_perms_images' => 'You are lacking the required permissions to create images.',
-    'import_perms_attachments' => 'You are lacking the required permission to create attachments.',
+    'import_zip_cant_read' => 'No se pudo leer el archivo ZIP.',
+    'import_zip_cant_decode_data' => 'No se pudo encontrar y decodificar el archivo data.json. en el archivo ZIP.',
+    'import_zip_no_data' => 'Los datos del archivo ZIP no contienen ningún libro, capítulo o contenido de página.',
+    'import_validation_failed' => 'Error al validar la importación del ZIP con errores:',
+    'import_zip_failed_notification' => 'Error al importar archivo ZIP.',
+    'import_perms_books' => 'Le faltan los permisos necesarios para crear libros.',
+    'import_perms_chapters' => 'Le faltan los permisos necesarios para crear capítulos.',
+    'import_perms_pages' => 'Le faltan los permisos necesarios para crear páginas.',
+    'import_perms_images' => 'Le faltan los permisos necesarios para crear imágenes.',
+    'import_perms_attachments' => 'Le faltan los permisos necesarios para crear adjuntos.',
 
     // API errors
     'api_no_authorization_found' => 'No se encontró ningún token de autorización en la solicitud',
