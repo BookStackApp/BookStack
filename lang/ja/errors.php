b3e30375cb1c51c2bb4468c6a6b7febc94d3ae49
Dan Brown <email@danb.me>
2024-12-23 16:46:40 +0000

    New translations errors.php (Japanese)

--- lang/ja/errors.php
+++ lang/ja/errors.php
@@ -106,16 +106,16 @@ return [
     'back_soon' => '回復までしばらくお待ちください。',
 
     // Import
-    'import_zip_cant_read' => 'Could not read ZIP file.',
-    'import_zip_cant_decode_data' => 'Could not find and decode ZIP data.json content.',
-    'import_zip_no_data' => 'ZIP file data has no expected book, chapter or page content.',
-    'import_validation_failed' => 'Import ZIP failed to validate with errors:',
-    'import_zip_failed_notification' => 'Failed to import ZIP file.',
-    'import_perms_books' => 'You are lacking the required permissions to create books.',
-    'import_perms_chapters' => 'You are lacking the required permissions to create chapters.',
-    'import_perms_pages' => 'You are lacking the required permissions to create pages.',
-    'import_perms_images' => 'You are lacking the required permissions to create images.',
-    'import_perms_attachments' => 'You are lacking the required permission to create attachments.',
+    'import_zip_cant_read' => 'ZIPファイルを読み込めません。',
+    'import_zip_cant_decode_data' => 'ZIPファイル内に data.json が見つからないかデコードできませんでした。',
+    'import_zip_no_data' => 'ZIPファイルのデータにブック、チャプター、またはページコンテンツがありません。',
+    'import_validation_failed' => 'エラーによりインポートZIPの検証に失敗しました:',
+    'import_zip_failed_notification' => 'ZIP ファイルのインポートに失敗しました。',
+    'import_perms_books' => 'ブックを作成するために必要な権限がありません。',
+    'import_perms_chapters' => 'チャプタを作成するために必要な権限がありません。',
+    'import_perms_pages' => 'ページを作成するために必要な権限がありません。',
+    'import_perms_images' => '画像を作成するために必要な権限がありません。',
+    'import_perms_attachments' => '添付ファイルを作成するために必要な権限がありません。',
 
     // API errors
     'api_no_authorization_found' => 'リクエストに認証トークンが見つかりません',
