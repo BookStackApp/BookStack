8cb411cdf876c0d27a87c09af0fba9c6c9078731
Dan Brown <email@danb.me>
2024-12-31 15:38:32 +0000

    New translations validation.php (Portuguese, Brazilian)

--- lang/pt_BR/validation.php
+++ lang/pt_BR/validation.php
@@ -105,10 +105,10 @@ return [
     'url'                  => 'O formato da URL :attribute é inválido.',
     'uploaded'             => 'O arquivo não pôde ser carregado. O servidor pode não aceitar arquivos deste tamanho.',
 
-    'zip_file' => 'The :attribute needs to reference a file within the ZIP.',
-    'zip_file_mime' => 'The :attribute needs to reference a file of type :validTypes, found :foundType.',
-    'zip_model_expected' => 'Data object expected but ":type" found.',
-    'zip_unique' => 'The :attribute must be unique for the object type within the ZIP.',
+    'zip_file' => 'O :attribute precisa fazer referência a um arquivo do ZIP.',
+    'zip_file_mime' => 'O :attribute precisa fazer referência a um arquivo do tipo :validTypes, encontrado :foundType.',
+    'zip_model_expected' => 'Objeto de dados esperado, mas ":type" encontrado.',
+    'zip_unique' => 'O :attribute deve ser único para o tipo de objeto dentro do ZIP.',
 
     // Custom validation lines
     'custom' => [
