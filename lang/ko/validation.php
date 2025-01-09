6452e6707343bb61a05c84fa536c1aac39f2e897
Dan Brown <email@danb.me>
2025-01-09 14:37:59 +0000

    New translations validation.php (Korean)

--- lang/ko/validation.php
+++ lang/ko/validation.php
@@ -105,10 +105,10 @@ return [
     'url'                  => ':attribute(은)는 유효하지 않은 형식입니다.',
     'uploaded'             => '파일 크기가 서버에서 허용하는 수치를 넘습니다.',
 
-    'zip_file' => ':attribute은(는) ZIP 파일 내의 파일을 참조해야 합니다.',
+    'zip_file' => ':attribute은(는) 컨텐츠 ZIP 파일 내의 객체 유형에 대해 고유해야 합니다.',
     'zip_file_mime' => ':attribute은(는)  :validTypes, found :foundType 유형의 파일을 참조해야 합니다.',
     'zip_model_expected' => '데이터 객체가 필요하지만 ":type" 타입이 발견되었습니다.',
-    'zip_unique' => ':attribute은(는)  ZIP 파일 내의 객체 유형에 대해 고유해야 합니다.',
+    'zip_unique' => ':attribute은(는) 컨텐츠 ZIP 파일 내의 객체 유형에 대해 고유해야 합니다.',
 
     // Custom validation lines
     'custom' => [
