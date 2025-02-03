690d534b1b2863e986d189446cfd03da34597d2a
Silverlan <silverlann@gmail.com>
2025-02-03 19:01:08 +0100

    Fix incorrect condition for displaying new books section

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -23,7 +23,7 @@
 
     <div id="new" class="mb-xl">
         <h5>{{ trans('entities.books_new') }}</h5>
-        @if(count($popular) > 0)
+        @if(count($new) > 0)
             @include('entities.list', ['entities' => $new, 'style' => 'compact'])
         @else
             <p class="text-muted pb-l mb-none">{{ trans('entities.books_new_empty') }}</p>
