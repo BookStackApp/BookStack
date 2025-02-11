f55c466bc4e1e3224be330771700736250b2de5b
Dan Brown <email@danb.me>
2025-02-11 16:19:17 +0000

    New translations activities.php (Czech)

--- lang/cs/activities.php
+++ lang/cs/activities.php
@@ -127,6 +127,14 @@ return [
     'comment_update'              => 'aktualizoval komentář',
     'comment_delete'              => 'odstranil komentář',
 
+    // Sort Rules
+    'sort_rule_create' => 'created sort rule',
+    'sort_rule_create_notification' => 'Sort rule successfully created',
+    'sort_rule_update' => 'updated sort rule',
+    'sort_rule_update_notification' => 'Sort rule successfully update',
+    'sort_rule_delete' => 'deleted sort rule',
+    'sort_rule_delete_notification' => 'Sort rule successfully deleted',
+
     // Other
     'permissions_update'          => 'oprávnění upravena',
 ];
