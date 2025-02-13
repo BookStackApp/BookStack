38b90ccb48c962f4c8c4edad1abdd41678b8d90e
Dan Brown <email@danb.me>
2025-02-13 07:22:55 +0000

    New translations validation.php (Estonian)

--- lang/et/validation.php
+++ lang/et/validation.php
@@ -105,10 +105,10 @@ return [
     'url'                  => ':attribute on vigases formaadis.',
     'uploaded'             => 'Faili üleslaadimine ebaõnnestus. Server ei pruugi sellise suurusega faile vastu võtta.',
 
-    'zip_file' => 'The :attribute needs to reference a file within the ZIP.',
-    'zip_file_mime' => 'The :attribute needs to reference a file of type :validTypes, found :foundType.',
-    'zip_model_expected' => 'Data object expected but ":type" found.',
-    'zip_unique' => 'The :attribute must be unique for the object type within the ZIP.',
+    'zip_file' => ':attribute peab viitama failile ZIP-arhiivi sees.',
+    'zip_file_mime' => ':attribute peab viitama :validTypes tüüpi failile, leiti :foundType.',
+    'zip_model_expected' => 'Oodatud andmete asemel leiti ":type".',
+    'zip_unique' => ':attribute peab olema ZIP-arhiivi piires objekti tüübile unikaalne.',
 
     // Custom validation lines
     'custom' => [
