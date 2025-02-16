b33b16b36b0e363f085ac349dc0d0efc7094e0c6
Dan Brown <email@danb.me>
2025-02-16 11:34:22 +0000

    New translations settings.php (Dutch)

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -75,10 +75,10 @@ return [
     'reg_confirm_restrict_domain_placeholder' => 'Geen beperkingen ingesteld',
 
     // Sorting Settings
-    'sorting' => 'Sorting',
-    'sorting_book_default' => 'Default Book Sort',
-    'sorting_book_default_desc' => 'Select the default sort role to apply to new books. This won\'t affect existing books, and can be overridden per-book.',
-    'sorting_rules' => 'Sort Rules',
+    'sorting' => 'Sorteren',
+    'sorting_book_default' => 'Standaard Sorteerregel Boek',
+    'sorting_book_default_desc' => 'Selecteer de standaard sorteerregel om toe te passen op nieuwe boeken. Dit heeft geen invloed op bestaande boeken, en kan per boek worden overschreven.',
+    'sorting_rules' => 'Sorteerregels',
     'sorting_rules_desc' => 'These are predefined sorting operations which can be applied to content in the system.',
     'sort_rule_assigned_to_x_books' => 'Assigned to :count Book|Assigned to :count Books',
     'sort_rule_create' => 'Create Sort Rule',
