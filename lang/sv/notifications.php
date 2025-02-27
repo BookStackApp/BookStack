37f076b10145180327d8de4bf9a6adf7de1fff18
Dan Brown <email@danb.me>
2025-02-27 00:55:03 +0000

    New translations notifications.php (Swedish)

--- lang/sv/notifications.php
+++ lang/sv/notifications.php
@@ -4,24 +4,24 @@
  */
 return [
 
-    'new_comment_subject' => 'New comment on page: :pageName',
-    'new_comment_intro' => 'A user has commented on a page in :appName:',
-    'new_page_subject' => 'New page: :pageName',
-    'new_page_intro' => 'A new page has been created in :appName:',
-    'updated_page_subject' => 'Updated page: :pageName',
-    'updated_page_intro' => 'A page has been updated in :appName:',
-    'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
+    'new_comment_subject' => 'Ny kommentar på sidan: :pageName',
+    'new_comment_intro' => 'En användare har kommenterat en sida i :appName:',
+    'new_page_subject' => 'Ny sida: :pageName',
+    'new_page_intro' => 'En ny sida har blivit skapad i :appName:',
+    'updated_page_subject' => 'Uppdaterad sida: :pageName',
+    'updated_page_intro' => 'En sida har blivit uppdaterad i :appName:',
+    'updated_page_debounce' => 'För att förhindra en massa notiser, så kommer det inte skickas nya notiser på ett tag för ytterligare ändringar till denna sida av samma skribent.',
 
-    'detail_page_name' => 'Page Name:',
-    'detail_page_path' => 'Page Path:',
-    'detail_commenter' => 'Commenter:',
-    'detail_comment' => 'Comment:',
-    'detail_created_by' => 'Created By:',
-    'detail_updated_by' => 'Updated By:',
+    'detail_page_name' => 'Sidonamn:',
+    'detail_page_path' => 'Sidosökväg:',
+    'detail_commenter' => 'Kommentars-skapare:',
+    'detail_comment' => 'Kommentar:',
+    'detail_created_by' => 'Skapad av:',
+    'detail_updated_by' => 'Uppdaterad av:',
 
     'action_view_comment' => 'Visa kommentar',
-    'action_view_page' => 'View Page',
+    'action_view_page' => 'Visa sida',
 
-    'footer_reason' => 'This notification was sent to you because :link cover this type of activity for this item.',
-    'footer_reason_link' => 'your notification preferences',
+    'footer_reason' => 'Detta meddelande skickades till dig eftersom :link täcker denna typ av aktivitet för detta objekt.',
+    'footer_reason_link' => 'dina notifikationspreferenser',
 ];
