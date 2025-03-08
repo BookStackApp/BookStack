4278a5adc643de3a878e73b15ee901c73a32af71
Dan Brown <email@danb.me>
2025-03-08 17:50:58 +0000

    New translations common.php (Arabic)

--- lang/ar/common.php
+++ lang/ar/common.php
@@ -109,5 +109,5 @@ return [
     'terms_of_service' => 'اتفاقية شروط الخدمة',
 
     // OpenSearch
-    'opensearch_description' => 'Search :appName',
+    'opensearch_description' => 'البحث عن :appName',
 ];
