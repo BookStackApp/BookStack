4c8507a3a5154b44dcba3c6353dd5219051f868d
Dan Brown <email@danb.me>
2025-03-08 19:03:09 +0000

    New translations components.php (Arabic)

--- lang/ar/components.php
+++ lang/ar/components.php
@@ -9,7 +9,7 @@ return [
     'image_list' => 'Image List',
     'image_details' => 'Image Details',
     'image_upload' => 'Upload Image',
-    'image_intro' => 'Here you can select and manage images that have been previously uploaded to the system.',
+    'image_intro' => 'هنا يمكنك تحديد وإدارة الصور التي تم تحميلها مسبقًا إلى النظام.',
     'image_intro_upload' => 'Upload a new image by dragging an image file into this window, or by using the "Upload Image" button above.',
     'image_all' => 'الكل',
     'image_all_title' => 'عرض جميع الصور',
@@ -26,7 +26,7 @@ return [
     'image_delete_confirm_text' => 'هل أنت متأكد من أنك تريد حذف هذه الصورة؟',
     'image_select_image' => 'تحديد الصورة',
     'image_dropzone' => 'قم بإسقاط الصورة أو اضغط هنا للرفع',
-    'image_dropzone_drop' => 'Drop images here to upload',
+    'image_dropzone_drop' => 'إسقاط صورة أو اضغط هنا للرفع',
     'images_deleted' => 'تم حذف الصور',
     'image_preview' => 'معاينة الصور',
     'image_upload_success' => 'تم رفع الصورة بنجاح',
@@ -34,7 +34,7 @@ return [
     'image_delete_success' => 'تم حذف الصورة بنجاح',
     'image_replace' => 'Replace Image',
     'image_replace_success' => 'Image file successfully updated',
-    'image_rebuild_thumbs' => 'Regenerate Size Variations',
+    'image_rebuild_thumbs' => 'تجديد تغيرات الحجم',
     'image_rebuild_thumbs_success' => 'Image size variations successfully rebuilt!',
 
     // Code Editor
