25933d33df4c355eab1bdf33f4ea4102a0691ca7
Dan Brown <email@danb.me>
2025-05-22 17:24:58 +0100

    New translations common.php (Bengali)

--- lang/bn/common.php
+++ lang/bn/common.php
@@ -30,6 +30,8 @@ return [
     'create' => 'তৈরী করুন',
     'update' => 'হালনাগাদ করুন',
     'edit' => 'সম্পাদন করুন',
+    'archive' => 'Archive',
+    'unarchive' => 'Un-Archive',
     'sort' => 'ক্রমান্বয় করুন',
     'move' => 'স্থানান্তর করুন',
     'copy' => 'অনুলিপি করুন',
