ca5bd6aef4ab6b811ee462563b8b98ce7d9945c4
Dan Brown <email@danb.me>
2025-05-22 17:24:25 +0100

    New translations common.php (German)

--- lang/de/common.php
+++ lang/de/common.php
@@ -30,6 +30,8 @@ return [
     'create' => 'Erstellen',
     'update' => 'Aktualisieren',
     'edit' => 'Bearbeiten',
+    'archive' => 'Archive',
+    'unarchive' => 'Un-Archive',
     'sort' => 'Sortieren',
     'move' => 'Verschieben',
     'copy' => 'Kopieren',
