04836db2cfbb047e42988b45c225c636647c4b6f
Dan Brown <email@danb.me>
2025-06-24 11:20:02 +0100

    New translations validation.php (Vietnamese)

--- lang/vi/validation.php
+++ lang/vi/validation.php
@@ -105,10 +105,10 @@ return [
     'url'                  => 'Định dạng của :attribute không hợp lệ.',
     'uploaded'             => 'Tệp tin đã không được tải lên. Máy chủ không chấp nhận các tệp tin với dung lượng lớn như tệp tin trên.',
 
-    'zip_file' => 'The :attribute needs to reference a file within the ZIP.',
-    'zip_file_mime' => 'The :attribute needs to reference a file of type :validTypes, found :foundType.',
-    'zip_model_expected' => 'Data object expected but ":type" found.',
-    'zip_unique' => 'The :attribute must be unique for the object type within the ZIP.',
+    'zip_file' => ':attribute cần tham chiếu đến một tệp trong ZIP.',
+    'zip_file_mime' => ':attribute cần tham chiếu đến một tệp có kiểu: :validTypes, tìm thấy :foundType.',
+    'zip_model_expected' => 'Đối tượng dữ liệu được mong đợi nhưng tìm thấy ":type".',
+    'zip_unique' => ':attribute phải là duy nhất cho kiểu đối tượng trong ZIP.',
 
     // Custom validation lines
     'custom' => [
