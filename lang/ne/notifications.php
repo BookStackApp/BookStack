602416f77a3d303ca2fbe16081df4bf46125f4b3
Dan Brown <email@danb.me>
2025-06-26 04:19:16 +0100

    New translations notifications.php (Nepali)

--- lang/ne/notifications.php
+++ lang/ne/notifications.php
@@ -4,24 +4,24 @@
  */
 return [
 
-    'new_comment_subject' => 'New comment on page: :pageName',
-    'new_comment_intro' => 'A user has commented on a page in :appName:',
-    'new_page_subject' => 'New page: :pageName',
-    'new_page_intro' => 'A new page has been created in :appName:',
-    'updated_page_subject' => 'Updated page: :pageName',
-    'updated_page_intro' => 'A page has been updated in :appName:',
-    'updated_page_debounce' => 'To prevent a mass of notifications, for a while you won\'t be sent notifications for further edits to this page by the same editor.',
+    'new_comment_subject' => 'नयाँ टिप्पणी: :pageName पानामा',
+    'new_comment_intro' => 'एक प्रयोगकर्ताले :appName मा रहेको पानामा टिप्पणी गरेका छन्:',
+    'new_page_subject' => 'नयाँ पाना: :pageName',
+    'new_page_intro' => ':appName मा नयाँ पाना बनाइएको छ:',
+    'updated_page_subject' => 'पाना अपडेट भयो: :pageName',
+    'updated_page_intro' => ':appName मा पाना अपडेट गरिएको छ:',
+    'updated_page_debounce' => 'धेरै सूचना नपरोस् भनेर, केही समयको लागि एउटै सम्पादकबाट हुने थप सम्पादनहरूका सूचना तपाईंलाई पठाइने छैन।',
 
-    'detail_page_name' => 'Page Name:',
-    'detail_page_path' => 'Page Path:',
-    'detail_commenter' => 'Commenter:',
-    'detail_comment' => 'Comment:',
-    'detail_created_by' => 'Created By:',
-    'detail_updated_by' => 'Updated By:',
+    'detail_page_name' => 'पानाको नाम:',
+    'detail_page_path' => 'पानाको स्थान:',
+    'detail_commenter' => 'टिप्पणी गर्ने:',
+    'detail_comment' => 'टिप्पणी:',
+    'detail_created_by' => 'बनाउने व्यक्ति:',
+    'detail_updated_by' => 'अपडेट गर्ने व्यक्ति:',
 
-    'action_view_comment' => 'View Comment',
-    'action_view_page' => 'View Page',
+    'action_view_comment' => 'टिप्पणी हेर्नुहोस्',
+    'action_view_page' => 'पाना हेर्नुहोस्',
 
-    'footer_reason' => 'This notification was sent to you because :link cover this type of activity for this item.',
-    'footer_reason_link' => 'your notification preferences',
+    'footer_reason' => 'तपाईंलाई यो सूचना :link अनुसार पठाइएको हो, जुन यस प्रकारको गतिविधिमा लागु हुन्छ।',
+    'footer_reason_link' => 'तपाईंको सूचना प्राथमिकता',
 ];
