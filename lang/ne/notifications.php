195409f2b2553f9d9ce3b562b9b224d780f331d3
Dan Brown <email@danb.me>
2025-06-26 08:10:08 +0100

    New translations notifications.php (Nepali)

--- lang/ne/notifications.php
+++ lang/ne/notifications.php
@@ -9,7 +9,7 @@ return [
     'new_page_subject' => 'नयाँ पाना: :pageName',
     'new_page_intro' => ':appName मा नयाँ पाना बनाइएको छ',
     'updated_page_subject' => 'पाना अपडेट भयो: :pageName',
-    'updated_page_intro' => ':appName मा पाना अपडेट गरिएको छ:',
+    'updated_page_intro' => ':appName मा पाना अपडेट गरिएको छ',
     'updated_page_debounce' => 'धेरै सूचना नपरोस् भनेर, केही समयको लागि एउटै सम्पादकबाट हुने थप सम्पादनहरूका सूचना तपाईंलाई पठाइने छैन।',
 
     'detail_page_name' => 'पानाको नाम:',
