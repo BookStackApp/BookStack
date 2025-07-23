dc623786738210d8d8ba12589013e5de891ac159
Dan Brown <email@danb.me>
2025-07-23 16:44:36 +0100

    New translations entities.php (Slovak)

--- lang/sk/entities.php
+++ lang/sk/entities.php
@@ -268,6 +268,7 @@ return [
     'pages_md_insert_drawing' => 'Vložiť kresbu',
     'pages_md_show_preview' => 'Zobraziť náhľad',
     'pages_md_sync_scroll' => 'Posúvanie ukážky synchronizácie',
+    'pages_md_plain_editor' => 'Plaintext editor',
     'pages_drawing_unsaved' => 'Unsaved Drawing Found',
     'pages_drawing_unsaved_confirm' => 'Unsaved drawing data was found from a previous failed drawing save attempt. Would you like to restore and continue editing this unsaved drawing?',
     'pages_not_in_chapter' => 'Stránka nie je v kapitole',
