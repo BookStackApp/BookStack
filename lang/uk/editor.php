d6a8443daa9209d8cd6a7e768a64a30d38d93b89
Dan Brown <email@danb.me>
2025-07-26 10:12:49 +0100

    New translations editor.php (Ukrainian)

--- lang/uk/editor.php
+++ lang/uk/editor.php
@@ -48,6 +48,7 @@ return [
     'superscript' => 'Верхній індекс',
     'subscript' => 'Нижній індекс',
     'text_color' => 'Колір тексту',
+    'highlight_color' => 'Highlight color',
     'custom_color' => 'Власний колір',
     'remove_color' => 'Видалити колір',
     'background_color' => 'Колір фону',
