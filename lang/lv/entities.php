25c8084a09ee45cdb191a9acbe842bfea4c50556
Dan Brown <email@danb.me>
2025-08-08 17:13:01 +0100

    New translations entities.php (Latvian)

--- lang/lv/entities.php
+++ lang/lv/entities.php
@@ -63,6 +63,10 @@ return [
     'import_delete_desc' => 'Šis izdzēsīs augšupielādēto importa ZIP failu, un šo darbību nevarēs atcelt.',
     'import_errors' => 'Importa kļūdas',
     'import_errors_desc' => 'Importa mēģinājumā atgadījās šīs kļūdas:',
+    'breadcrumb_siblings_for_page' => 'Navigate siblings for page',
+    'breadcrumb_siblings_for_chapter' => 'Navigate siblings for chapter',
+    'breadcrumb_siblings_for_book' => 'Navigate siblings for book',
+    'breadcrumb_siblings_for_bookshelf' => 'Navigate siblings for shelf',
 
     // Permissions and restrictions
     'permissions' => 'Atļaujas',
