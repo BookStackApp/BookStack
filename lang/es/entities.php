1a483c98fb6b51ba800f1f87e4af818e496c599f
Dan Brown <email@danb.me>
2025-08-08 17:12:17 +0100

    New translations entities.php (Spanish)

--- lang/es/entities.php
+++ lang/es/entities.php
@@ -63,6 +63,10 @@ return [
     'import_delete_desc' => 'Esto eliminará el archivo ZIP de importación subido y no se puede deshacer.',
     'import_errors' => 'Errores de Importación',
     'import_errors_desc' => 'Se han producido los siguientes errores durante el intento de importación:',
+    'breadcrumb_siblings_for_page' => 'Navigate siblings for page',
+    'breadcrumb_siblings_for_chapter' => 'Navigate siblings for chapter',
+    'breadcrumb_siblings_for_book' => 'Navigate siblings for book',
+    'breadcrumb_siblings_for_bookshelf' => 'Navigate siblings for shelf',
 
     // Permissions and restrictions
     'permissions' => 'Permisos',
