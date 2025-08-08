cd75fd51dab89e27efe6ab8c2604f96630ed09fa
Dan Brown <email@danb.me>
2025-08-08 17:13:00 +0100

    New translations entities.php (Estonian)

--- lang/et/entities.php
+++ lang/et/entities.php
@@ -63,6 +63,10 @@ return [
     'import_delete_desc' => 'See kustutab üleslaaditud ZIP-faili, ja seda ei saa tagasi võtta.',
     'import_errors' => 'Importimise vead',
     'import_errors_desc' => 'Importimisel esinesid järgmised vead:',
+    'breadcrumb_siblings_for_page' => 'Navigate siblings for page',
+    'breadcrumb_siblings_for_chapter' => 'Navigate siblings for chapter',
+    'breadcrumb_siblings_for_book' => 'Navigate siblings for book',
+    'breadcrumb_siblings_for_bookshelf' => 'Navigate siblings for shelf',
 
     // Permissions and restrictions
     'permissions' => 'Õigused',
