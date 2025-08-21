2ffb47fc8060c7569303e5eddca4e8843f953fcd
Dan Brown <email@danb.me>
2025-08-21 17:27:21 +0100

    New translations preferences.php (Dutch)

--- lang/nl/preferences.php
+++ lang/nl/preferences.php
@@ -27,7 +27,7 @@ return [
     'notifications_save' => 'Voorkeuren opslaan',
     'notifications_update_success' => 'Voorkeuren voor meldingen zijn bijgewerkt!',
     'notifications_watched' => 'Gevolgde & Genegeerde Items',
-    'notifications_watched_desc' => 'Hieronder staan de items waarvoor aangepaste \'Volg\'-voorkeuren zijn toegepast. Om je voorkeuren voor deze items bij te werken, bekijk je het item en zoek je naar de \'Volg\' opties in de zijbalk.',
+    'notifications_watched_desc' => 'Hieronder staan de items waarvoor aangepaste volgvoorkeuren zijn toegepast. Om je voorkeuren voor deze items bij te werken, bekijk je het item en zoek je naar de volgopties in de zijbalk.',
 
     'auth' => 'Toegang & Beveiliging',
     'auth_change_password' => 'Wachtwoord Wijzigen',
