bff33e781d7bd75287a5fd6742502c34fa7145df
Dan Brown <development@danb.me>
2025-11-24 13:55:11 +0000

    Slugs: Added lookup system using history
    Switched page lookup to use this.

--- app/Entities/Tools/SlugHistory.php
+++ app/Entities/Tools/SlugHistory.php
@@ -4,10 +4,16 @@ namespace BookStack\Entities\Tools;
 
 use BookStack\Entities\Models\BookChild;
 use BookStack\Entities\Models\Entity;
-use Illuminate\Support\Facades\DB;
+use BookStack\Entities\Models\SlugHistory as SlugHistoryModel;
+use BookStack\Permissions\PermissionApplicator;
 
 class SlugHistory
 {
+    public function __construct(
+        protected PermissionApplicator $permissions,
+    ) {
+    }
+
     /**
      * Record the current slugs for the given entity.
      */
@@ -17,14 +23,14 @@ class SlugHistory
             return;
         }
 
-        $latest = $this->getLatestEntryForEntity($entity);
-        if ($latest && $latest->slug === $entity->slug && $latest->parent_slug === $entity->getParent()?->slug) {
-            return;
-        }
-
         $parentSlug = null;
         if ($entity instanceof BookChild) {
             $parentSlug = $entity->book()->first()?->slug;
+        }
+
+        $latest = $this->getLatestEntryForEntity($entity);
+        if ($latest && $latest->slug === $entity->slug && $latest->parent_slug === $parentSlug) {
+            return;
         }
 
         $info = [
@@ -32,16 +38,37 @@ class SlugHistory
             'sluggable_id'   => $entity->id,
             'slug'           => $entity->slug,
             'parent_slug'    => $parentSlug,
-            'created_at'     => now(),
-            'updated_at'     => now(),
         ];
 
-        DB::table('slug_history')->insert($info);
+        $entry = new SlugHistoryModel();
+        $entry->forceFill($info);
+        $entry->save();
     }
 
-    protected function getLatestEntryForEntity(Entity $entity): \stdClass|null
+    /**
+     * Find the latest visible entry for an entity which uses the given slug(s) in the history.
+     */
+    public function lookupEntityIdUsingSlugs(string $type, string $slug, string $parentSlug = ''): ?int
     {
-        return DB::table('slug_history')
+        $query = SlugHistoryModel::query()
+            ->where('sluggable_type', '=', $type)
+            ->where('slug', '=', $slug);
+
+        if ($parentSlug) {
+            $query->where('parent_slug', '=', $parentSlug);
+        }
+
+        $query = $this->permissions->restrictEntityRelationQuery($query, 'slug_history', 'sluggable_id', 'sluggable_type');
+
+        /** @var SlugHistoryModel|null $result */
+        $result = $query->orderBy('created_at', 'desc')->first();
+
+        return $result?->sluggable_id;
+    }
+
+    protected function getLatestEntryForEntity(Entity $entity): SlugHistoryModel|null
+    {
+        return SlugHistoryModel::query()
             ->where('sluggable_type', '=', $entity->getMorphClass())
             ->where('sluggable_id', '=', $entity->id)
             ->orderBy('created_at', 'desc')
