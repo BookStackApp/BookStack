dd4c68bc217a38c8f8e8f9d607ffbbda40379c8a
Dan Brown <email@danb.me>
2025-12-07 00:14:00 +0000

    New translations settings.php (Russian)

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -75,8 +75,8 @@ return [
     'reg_confirm_restrict_domain_placeholder' => 'Без ограничений',
 
     // Sorting Settings
-    'sorting' => 'Сортировка',
-    'sorting_book_default' => 'Сортировка книг по умолчанию',
+    'sorting' => 'Lists & Sorting',
+    'sorting_book_default' => 'Default Book Sort Rule',
     'sorting_book_default_desc' => 'Выберите правило сортировки по умолчанию для новых книг. Это не повлияет на существующие книги, и может быть изменено для каждой книги отдельно.',
     'sorting_rules' => 'Правила сортировки',
     'sorting_rules_desc' => 'Выберите правило сортировки по умолчанию для новых книг. Это не повлияет на существующие книги и может быть изменено для каждой книги отдельно.',
@@ -103,6 +103,8 @@ return [
     'sort_rule_op_updated_date' => 'Updated Date',
     'sort_rule_op_chapters_first' => 'Chapters First',
     'sort_rule_op_chapters_last' => 'Главы в конце',
+    'sorting_page_limits' => 'Per-Page Display Limits',
+    'sorting_page_limits_desc' => 'Set how many items to show per-page in various lists within the system. Typically a lower amount will be more performant, while a higher amount avoids the need to click through multiple pages. Using an even multiple of 3 (18, 24, 30, etc...) is recommended.',
 
     // Maintenance settings
     'maint' => 'Обслуживание',
