be90ae138ccc3442f887045589841a907075a355
Dan Brown <email@danb.me>
2025-12-07 00:14:20 +0000

    New translations settings.php (Estonian)

--- lang/et/settings.php
+++ lang/et/settings.php
@@ -75,8 +75,8 @@ return [
     'reg_confirm_restrict_domain_placeholder' => 'Piirangut ei ole',
 
     // Sorting Settings
-    'sorting' => 'Sorteerimine',
-    'sorting_book_default' => 'Vaikimisi raamatu sorteerimine',
+    'sorting' => 'Lists & Sorting',
+    'sorting_book_default' => 'Default Book Sort Rule',
     'sorting_book_default_desc' => 'Vali vaikimisi uutele raamatutele rakenduv sorteerimisreegel. See ei mõjuta olemasolevaid raamatuid ning seda saab raamatupõhiselt muuta.',
     'sorting_rules' => 'Sorteerimisreeglid',
     'sorting_rules_desc' => 'Need on eeldefineeritud sorteerimistoimingud, mida saab süsteemis olevale sisule rakendada.',
@@ -103,6 +103,8 @@ return [
     'sort_rule_op_updated_date' => 'Muutmise aeg',
     'sort_rule_op_chapters_first' => 'Peatükid eespool',
     'sort_rule_op_chapters_last' => 'Peatükid tagapool',
+    'sorting_page_limits' => 'Per-Page Display Limits',
+    'sorting_page_limits_desc' => 'Set how many items to show per-page in various lists within the system. Typically a lower amount will be more performant, while a higher amount avoids the need to click through multiple pages. Using an even multiple of 3 (18, 24, 30, etc...) is recommended.',
 
     // Maintenance settings
     'maint' => 'Hooldus',
