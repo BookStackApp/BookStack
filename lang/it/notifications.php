4c36bf564100211a5eb5a4b46d2afe915c33a37d
Dan Brown <email@danb.me>
2025-12-30 18:37:58 +0000

    New translations notifications.php (Italian)

--- lang/it/notifications.php
+++ lang/it/notifications.php
@@ -11,8 +11,8 @@ return [
     'updated_page_subject' => 'Pagina aggiornata: :pageName',
     'updated_page_intro' => 'Una pagina è stata aggiornata in :appName:',
     'updated_page_debounce' => 'Per evitare una massa di notifiche, per un po\' non ti verranno inviate notifiche per ulteriori modifiche a questa pagina dallo stesso editor.',
-    'comment_mention_subject' => 'You have been mentioned in a comment on page: :pageName',
-    'comment_mention_intro' => 'You were mentioned in a comment on :appName:',
+    'comment_mention_subject' => 'Sei stato menzionato in un commento nella pagina: :pageName',
+    'comment_mention_intro' => 'Sei stato menzionato in un commento su :appName:',
 
     'detail_page_name' => 'Nome della pagina:',
     'detail_page_path' => 'Percorso della pagina:',
