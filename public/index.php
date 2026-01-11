501e7dba8e8e221a952871c94fd09cc30b8c49bb
Oanh Kim <ngothikimoanh2005nb@gmail.com>
2026-01-11 14:57:06 +0700

    oanh test1

--- public/index.php
+++ public/index.php
@@ -23,6 +23,6 @@ $kernel = $app->make(Kernel::class);
 $response = tap($kernel->handle(
     $request = Request::capture()
 ))->send();
-
+// 12121
 $kernel->terminate($request, $response);
 // kim oanh test 
