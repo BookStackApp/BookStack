01d30504a4f462d6e4ad9064abc92ce3f6f4da70
vu2xx5 <vu2xx5@gmail.com>
2026-01-11 14:16:41 +0700

    vu test

--- resources/views/misc/opensearch.blade.php
+++ resources/views/misc/opensearch.blade.php
@@ -1,4 +1,3 @@
-@php echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n"; @endphp
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
   <ShortName>{{ mb_strimwidth(setting('app-name'), 0, 16) }}</ShortName>
   <Description>{{ trans('common.opensearch_description', ['appName' => setting('app-name')]) }}</Description>
@@ -10,3 +9,4 @@
   <Url type="text/html" rel="results" template="{{ url('/search') }}?term={searchTerms}"/>
   <Url type="application/opensearchdescription+xml" rel="self" template="{{ url('/opensearch.xml') }}"/>
 </OpenSearchDescription>
+<!-- helo  -->
