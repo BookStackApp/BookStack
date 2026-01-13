b2aea688e730ac0510ae3d5c391af582fd526c48
Dan Brown <email@danb.me>
2026-01-13 10:17:49 +0000

    New translations validation.php (Uzbek)

--- lang/uz/validation.php
+++ lang/uz/validation.php
@@ -105,11 +105,11 @@ return [
     'url'                  => ':attribute URL formatida emas.',
     'uploaded'             => 'Faylni yuklashda xatolik. Server bunday hajmdagi faylllarni yuklamasligi mumkin.',
 
-    'zip_file' => 'The :attribute needs to reference a file within the ZIP.',
-    'zip_file_size' => 'The file :attribute must not exceed :size MB.',
-    'zip_file_mime' => 'The :attribute needs to reference a file of type :validTypes, found :foundType.',
-    'zip_model_expected' => 'Data object expected but ":type" found.',
-    'zip_unique' => 'The :attribute must be unique for the object type within the ZIP.',
+    'zip_file' => ':attribute ZIP ichidagi faylga havola qilishi kerak.',
+    'zip_file_size' => ':attribute fayli :size MB dan oshmasligi kerak.',
+    'zip_file_mime' => ':attribute :validTypes turidagi faylga havola qilishi kerak, lekin :foundType turida keldi.',
+    'zip_model_expected' => 'Ma\'lumotlar obyekti kutilmoqda, ammo ":type" topildi.',
+    'zip_unique' => ':attribute ZIP ichidagi obyekt turi uchun noyob bo\'lishi kerak.',
 
     // Custom validation lines
     'custom' => [
