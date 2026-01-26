aa7935fc78a0c4f65bce10dd7ec7da6690a0f62c
Dan Brown <email@danb.me>
2026-01-26 13:19:00 +0000

    New translations editor.php (Danish)

--- lang/da/editor.php
+++ lang/da/editor.php
@@ -48,7 +48,7 @@ return [
     'superscript' => 'Hævet',
     'subscript' => 'Sænket',
     'text_color' => 'Tekstfarve',
-    'highlight_color' => 'Highlight color',
+    'highlight_color' => 'Fremhævelsesfarve',
     'custom_color' => 'Tilpasset farve',
     'remove_color' => 'Fjern farve',
     'background_color' => 'Baggrundsfarve',
