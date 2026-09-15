7782ea00dfc68d87852c07170a3a5a8e9c07fa85
Dan Brown <development@danb.me>
2026-09-15 12:07:00 +0100

    A11y: Improved alt tag usage for header logo
    Set to blank if text is already part of the header, and the app name if
    the name is not shown in the header.
    
    For #6171
    References:
    https://webaim.org/techniques/alttext/
    https://www.scottohara.me/blog/2018/05/05/hidden-vs-none.html

--- resources/views/layouts/parts/header-logo.blade.php
+++ resources/views/layouts/parts/header-logo.blade.php
@@ -1,6 +1,8 @@
 <a href="{{ url('/') }}" data-shortcut="home_view" class="logo">
     @if(setting('app-logo', '') !== 'none')
-        <img class="logo-image" src="{{ setting('app-logo', '') === '' ? url('/logo.png') : url(setting('app-logo', '')) }}" alt="Logo">
+        <img class="logo-image"
+             src="{{ setting('app-logo', '') === '' ? url('/logo.png') : url(setting('app-logo', '')) }}"
+             alt="{{ setting('app-name-header') ? '' : setting('app-name') }}">
     @endif
     @if (setting('app-name-header'))
         <span class="logo-text">{{ setting('app-name') }}</span>
